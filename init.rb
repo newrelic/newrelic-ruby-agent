7f7abadf424400d7e0cbeeda65a714c035a1dfff
Lew Cirne <cirne@newrelic.com>
2007-08-24 22:55:38 +0000

    Working on clustered agents, using single table inheritance.  Hard.  Still broken.

--- init.rb
+++ init.rb
@@ -2,8 +2,5 @@ $:.unshift '/Users/cirne/dev/rubystuff/Seldon/Agent'
 $:.unshift '/Users/cirne/dev/rubystuff/Seldon/Common'
 
 # don't use the agent if we're running a rake task, like unit tests or the like.
-# TODO fix me.  must be a better way to determine that we are not running a test suite
-unless $0.include?('rake') || $0.include?('irb')
-  require 'seldon/agent'
-end
+require 'seldon/agent'
 
