d1e6c732da0c41b10e8efa50e060153a81804e61
Lew Cirne <cirne@newrelic.com>
2007-08-30 06:16:36 +0000

    Make some load paths relative rather than absolute

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
-$:.unshift '/Users/cirne/dev/rubystuff/Seldon/Agent'
-$:.unshift '/Users/cirne/dev/rubystuff/Seldon/Common'
+$:.unshift "../Agent"
+$:.unshift "../Common"
 
 # don't use the agent if we're running a rake task, like unit tests or the like.
 require 'seldon/agent'
