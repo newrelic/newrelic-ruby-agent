58c4ef19f68b06c1e5f663bc18f0e136d0a13df2
Lew Cirne <cirne@newrelic.com>
2007-09-24 16:15:10 +0000

    Big structural change - move the agent inside the project to make it a full-fledged rails plugin.

--- init.rb
+++ init.rb
@@ -1,13 +1,12 @@
-$:.unshift "../Agent"
-$:.unshift "../Common"
-
 require 'yaml'
+require 'seldon/agent'
 
+# Initializer for the Seldon Agent
 config_file = File.read(RAILS_ROOT+"/config/seldon.yml")
 agent_config = YAML.load(config_file)[RAILS_ENV]
 
-require 'seldon/agent'
-
-if agent_config[:enabled]
+p agent_config
+puts "Enabled: #{agent_config['enabled']}"
+if agent_config['enabled']
   Seldon::Agent.instance.start(agent_config)
 end
