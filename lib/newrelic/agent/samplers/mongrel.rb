1d16fe13cf5079b816193680295d9b34c85f6604
Lew Cirne <cirne@newrelic.com>
2008-03-19 00:18:33 +0000

    Add a metric that measures Mongrel queue length.  Change the load graph in the controllers viwe to overlay the mongrel queue length.  TODO need to add documentation for this metric.

---
+++ lib/newrelic/agent/samplers/mongrel.rb
@@ -0,0 +1,20 @@
+# NewRelic Instrumentation for Mongrel
+
+agent = NewRelic::Agent.instance
+mongrel = nil;
+ObjectSpace.each_object(Mongrel::HttpServer) do |mongrel_instance|
+  # should only be one mongrel instance in the vm
+  if mongrel
+    agent.log.info("Discovered multiple mongrel instances in one Ruby VM.  "+
+      "This is unexpected and might affect the Accuracy of the Mongrel Request Queue metric.")
+  end
+  
+  mongrel = mongrel_instance
+end
+
+if mongrel
+  agent.stats_engine.add_sampled_metric("Mongrel/Queue Length") do |stats|
+    qsize = mongrel.workers.list.length
+    stats.record_data_point qsize
+  end
+end
