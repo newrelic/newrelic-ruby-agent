73d41853b08698ceb0936c431876e25970a41b90
Lew Cirne <cirne@newrelic.com>
2008-03-31 19:26:53 +0000

    Fix bug #42: remove time window selection from agents overview page.  (User can no longer find agents that have not reported data in the last 6 hours, but that is not a common use case and the ui for this was very confusing).

--- lib/newrelic/agent/agent.rb
+++ lib/newrelic/agent/agent.rb
@@ -368,7 +368,7 @@ module NewRelic::Agent
           # gathering SQL explanations, stripping out stack traces, and normalizing SQL.
           # note that we explain only the sql statements whose segments' execution times exceed 
           # our threshold (to avoid unnecessary overhead of running explains on fast queries.)
-          sample = @slowest_sample.prepare_to_send(:explain_sql => 0.1)
+          sample = @slowest_sample.prepare_to_send(:explain_sql => 0.5)
           invoke_remote :transaction_sample_data, @agent_id, sample
         end
         
