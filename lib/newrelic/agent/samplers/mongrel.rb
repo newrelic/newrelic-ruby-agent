aaf3f5e657ccd172b3ea1ee73288c78d4745309d
Lew Cirne <cirne@newrelic.com>
2008-05-20 16:10:43 +0000

    Fixed bug #135 (error msg in log when running under thin server)

--- lib/newrelic/agent/samplers/mongrel.rb
+++ lib/newrelic/agent/samplers/mongrel.rb
@@ -1,20 +1,23 @@
-# NewRelic Instrumentation for Mongrel
-
-agent = NewRelic::Agent.instance
-mongrel = nil;
-ObjectSpace.each_object(Mongrel::HttpServer) do |mongrel_instance|
-  # should only be one mongrel instance in the vm
-  if mongrel
-    agent.log.info("Discovered multiple mongrel instances in one Ruby VM.  "+
-      "This is unexpected and might affect the Accuracy of the Mongrel Request Queue metric.")
-  end
+# NewRelic Instrumentation for Mongrel - tracks the queue length of the mongrel server
+if defined? Mongrel::HttpServer
   
-  mongrel = mongrel_instance
-end
+  agent = NewRelic::Agent.instance
+  mongrel = nil;
+  ObjectSpace.each_object(Mongrel::HttpServer) do |mongrel_instance|
+    # should only be one mongrel instance in the vm
+    if mongrel
+      agent.log.info("Discovered multiple mongrel instances in one Ruby VM.  "+
+        "This is unexpected and might affect the Accuracy of the Mongrel Request Queue metric.")
+    end
 
-if mongrel
-  agent.stats_engine.add_sampled_metric("Mongrel/Queue Length") do |stats|
-    qsize = mongrel.workers.list.length
-    stats.record_data_point qsize
+    mongrel = mongrel_instance
   end
+
+  if mongrel
+    agent.stats_engine.add_sampled_metric("Mongrel/Queue Length") do |stats|
+      qsize = mongrel.workers.list.length
+      stats.record_data_point qsize
+    end
+  end
+
 end
