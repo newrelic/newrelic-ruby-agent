62002d274bc6e2a355353b5c793566b8a7df1ac4
Lew Cirne <cirne@newrelic.com>
2008-05-22 15:27:27 +0000

    Agent cannot use Hash#to_query because that api is not present in some platforms mei tested.

--- lib/newrelic/agent/agent.rb
+++ lib/newrelic/agent/agent.rb
@@ -422,8 +422,10 @@ module NewRelic::Agent
         request.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
       
-      params = {:method => method, :license_key => license_key, :protocol_version => PROTOCOL_VERSION }
-      uri = "/agent_listener/invoke_raw_method?#{params.to_query}"
+      # we'd like to use to_query but it is not present in all supported rails platforms
+      # params = {:method => method, :license_key => license_key, :protocol_version => PROTOCOL_VERSION }
+      # uri = "/agent_listener/invoke_raw_method?#{params.to_query}"
+      uri = "/agent_listener/invoke_raw_method?method=#{method}&license_key=#{license_key}&protocol_version=#{PROTOCOL_VERSION}"
       response = request.start do |http|
         http.post(uri, post_data) 
       end
