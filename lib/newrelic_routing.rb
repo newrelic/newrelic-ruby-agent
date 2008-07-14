611c5b78ee5b00dfaa773d4bc80ed8e7e92fa175
Saxon D'Aubin <saxon@newrelic.com>
2008-07-14 17:17:12 +0000

    Spelling fix in comments

--- lib/newrelic_routing.rb
+++ lib/newrelic_routing.rb
@@ -1,6 +1,6 @@
 # REVIEW This is a monkey patch to inject the developer tool route into the
 # parent app without requiring users to modify their routes. Of course this 
-# has the effect of adding a route indiscriminantly which is frowned upon by 
+# has the effect of adding a route indiscriminately which is frowned upon by 
 # some: http://www.ruby-forum.com/topic/126316#563328
 module ActionController
   module Routing
