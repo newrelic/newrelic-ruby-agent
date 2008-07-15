998742fbce19515f638eb28538a382947946fb59
Bill Kayser <bkayser@newrelic.com>
2008-07-15 22:36:07 +0000

    Fix minor typos in agent.rb which crept in.

--- lib/newrelic/agent/agent.rb
+++ lib/newrelic/agent/agent.rb
@@ -1,5 +1,3 @@
-require "cgi"
-require "socket"
 require 'net/https' 
 require 'net/http'
 require 'logger'
@@ -300,7 +298,7 @@ module NewRelic::Agent
       
       # determine the reporting period (server based)
       # note if the agent attempts to report more frequently than the specified
-      # report data, then it will ignored.
+      # report data, then it will be ignored.
       report_period = invoke_remote :get_data_report_period, @agent_id
       log! "Reporting performance data every #{report_period} seconds"        
       @worker_loop.add_task(report_period) do 
