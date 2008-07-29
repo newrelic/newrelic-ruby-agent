39a91f0c6952c621c376d87c1cc3c05cfe319ffa
Jim Gochee <jim@newrelic.com>
2008-07-29 21:10:54 +0000

    <#275> Don't record a datapoint when CPU times < 0. This happens when apps are fork()ed

--- lib/newrelic/agent/samplers/cpu.rb
+++ lib/newrelic/agent/samplers/cpu.rb
@@ -1,21 +1,24 @@
 module NewRelic::Agent
   class CPUSampler
     def initialize
-      t = Process.times
-      @last_utime = t.utime
-      @last_stime = t.stime
   
       agent = NewRelic::Agent.instance
   
       agent.stats_engine.add_sampled_metric("CPU/User Time") do | stats |
-        utime = Process.times.utime
-        stats.record_data_point utime - @last_utime
+        t = Process.times
+        @last_utime ||= t.utime
+        
+        utime = t.utime
+        stats.record_data_point (utime - @last_utime) if (utime - @last_utime) >= 0
         @last_utime = utime
       end
   
       agent.stats_engine.add_sampled_metric("CPU/System Time") do | stats |
-        stime = Process.times.stime
-        stats.record_data_point stime - @last_stime
+        t = Process.times
+        @last_stime ||= t.stime
+        
+        stime = t.stime
+        stats.record_data_point (stime - @last_stime) if (stime - @last_stime) >= 0
         @last_stime = stime
       end
     end
