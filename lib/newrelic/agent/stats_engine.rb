393a300e48945583020a10dc262656a4df2e3f9c
Jim Gochee <jim@newrelic.com>
2008-08-27 16:29:21 +0000

    Agent - optimization to not send metric spec every time

--- lib/newrelic/agent/stats_engine.rb
+++ lib/newrelic/agent/stats_engine.rb
@@ -177,7 +177,10 @@ module NewRelic::Agent
         # don't bother collecting and reporting stats that have zero-values for this timeslice.
         # significant performance boost and storage savings.
         unless stats_copy.call_count == 0
-          metric_data = NewRelic::MetricData.new(metric_spec, stats_copy, metric_ids[metric_spec])
+          
+          metric_spec_for_transport = (metric_ids[metric_spec].nil?) ? metric_spec : nil
+          
+          metric_data = NewRelic::MetricData.new(metric_spec_for_transport, stats_copy, metric_ids[metric_spec])
           
           timeslice_data[metric_spec] = metric_data
         end
