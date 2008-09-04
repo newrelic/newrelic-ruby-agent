d0477194080170cec355ced3c9640b4b309a99dc
Jim Gochee <jim@newrelic.com>
2008-09-04 21:18:00 +0000

    BugFix #331. Check for new Dependencies location.

--- init.rb
+++ init.rb
@@ -67,8 +67,15 @@ begin catch(:disabled) do
     helper_path = File.join(File.dirname(__FILE__), 'ui', 'helpers')
     $LOAD_PATH << controller_path
     $LOAD_PATH << helper_path
-    Dependencies.load_paths << controller_path
-    Dependencies.load_paths << helper_path
+    
+    # Rails 2.1
+    if defined? ActiveSupport::Dependencies
+      ActiveSupport::Dependencies.load_paths << controller_path
+      ActiveSupport::Dependencies.load_paths << helper_path
+    else
+      Dependencies.load_paths << controller_path
+      Dependencies.load_paths << helper_path
+    end    
     config.controller_paths << controller_path
     require 'newrelic_routing'
     
