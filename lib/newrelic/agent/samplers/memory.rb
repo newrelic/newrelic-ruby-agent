dd338d14b6f19aeaf18d213a0d12d7c812011c1c
Jim Gochee <jim@newrelic.com>
2008-09-04 17:48:59 +0000

    Agent BugFix #371 - passenger memory polling was using the wrong process ID, which caused failure when that process went away.

--- lib/newrelic/agent/samplers/memory.rb
+++ lib/newrelic/agent/samplers/memory.rb
@@ -9,31 +9,34 @@ module NewRelic::Agent
       
       # macos, linux, solaris
       if platform =~ /darwin|linux/
-        @ps = "ps -o rsz #{$$}"
+        @ps = "ps -o rsz"
       elsif platform =~ /freebsd/
-        @ps = "ps -o rss #{$$}"
+        @ps = "ps -o rss"
       elsif platform =~ /solaris/
-        @ps = "ps -o rss -p #{$$}"
+        @ps = "ps -o rss -p"
       end
       if !@ps
         raise SamplerInitFailure, "Unsupported platform for getting memory: #{platform}"
       end
       
-      if @ps
-        agent = NewRelic::Agent.instance
+      if @ps        
+        @broken = false
+        
+        agent = NewRelic::Agent.instance        
         agent.stats_engine.add_sampled_metric("Memory/Physical") do |stats|
-          return if @broken
-          memory = `#{@ps}`.split("\n")[1].to_f / 1024
-          
-          # if for some reason the ps command doesn't work on the resident os,
-          # then don't execute it any more.
-          if memory > 0
-            stats.record_data_point memory
+          if !@broken
+            memory = `#{@ps} #{$$}`.split("\n")[1].to_f / 1024
             
-          else 
-            NewRelic::Agent.instance.log.error "Error attempting to determine resident memory.  Disabling this metric."
-            NewRelic::Agent.instance.log.error "Faulty command: `#{@ps}`"
-            @broken = true
+            # if for some reason the ps command doesn't work on the resident os,
+            # then don't execute it any more.
+            if memory > 0
+              stats.record_data_point memory
+              
+            else 
+              NewRelic::Agent.instance.log.error "Error attempting to determine resident memory (got result of #{memory}).  Disabling this metric."
+              NewRelic::Agent.instance.log.error "Faulty command: `#{@ps}`"
+              @broken = true
+            end
           end
         end
       end
