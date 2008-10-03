28d392d48c6189aab623f85c04f74f17a3b75266
Bill Kayser <bkayser@newrelic.com>
2008-10-03 20:59:38 +0000

    Agent test, fixed I think.

--- test/newrelic/agent/tc_active_record.rb
+++ test/newrelic/agent/tc_active_record.rb
@@ -59,11 +59,14 @@ class ActiveRecordInstrumentationTests < Test::Unit::TestCase
     segment = sample.root_segment.called_segments.first.called_segments.first
   end
   def test_prepare_to_send
+    t0 = Time.now
     TestModel.find(:all)
     sample = @agent.transaction_sampler.harvest_slowest_sample
     segment = sample.root_segment.called_segments.first.called_segments.first
     assert_match /^SELECT /, segment.params[:sql]
-    assert segment.duration > 0.0, "Segment duration must be positive value."
+    puts segment.duration
+    puts sample
+    assert segment.duration > 0.0, "Segment duration must be greater than zero."
     sample = sample.prepare_to_send(:record_sql => :raw, :explain_enabled => true, :explain_sql => 0.0)
     segment = sample.root_segment.called_segments.first.called_segments.first
     assert_match /^SELECT /, segment.params[:sql]
