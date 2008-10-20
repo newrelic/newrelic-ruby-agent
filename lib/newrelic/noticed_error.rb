b0a4f894623e80720c5a9109284ad4ecd5b82131
Bill Kayser <bkayser@newrelic.com>
2008-10-20 20:24:41 +0000

    Fix comment.

--- lib/newrelic/noticed_error.rb
+++ lib/newrelic/noticed_error.rb
@@ -1,8 +1,4 @@
 # this class encapsulates an error that was noticed by RPM in a managed app.
-# NOTE we do not capture the stack trace because RPM does not have
-# access to the app's source code, so it has little value and is
-# sizeable.  We can add this later if customers demand it. (The stack
-# trace will obviously be available elsewhere like the logs...)
 class NoticedError
   attr_accessor :path, :timestamp, :params, :exception_class, :message
   
