f4ba53cd00fa780d8307617f9882622bceec415b
Jim Gochee <jim@newrelic.com>
2008-10-21 16:52:20 +0000

    AGENT - capture remote address of http client in errors

--- lib/newrelic/agent/instrumentation/errors.rb
+++ lib/newrelic/agent/instrumentation/errors.rb
@@ -10,6 +10,8 @@ module ActionController
     def newrelic_notice_error(exception)
       local_params = (respond_to? :filter_parameters) ? filter_parameters(params) : params
       
+      local_params[:remote_addr] = request.env['REMOTE_ADDR'] if request && request.env
+      
       NewRelic::Agent.agent.error_collector.notice_error(_determine_metric_path, (request) ? request.path : nil,
             local_params, exception)
     end
