87b8f85770b4e0837edd3a038c22f51859d55711
Jim Gochee <jim@newrelic.com>
2008-11-05 17:19:56 +0000

    Agent - don't disable memory collection when `ps` fails. http://www.pivotaltracker.com/story/show/267477

--- lib/new_relic/agent/samplers/memory.rb
+++ lib/new_relic/agent/samplers/memory.rb
@@ -25,17 +25,24 @@ module NewRelic::Agent
         agent = NewRelic::Agent.instance        
         agent.stats_engine.add_sampled_metric("Memory/Physical") do |stats|
           if !@broken
-            memory = `#{@ps} #{$$}`.split("\n")[1].to_f / 1024
-            
-            # if for some reason the ps command doesn't work on the resident os,
-            # then don't execute it any more.
-            if memory > 0
-              stats.record_data_point memory
-              
-            else 
-              NewRelic::Agent.instance.log.error "Error attempting to determine resident memory (got result of #{memory}).  Disabling this metric."
-              NewRelic::Agent.instance.log.error "Faulty command: `#{@ps}`"
-              @broken = true
+            begin
+              memory = `#{@ps} #{$$}`.split("\n")[1].to_f / 1024
+
+              # if for some reason the ps command doesn't work on the resident os,
+              # then don't execute it any more.
+              if memory > 0
+                stats.record_data_point memory
+              else 
+                NewRelic::Agent.instance.log.error "Error attempting to determine resident memory (got result of #{memory}).  Disabling this metric."
+                NewRelic::Agent.instance.log.error "Faulty command: `#{@ps}`"
+                @broken = true
+              end
+            rescue Exception => e
+              if e.is_a? Errno::ENOMEM
+                NewRelic::Agent.instance.log.error "Got OOM trying to determine process memory usage"
+              else
+                raise e
+              end
             end
           end
         end
