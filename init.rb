78994c111aa327c283cd47c4fc9b8fe5c5d96ccd
Bill Kayser <bkayser@newrelic.com>
2008-11-21 15:20:31 -0800

    Update the init.rb with troubleshooting help

--- init.rb
+++ init.rb
@@ -1,26 +1,20 @@
 # This is the initialization for the RPM Rails plugin
-
-# If you are having problems seeing data, be sure and check the
-# newrelic_agent log files. 
-# 
-# If you can't find any log files and you don't see anything in your
-# application log files, try uncommenting these lines to verify
-# the plugin is being loaded, then contact support@newrelic.com 
-# if you are unable to resolve the issue.
-
-# STDOUT.puts "RPM detected environment: #{NewRelic::LocalEnvironment.new}"
+##require 'new_relic/config'
 
 # Initializer for the NewRelic Agent
+# JRuby's glassfish plugin is trying to run the Initializer twice,
+# which isn't a good thing so we ignore subsequent invocations here.
 begin
-  # JRuby's glassfish plugin is trying to run the Initializer twice,
-  # which isn't a good thing so we ignore subsequent invocations here.
+  # START THE AGENT
+  # We install the shim agent unless the tracers are enabled, the plugin
+  # env setting is not false, and the agent started okay. 
   if ! defined?(::NEWRELIC_STARTED)
     ::NEWRELIC_STARTED = "#{caller.join("\n")}"
     NewRelic::Config.instance.start_plugin (defined?(config) ? config : nil)
   else
     NewRelic::Config.instance.log.debug "Attempt to initialize the plugin twice!"
-    NewRelic::Config.instance.log.debug "Original call: \n#{::NEWRELIC_STARTED}"
-    NewRelic::Config.instance.log.debug "Here we are now: \n#{caller.join("\n")}"
+    #NewRelic::Config.instance.log.debug "Original call: \n#{::NEWRELIC_STARTED}"
+    #NewRelic::Config.instance.log.debug "Here we are now: \n#{caller.join("\n")}"
   end
 rescue => e
   NewRelic::Config.instance.log! "Error initializing New Relic plugin (#{e})", :error
