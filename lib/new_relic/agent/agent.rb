9afe09432bc3f38f5d2e704fd69419d92c744bcd
Jim Gochee <jim@newrelic.com>
2008-11-24 19:19:56 +0000

    Agent - transfer app_name to RPM in 'launch' command. http://www.pivotaltracker.com/story/show/286681

--- lib/new_relic/agent/agent.rb
+++ lib/new_relic/agent/agent.rb
@@ -488,7 +488,7 @@ module NewRelic::Agent
       # wait a few seconds for the web server to boot, necessary in development
       sleep @connect_retry_period.to_i
       @agent_id = invoke_remote :launch, @local_host,
-      @identifier, determine_home_directory, $$, @launch_time.to_f, NewRelic::VERSION::STRING, config.app_config_info
+      @identifier, determine_home_directory, $$, @launch_time.to_f, NewRelic::VERSION::STRING, config.app_config_info, config['app_name']
       
       log! "Connected to NewRelic Service at #{@remote_host}:#{@remote_port}."
       log.debug "Agent ID = #{@agent_id}."
