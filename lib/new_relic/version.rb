1eb60fdc6c162a3f0e2c4e884aef65677a265631
Bill Kayser <bkayser@newrelic.com>
2008-11-24 11:23:24 -0800

    Fixing gemspec, version.rb again

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -3,7 +3,7 @@ module NewRelic
   module VERSION #:nodoc:
   MAJOR = 2
   MINOR = 7
-  TINY  = 0
+  TINY  = 1
   STRING = [MAJOR, MINOR, TINY].join('.')
   def self.changes
     puts "NewRelic RPM Plugin Version: #{NewRelic::VERSION}"
