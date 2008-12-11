dabb65c34b26774fa07032b98b3763e7d938a81d
Bill Kayser <bkayser@newrelic.com>
2008-12-11 23:57:14 +0000

    Make the notfy_rpm a bit more robust: will not break the deploy and can be utilized via require or loaded from the Capfile.

--- recipes/newrelic.rb
+++ recipes/newrelic.rb
@@ -1,10 +1,11 @@
 # To be included in Capistrano deploy.rb files
 #
 # Defined deploy:notify_rpm which will send information about the deploy to RPM.
-# The task will run on app servers except where no_release is true
+# The task will run on app servers except where no_release is true.
+# If it fails, it will not affect the task execution or do a rollback.
 #
 
-Capistrano::Configuration.instance(:must_exist).load do
+make_notify_task = lambda do
   
   namespace :deploy do
     # on all deployments, notify RPM 
@@ -12,7 +13,20 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :notify_rpm, :roles => :app, :except => {:no_release => true } do
       rails_env = fetch(:rails_env, "production")
       script = File.expand_path(File.join(File.dirname(__FILE__), "..","newrelic_api.rb"))
-      run "cd #{current_release}; script/runner -e #{rails_env} #{script} -a #{application_id} -u '#{ENV['USER']}' '#{ENV['USER']} deploying #{File.basename(repository)}'"
+      begin
+        run "cd #{current_release}; script/runner -e #{rails_env} #{script} -a #{application_id} -u '#{ENV['USER']}' '#{ENV['USER']} deploying #{File.basename(repository)}'" do | ssh, stream_id, output |
+          logger.trace(output)
+        end
+      rescue CommandError
+        logger.important "Unable to notify RPM of the deployment... skipping"
+      end
     end
   end
+end
+
+instance = Capistrano::Configuration.instance
+if instance
+  instance.load make_notify_task
+else
+  make_notify_task.call
 end
