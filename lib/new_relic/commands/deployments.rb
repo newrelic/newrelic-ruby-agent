0890a3d9e9b4370619e87b5e6e5bfbd2f024446e
Saxon D'Aubin <saxon@newrelic.com>
2009-01-21 19:27:29 +0000

    That was easy!

--- lib/new_relic/commands/deployments.rb
+++ lib/new_relic/commands/deployments.rb
@@ -47,7 +47,7 @@ module NewRelic
           uri = "/deployments.xml"
 
 					raise "license_key was not set in newrelic.yml for #{config.env}" if config['license_key'].nil?
-          request = Net::HTTP::Post.new(uri, 'HTTP_X_LICENSE_KEY' => config['license_key'])
+          request = Net::HTTP::Post.new(uri, {'x-license-key' => config['license_key']})
           request.content_type = "application/octet-stream"
 
           request.set_form_data(create_params)
