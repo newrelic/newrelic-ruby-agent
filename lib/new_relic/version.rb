62653134a7a50c5e7ecd0640c3a4915dbb3f144a
Bill Kayser <bkayser@newrelic.com>
2009-01-27 20:20:53 +0000

    fix version number

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -2,8 +2,8 @@
 module NewRelic
   module VERSION #:nodoc:
   MAJOR = 2
-  MINOR = 9
-  TINY  = 0
+  MINOR = 8
+  TINY  = 1
   STRING = [MAJOR, MINOR, TINY].join('.')
   def self.changes
     puts "NewRelic RPM Plugin Version: #{NewRelic::VERSION::STRING}"
