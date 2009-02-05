ba9ca5ba0c0c8626780ee433a6ef74f01a940bf7
Bill Kayser <bkayser@newrelic.com>
2009-02-05 00:48:13 +0000

    update the release notes

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -13,6 +13,8 @@ module NewRelic
   CHANGELOG = <<EOF
 
 2009-02-07 version 2.8.2
+  * fix Ruby 1.9 syntax compatibility errors
+  * update the class loading sanity check, will notify server of errors
   * fix agent output on script and rake task execution
 2009-01-27 version 2.8.1
   * Convert the deployment information upload script to an executable
