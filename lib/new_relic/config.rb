64375cf4c078065479a7620a06c785f7d4437de6
Bill Kayser <bkayser@newrelic.com>
2009-02-06 23:20:35 +0000

    Trying out a dodge of the autoloader by the agent

--- lib/new_relic/config.rb
+++ lib/new_relic/config.rb
@@ -187,6 +187,7 @@ module NewRelic
     end
     
     def start_agent
+      require 'new_relic/agent.rb'
       NewRelic::Agent::Agent.instance.start(local_env.environment, local_env.identifier)
     end
     
