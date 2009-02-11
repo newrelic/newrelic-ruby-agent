04d70caf6f8d66516a8ef87c8c8dabddcfbed6d5
Bill Kayser <bkayser@newrelic.com>
2009-02-11 22:37:26 +0000

    Fix unit test

--- test/new_relic/agent/model_fixture.rb
+++ test/new_relic/agent/model_fixture.rb
@@ -2,9 +2,14 @@
 # Be sure and call setup and teardown
 class NewRelic::Agent::ModelFixture < ActiveRecord::Base
   self.table_name = 'test_data'
+  
   def self.setup
     connection.create_table :test_data, :force => true do |t|
       t.column :name, :string
+    end
+    def connection.log_info *args
+      sleep 0.2
+      super *args
     end
   end
   
