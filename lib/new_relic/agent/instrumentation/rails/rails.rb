1571d1ac9cc90a7ef53e6503c798fc3c8efe8838
George Feil <george@newrelic.com>
2009-02-24 04:58:39 +0000

    merged in revision 7130 from tags/m09-02-23

--- lib/new_relic/agent/instrumentation/rails/rails.rb
+++
@@ -1,7 +0,0 @@
-# Instrument the compilation of ERB files.
-# For developer mode, we subtract compile from total TT time
-ERB::Compiler.class_eval do
-  add_method_tracer :compile, 'View/.rhtml Processing'
-end
-
-
