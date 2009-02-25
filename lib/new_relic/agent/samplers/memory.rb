1fde5240c99eecf44c502f1c3f79759a296be903
Jim Gochee <jim@newrelic.com>
2009-02-25 20:03:52 +0000

    Agent - on linux, read process memory in use via /proc/$$/status. http://www.pivotaltracker.com/story/show/282332

--- lib/new_relic/agent/samplers/memory.rb
+++ lib/new_relic/agent/samplers/memory.rb
@@ -1,5 +1,91 @@
 module NewRelic::Agent
+  
+  # PSSampler
+  #
+  # A class that samples memory by forking the "ps" command
+  #
+  class PSSampler
+    def initialize(command)
+      @broken = false
+      @command = command
+    end
+    
+    # Returns the amount of resident memory this process is using in MB
+    #
+    def get_memory_sample
+      return nil if @broken
+      
+      process = $$
+      memory = `#{@command} #{process}`.split("\n")[1].to_f / 1024.0
+
+      # if for some reason the ps command doesn't work on the resident os,
+      # then don't execute it any more.
+      if memory >= 0
+        memory
+      else 
+        NewRelic::Agent.instance.log.error "Error attempting to determine resident memory for pid #{process} (got result of #{memory}, this process = #{$$}).  Disabling this metric."
+        NewRelic::Agent.instance.log.error "Faulty command: `#{command}`"
+        @broken = true
+        nil
+      end
+    rescue Exception => e
+      if e.is_a? Errno::ENOMEM
+        NewRelic::Agent.instance.log.error "Got OOM trying to determine process memory usage"
+      else
+        raise e
+      end
+    end
+  end
+
+  
+  # ProcStatusSampler
+  #
+  # A class that samples memory by reading the file /proc/$$/status, which is specific to linux
+  #
+  class ProcStatusSampler
+    
+    # This is called first to make sure the sampler can run on this host, otherwise a ps sampler is used
+    #
+    def can_run?
+      sample = get_memory_sample
+      if (sample == 0.0)
+        false
+      else
+        true
+      end
+    rescue
+      false
+    end
+    
+    # Returns the amount of resident memory this process is using in MB
+    #
+    def get_memory_sample
+      f = File.new("/proc/#{$$}/status")
+      begin
+        while (line = f.readline)
+          line.chomp
+          if line =~ /RSS/
+            value = line.split(" ")[1]
+            
+            f.close
+              
+            return value.to_f / 1024.0
+          end
+        end
+      end
+    rescue EOFError
+      f.close
+      return 0.0
+    end
+  end
+  
+  
+  # MemorySampler
+  #
+  # Master memory sampler class. Picks the correct sampler to use based on the environment
+  # 
   class MemorySampler
+
     def initialize
       if RUBY_PLATFORM =~ /java/
         platform = %x[uname -s].downcase
@@ -8,46 +94,29 @@ module NewRelic::Agent
       end
       
       # macos, linux, solaris
-      if platform =~ /darwin|linux/
-        @ps = "ps -o rsz"
+      if platform =~ /linux/
+        sampler = ProcStatusSampler.new
+        unless sampler.can_run?
+          NewRelic::Agent.instance.log.error "Error attempting to use /proc/$$/status file for reading memory. Using ps command instead"
+          sampler = PSSampler.new("ps -o rsz")
+        else
+          NewRelic::Agent.instance.log.info "Using /proc/$$/status file for reading memory."
+        end
+      elsif platform =~ /darwin/
+        sampler = PSSampler.new("ps -o rsz")
       elsif platform =~ /freebsd/
-        @ps = "ps -o rss"
+        sampler = PSSampler.new("ps -o rss")
       elsif platform =~ /solaris/
-        @ps = "/usr/bin/ps -o rss -p"
-      end
-      if !@ps
+        sampler = PSSampler.new("/usr/bin/ps -o rss -p")
+      else
         raise "Unsupported platform for getting memory: #{platform}"
       end
       
-      if @ps        
-        @broken = false
-        
-        agent = NewRelic::Agent.instance        
-        agent.stats_engine.add_sampled_metric("Memory/Physical") do |stats|
-          if !@broken
-            begin
-              process = $$
-              memory = `#{@ps} #{process}`.split("\n")[1].to_f / 1024
-
-              # if for some reason the ps command doesn't work on the resident os,
-              # then don't execute it any more.
-              if memory >= 0
-                stats.record_data_point memory
-              else 
-                NewRelic::Agent.instance.log.error "Error attempting to determine resident memory for pid #{process} (got result of #{memory}, this process = #{$$}).  Disabling this metric."
-                NewRelic::Agent.instance.log.error "Faulty command: `#{@ps}`"
-                @broken = true
-              end
-            rescue Exception => e
-              if e.is_a? Errno::ENOMEM
-                NewRelic::Agent.instance.log.error "Got OOM trying to determine process memory usage"
-              else
-                raise e
-              end
-            end
-          end
-        end
+      NewRelic::Agent.instance.stats_engine.add_sampled_metric("Memory/Physical") do |stats|
+        sample = sampler.get_memory_sample
+        stats.record_data_point sample if sample
       end
+      
     end
   end
 end
