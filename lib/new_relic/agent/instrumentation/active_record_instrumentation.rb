3978cfa4df2f5d4483715482a63beb08afb1c085
Bill Kayser <bkayser@newrelic.com>
2009-02-26 22:43:56 +0000

    Fix issue with no active record

--- lib/new_relic/agent/instrumentation/active_record_instrumentation.rb
+++ lib/new_relic/agent/instrumentation/active_record_instrumentation.rb
@@ -1,6 +1,6 @@
 
 # NewRelic instrumentation for ActiveRecord
-if defined?(ActiveRecord) && !NewRelic::Config.instance['skip_ar_instrumentation']
+if defined?(ActiveRecord::Base) && !NewRelic::Config.instance['skip_ar_instrumentation']
   
   ActiveRecord::Base.class_eval do
     class << self
