d6d1551aebaee95266082486f8183854e8e86406
Bill Kayser <bkayser@newrelic.com>
2009-03-05 19:57:10 +0000

    fix merb bug in 2.9.0

--- lib/new_relic/agent/instrumentation/merb/dispatcher.rb
+++ lib/new_relic/agent/instrumentation/merb/dispatcher.rb
@@ -6,7 +6,7 @@ Merb::Request.class_eval do
 
   alias_method :dispatch_without_newrelic, :handle
   alias_method :handle, :dispatch_newrelic
-  def newrelic_status_code
+  def newrelic_response_code
     # Don't have an easy way to get the HTTP status from here yet
     nil
   end
