22f145b4b7ea5655e5a05de6e9fd81493faa4237
Bill Kayser <bkayser@newrelic.com>
2009-03-26 23:27:45 +0000

    Fix jruby on windows

--- lib/new_relic/config.rb
+++ lib/new_relic/config.rb
@@ -180,17 +180,22 @@ module NewRelic
     
     def convert_to_ip_address(host)
       return nil unless host
+      return host if RUBY_PLATFORM == "java"
       if host.downcase == "localhost"
         ip_address = host
       else
         begin
           ip_address = Resolv.getaddress(host)
         rescue => e
-          log.error "DNS Error: #{e}"
-          raise NewRelic::Agent::IgnoreSilentlyException.new
+          log.debug "DNS Error caching with Resolv: #{e}"
+          begin
+            ip_address = IPSocket::getaddress host
+          rescue => e
+            log.warn "DNS Error looking up IP address: #{e}"
+            raise NewRelic::Agent::IgnoreSilentlyException.new
+          end
         end
       end
-      
       log.info "Resolved #{host} to #{ip_address}"
       ip_address
     end
