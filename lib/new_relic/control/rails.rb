102cfa8432c9bca8402bf0d2734dcb0a96532d01
Bill Kayser <bkayser@newrelic.com>
2009-06-03 00:53:08 +0000

    Add notice error global method

--- lib/new_relic/control/rails.rb
+++ lib/new_relic/control/rails.rb
@@ -133,7 +133,8 @@ class NewRelic::Control::Rails < NewRelic::Control
   def install_shim
     super
     require 'new_relic/agent/instrumentation/controller_instrumentation'
-    ActionController::Base.send :include, NewRelic::Agent::Instrumentation::ControllerInstrumentation::Shim  
+    ActionController::Base.send :include, NewRelic::Agent::Instrumentation::ControllerInstrumentation::Shim
+    Object.send :include, NewRelic::Agent::Instrumentation::ErrorInstrumentation::Shim
   end
   
 end
