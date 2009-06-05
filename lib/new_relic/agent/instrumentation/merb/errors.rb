dda1f4eb97abd7504c47e6ff226980f379568e8d
Justin George <justin@newrelic.com>
2009-06-05 21:54:38 +0000

    add a fix for people using the Exceptions controller for non-error conditions

--- lib/new_relic/agent/instrumentation/merb/errors.rb
+++ lib/new_relic/agent/instrumentation/merb/errors.rb
@@ -1,6 +1,8 @@
 # Hook in the notification to merb
 error_notifier = Proc.new {
-  NewRelic::Agent.agent.error_collector.notice_error(request.exceptions.first, request, "#{request.controller.name}/#{params[:action]}", params)
+  if request.exceptions #check that there's actually an exception
+    NewRelic::Agent.agent.error_collector.notice_error(request.exceptions.first, request, "#{request.controller.name}/#{params[:action]}", params)
+  end
 }
 Merb::Dispatcher::DefaultException.before error_notifier
 Exceptions.before error_notifier
