fee28b249dd3be685d6e0c293a7b906516c39923
Bill Kayser <bkayser@newrelic.com>
2009-07-07 17:25:48 +0000

    Revert the revert

--- lib/new_relic/local_environment.rb
+++ lib/new_relic/local_environment.rb
@@ -75,6 +75,10 @@ module NewRelic
       append_environment_value('Ruby version'){ RUBY_VERSION }
       append_environment_value('Ruby platform') { RUBY_PLATFORM }
       append_environment_value('Ruby patchlevel') { RUBY_PATCHLEVEL }
+      if defined? Java
+        append_environment_value('JRuby version') { JRUBY_VERSION }
+        append_environment_value('Java VM version') { ENV_JAVA['java.vm.version']}
+      end
       append_environment_value('OS version') { `uname -v` }
       append_environment_value('OS') { `uname -s` } ||
       append_environment_value('OS') { ENV['OS'] } 
