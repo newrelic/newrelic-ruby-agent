3320e274e2e7248ece135e9bdedcd3045ebba127
Bill Kayser <bkayser@newrelic.com>
2009-08-06 06:36:23 +0000

    Update the metric docs framework

--- lib/new_relic/agent/instrumentation/rails/action_controller.rb
+++ lib/new_relic/agent/instrumentation/rails/action_controller.rb
@@ -12,6 +12,7 @@ if defined? ActionController
     ActionView::PartialTemplate.class_eval do
       add_method_tracer :render, 'View/#{path_without_extension}.#{@view.template_format}.#{extension}/Partial'
     end
+    # this is for template rendering, as opposed to partial rendering.
     ActionView::Template.class_eval do
       add_method_tracer :render, 'View/#{path_without_extension}.#{@view.template_format}.#{extension}/Rendering'
     end
