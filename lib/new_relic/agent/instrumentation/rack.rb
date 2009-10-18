bdea5e1d983a76dd5639acf5386b93905b05bcd3
Bill Kayser <bkayser@newrelic.com>
2009-10-18 04:50:35 +0000

    Updating docs

--- lib/new_relic/agent/instrumentation/rack.rb
+++ lib/new_relic/agent/instrumentation/rack.rb
@@ -1,20 +1,4 @@
-# == Instrumentation for Rack ==
-#
-# New Relic will instrument a #call method as if it were a controller
-# action, collecting transaction traces and errors.  The middleware will
-# be identified only by it's class, so if you want to instrument multiple
-# actions in a middleware, you need to use
-# NewRelic::Agent::Instrumentation::ControllerInstrumentation::ClassMethods#add_transaction_tracer
-#
-# Example:
-#   require 'new_relic/agent/instrumentation/rack'
-#   class Middleware
-#     def call(env)
-#       ...
-#     end
-#     # Do the include after the call method is defined:
-#     include NewRelic::Agent::Instrumentation::Rack
-#   end
+
 
 require 'new_relic/agent/instrumentation/controller_instrumentation'
 
@@ -22,19 +6,51 @@ module NewRelic
   module Agent
     module Instrumentation
       module Rack
-        include ControllerInstrumentation
+        # == Instrumentation for Rack
+        #
+        # New Relic will instrument a #call method as if it were a controller
+        # action, collecting transaction traces and errors.  The middleware will
+        # be identified only by it's class, so if you want to instrument multiple
+        # actions in a middleware, you need to use
+        # NewRelic::Agent::Instrumentation::ControllerInstrumentation::ClassMethods#add_transaction_tracer
+        #
+        # Example:
+        #   require 'new_relic/agent/instrumentation/rack'
+        #   class Middleware
+        #     def call(env)
+        #       ...
+        #     end
+        #     # Do the include after the call method is defined:
+        #     include NewRelic::Agent::Instrumentation::Rack
+        #   end
+        #
+        # == Instrumenting Metal
+        #
+        # If you are using Metal, be sure and extend the your Metal class with the
+        # Rack instrumentation:
+        #
+        #   require 'new_relic/agent/instrumentation/rack'
+        #   class MetalApp
+        #     def self.call(env)
+        #       ...
+        #     end
+        #     # Do the include after the call method is defined:
+        #     extend NewRelic::Agent::Instrumentation::Rack
+        #   end
+        #
         def call_with_newrelic(*args)
           perform_action_with_newrelic_trace(:category => :rack) do
             call_without_newrelic(*args)
           end
         end
-        def self.included middleware
+        def self.included middleware #:nodoc:
           middleware.class_eval do
             alias call_without_newrelic call
             alias call call_with_newrelic
           end
         end
-        def self.extended middleware
+        include ControllerInstrumentation
+        def self.extended middleware #:nodoc:
           middleware.class_eval do
             class << self
               alias call_without_newrelic call
