9c9a4d349ac20b874733b53cd945db8310198c26
Bill Kayser <bkayser@newrelic.com>
2009-10-23 18:36:58 +0000

    fix test

--- lib/new_relic/agent/stats_engine.rb
+++ lib/new_relic/agent/stats_engine.rb
@@ -7,7 +7,8 @@ module NewRelic::Agent
     include MetricStats
     include Samplers
     include Transactions
-    include Transactions::Shim if NewRelic::Control.instance['disable_scope_tracking']
+    # race condition
+#    include Transactions::Shim if NewRelic::Control.instance['disable_scope_tracking']
     
     def initialize
       # Makes the unit tests happy
