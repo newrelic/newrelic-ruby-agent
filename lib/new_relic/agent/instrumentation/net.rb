21f4f7c8fd3b1828ca33a92b9a26ad530d10a714
Bill Kayser <bkayser@newrelic.com>
2009-11-13 21:14:39 +0000

    Change External/all to External/allWeb

--- lib/new_relic/agent/instrumentation/net.rb
+++ lib/new_relic/agent/instrumentation/net.rb
@@ -1,12 +1,9 @@
 # ActiveMerchant Instrumentation.
 
 if defined? Net::HTTP
-#  operations = %w[request]
-#  for op in operations do
-    Net::HTTP.class_eval do
-      add_method_tracer "request", 'External/#{@address}/Net::HTTP/#{args[0].method}'# + op 
-      add_method_tracer "request", 'External/#{@address}/all', :push_scope => false
-      add_method_tracer "request", 'External/all', :push_scope => false
-    end
-#  end
+  Net::HTTP.class_eval do
+    add_method_tracer "request", 'External/#{@address}/Net::HTTP/#{args[0].method}'# + op 
+    add_method_tracer "request", 'External/#{@address}/all', :push_scope => false
+    add_method_tracer "request", 'External/allWeb', :push_scope => false
+  end
 end
