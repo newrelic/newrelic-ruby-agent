8442571241259a625e52883691a22c25cf44e2dc
Bill Kayser <bkayser@newrelic.com>
2009-11-17 17:36:51 +0000

    Fix net bug

--- lib/new_relic/agent/instrumentation/net.rb
+++ lib/new_relic/agent/instrumentation/net.rb
@@ -2,8 +2,22 @@
 
 if defined? Net::HTTP
   Net::HTTP.class_eval do
-    add_method_tracer "request", 'External/#{@address}/Net::HTTP/#{args[0].method}'# + op 
-    add_method_tracer "request", 'External/#{@address}/all', :push_scope => false
-    add_method_tracer "request", 'External/allWeb', :push_scope => false
+    def request_with_newrelic_trace(*args)
+      if Thread::current[:newrelic_scope_stack].nil?
+        request_without_newrelic_trace(*args)
+      else
+        trace_method_execution_with_scope("External/#{@address}/Net::HTTP/#{args[0].method}",
+                                          true,
+                                          true) do
+          trace_method_execution_no_scope("External/#{@address}/all") do
+            trace_method_execution_no_scope("External/allWeb") do
+              request_without_newrelic_trace(*args)
+            end
+          end
+        end
+      end
+    end
+    alias request_without_newrelic_trace request
+    alias request request_with_newrelic_trace
   end
 end
