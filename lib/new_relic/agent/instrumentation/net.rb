cdbd27f3bd4a25e25e74bf9c40502ae1a31a7591
Bill Kayser <bkayser@newrelic.com>
2009-11-19 01:32:42 +0000

    Fix instrumentation

--- lib/new_relic/agent/instrumentation/net.rb
+++ lib/new_relic/agent/instrumentation/net.rb
@@ -1,17 +1,15 @@
-# ActiveMerchant Instrumentation.
-
 if defined? Net::HTTP
   Net::HTTP.class_eval do
-    def request_with_newrelic_trace(*args)
+    def request_with_newrelic_trace(*args, &block)
       if Thread::current[:newrelic_scope_stack].nil?
-        request_without_newrelic_trace(*args)
+        request_without_newrelic_trace(*args, &block)
       else
         self.class.trace_method_execution_with_scope("External/#{@address}/Net::HTTP/#{args[0].method}",
                                                      true,
                                                      true) do
           self.class.trace_method_execution_no_scope("External/#{@address}/all") do
             self.class.trace_method_execution_no_scope("External/allWeb") do
-              request_without_newrelic_trace(*args)
+              request_without_newrelic_trace(*args, &block)
             end
           end
         end
