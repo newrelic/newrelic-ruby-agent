b6058ab1b4676600a2f8387d22841a7b12dd40ef
Bill Kayser <bkayser@newrelic.com>
2009-12-14 15:02:47 -0800

    Fixing some tests, memcache instrumentation error

--- lib/new_relic/agent/instrumentation/memcache.rb
+++ lib/new_relic/agent/instrumentation/memcache.rb
@@ -3,19 +3,22 @@
 # See:
 #     http://www.deveiate.org/code/Ruby-MemCache/ (Gem: Ruby-MemCache)
 #     http://dev.robotcoop.com/Libraries/memcache-client/ (Gem: memcache-client)
-MemCache.class_eval do
-  add_method_tracer :get, 'MemCache/read' if self.method_defined? :get
-  add_method_tracer :get_multi, 'MemCache/read' if self.method_defined? :get_multi
+unless NewRelic::Control.instance['disable_memcache_instrumentation']
+  MemCache.class_eval do
+    add_method_tracer :get, 'MemCache/read' if self.method_defined? :get
+    add_method_tracer :get_multi, 'MemCache/read' if self.method_defined? :get_multi
   %w[set add incr decr delete].each do | method |
-    add_method_tracer method, 'MemCache/write' if self.method_defined? method
-  end
-end if defined?(MemCache) && !NewRelic::Config.instance['disable_memcache_instrumentation']
-
-# Support for libmemcached through Evan Weaver's memcached wrapper
-# http://blog.evanweaver.com/files/doc/fauna/memcached/classes/Memcached.html    
-Memcached.class_eval do
-  add_method_tracer :get, 'MemCache/read' if self.method_defined? :get
+      add_method_tracer method, 'MemCache/write' if self.method_defined? method
+    end
+  end if defined?(MemCache) 
+  
+  # Support for libmemcached through Evan Weaver's memcached wrapper
+  # http://blog.evanweaver.com/files/doc/fauna/memcached/classes/Memcached.html    
+  Memcached.class_eval do
+    add_method_tracer :get, 'MemCache/read' if self.method_defined? :get
   %w[set add increment decrement delete replace append prepend cas].each do | method |
-    add_method_tracer method, "MemCache/write" if self.method_defined? method
-  end
-end if defined? Memcached && !NewRelic::Config.instance['disable_memcache_instrumentation']
+      add_method_tracer method, "MemCache/write" if self.method_defined? method
+    end
+  end if defined? Memcached
+
+end
