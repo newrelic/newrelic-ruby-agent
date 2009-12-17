023fcfbd2011b44e874876c6e4a867bc6ee3fff9
Bill Kayser <bkayser@newrelic.com>
2009-12-17 13:18:56 -0800

    Updating changelog and version

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -4,7 +4,7 @@ module NewRelic
     MAJOR = 2
     MINOR = 10
     TINY  = 1
-    EXPERIMENTAL = 10
+    EXPERIMENTAL = 11
     STRING = [MAJOR, MINOR, TINY, EXPERIMENTAL].compact.join('.')
   end
   
