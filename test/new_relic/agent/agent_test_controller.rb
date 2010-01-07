439592bebfbffb50c05451e94b99eef5fecd4a5c
Bill Kayser <bkayser@newrelic.com>
2010-01-07 22:39:55 +0000

    More tests

--- test/new_relic/agent/agent_test_controller.rb
+++ test/new_relic/agent/agent_test_controller.rb
@@ -16,7 +16,6 @@ class NewRelic::Agent::AgentTestController < NewRelic::Agent::SuperclassControll
   end
   
   def index
-    puts params['wait']
     sleep params['wait'].to_i if params['wait']
     render :text => params.inspect
   end
