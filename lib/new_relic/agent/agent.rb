420f1ee565430f7bc05a9c7ede4ad20ead64701d
Bill Kayser <bkayser@newrelic.com>
2010-02-05 00:39:56 +0000

    Improve log output on startup

--- lib/new_relic/agent/agent.rb
+++ lib/new_relic/agent/agent.rb
@@ -154,8 +154,9 @@ module NewRelic::Agent
       if control.dispatcher.nil? || control.dispatcher.to_s.empty?
         log.info "No dispatcher detected."
       else
-        log.info "Web dispatcher: #{control.dispatcher.to_s}"
+        log.info "Dispatcher: #{control.dispatcher.to_s}"
       end
+      log.info "Application: #{control.app_names.join(", ")}" unless control.app_names.empty?
       
       @started = true
       
