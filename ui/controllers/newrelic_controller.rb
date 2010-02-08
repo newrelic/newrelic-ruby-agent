73c9912c2dfb968e1c49ca784205d3ae871e0fbd
Bill Kayser <bkayser@newrelic.com>
2010-02-08 23:18:29 +0000

    Fix 1.9.1. incompatibility in newrelic_controller

--- ui/controllers/newrelic_controller.rb
+++ ui/controllers/newrelic_controller.rb
@@ -47,7 +47,7 @@ class NewrelicController < ActionController::Base
   end
   
   def file
-    file_name=params[:file].to_s
+    file_name=params[:file].join
     file_name=~/^.*[.]([^.]*)$/
     ext=$1
     case ext
