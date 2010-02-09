9f8b4f5683b372a0ca89af9ccfb240f3bc4ef38c
Ralph Bodenner <ralph@newrelic.com>
2010-02-09 23:47:37 +0000

    Use RACK_ENV to set environment http://www.pivotaltracker.com/story/show/2446115

--- lib/newrelic_rpm.rb
+++ lib/newrelic_rpm.rb
@@ -10,6 +10,10 @@
 #    dependency 'newrelic_rpm'
 # in the Merb config/init.rb
 #
+# For Sinatra, do
+#    require 'newrelic_rpm'
+# after requiring 'sinatra'.
+#
 # For other frameworks, or to manage the agent manually, invoke NewRelic::Agent#manual_start
 # directly.
 #
@@ -30,7 +34,9 @@ elsif defined? Merb
         NewRelic::Control.instance.init_plugin
       end
     end
-  end  
+  end
+elsif defined? Rack && ENV['RACK_ENV']
+  NewRelic::Control.instance.init_plugin(:env => ENV['RACK_ENV'])
 else
   NewRelic::Control.instance.init_plugin
 end
