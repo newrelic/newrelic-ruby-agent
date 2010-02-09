8576e220c82a9b2321497fe3272410a62a1f6ab7
Bill Kayser <bkayser@newrelic.com>
2010-02-09 13:00:03 -0800

    Updates to delayed job instrumentation

---
+++ lib/new_relic/delayed_job_injection.rb
@@ -0,0 +1,18 @@
+# This installs some code to manually start the agent when a delayed job worker starts.
+
+Delayed::Worker.class_eval do
+  def initialize_with_new_relic(*args)
+    initialize_without_new_relic(*args)
+    dispatcher_instance_id = case
+    when self.respond_to?(:name) then self.name
+    when self.class.respond_to?(:default_name) then self.class.default_name
+    else
+      "host:#{Socket.gethostname} pid:#{Process.pid}" rescue "pid:#{Process.pid}"
+    end
+    
+    NewRelic::Agent.manual_start :dispatcher => :delayed_job, :dispatcher_instance_id => dispatcher_instance_id
+  end
+  
+  alias initialize_without_new_relic initialize
+  alias initialize initialize_with_new_relic
+end if defined? Delayed::Worker
