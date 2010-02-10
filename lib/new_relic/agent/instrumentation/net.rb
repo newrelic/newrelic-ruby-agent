f65cb2383abee7f59868144934b308abc01ebe86
Bill Kayser <bkayser@newrelic.com>
2010-02-10 19:38:45 +0000

    Use recording web transaction

--- lib/new_relic/agent/instrumentation/net.rb
+++ lib/new_relic/agent/instrumentation/net.rb
@@ -1,16 +1,16 @@
 if defined? Net::HTTP
   Net::HTTP.class_eval do
     def request_with_newrelic_trace(*args, &block)
-      if Thread::current[:newrelic_scope_name].nil?
-        self.class.trace_execution_unscoped(["External/#{@address}/Net::HTTP/#{args[0].method}", 
-                                             "External/#{@address}/all",
-                                             "External/allOther"]) do
-          request_without_newrelic_trace(*args, &block)
-        end
-      else
+      if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
         self.class.trace_execution_scoped(["External/#{@address}/Net::HTTP/#{args[0].method}",
                                            "External/#{@address}/all",
                                            "External/allWeb"]) do
+          request_without_newrelic_trace(*args, &block)
+        end
+      else
+        self.class.trace_execution_unscoped(["External/#{@address}/Net::HTTP/#{args[0].method}", 
+                                             "External/#{@address}/all",
+                                             "External/allOther"]) do
           request_without_newrelic_trace(*args, &block)
         end
       end
