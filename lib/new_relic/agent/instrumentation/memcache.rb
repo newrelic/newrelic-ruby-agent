2690a65e3916003fae63db21b552cfac7588bb09
Ralph Bodenner <ralph@newrelic.com>
2010-02-13 00:57:18 +0000

    Separate web/non-web MemCache metrics, only for memcache-client gem implementation http://www.pivotaltracker.com/story/show/2374762

--- lib/new_relic/agent/instrumentation/memcache.rb
+++ lib/new_relic/agent/instrumentation/memcache.rb
@@ -2,13 +2,138 @@
 # each with slightly different API's and semantics.  
 # See:
 #     http://www.deveiate.org/code/Ruby-MemCache/ (Gem: Ruby-MemCache)
-#     http://dev.robotcoop.com/Libraries/memcache-client/ (Gem: memcache-client)
+#     http://seattlerb.rubyforge.org/memcache-client/ (Gem: memcache-client)
 unless NewRelic::Control.instance['disable_memcache_instrumentation']
   MemCache.class_eval do
-    add_method_tracer :get, 'MemCache/read' if self.method_defined? :get
-    add_method_tracer :get_multi, 'MemCache/read' if self.method_defined? :get_multi
-  %w[set add incr decr delete].each do | method |
-      add_method_tracer method, 'MemCache/write' if self.method_defined? method
+    
+    # This is in the memcache-client implementation and has never existed in Ruby-MemCache
+    if self.method_defined? :cache_get
+
+      if self.method_defined? :get
+        def get_with_newrelic_trace(key, raw=false)
+          if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+            self.class.trace_execution_scoped(['MemCache/read', 'MemCache/allWeb']) do
+              get_without_newrelic_trace(key, raw)
+            end
+          else
+            self.class.trace_execution_scoped(['MemCache/read', 'MemCache/allOther']) do
+              get_without_newrelic_trace(key, raw)
+            end
+          end
+        end
+        
+        alias get_without_newrelic_trace get
+        alias get get_with_newrelic_trace
+      end
+
+      if self.method_defined? :get_multi
+        def get_multi_with_newrelic_trace(*keys)
+          if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+            self.class.trace_execution_scoped(['MemCache/read', 'MemCache/allWeb']) do
+              get_without_newrelic_trace(keys)
+            end
+          else
+            self.class.trace_execution_scoped(['MemCache/read', 'MemCache/allOther']) do
+              get_without_newrelic_trace(keys)
+            end
+          end
+        end
+        
+        alias get_multi_without_newrelic_trace get_multi
+        alias get_multi get_multi_with_newrelic_trace
+      end
+
+      if self.method_defined? :add
+        def add_with_newrelic_trace(key, value, expiry=0, raw=false)
+          if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allWeb']) do
+              add_without_newrelic_trace(key, value, expiry, raw)
+            end
+          else
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allOther']) do
+              add_without_newrelic_trace(key, value, expiry, raw)
+            end
+          end
+        end
+        
+        alias add_without_newrelic_trace add
+        alias add add_with_newrelic_trace
+      end
+
+      if self.method_defined? :decr
+        def decr_with_newrelic_trace(key, amount=1)
+          if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allWeb']) do
+              decr_without_newrelic_trace(key, amount)
+            end
+          else
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allOther']) do
+              decr_without_newrelic_trace(key, amount)
+            end
+          end
+        end
+
+        alias decr_without_newrelic_trace decr
+        alias decr decr_with_newrelic_trace
+      end
+
+      if self.method_defined? :delete
+        def delete_with_newrelic_trace(key, expiry=0)
+          if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allWeb']) do
+              delete_without_newrelic_trace(key, expiry)
+            end
+          else
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allOther']) do
+              delete_without_newrelic_trace(key, expiry)
+            end
+          end
+        end
+        
+        alias delete_without_newrelic_trace delete
+        alias delete delete_with_newrelic_trace
+      end
+
+      if self.method_defined? :incr
+        def incr_with_newrelic_trace(key, amount=1)
+          if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allWeb']) do
+              incr_without_newrelic_trace(key, amount)
+            end
+          else
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allOther']) do
+              incr_without_newrelic_trace(key, amount)
+            end
+          end
+        end
+
+        alias incr_without_newrelic_trace incr
+        alias incr incr_with_newrelic_trace
+      end
+
+      if self.method_defined? :set
+        def set_with_newrelic_trace(key, value, expiry=0, raw=false)
+          if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allWeb']) do
+              set_without_newrelic_trace(key, value, expiry, raw)
+            end
+          else
+            self.class.trace_execution_scoped(['MemCache/write', 'MemCache/allOther']) do
+              set_without_newrelic_trace(key, value, expiry, raw)
+            end
+          end
+        end
+        
+        alias set_without_newrelic_trace set
+        alias set set_with_newrelic_trace
+      end
+
+    else  # Ruby-MemCache
+      add_method_tracer :get, 'MemCache/read' if self.method_defined? :get
+      add_method_tracer :get_multi, 'MemCache/read' if self.method_defined? :get_multi
+      %w[set add incr decr delete].each do | method |
+        add_method_tracer method, 'MemCache/write' if self.method_defined? method
+      end
     end
   end if defined? MemCache 
   
---
+++ test/new_relic/agent/memcache_instrumentation_test.rb
@@ -0,0 +1,101 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','test_helper')) 
+
+class NewRelic::Agent::MemcacheInstrumentationTest < Test::Unit::TestCase
+  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+
+  # This implementation: http://seattlerb.rubyforge.org/memcache-client/
+  def using_memcache_client?
+    MemCache.method_defined? :cache_get
+  end
+
+  def setup
+    NewRelic::Agent.manual_start
+    @engine = NewRelic::Agent.instance.stats_engine
+    
+    if using_memcache_client?
+      @cache = MemCache.new('localhost')
+    else
+      server = MemCache::Server.new('localhost')
+      @cache = MemCache.new(server)
+    end
+    @key = 'schluessel'
+    @task = 'task'
+  end
+
+  def _call_test_method_in_web_transaction(method, *args)
+    @engine.clear_stats
+    begin
+      perform_action_with_newrelic_trace(@task) do
+        @cache.send(method.to_sym, *[@key, *args])
+      end
+    rescue MemCache::MemCacheError
+      # There's probably no memcached around
+    end
+  end
+
+  def _call_test_method_in_background_task(method, *args)
+    @engine.clear_stats
+    begin
+      perform_action_with_newrelic_trace(@task, :category => :task) do
+        @cache.send(method.to_sym, *[@key, *args])
+      end
+    rescue MemCache::MemCacheError
+      # There's probably no memcached around
+    end
+  end
+
+  def test_reads__web
+    %w[get get_multi].each do |method|
+      if @cache.class.method_defined?(method)
+        _call_test_method_in_web_transaction(method)
+        assert_equal ["MemCache/read", "MemCache/allWeb", "MemCache/read:Controller/NewRelic::Agent::MemcacheInstrumentationTest/#{@task}"].sort, @engine.metrics.select{|m| m =~ /^memcache.*/i}.sort, "Failed on method #{method}"
+      end
+    end
+  end
+  
+  def test_writes__web
+    expected_metrics = ["MemCache/write", "MemCache/allWeb", "MemCache/write:Controller/NewRelic::Agent::MemcacheInstrumentationTest/#{@task}"]
+
+    %w[incr decr delete].each do |method|
+      if @cache.class.method_defined?(method)
+        _call_test_method_in_web_transaction(method)
+        assert_equal expected_metrics.sort, @engine.metrics.select{|m| m =~ /^memcache.*/i}.sort, "Failed on method #{method}"
+      end
+    end
+
+    %w[set add].each do |method|
+      if @cache.class.method_defined?(method)
+        _call_test_method_in_web_transaction(method, 'value')
+        assert_equal expected_metrics.sort, @engine.metrics.select{|m| m =~ /^memcache.*/i}.sort, "Failed on method #{method}"
+      end
+    end
+  end
+
+  def test_reads__background
+    %w[get get_multi].each do |method|
+      if @cache.class.method_defined?(method)
+        _call_test_method_in_background_task(method)
+        assert_equal ["MemCache/read", "MemCache/allOther", "MemCache/read:OtherTransaction/Background/NewRelic::Agent::MemcacheInstrumentationTest/#{@task}"].sort, @engine.metrics.select{|m| m =~ /^memcache.*/i}.sort, "Failed on method #{method}"
+      end
+    end
+  end
+
+  def test_writes__background
+    expected_metrics = ["MemCache/write", "MemCache/allOther", "MemCache/write:OtherTransaction/Background/NewRelic::Agent::MemcacheInstrumentationTest/#{@task}"]
+
+    %w[incr decr delete].each do |method|
+      if @cache.class.method_defined?(method)
+        _call_test_method_in_background_task(method)
+        assert_equal expected_metrics.sort, @engine.metrics.select{|m| m =~ /^memcache.*/i}.sort, "Failed on method #{method}"
+      end
+    end
+
+    %w[set add].each do |method|
+      if @cache.class.method_defined?(method)
+        _call_test_method_in_background_task(method, 'value')
+        assert_equal expected_metrics.sort, @engine.metrics.select{|m| m =~ /^memcache.*/i}.sort, "Failed on method #{method}"
+      end
+    end
+  end
+
+end
