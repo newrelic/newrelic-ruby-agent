cb7cb81e5b38be3a127fc9067f8d8901c4ce1b55
Bill Kayser <bkayser@newrelic.com>
2010-02-12 17:43:59 -0800

    Segregate net instrumentation into web and non-web

--- lib/new_relic/agent/instrumentation/net.rb
+++ lib/new_relic/agent/instrumentation/net.rb
@@ -1,18 +1,14 @@
 if defined? Net::HTTP
   Net::HTTP.class_eval do
     def request_with_newrelic_trace(*args, &block)
-      if Thread::current[:newrelic_scope_name].nil?
-        self.class.trace_execution_unscoped(["External/#{@address}/Net::HTTP/#{args[0].method}", 
-                                             "External/#{@address}/all",
-                                             "External/allOther"]) do
-          request_without_newrelic_trace(*args, &block)
-        end
+      metrics = ["External/#{@address}/Net::HTTP/#{args[0].method}","External/#{@address}/all"]
+      if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+        metrics << "External/allWeb"
       else
-        self.class.trace_execution_scoped(["External/#{@address}/Net::HTTP/#{args[0].method}",
-                                           "External/#{@address}/all",
-                                           "External/allWeb"]) do
-          request_without_newrelic_trace(*args, &block)
-        end
+        metrics << "External/allOther"
+      end
+      self.class.trace_execution_scoped metrics do
+        request_without_newrelic_trace(*args, &block)
       end
     end
     alias request_without_newrelic_trace request
