6ffa080947cf2d93940d14645a1163d2ea9f4a15
Bill Kayser <bkayser@newrelic.com>
2010-03-21 20:22:03 +0000

    bump version to 2.11

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -2,9 +2,9 @@
 module NewRelic
   module VERSION #:nodoc:
     MAJOR = 2
-    MINOR = 10
-    TINY  = 7
-    BUILD = nil #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
+    MINOR = 11
+    TINY  = 0
+    BUILD = 0 #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
   
