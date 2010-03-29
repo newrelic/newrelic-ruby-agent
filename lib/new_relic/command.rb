6bc961f5dcecbad1160cd87a6c211012da8c6b78
Bill Kayser <bkayser@newrelic.com>
2010-03-29 22:31:27 +0000

    Fix the commands.

--- lib/new_relic/command.rb
+++ lib/new_relic/command.rb
@@ -3,7 +3,7 @@ require 'optparse'
 # Run the command given by the first argument.  Right
 # now all we have is deployments. We hope to have other
 # kinds of events here later.
-
+$LOAD_PATH << "#{File.dirname(__FILE__)}/.."
 module NewRelic
   class Command
     attr_accessor :leftover
