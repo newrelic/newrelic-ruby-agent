9f99897ca4c93f9f0239b13ae93e5cac1d1a4ad3
Bill Kayser <bkayser@newrelic.com>
2010-03-29 23:15:18 +0000

    allow a force reconnection in the after_fork call to force a new agent run creation even if one already connected in the master Make reset_stats part of the api.

--- lib/new_relic/agent/instrumentation/passenger_instrumentation.rb
+++ lib/new_relic/agent/instrumentation/passenger_instrumentation.rb
@@ -11,7 +11,7 @@ if defined?(PhusionPassenger)
       # We want to reset the stats from the stats engine in case any carried
       # over into the spawned process.  Don't clear them in case any were
       # cached.
-      NewRelic::Agent.after_fork
+      NewRelic::Agent.after_fork(:force_reconnect => true)
     else 
       # We're in conservative spawning mode. We don't need to do anything.
     end
