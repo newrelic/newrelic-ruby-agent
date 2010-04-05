d5c688825117c4882b2d1dc94e223aed504c94ba
Thomas Bukowski <thomas@newrelic.com>
2010-04-05 18:07:50 +0000

    episodes wip - user_agent normalization and metrics

--- lib/new_relic/rack/episodes.rb
+++ lib/new_relic/rack/episodes.rb
@@ -28,6 +28,8 @@ module NewRelic
       def process
         measures = @request['ets'].split(',').map { |str| str.split(':') }
         url = @request['url']
+        user_agent = @request['userAgent']
+        puts "useragent: #{user_agent}"
         if false and defined?(::ActionController::Routing::Routes)
           params = ::ActionController::Routing::Routes.recognize_path(url) rescue {}
           controller, action = params.values_at :controller, :action
@@ -37,9 +39,59 @@ module NewRelic
         measures.each do | name, value |
           metric_name = "Client/#{name}"
           NewRelic::Agent.instance.stats_engine.get_stats_no_scope(metric_name).record_data_point(value.to_f / 1000.0)
+          
+          if user_agent
+            browser, version, os = identify_browser_and_os(user_agent)
+            browser_metric_name = "Client/#{name}/#{os} #{browser} #{version}"
+            NewRelic::Agent.instance.stats_engine.get_stats_no_scope(browser_metric_name).record_data_point(value.to_f / 1000.0)
+          end
         end
         @response.finish
       end
+      
+      def identify_browser_and_os(user_agent)
+        user_agent = user_agent.downcase
+
+        # tests for browser version
+        # for safari and firefox we take the major and minor version - e.g. Firefox 2.1 or Safari 4.0
+        # for all other browsers we take only the major version - e.g. IE 7, Chrome 3
+
+        # ignored but potentially popular browsers: 
+        # - mozilla or gecko-based but non-firefox browsers
+
+        if user_agent[/opera/]
+          browser = 'Opera'
+          version = ($1 || 0).to_i if user_agent[/opera(?:.*version)?[ \/](\d+)\./]
+        elsif user_agent[/chrome(?:\/(\d+)\.)?/] # chrome must go before safari
+          browser = 'Chrome'
+          version = ($1 || 0).to_i
+        elsif user_agent[/safari|applewebkit\//]
+          browser = 'Safari'
+          version = $1.to_f if user_agent[/version\/(\d+\.\d+)/] # safari used Version/* starting from 3.0
+        elsif user_agent[/msie (\d+)\./]
+          browser = 'IE'
+          version = $1.to_i
+        elsif user_agent[/compatible/].nil? && user_agent[/like firefox\//].nil? && user_agent[/firefox\//]
+          browser = 'Firefox'
+          version = $1.to_f if user_agent[/firefox\/(\d+\.\d+)/]
+        elsif user_agent[/compatible/].nil? && user_agent[/gecko\/?/]
+          browser = 'Mozilla Gecko'
+          version = $1.to_f if user_agent[/rv:(\d+\.\d+)/]
+        end
+
+        if user_agent[/windows/]
+          os = 'Windows'
+        elsif user_agent[/iphone/] # iphone must go before mac
+          os = 'iPhone'
+        elsif user_agent[/macintosh|mac os/]
+          os = 'Mac'
+        elsif user_agent[/linux/]
+          os = 'Linux'
+        end
+
+        return browser || 'Unknown', version || 0, os || 'Unknown'
+      end
+
     end
   end
 end
