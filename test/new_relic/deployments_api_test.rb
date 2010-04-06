f02a80459bd2b2f8cc949223976b109c683986eb
Bill Kayser <bkayser@newrelic.com>
2010-04-06 20:15:51 +0000

    fix deployments test; add jeweler dependency

--- test/new_relic/deployments_api_test.rb
+++ test/new_relic/deployments_api_test.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.join(File.dirname(__FILE__),'../../lib/new_relic/commands/deployments'))
+require File.expand_path(File.join(File.dirname(__FILE__),'/../test_helper'))
+require File.expand_path(File.join(File.dirname(__FILE__),'/../../lib/new_relic/command'))
 require 'rubygems'
 require 'mocha'
 
