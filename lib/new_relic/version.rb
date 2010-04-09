d786ec48c5f8f885477e94d83ab79257670ad4b3
Bill Kayser <bkayser@newrelic.com>
2010-04-09 00:43:32 +0000

    Fix regression for Passenger 2.0 and earlier

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -4,7 +4,7 @@ module NewRelic
     MAJOR = 2
     MINOR = 11
     TINY  = 2
-    BUILD = 'beta' #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
+    BUILD = 'beta2' #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
   
