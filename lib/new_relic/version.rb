8cd7e77d2b41bf66161cd5117d09b24cdc61b98c
Bill Kayser <bkayser@newrelic.com>
2010-05-05 00:10:32 +0000

    shift trunk to version 2.13.0

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -2,7 +2,7 @@
 module NewRelic
   module VERSION #:nodoc:
     MAJOR = 2
-    MINOR = 12
+    MINOR = 13
     TINY  = 0
     BUILD = 'dev' #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
