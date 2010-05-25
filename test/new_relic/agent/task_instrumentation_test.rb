85b5e78db999b1a1f18c3b9bb0649677ee5a1294
Bill Kayser <bkayser@newrelic.com>
2010-05-25 18:02:06 +0000

    cleanup, shoulda conversion

--- test/new_relic/agent/task_instrumentation_test.rb
+++ test/new_relic/agent/task_instrumentation_test.rb
@@ -1,168 +1,160 @@
 # Run faster standalone
 ENV['SKIP_RAILS'] = 'true'
 
-require File.expand_path(File.join(File.dirname(__FILE__),'..','..','test_helper')) 
+require File.expand_path('../../../test_helper', __FILE__) 
 
 class TaskInstrumentationTest < Test::Unit::TestCase
   include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  extend TestContexts
   attr_accessor :agent
-  def setup
-    super
-    NewRelic::Agent.manual_start
-    @agent = NewRelic::Agent.instance
-    @agent.transaction_sampler.send :clear_builder
-    @agent.transaction_sampler.reset!
-    @agent.stats_engine.clear_stats
-  end
-  def teardown
-    NewRelic::Agent.shutdown
-    super
-  end
   
-  def test_run
-    run_task_inner 0
-    stat_names = %w[Controller/TaskInstrumentationTest/inner_task_0
+  with_running_agent do
+    
+    should "run" do
+      run_task_inner 0
+      stat_names = %w[Controller/TaskInstrumentationTest/inner_task_0
                     HttpDispatcher
                     Apdex Apdex/TaskInstrumentationTest/inner_task_0].sort
-    expected_but_missing = stat_names - @agent.stats_engine.metrics
-    assert_equal 0, expected_but_missing.size, @agent.stats_engine.metrics.map  { |n|
-      stat = @agent.stats_engine.get_stats_no_scope(n)
+      expected_but_missing = stat_names - @agent.stats_engine.metrics
+      assert_equal 0, expected_but_missing.size, @agent.stats_engine.metrics.map  { |n|
+        stat = @agent.stats_engine.get_stats_no_scope(n)
       "#{'%-26s' % n}: #{stat.call_count} calls @ #{stat.average_call_time} sec/call"
-    }.join("\n  ") + "\nmissing: #{expected_but_missing.inspect}"
-    assert_equal 0, @agent.stats_engine.get_stats_no_scope('Controller').call_count
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
-  end
-  
-  def test_run_recursive
-    run_task_inner(3)
-    assert_equal 1, @agent.stats_engine.lookup_stats(
+      }.join("\n  ") + "\nmissing: #{expected_but_missing.inspect}"
+      assert_equal 0, @agent.stats_engine.get_stats_no_scope('Controller').call_count
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
+    end
+    
+    should "run_recursive" do
+      run_task_inner(3)
+      assert_equal 1, @agent.stats_engine.lookup_stats(
                           'Controller/TaskInstrumentationTest/inner_task_0',
                           'Controller/TaskInstrumentationTest/inner_task_1').call_count
-    assert_equal 1, @agent.stats_engine.lookup_stats(
+      assert_equal 1, @agent.stats_engine.lookup_stats(
                           'Controller/TaskInstrumentationTest/inner_task_1',
                           'Controller/TaskInstrumentationTest/inner_task_2').call_count
-    assert_equal 1, @agent.stats_engine.lookup_stats(
+      assert_equal 1, @agent.stats_engine.lookup_stats(
                           'Controller/TaskInstrumentationTest/inner_task_2',
                           'Controller/TaskInstrumentationTest/inner_task_3').call_count
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_1').call_count
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_2').call_count
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_3').call_count
-    assert_equal 0, @agent.stats_engine.get_stats_no_scope('Controller').call_count
-  end
-  
-  def test_run_nested
-    run_task_outer(3)
-    @agent.stats_engine.metrics.sort.each do |n|
-      stat = @agent.stats_engine.get_stats_no_scope(n)
-      #      puts "#{'%-26s' % n}: #{stat.call_count} calls @ #{stat.average_call_time} sec/call"
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_1').call_count
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_2').call_count
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_3').call_count
+      assert_equal 0, @agent.stats_engine.get_stats_no_scope('Controller').call_count
     end
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/outer_task').call_count
-    assert_equal 2, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
-  end
-  
-  def test_reentrancy
-    assert_equal 0, NewRelic::Agent::BusyCalculator.busy_count
-    run_task_inner(1)
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_1').call_count
-    compare_metrics %w[
+    
+    should "run_nested" do
+      run_task_outer(3)
+      @agent.stats_engine.metrics.sort.each do |n|
+        stat = @agent.stats_engine.get_stats_no_scope(n)
+        #      puts "#{'%-26s' % n}: #{stat.call_count} calls @ #{stat.average_call_time} sec/call"
+      end
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/outer_task').call_count
+      assert_equal 2, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
+    end
+    
+    should "reentrancy" do
+      assert_equal 0, NewRelic::Agent::BusyCalculator.busy_count
+      run_task_inner(1)
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_1').call_count
+      compare_metrics %w[
       Controller/TaskInstrumentationTest/inner_task_0:Controller/TaskInstrumentationTest/inner_task_1
       Controller/TaskInstrumentationTest/inner_task_0
       Controller/TaskInstrumentationTest/inner_task_1
       ], @agent.stats_engine.metrics.grep(/^Controller/)
-  end
-  
-  def test_transaction
-    assert_equal 0, @agent.transaction_sampler.scope_depth, "existing unfinished sample"
-    assert_nil @agent.transaction_sampler.last_sample
-    assert_equal @agent.transaction_sampler, @agent.stats_engine.instance_variable_get("@transaction_sampler")
-    run_task_outer(10)
-    assert_equal 0, @agent.transaction_sampler.scope_depth, "existing unfinished sample"
-    @agent.stats_engine.metrics.sort.each do |n|
-      stat = @agent.stats_engine.get_stats_no_scope(n)
-      #      puts "#{'%-26s' % n}: #{stat.call_count} calls @ #{stat.average_call_time} sec/call"
     end
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/outer_task').call_count
-    assert_equal 0, @agent.stats_engine.get_stats_no_scope('Controller').call_count
-    assert_equal 2, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
-    assert_equal 0, @agent.transaction_sampler.scope_depth, "existing unfinished sample"
-    sample = @agent.transaction_sampler.last_sample
-    assert_not_nil sample
-    assert_not_nil sample.params[:cpu_time], "cpu time nil: \n#{sample}"
-    assert sample.params[:cpu_time] >= 0, "cpu time: #{sample.params[:cpu_time]},\n#{sample}"
-    assert_equal '10', sample.params[:request_params][:level]
-  end
-  
-  def test_abort
-    @acct = 'Redrocks'
-    perform_action_with_newrelic_trace(:name => 'hello', :force => true, :params => { :account => @acct}) do
-      self.class.inspect
-      NewRelic::Agent.abort_transaction!
-    end
-    # We record the controller metric still, but abort any transaction recording.
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/hello').call_count
-    assert_nil @agent.transaction_sampler.last_sample
-  end
-  
-  def test_block
-    assert_equal @agent, NewRelic::Agent.instance
-    @acct = 'Redrocks'
-    perform_action_with_newrelic_trace(:name => 'hello', :force => true, :params => { :account => @acct}) do
-      self.class.inspect
-    end
-    @agent.stats_engine.metrics.sort.each do |n|
-      stat = @agent.stats_engine.get_stats_no_scope(n)
-      #puts "#{'%-26s' % n}: #{stat.call_count} calls @ #{stat.average_call_time} sec/call"
-    end
-    assert_equal @agent, NewRelic::Agent.instance
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/hello').call_count
-    sample = @agent.transaction_sampler.last_sample
-    assert_not_nil sample
-    assert_equal 'Redrocks', sample.params[:request_params][:account]
     
-  end
-  
-  def test_error_handling
-    @agent.error_collector.ignore_error_filter
-    @agent.error_collector.harvest_errors([])
-    @agent.error_collector.expects(:notice_error).once
-    assert_equal @agent.error_collector, NewRelic::Agent.instance.error_collector
-    assert_raise RuntimeError do
-      run_task_exception
+    should "transaction" do
+      assert_equal 0, @agent.transaction_sampler.scope_depth, "existing unfinished sample"
+      assert_nil @agent.transaction_sampler.last_sample
+      assert_equal @agent.transaction_sampler, @agent.stats_engine.instance_variable_get("@transaction_sampler")
+      run_task_outer(10)
+      assert_equal 0, @agent.transaction_sampler.scope_depth, "existing unfinished sample"
+      @agent.stats_engine.metrics.sort.each do |n|
+        stat = @agent.stats_engine.get_stats_no_scope(n)
+        #      puts "#{'%-26s' % n}: #{stat.call_count} calls @ #{stat.average_call_time} sec/call"
+      end
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/outer_task').call_count
+      assert_equal 0, @agent.stats_engine.get_stats_no_scope('Controller').call_count
+      assert_equal 2, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/inner_task_0').call_count
+      assert_equal 0, @agent.transaction_sampler.scope_depth, "existing unfinished sample"
+      sample = @agent.transaction_sampler.last_sample
+      assert_not_nil sample
+      assert_not_nil sample.params[:cpu_time], "cpu time nil: \n#{sample}"
+      assert sample.params[:cpu_time] >= 0, "cpu time: #{sample.params[:cpu_time]},\n#{sample}"
+      assert_equal '10', sample.params[:request_params][:level]
     end
-  end
-  
-  def test_custom_params
-    @agent.error_collector.stubs(:enabled).returns(true)
-    @agent.error_collector.ignore_error_filter
-    @agent.error_collector.harvest_errors([])
-    assert_equal @agent.error_collector, NewRelic::Agent.instance.error_collector
-    assert_raise RuntimeError do
-      run_task_exception
+    
+    should "abort" do
+      @acct = 'Redrocks'
+      perform_action_with_newrelic_trace(:name => 'hello', :force => true, :params => { :account => @acct}) do
+        self.class.inspect
+        NewRelic::Agent.abort_transaction!
+      end
+      # We record the controller metric still, but abort any transaction recording.
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/hello').call_count
+      assert_nil @agent.transaction_sampler.last_sample
     end
-    errors = @agent.error_collector.harvest_errors([])
-    assert_equal 1, errors.size
-    error = errors.first
-    assert_equal "Controller/TaskInstrumentationTest/run_task_exception", error.path
-    assert_not_nil error.params[:stack_trace]
-    assert_not_nil error.params[:custom_params]
-  end
-  
-  def test_instrument_bg
-    run_background_job
-    stat_names = %w[OtherTransaction/Background/TaskInstrumentationTest/run_background_job
+    should "block" do
+      assert_equal @agent, NewRelic::Agent.instance
+      @acct = 'Redrocks'
+      perform_action_with_newrelic_trace(:name => 'hello', :force => true, :params => { :account => @acct}) do
+        self.class.inspect
+      end
+      @agent.stats_engine.metrics.sort.each do |n|
+        stat = @agent.stats_engine.get_stats_no_scope(n)
+        #puts "#{'%-26s' % n}: #{stat.call_count} calls @ #{stat.average_call_time} sec/call"
+      end
+      assert_equal @agent, NewRelic::Agent.instance
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('Controller/TaskInstrumentationTest/hello').call_count
+      sample = @agent.transaction_sampler.last_sample
+      assert_not_nil sample
+      assert_equal 'Redrocks', sample.params[:request_params][:account]
+      
+    end
+    
+    should "error_handling" do
+      @agent.error_collector.ignore_error_filter
+      @agent.error_collector.harvest_errors([])
+      @agent.error_collector.expects(:notice_error).once
+      assert_equal @agent.error_collector, NewRelic::Agent.instance.error_collector
+      assert_raise RuntimeError do
+        run_task_exception
+      end
+    end
+    
+    should "custom_params" do
+      @agent.error_collector.stubs(:enabled).returns(true)
+      @agent.error_collector.ignore_error_filter
+      @agent.error_collector.harvest_errors([])
+      assert_equal @agent.error_collector, NewRelic::Agent.instance.error_collector
+      assert_raise RuntimeError do
+        run_task_exception
+      end
+      errors = @agent.error_collector.harvest_errors([])
+      assert_equal 1, errors.size
+      error = errors.first
+      assert_equal "Controller/TaskInstrumentationTest/run_task_exception", error.path
+      assert_not_nil error.params[:stack_trace]
+      assert_not_nil error.params[:custom_params]
+    end
+    
+    should "instrument_bg" do
+      run_background_job
+      stat_names = %w[OtherTransaction/Background/TaskInstrumentationTest/run_background_job
                     OtherTransaction/Background/all
                     OtherTransaction/all].sort
-                    
-    expected_but_missing = stat_names - @agent.stats_engine.metrics
-    assert_equal 0, expected_but_missing.size, @agent.stats_engine.metrics.map  { |n|
-      stat = @agent.stats_engine.get_stats_no_scope(n)
+      
+      expected_but_missing = stat_names - @agent.stats_engine.metrics
+      assert_equal 0, expected_but_missing.size, @agent.stats_engine.metrics.map  { |n|
+        stat = @agent.stats_engine.get_stats_no_scope(n)
       "#{'%-26s' % n}: #{stat.call_count} calls @ #{stat.average_call_time} sec/call"
-    }.join("\n  ") + "\nmissing: #{expected_but_missing.inspect}"
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('OtherTransaction/all').call_count
-    assert_equal 1, @agent.stats_engine.get_stats_no_scope('OtherTransaction/Background/all').call_count
+      }.join("\n  ") + "\nmissing: #{expected_but_missing.inspect}"
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('OtherTransaction/all').call_count
+      assert_equal 1, @agent.stats_engine.get_stats_no_scope('OtherTransaction/Background/all').call_count
+    end
   end
+  
   private
   
   def run_task_inner(n)
