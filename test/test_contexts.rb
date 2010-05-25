5c130f4176e707879efafa3d9a9cdecc4bd9db4a
Bill Kayser <bkayser@newrelic.com>
2010-05-25 18:02:37 +0000

    Cleanup, shoulda conversion

--- test/test_contexts.rb
+++ test/test_contexts.rb
@@ -5,10 +5,23 @@ module TestContexts
     context 'with running agent' do # this is needed for the nested setups
       
       setup do
-        NewRelic::Agent.manual_start
+        @log_data = StringIO.new
+        @log = Logger.new(@log_data)
+        NewRelic::Agent.manual_start :log => @log 
+        @agent = NewRelic::Agent.instance
+        @agent.transaction_sampler.send :clear_builder
+        @agent.transaction_sampler.reset!
+        @agent.stats_engine.clear_stats
       end
       
       yield
+      
+      def teardown
+        NewRelic::Agent.shutdown
+        @log_data.reset
+        NewRelic::Control.instance['dispatcher']=nil
+        NewRelic::Control.instance['dispatcher_instance_id']=nil
+      end
       
     end
   end
