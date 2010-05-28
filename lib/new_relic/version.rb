55f51f173225f5288f604600167e22f8f1accfa0
Bill Kayser <bkayser@newrelic.com>
2010-05-28 23:02:14 +0000

    Fixes for segfaults

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -3,8 +3,8 @@ module NewRelic
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 12
-    TINY  = 1
-    BUILD = nil #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
+    TINY  = 2
+    BUILD = 'beta' #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
   
