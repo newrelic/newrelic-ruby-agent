1285f74109b865d520b4f009ce939585c4e2437b
Justin George <justin.george@gmail.com>
2010-08-26 16:34:10 -0700

    merge patches from development

--- lib/new_relic/agent/sampler.rb
+++ lib/new_relic/agent/sampler.rb
@@ -6,41 +6,45 @@
 # Samplers can be added to New Relic by subclassing NewRelic::Agent::Sampler.
 # Instances are created when the agent is enabled and installed.  Subclasses
 # are registered for instantiation automatically.
-class NewRelic::Agent::Sampler
-  
-  # Exception denotes a sampler is not available and it will not be registered.
-  class Unsupported < StandardError;  end
-  
-  attr_accessor :stats_engine
-  attr_reader :id
-  @sampler_classes = []
-  
-  def self.inherited(subclass)
-    @sampler_classes << subclass
+module NewRelic
+  module Agent
+    class Sampler
+      
+      # Exception denotes a sampler is not available and it will not be registered.
+      class Unsupported < StandardError;  end
+      
+      attr_accessor :stats_engine
+      attr_reader :id
+      @sampler_classes = []
+      
+      def self.inherited(subclass)
+        @sampler_classes << subclass
+      end
+      
+      # Override with check.  Called before instantiating.
+      def self.supported_on_this_platform?
+        true
+      end
+      
+      # Override to use the periodic sampler instead of running the sampler on the
+      # minute during harvests.
+      def self.use_harvest_sampler?
+        true
+      end
+      
+      def self.sampler_classes
+        @sampler_classes
+      end
+      
+      def initialize(id)
+        @id = id
+      end
+      
+      def poll
+        raise "Implement in the subclass"
+      end
+      
+      
+    end
   end
-  
-  # Override with check.  Called before instantiating.
-  def self.supported_on_this_platform?
-    true
-  end
-  
-  # Override to use the periodic sampler instead of running the sampler on the
-  # minute during harvests.
-  def self.use_harvest_sampler?
-    true
-  end
-  
-  def self.sampler_classes
-    @sampler_classes
-  end
-  
-  def initialize(id)
-    @id = id
-  end
-  
-  def poll
-    raise "Implement in the subclass"
-  end
-  
-  
 end
