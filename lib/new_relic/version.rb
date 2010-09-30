6706494364bd30c059a8874a73fedd1b78cc5e06
Justin George <justin.george@gmail.com>
2010-09-30 16:39:25 -0700

    update version and gemspec

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -3,7 +3,7 @@ module NewRelic
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 13
-    TINY  = 3
+    TINY  = 4
     BUILD = 'alpha' #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
