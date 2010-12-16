5f3586b1ee8f381aca1e3c8898fe2ff609305fca
Justin George <justin.george@gmail.com>
2010-12-15 20:51:33 -0800

    try requiring the action controller test case

--- test/new_relic/agent/agent_controller_test.rb
+++ test/new_relic/agent/agent_controller_test.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.join(File.dirname(__FILE__),'..','..','test_helper')) 
-
+require 'action_controller/test_case'
 class AgentControllerTest < ActionController::TestCase
   require 'action_controller/base'
   require 'new_relic/agent/agent_test_controller'
