495d66d63d8783ace93136140b60463504e2a038
Jim Gochee <jim@newrelic.com>
2011-02-18 15:36:25 -0800

    Renamed EPISODES to NREUM

--- lib/new_relic/agent/browser_monitoring.rb
+++ lib/new_relic/agent/browser_monitoring.rb
@@ -13,7 +13,7 @@ module NewRelic
           load_js = "(function() { var e=document.createElement(\"script\");e.type=\"text/javascript\";e.async=true; e.src=document.location.protocol+\"#{episodes_file}\"; var s=document.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(e,s);})();"
         end
       
-        "<script>var EPISODES=EPISODES||{};EPISODES.q=[];EPISODES.q.push([\"mark\",\"firstbyte\",new Date().getTime()]);#{load_js}</script>"
+        "<script>var NREUM=NREUM||{};NREUM.q=[];NREUM.q.push([\"mark\",\"firstbyte\",new Date().getTime()]);#{load_js}</script>"
       end
       
       def browser_instrumentation_footer(options={})
@@ -34,7 +34,7 @@ module NewRelic
           app_time = ((Time.now - frame.start).to_f * 1000.0).round
  
 <<-eos
-<script type="text/javascript" charset="utf-8">EPISODES.q.push(["nrfinish","#{beacon}","#{license_key}","#{application_id}","#{transaction_name}","#{queue_time}","#{app_time}"])</script>
+<script type="text/javascript" charset="utf-8">NREUM.q.push(["nrfinish","#{beacon}","#{license_key}","#{application_id}","#{transaction_name}","#{queue_time}","#{app_time}"])</script>
 eos
         end
       end
