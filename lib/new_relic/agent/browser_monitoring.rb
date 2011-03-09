bc6ec537eeba7138b2cd548df178e5a98b661bd0
Darin Wright <dwright@newrelic.com>
2011-03-09 13:50:57 -0600

    Obfuscate with first 13 chars of license key

--- lib/new_relic/agent/browser_monitoring.rb
+++ lib/new_relic/agent/browser_monitoring.rb
@@ -1,3 +1,5 @@
+require 'base64'
+
 module NewRelic
   module Agent
     module BrowserMonitoring
@@ -40,82 +42,81 @@ eos
         end
       end
       
-      BASE64 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
-                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"]
-      HI6 = 0b11111100
-      LO2 = 0b00000011
-      HI4 = 0b11110000
-      LO4 = 0b00001111
-      HI2 = 0b11000000
-      LO6 = 0b00111111
-      
       private
-      # Obfuscates ASCII text via XOR with the account license key then encoded in base64.
-      # New lines are removed.
+
       def obfuscate(text)
         obfuscated = ""
         keyIndex = -1
         key = NewRelic::Control.instance.license_key
         text.bytes.each { |byte|
           keyIndex = keyIndex + 1
-          keyIndex = 0 if keyIndex >= key.length
+          keyIndex = 0 if keyIndex >= 13
           obfuscated.concat(byte ^ key[keyIndex])
         }
-        encode(obfuscated)
+        Base64.encode64(obfuscated).gsub(/\n/, '')
       end
       
-      # Encodes a string into base64
-      def encode(text)
-        result = ""
-        num = 0
-        b1 = 0
-        b2 = 0
-        b3 = 0
-        text.bytes.each { |byte|
-          #process in groups of 3 bytes
-          case num
-            when 0
-              b1 = byte
-              num = num + 1
-            when 1
-              b2 = byte
-              num = num + 1
-            when 2
-              b3 = byte
-              num = 0
-              result = result + chunk(b1, b2, b3, 0)
-              b1 = 0
-              b2 = 0
-              b3 = 0
-          end
-        }
-        if num > 0
-          result = result + chunk(b1, b2, b3, 3 - num)
-        end
-        result
-      end
-      
-      def chunk(b1, b2, b3, pad)
-        result = ""
-        c1 = (HI6 & b1) >> 2
-        c2 = ((LO2 & b1) << 4) | ((HI4 & b2) >> 4)
-        c3 = ((LO4 & b2) << 2) | ((HI2 & b3) >> 6)
-        c4 = LO6 & b3
-        
-        result = result + BASE64[c1] + BASE64[c2]
-        if pad == 2
-          result = result + "=="
-        else
-          result = result + BASE64[c3]
-          if pad == 1
-            result = result + "="
-          else
-            result = result + BASE64[c4]
-          end
-        end
-        result
-      end
+#      BASE64 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+#                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+#                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"]
+#      HI6 = 0b11111100
+#      LO2 = 0b00000011
+#      HI4 = 0b11110000
+#      LO4 = 0b00001111
+#      HI2 = 0b11000000
+#      LO6 = 0b00111111
+#      
+#      # Encodes a string into base64
+#      def encode(text)
+#        result = ""
+#        num = 0
+#        b1 = 0
+#        b2 = 0
+#        b3 = 0
+#        text.bytes.each { |byte|
+#          #process in groups of 3 bytes
+#          case num
+#            when 0
+#              b1 = byte
+#              num = num + 1
+#            when 1
+#              b2 = byte
+#              num = num + 1
+#            when 2
+#              b3 = byte
+#              num = 0
+#              result = result + chunk(b1, b2, b3, 0)
+#              b1 = 0
+#              b2 = 0
+#              b3 = 0
+#          end
+#        }
+#        if num > 0
+#          result = result + chunk(b1, b2, b3, 3 - num)
+#        end
+#        result
+#      end
+#      
+#      def chunk(b1, b2, b3, pad)
+#        result = ""
+#        c1 = (HI6 & b1) >> 2
+#        c2 = ((LO2 & b1) << 4) | ((HI4 & b2) >> 4)
+#        c3 = ((HI4 & b2) << 2) | ((HI2 & b3) >> 6)
+#        c4 = LO6 & b3
+#        
+#        result = result + BASE64[c1] + BASE64[c2]
+#        if pad == 2
+#          result = result + "=="
+#        else
+#          result = result + BASE64[c3]
+#          if pad == 1
+#            result = result + "="
+#          else
+#            result = result + BASE64[c4]
+#          end
+#        end
+#        result
+#      end
       
     end
   end
