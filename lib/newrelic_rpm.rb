3d4a531d145062f8dffb873f23fa165757b7da68
Justin George <justin.george@gmail.com>
2011-03-23 11:42:33 -0700

    revert to old newrelic_rpm file

--- lib/newrelic_rpm.rb
+++ lib/newrelic_rpm.rb
@@ -18,85 +18,31 @@
 # directly.
 #
 require 'new_relic/control'
-
-DependencyDetection.defer do
-  depends_on do
-    defined?(Merb)
-  end
-
-  executes do
-    module NewRelic
-      class MerbBootLoader < Merb::BootLoader
-        after Merb::BootLoader::ChooseAdapter
-        def self.run
-          NewRelic::Control.instance.init_plugin
-        end
+if defined? Merb
+  module NewRelic
+    class MerbBootLoader < Merb::BootLoader
+      after Merb::BootLoader::ChooseAdapter
+      def self.run
+        NewRelic::Control.instance.init_plugin
       end
     end
   end
-end
-
-
-DependencyDetection.defer do
-  depends_on do
-    defined?(Rails)
-  end
-
-  depends_on do
-    Rails.respond_to?(:version) && Rails.version =~ /^3/
-  end
-  
-  executes do
+elsif defined? Rails
+  if Rails.respond_to?(:version) && Rails.version =~ /^3/
     module NewRelic
       class Railtie < Rails::Railtie
 
         initializer "newrelic_rpm.start_plugin" do |app|
-          NewRelic::Control.instance.init_plugin(:config => app.config)
+	  NewRelic::Control.instance.init_plugin(:config => app.config)
         end
       end
     end
-  end
-end
-
-DependencyDetection.defer do
-  depends_on do
-    defined?(Rails)
-  end
-
-  depends_on do
-    Rails.respond_to?(:configuration)
-  end
-
-  depends_on do
-    Rails.respond_to?(:version) && Rails.version =~ /^2/
-  end
-
-  executes do
+  else
     # After verison 2.0 of Rails we can access the configuration directly.
     # We need it to add dev mode routes after initialization finished.
-    config = Rails.configuration
-    NewRelic::Control.instance.init_plugin :config => config
+      config = Rails.configuration
+      NewRelic::Control.instance.init_plugin :config => config
   end
+else
+  NewRelic::Control.instance.init_plugin
 end
-
-DependencyDetection.defer do
-  depends_on do
-    defined?(Rails) && !Rails.respond_to?(:configuration)
-  end
-
-  executes do
-    NewRelic::Control.instance.init_plugin
-  end
-end
-
-DependencyDetection.defer do
-  depends_on do
-    !defined?(Rails)
-  end
-
-  executes do
-    NewRelic::Control.instance.init_plugin
-  end
-end
-
-DependencyDetection.detect!
