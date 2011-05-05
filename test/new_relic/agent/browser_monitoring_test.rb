d16be784e60781fd83580451454cecc65325ae01
Darin Swanson <dswanson@newrelic.com>
2011-05-05 16:58:28 -0700

    rollback changes to catch dbl instrumentation

--- test/new_relic/agent/browser_monitoring_test.rb
+++ test/new_relic/agent/browser_monitoring_test.rb
@@ -12,7 +12,7 @@ class NewRelic::Agent::BrowserMonitoringTest < Test::Unit::TestCase
     NewRelic::Agent.instance.instance_eval do
       @beacon_configuration = NewRelic::Agent::BeaconConfiguration.new({"rum.enabled" => true, "browser_key" => "browserKey", "application_id" => "apId", "beacon"=>"beacon", "episodes_url"=>"this_is_my_file"})
     end
-    Thread.current[:newrelic_most_recent_transaction] = {:scope_name => "MyCoolTransaction", :rum_header_added => false, :rum_footer_added => false}
+    Thread.current[:newrelic_most_recent_transaction] = "MyCoolTransaction"
   end
 
   def teardown
@@ -33,20 +33,6 @@ class NewRelic::Agent::BrowserMonitoringTest < Test::Unit::TestCase
     assert_equal "<script>var NREUMQ=[];NREUMQ.push([\"mark\",\"firstbyte\",new Date().getTime()]);(function(){var d=document;var e=d.createElement(\"script\");e.type=\"text/javascript\";e.async=true;e.src=\"this_is_my_file\";var s=d.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(e,s);})()</script>", header
   end
   
-  def test_browser_timing_header_outside_transaction
-    Thread.current[:newrelic_most_recent_transaction] = nil
-    header = browser_timing_header
-    assert_equal "", header
-  end
-
-  
-  def test_browser_timing_header_twice
-    header = browser_timing_header
-    assert_equal "<script>var NREUMQ=[];NREUMQ.push([\"mark\",\"firstbyte\",new Date().getTime()]);(function(){var d=document;var e=d.createElement(\"script\");e.type=\"text/javascript\";e.async=true;e.src=\"this_is_my_file\";var s=d.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(e,s);})()</script>", header
-    header = browser_timing_header
-    assert_equal "", header
-  end
-
   def test_browser_timing_header_with_rum_enabled_not_specified
     NewRelic::Agent.instance.expects(:beacon_configuration).at_least_once.returns( NewRelic::Agent::BeaconConfiguration.new({"browser_key" => "browserKey", "application_id" => "apId", "beacon"=>"beacon", "episodes_url"=>"this_is_my_file"}))
     header = browser_timing_header
@@ -85,30 +71,11 @@ class NewRelic::Agent::BrowserMonitoringTest < Test::Unit::TestCase
     assert footer.include?("<script type=\"text/javascript\" charset=\"utf-8\">NREUMQ.push([\"nrf2\",")
   end
   
-  def test_browser_timing_footer_outside_transaction
-    Thread.current[:newrelic_most_recent_transaction] = nil
-    footer = browser_timing_footer
-    assert_equal "", footer
-  end
-
   def test_browser_timing_footer_without_calling_header
     footer = browser_timing_footer
     assert_equal "", footer
   end
   
-  def test_browser_timing_footer_twice
-    browser_timing_header
-    NewRelic::Control.instance.expects(:license_key).returns("a" * 13)
-
-    Thread.current[:newrelic_start_time] = Time.now
-
-    footer = browser_timing_footer
-    assert footer.include?("<script type=\"text/javascript\" charset=\"utf-8\">NREUMQ.push([\"nrf2\",")
-
-    footer = browser_timing_footer
-    assert_equal "", footer
-  end
-
   def test_browser_timing_footer_with_no_browser_key_rum_enabled
     browser_timing_header
     NewRelic::Agent.instance.expects(:beacon_configuration).returns( NewRelic::Agent::BeaconConfiguration.new({"rum.enabled" => true, "application_id" => "apId", "beacon"=>"beacon", "episodes_url"=>"this_is_my_file"}))
