03295518082641d9c9d4845903c5dcdd83ffad5a
Justin George <justin.george@gmail.com>
2011-05-09 18:58:16 -0700

    refactor and test the class methods module of control, add tests to transaction samples

--- lib/new_relic/control/class_methods.rb
+++ lib/new_relic/control/class_methods.rb
@@ -6,26 +6,37 @@ module NewRelic
         @instance ||= new_instance
       end
 
+      def local_env
+        @local_env ||= NewRelic::LocalEnvironment.new
+      end
+
       # Create the concrete class for environment specific behavior:
       def new_instance
-        @local_env = NewRelic::LocalEnvironment.new
-        if @local_env.framework == :test
-          config = File.expand_path("../../../../test/config/newrelic.yml", __FILE__)
-          require "config/test_control"
-          NewRelic::Control::Frameworks::Test.new @local_env, config
+        if local_env.framework == :test
+          load_test_framework
         else
-          begin
-            require "new_relic/control/frameworks/#{@local_env.framework}.rb"
-          rescue LoadError
-          end
-          klass = NewRelic::Control::Frameworks.const_get(@local_env.framework.to_s.capitalize)
-          klass.new @local_env
+          load_framework_class(local_env.framework).new(local_env)
         end
+      end
+
+      # nb this does not 'load test' the framework, it loads the 'test framework'
+      def load_test_framework
+        config = File.expand_path(File.join('..','..','..','..', "test","config","newrelic.yml"), __FILE__)
+        require "config/test_control"
+        NewRelic::Control::Frameworks::Test.new(local_env, config)
+      end
+
+      def load_framework_class(framework)
+        begin
+          require "new_relic/control/frameworks/#{framework}.rb"
+        rescue LoadError
+        end
+        NewRelic::Control::Frameworks.const_get(framework.to_s.capitalize)
       end
 
       # The root directory for the plugin or gem
       def newrelic_root
-        File.expand_path("../../../..", __FILE__)
+        File.expand_path(File.join("..", "..", "..", ".."), __FILE__)
       end
     end
     extend ClassMethods
