3aa2a6dc04d2e450196ade8eff0f8ff4a0835020
Justin George <justin.george@gmail.com>
2011-05-13 12:57:00 -0700

    update version pending 3.0.1 release

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -3,8 +3,8 @@ module NewRelic
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 0
-    BUILD = nil #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
+    TINY  = 1
+    BUILD = 'alpha' #'0' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
 
