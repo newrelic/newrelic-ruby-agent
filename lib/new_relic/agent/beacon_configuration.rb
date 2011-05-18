4a731bc794d57e470f058e4f64dfc13aa0ed8879
Jim Gochee <jim@newrelic.com>
2011-05-18 11:51:29 -0700

    Move RUM.js file to footer. Remove charset from script tag

--- lib/new_relic/agent/beacon_configuration.rb
+++ lib/new_relic/agent/beacon_configuration.rb
@@ -2,11 +2,14 @@ module NewRelic
   module Agent
     class BeaconConfiguration
       attr_reader :browser_timing_header
+      attr_reader :browser_timing_static_footer
       attr_reader :application_id
       attr_reader :browser_monitoring_key
       attr_reader :beacon
       attr_reader :rum_enabled
       attr_reader :license_bytes
+
+      JS_HEADER = "<script>var NREUMQ=[];NREUMQ.push([\"mark\",\"firstbyte\",new Date().getTime()]);</script>"
 
       def initialize(connect_data)
         @browser_monitoring_key = connect_data['browser_key']
@@ -14,7 +17,8 @@ module NewRelic
         @beacon = connect_data['beacon']
         @rum_enabled = connect_data['rum.enabled']
         @rum_enabled = true if @rum_enabled.nil?
-        @browser_timing_header = build_browser_timing_header(connect_data)
+        @browser_timing_header = build_browser_timing_header
+        @browser_timing_static_footer = build_load_file_js(connect_data) 
       end
 
       def license_bytes
@@ -25,22 +29,19 @@ module NewRelic
         @license_bytes
       end
 
-      def load_file_js(connect_data)
+      def build_load_file_js(connect_data)
         return "" unless connect_data.fetch('rum.load_episodes_file', true)
 
         episodes_url = connect_data.fetch('episodes_url', '')
-        "(function(){var d=document;var e=d.createElement(\"script\");e.async=true;e.src=\"#{episodes_url}\";var s=d.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(e,s);})()"
+        "(function(){var d=document;var e=d.createElement(\"script\");e.async=true;e.src=\"#{episodes_url}\";var s=d.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(e,s);})();"
       end
 
-      def basic_javascript(connect_data)
-        "<script>var NREUMQ=[];NREUMQ.push([\"mark\",\"firstbyte\",new Date().getTime()]);#{load_file_js(connect_data)}</script>"
-      end
-
-      def build_browser_timing_header(connect_data)
+      def build_browser_timing_header
         return "" if !@rum_enabled
         return "" if @browser_monitoring_key.nil?
         
-        value = basic_javascript(connect_data)
+        # TODO: Justin - can you review this? Given that this is a constant string is this work necessary?
+        value = JS_HEADER
         if value.respond_to?(:html_safe)
           value.html_safe
         else
