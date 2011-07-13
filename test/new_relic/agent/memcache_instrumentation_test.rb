c3cd395c6c30f28eaf6ffd92bacc2abc2a48ad1f
Jon Guymon <jon@newrelic.com>
2011-07-13 14:26:01 -0700

    fix for instrumentation of memcache cas method

--- test/new_relic/agent/memcache_instrumentation_test.rb
+++ test/new_relic/agent/memcache_instrumentation_test.rb
@@ -2,8 +2,9 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','..','test_helper
 
 memcached_ready = false
 classes = {
-  'memcache' => 'MemCache',
-  'dalli' => 'Dalli::Client',
+  'memcache' => 'MemCache'
+#   'dalli' => 'Dalli::Client'
+#   'memcached' => 'Memcached'
 }
 begin
   TCPSocket.new('localhost', 11211)
@@ -27,10 +28,16 @@ class NewRelic::Agent::MemcacheInstrumentationTest < Test::Unit::TestCase
   def setup
     NewRelic::Agent.manual_start
     @engine = NewRelic::Agent.instance.stats_engine
-
-    @cache = MEMCACHED_CLASS.new('localhost')
-    @cache.flush_all
+    
+    if MEMCACHED_CLASS.name == 'Memcached'
+      @cache = MEMCACHED_CLASS.new('localhost', :support_cas => true)
+      @cache.flush
+    else
+      @cache = MEMCACHED_CLASS.new('localhost')
+      @cache.flush_all
+    end
     @key = 'schluessel'
+    @cache.set('schluessel', 1)
   end
 
   def _call_test_method_in_web_transaction(method, *args)
@@ -58,7 +65,7 @@ class NewRelic::Agent::MemcacheInstrumentationTest < Test::Unit::TestCase
   end
 
   def test_writes__web
-    %w[incr decr delete].each do |method|
+    %w[delete].each do |method|
       if @cache.class.method_defined?(method)
         _call_test_method_in_web_transaction(method)
         expected_metrics = ["MemCache/#{method}", "MemCache/allWeb", "MemCache/#{method}:Controller/NewRelic::Agent::MemcacheInstrumentationTest/action"]
@@ -67,6 +74,7 @@ class NewRelic::Agent::MemcacheInstrumentationTest < Test::Unit::TestCase
     end
 
     %w[set add].each do |method|
+      @cache.delete(@key) rescue nil
       if @cache.class.method_defined?(method)
         expected_metrics = ["MemCache/#{method}", "MemCache/allWeb", "MemCache/#{method}:Controller/NewRelic::Agent::MemcacheInstrumentationTest/action"]
         _call_test_method_in_web_transaction(method, 'value')
@@ -86,8 +94,7 @@ class NewRelic::Agent::MemcacheInstrumentationTest < Test::Unit::TestCase
   end
 
   def test_writes__background
-
-    %w[incr decr delete].each do |method|
+    %w[delete].each do |method|
       expected_metrics = ["MemCache/#{method}", "MemCache/allOther", "MemCache/#{method}:OtherTransaction/Background/NewRelic::Agent::MemcacheInstrumentationTest/bg_task"]
       if @cache.class.method_defined?(method)
         _call_test_method_in_background_task(method)
@@ -96,12 +103,25 @@ class NewRelic::Agent::MemcacheInstrumentationTest < Test::Unit::TestCase
     end
 
     %w[set add].each do |method|
+      @cache.delete(@key) rescue nil
       expected_metrics = ["MemCache/#{method}", "MemCache/allOther", "MemCache/#{method}:OtherTransaction/Background/NewRelic::Agent::MemcacheInstrumentationTest/bg_task"]
       if @cache.class.method_defined?(method)
         _call_test_method_in_background_task(method, 'value')
         compare_metrics expected_metrics, @engine.metrics.select{|m| m =~ /^memcache.*/i}
       end
     end
+  end
+
+  def test_handles_cas
+    expected_metrics = ["MemCache/cas", "MemCache/allOther", "MemCache/cas:OtherTransaction/Background/NewRelic::Agent::MemcacheInstrumentationTest/bg_task"]
+    if @cache.class.method_defined?(:cas)
+      @engine.clear_stats
+      perform_action_with_newrelic_trace(:name => 'bg_task', :category => :task) do
+        @cache.cas(@key) {|val| val += 2 }
+      end
+      compare_metrics expected_metrics, @engine.metrics.select{|m| m =~ /^memcache.*/i}
+      assert_equal 3, @cache.get(@key)
+    end    
   end
 
 end if memcached_ready
