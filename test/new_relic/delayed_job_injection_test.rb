50f966a31bd77ad316efcd237cfb13e74fdf39b1
Jon Guymon <jon@newrelic.com>
2011-07-15 16:24:28 -0700

    minor test fix

--- test/new_relic/delayed_job_injection_test.rb
+++ test/new_relic/delayed_job_injection_test.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','test_helper'))
 
 class NewRelic::DelayedJobInstrumentationTest < Test::Unit::TestCase
   def test_skip_logging_if_no_logger_found
-    suppress_warnings { Object.const_set('Delayed', Module.new) }
+    Object.const_set('Delayed', Module.new) unless defined?(Delayed)
     ::Delayed.const_set('Worker', Class.new)
     
     NewRelic::Agent.stubs(:logger).raises(NoMethodError,
