f87562ea7a88776ff99a5a3f6b2ded6e32797a57
Jon Guymon <jon@newrelic.com>
2011-07-28 14:54:54 -0700

    changed version on new dev branch

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -3,8 +3,8 @@ module NewRelic
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 1
-    TINY  = 1
-    BUILD = nil # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
+    TINY  = 2
+    BUILD = 'alpha' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
 
