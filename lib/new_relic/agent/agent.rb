bbf68f5999d279296cd4a0bc3bc25835bc2256fe
Jon Guymon <jon@newrelic.com>
2011-10-20 15:14:55 -0700

    configuration tweaks

--- lib/new_relic/agent/agent.rb
+++ lib/new_relic/agent/agent.rb
@@ -857,14 +857,17 @@ module NewRelic
             @agent_id = config_data['agent_run_id']
             @report_period = config_data['data_report_period']
             @url_rules = config_data['url_rules']
-            @beacon_configuration = BeaconConfiguration.new(config_data)
             server_side_config = config_data['agent_config']
             
             if server_side_config
-              control.merge_server_side_config(server_side_config)
+              config_data.merge!(server_side_config)
+              control.merge_server_side_config(config_data)
               log.info "Using config from server"
               log.debug "Server provided config: #{server_side_config.inspect}"
             end
+            
+            @beacon_configuration = BeaconConfiguration.new(config_data)            
+            
             config_transaction_tracer
             log_connection!(config_data)
             configure_transaction_tracer!(config_data['collect_traces'], config_data['sample_rate'])
