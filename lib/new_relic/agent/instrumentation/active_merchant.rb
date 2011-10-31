2e20cc7330e73f3f705612c6a46481f246f0b2cd
Sam Goldstein <sam@aboutus.org>
2011-10-31 11:38:24 -0700

    RUBY-711 - Don't try to instrument old version of ActiveMerchant
    Check that ActiveMerchant::Billing::Gateway.implementations is defined
    before trying to call it for instrumentation

--- lib/new_relic/agent/instrumentation/active_merchant.rb
+++ lib/new_relic/agent/instrumentation/active_merchant.rb
@@ -2,7 +2,9 @@ DependencyDetection.defer do
   @name = :active_merchant
   
   depends_on do
-    defined?(ActiveMerchant) && defined?(ActiveMerchant::Billing)
+    defined?(ActiveMerchant) && defined?(ActiveMerchant::Billing) &&
+      defined?(ActiveMerchant::Billing::Gateway) &&
+      ActiveMerchant::Billing::Gateway.respond_to?(:implementations)
   end
   
   executes do
