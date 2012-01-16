ea133974c6cebd9971c95de08805669331d5f285
Alexander Lang <alex@upstre.am>
2012-01-16 12:45:00 +0100

    don't instrument browser when html file is being downloaded

--- test/new_relic/rack/browser_monitoring_test.rb
+++ test/new_relic/rack/browser_monitoring_test.rb
@@ -15,7 +15,7 @@ end
 if middleware_supported?
 class BrowserMonitoringTest < Test::Unit::TestCase
   include Rack::Test::Methods
-  
+
   class TestApp
     def self.doc=(other)
       @@doc = other
@@ -38,11 +38,11 @@ EOL
     end
     include NewRelic::Agent::Instrumentation::Rack
   end
-  
+
   def app
     NewRelic::Rack::BrowserMonitoring.new(TestApp.new)
   end
-  
+
   def setup
     super
     clear_cookies
@@ -54,36 +54,40 @@ EOL
     NewRelic::Agent.instance.stubs(:beacon_configuration).returns(config)
     NewRelic::Agent.stubs(:is_transaction_traced?).returns(true)
   end
-  
+
   def teardown
     super
     clear_cookies
     mocha_teardown
     TestApp.doc = nil
   end
-  
+
   def test_make_sure_header_is_set
     assert NewRelic::Agent.browser_timing_header.size > 0
   end
-  
+
   def test_make_sure_footer_is_set
     assert NewRelic::Agent.browser_timing_footer.size > 0
   end
-  
+
   def test_should_only_instrument_successfull_html_requests
     assert app.should_instrument?(200, {'Content-Type' => 'text/html'})
     assert !app.should_instrument?(500, {'Content-Type' => 'text/html'})
     assert !app.should_instrument?(200, {'Content-Type' => 'text/xhtml'})
   end
 
+  def test_should_not_instrument_when_content_disposition
+    assert !app.should_instrument?(200, {'Content-Type' => 'text/html', 'Content-Disposition' => 'attachment; filename=test.html'})
+  end
+
   def test_insert_timing_header_right_after_open_head_if_no_meta_tags
     get '/'
-    
+
     assert(last_response.body.include?("head>#{NewRelic::Agent.browser_timing_header}"),
            last_response.body)
     TestApp.doc = nil
-  end  
-  
+  end
+
   def test_insert_timing_header_right_before_head_close_if_ua_compatible_found
     TestApp.doc = <<-EOL
 <html>
@@ -98,30 +102,30 @@ EOL
 </html>
 EOL
     get '/'
-    
+
     assert(last_response.body.include?("#{NewRelic::Agent.browser_timing_header}</head>"),
            last_response.body)
   end
-  
+
   def test_insert_timing_footer_right_before_html_body_close
     get '/'
-    
+
     assert_match(/.*NREUMQ\.push.*new Date\(\)\.getTime\(\),"","","","",""\]\)<\/script><\/body>/,
                  last_response.body)
   end
-  
+
   def test_should_not_throw_exception_on_empty_reponse
     TestApp.doc = ''
     get '/'
 
     assert last_response.ok?
   end
-  
+
   def test_token_is_set_in_footer_when_set_by_cookie
     token = '1234567890987654321'
     set_cookie "NRAGENT=tk=#{token}"
     get '/'
-    
+
     assert(last_response.body.include?(token), last_response.body)
   end
 
