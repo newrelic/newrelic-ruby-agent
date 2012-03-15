ab3414ea5370c750f042bf2681f75eca92b3f873
Jon Guymon <jon@newrelic.com>
2012-03-15 14:52:06 -0700

    gemspec for 3.3.3.beta2

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -4,7 +4,7 @@ module NewRelic
     MAJOR = 3
     MINOR = 3
     TINY  = 3
-    BUILD = 'beta1' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
+    BUILD = 'beta2' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
 
