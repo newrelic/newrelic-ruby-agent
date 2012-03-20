09f98ccd8130b01c99b186595cef14c30a24d645
Sam Goldstein <sam@newrelic.com>
2012-03-20 12:07:51 -0700

    Only log segment limit reached once instead of every node

--- lib/new_relic/agent/transaction_sample_builder.rb
+++ lib/new_relic/agent/transaction_sample_builder.rb
@@ -38,8 +38,9 @@ module NewRelic
           segment = @sample.create_segment(time.to_f - @sample_start, metric_name)
           @current_segment.add_called_segment(segment)
           @current_segment = segment
-        elsif @sample.count_segments == @segment_limit
-          NewRelic::Control.instance.log.debug("Segment limit of #{@segment_limit} reached, ceasing collection.")
+          if @sample.count_segments == @segment_limit
+            NewRelic::Control.instance.log.debug("Segment limit of #{@segment_limit} reached, ceasing collection.")
+          end
         end
       end
 
