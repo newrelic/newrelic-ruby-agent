c8b887c0227a3cb5fe8bcddc631ef6439733631a
Rob Saul <rsaul@newrelic.com>
2012-04-27 16:18:27 -0700

    fleshed out to do some actual work.

--- test/script/ci_agent-tests_runner.sh
+++ test/script/ci_agent-tests_runner.sh
@@ -1,16 +1,37 @@
 #!/bin/bash -e
 
-
+SCRATH_DIR=./agent-tests_tmp
 #git pull git@github.com:newrelic/ruby_agent-tests.git
 
 echo $PWD 
+script_dirname=`dirname $0`
+echo $script_dirname
+echo $0
 
-exit 0
+# make sure that we're in the project root
+script_dirname=`dirname $0`
+cd "$script_dirname/../../"
+pwd
 
-if [ -x tmp ] ; then
+
+
+if [ -x $SCRATH_DIR ] ; then
 	echo "found tmp, deleting"
-	rm -fr tmp
+	rm -fr $SCRATH_DIR
 fi 
+
+mkdir $SCRATH_DIR
+cd $SCRATH_DIR
+
+git clone --depth=1 git@github.com:newrelic/ruby_agent-tests.git ruby_agent-tests
+git clone --depth=1 git@github.com:newrelic/rpm_contrib.git rpm_contrib
+
+ln -s ../../ruby_agent ./ruby_agent
+
+#exit 0
+
+cd ruby_agent-tests
+./ci_run.sh 
 
 #echo "creating tmp dir"
 #mkdir tmp
@@ -23,8 +44,8 @@ fi
 #env
 #ls -al /home/hudson/.rvm/bin
 # source ~/.rvm/scripts/rvm
-# rvm 1.9.2
-# ruby -v
+rvm 1.9.2
+ruby -v
 
 # rvm --force gemset delete ruby_agent-tests
 
