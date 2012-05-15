6193658e6270aee1cd442ba3fdeaff32b34bdad5
Rob Saul <rsaul@newrelic.com>
2012-05-14 17:12:06 -0700

    initial commit.

---
+++ test/script/ci_multiverse_runner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+SCRATH_DIR=./multiverse_tmp
+script_dirname=`dirname $0`
+
+# make sure that we're in the project root
+cd "$script_dirname/../../"
+
+if [ -x $SCRATH_DIR ] ; then
+	echo "found tmp, deleting"
+	rm -fr $SCRATH_DIR
+fi
+
+mkdir $SCRATH_DIR
+cd $SCRATH_DIR
+
+git clone --depth=1 git@github.com:newrelic/multiverse.git multiverse
+git clone --depth=1 git@github.com:newrelic/rpm_contrib.git rpm_contrib
+
+if [ -x ../../Ruby_Agent ] ; then
+	ln -s ../../Ruby_Agent ./ruby_agent
+else
+	echo "*********** Ruby_Agent can't be found ***********"
+	exit 1
+fi
+
+cd multiverse
+#./ci_run.sh
+
+
+
