2d4131a01c1df83b0e0d57e074c1b78ed109abb7
Rob Saul <rsaul@newrelic.com>
2012-05-22 13:29:59 -0700

    looks like some old bit good included that shouldn't have

--- test/script/ci.sh
+++ test/script/ci.sh
@@ -59,9 +59,9 @@ rpm_test_app_cache=~/workspace/.rpm_test_app_cache
 pwd
 git clone $rpm_test_app_cache rpm_test_app
 cd rpm_test_app || true # rvm overrides cd and it's f-ing up the build by exiting 2
-git remote update
-git --version
-git pull --all
+#git remote update
+#git --version
+#git pull --all
 
 git checkout -t origin/$BRANCH || git checkout $BRANCH
 
