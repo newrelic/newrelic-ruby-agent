592129d7400bbb239cc720552db511cf52125d62
Rob Saul <rsaul@newrelic.com>
2012-05-22 11:55:13 -0700

    duh, forgot quotes would mess up path.

--- test/script/ci.sh
+++ test/script/ci.sh
@@ -50,7 +50,7 @@ cd tmp
 
 
 #rpm_test_app_cache=~/.rpm_test_app_cache
-rpm_test_app_cache="~/workspace/.rpm_test_app_cache"
+rpm_test_app_cache=~/workspace/.rpm_test_app_cache
 (
   echo "updating local cache of rpm_test_app in $rpm_test_app_cache"
   git clone --mirror git://github.com/newrelic/rpm_test_app.git $rpm_test_app_cache || true
