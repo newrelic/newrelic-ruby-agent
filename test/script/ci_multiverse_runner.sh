7032fbb8ec87d5e2a264724bd5ea6f16a953c0c3
Rob Saul <rsaul@newrelic.com>
2012-05-30 17:03:53 -0700

    closing in on correct path for multi ruby version run

--- test/script/ci_multiverse_runner.sh
+++ test/script/ci_multiverse_runner.sh
@@ -25,7 +25,7 @@ cd $SCRATH_DIR
 
 pwd 
 if [[ $JOB_NAME =~ "Pangalactic" ]] ; then 
-	AGENT_LOCATION="../../../../../Ruby_Agent"
+	AGENT_LOCATION="../../../../../../../Ruby_Agent"
 else
 	AGENT_LOCATION="../../Ruby_Agent"
 fi
@@ -34,7 +34,7 @@ git clone --depth=1 git@github.com:newrelic/multiverse.git multiverse
 git clone --depth=1 git@github.com:newrelic/rpm_contrib.git rpm_contrib
 
 echo "Looking for Ruby Agent at $AGENT_LOCATION"
-ls -l ../../../../../
+ls -l ../../../../../../../
 ls -l /home/hudson/workspace/
 
 if [ -x $AGENT_LOCATION ] ; then
