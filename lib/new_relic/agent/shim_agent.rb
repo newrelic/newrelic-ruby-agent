b32cb9eb21d832b98086e6c6f6777a81472d5013
Jon Guymon <jon@newrelic.com>
2012-06-18 17:13:52 -0700

    RUBY-839 fix for extraneous DNS resolution - test in multiverse, couldnt hack up a proper unit test yet

--- lib/new_relic/agent/shim_agent.rb
+++ lib/new_relic/agent/shim_agent.rb
@@ -3,27 +3,27 @@
 # invoke the Agent.
 module NewRelic
   module Agent
-    class ShimAgent < NewRelic::Agent::Agent
+    class ShimAgent 
       def self.instance
         @instance ||= self.new
       end
       def initialize
-        super
         @stats_engine.extend NewRelic::Agent::StatsEngine::Shim
         @stats_engine.extend NewRelic::Agent::StatsEngine::Transactions::Shim
         @transaction_sampler.extend NewRelic::Agent::TransactionSampler::Shim
         @sql_sampler.extend NewRelic::Agent::SqlSampler::Shim
         @error_collector.extend NewRelic::Agent::ErrorCollector::Shim
       end
-      def after_fork *args; end
-      def start *args; end
-      def shutdown *args; end
+      def after_fork(*args); end
+      def start(*args); end
+      def shutdown(*args); end
       def serialize; end
-      def merge_data_from *args; end
+      def merge_data_from(*args); end
       def push_trace_execution_flag *args; end
       def pop_trace_execution_flag *args; end
       def browser_timing_header; "" end
       def browser_timing_footer; "" end
+      def started?; false end
     end
   end
 end
