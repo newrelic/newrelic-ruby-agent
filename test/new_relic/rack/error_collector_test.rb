f8c6e00faaf382c7764f4f4e200c5f7b95d24a28
Jon Guymon <jon@newrelic.com>
2012-09-13 17:22:42 -0700

    RUBY-579 make sure we handle all the error ignore scenarios

--- test/new_relic/rack/error_collector_test.rb
+++ test/new_relic/rack/error_collector_test.rb
@@ -9,6 +9,12 @@ module NewRelic::Rack
 
     class TestApp
       def call(env)
+        if env['PATH_INFO'] == '/ignored'
+          env['action_dispatch.request.parameters'] = {
+            'controller' => 'test_ignore',
+            'action'     => 'ignored'
+          }
+        end
         raise 'unhandled error'
       end
     end
@@ -20,14 +26,18 @@ module NewRelic::Rack
     def setup
       NewRelic::Agent.reset_config
       NewRelic::Agent.manual_start
+      NewRelic::Agent.instance.error_collector
       NewRelic::Agent.instance.error_collector.errors = []
 
-      # FIXME kick the error collector to make sure it's enabled
+      # sanity checks
       NewRelic::Agent.instance.configure_error_collector!(true)
       assert NewRelic::Agent.instance.error_collector.enabled
+      NewRelic::Agent.instance.error_collector \
+        .instance_variable_set(:@ignore_filter, nil)
+      assert !NewRelic::Agent.instance.error_collector.ignore_error_filter
     end
 
-    def test_capture_raised_errors
+    def test_notice_and_reraise_errors
       assert_raise RuntimeError do
         get '/'
       end
@@ -35,5 +45,31 @@ module NewRelic::Rack
       assert_equal('unhandled error',
                    NewRelic::Agent.instance.error_collector.errors[0].message)
     end
+
+    def test_ignore_filtered_errors
+      NewRelic::Agent.instance.error_collector.ignore_error_filter do |error|
+        !error.kind_of?(RuntimeError)
+      end
+
+      assert_raise RuntimeError do
+        get '/'
+      end
+
+      assert(NewRelic::Agent.instance.error_collector.errors.empty?,
+             'noticed an error that should have been ignored')
+    end
+
+    def test_ignore_errors_from_ignored_actions
+      assert_raise RuntimeError do
+        get '/ignored'
+      end
+
+      assert(NewRelic::Agent.instance.error_collector.errors.empty?,
+             'noticed an error that should have been ignored')
+    end
   end
+end
+
+class TestIgnoreController
+  @do_not_trace = { :only => :ignored }
 end
