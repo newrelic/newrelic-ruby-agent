d216f68b006ceadb55dce917ea7e489cc3b37d9c
Rob Saul <rsaul@newrelic.com>
2012-10-04 16:21:31 -0700

    updates for v3.5.0 of the Ruby Agent

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -4,7 +4,7 @@ module NewRelic
     MAJOR = 3
     MINOR = 5
     TINY  = 0
-    BUILD = 'beta1' # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
+    BUILD = nil # Set to nil for a release, 'beta1', 'alpha', etc for prerelease builds
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
 
