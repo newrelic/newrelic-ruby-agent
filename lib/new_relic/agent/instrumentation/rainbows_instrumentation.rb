4157c6641e41820bbef5db9a887418bfc967e7b1
Joseph Chen <joe1chen@hotmail.com>
2012-12-19 17:57:55 -0800

    When the Ruby Agent detects Rainbows as the dispatcher it creates an INFO level log message with additional information (similar to logging for Unicorn).

--- lib/new_relic/agent/instrumentation/rainbows_instrumentation.rb
+++ lib/new_relic/agent/instrumentation/rainbows_instrumentation.rb
@@ -7,12 +7,13 @@ DependencyDetection.defer do
   
   executes do
     NewRelic::Agent.logger.debug 'Installing Rainbows instrumentation'
+    NewRelic::Agent.logger.info 'Detected Rainbows, please see additional documentation: https://newrelic.com/docs/troubleshooting/im-using-unicorn-and-i-dont-see-any-data'
   end
   
   executes do
     Rainbows::HttpServer.class_eval do
       old_worker_loop = instance_method(:worker_loop)
-      define_method(:worker_loop) do | worker |
+      define_method(:worker_loop) do |worker|
         NewRelic::Agent.after_fork(:force_reconnect => true)
         old_worker_loop.bind(self).call(worker)
       end
