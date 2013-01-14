e1ed10b0633bd1cbb0de253e6687f53a9be9a15e
Jason R. Clark <jclark@newrelic.com>
2013-01-14 15:14:09 -0800

    RUBY-997 Consolidate multiverse tests--queue timing

--- test/multiverse/suites/rails/queue_time_test.rb
+++ test/multiverse/suites/rails/queue_time_test.rb
@@ -1,37 +1,6 @@
 # https://newrelic.atlassian.net/browse/RUBY-927
 
-ENV['NEW_RELIC_DISPATCHER'] = 'test'
-
-require 'action_controller/railtie'
-require 'rails/test_unit/railtie'
-require 'rails/test_help'
-require 'test/unit'
-
-# BEGIN RAILS APP
-
-class MyApp < Rails::Application
-  # We need a secret token for session, cookies, etc.
-  config.active_support.deprecation = :log
-  config.secret_token = "49837489qkuweoiuoqwehisuakshdjksadhaisdy78o34y138974xyqp9rmye8yrpiokeuioqwzyoiuxftoyqiuxrhm3iou1hrzmjk"
-end
-MyApp.initialize!
-
-MyApp.routes.draw do
-  match '/:controller(/:action(/:id))'
-end
-
-class ApplicationController < ActionController::Base; end
-
-class TestController < ApplicationController
-  include Rails.application.routes.url_helpers
-
-  def queued
-    respond_to do |format|
-      format.html { render :text => "<html><head></head><body>Queued</body></html>" }
-    end
-  end
-end
-# END RAILS APP
+require './app'
 
 class QueueTimeTest < ActionDispatch::IntegrationTest
   def setup
@@ -64,7 +33,7 @@ class QueueTimeTest < ActionDispatch::IntegrationTest
   end
 
   def get_queued(header="HTTP_X_QUEUE_START")
-    get('/test/queued', nil, \
+    get('/queue/queued', nil, \
         header => "t=#{(Time.now.to_i * 1_000_000) - 1_000}")
   end
 
