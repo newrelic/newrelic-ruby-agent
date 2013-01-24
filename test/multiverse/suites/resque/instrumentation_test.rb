1f39c1406f196b6df4883d8fc593846a5fbc502e
Ben Weintraub <ben@newrelic.com>
2013-01-24 11:40:32 -0800

    RUBY-857 Make background Resque tests conditional on Ruby 1.9+

--- test/multiverse/suites/resque/instrumentation_test.rb
+++ test/multiverse/suites/resque/instrumentation_test.rb
@@ -15,31 +15,75 @@ class ResqueTest < Test::Unit::TestCase
     $collector ||= NewRelic::FakeCollector.new
     $collector.reset
     $collector.run(COLLECTOR_PORT)
-
-    JOB_COUNT.times {|i| Resque.enqueue(JobForTesting, 'index_key', i + 1) }
-
-    begin
-    start_worker
-    wait_for_jobs
-    ensure
-    stop_worker
-  end
+    $redis.del('queue:resque_test')
+    $redis.set('index_key', 0)
+    @pidfile = "resque_test.#{$$}.pid"
+    JOB_COUNT.times do |i|
+      Resque.enqueue(JobForTesting, 'index_key', i + 1)
+    end
   end
 
-  def start_worker
+  def teardown
+    File.unlink(@pidfile) if File.file?(@pidfile)
+  end
+
+  def start_worker(opts={})
+    opts[:background] ? start_worker_background : start_worker_child
+  end
+
+  def stop_worker(opts={})
+    opts[:background] ? stop_worker_background : stop_worker_child
+  end
+
+  def start_worker_child
     worker_cmd = "NEWRELIC_DISPATCHER=resque QUEUE=* bundle exec rake resque:work"
     @worker_pid = Process.fork
     Process.exec(worker_cmd) if @worker_pid.nil?
   end
 
-  def stop_worker
+  def stop_worker_child
     Process.kill("QUIT", @worker_pid)
     Process.waitpid(@worker_pid)
   end
 
+  def start_worker_background
+    worker_cmd = "PIDFILE=#{@pidfile} TERM_CHILD=1 RESQUE_TERM_TIMEOUT=1 BACKGROUND=1 " +
+      "NEWRELIC_DISPATCHER=resque QUEUE=* bundle exec rake resque:work"
+    system(worker_cmd)
+  end
+
+  def stop_worker_background
+    daemon_pid = File.read(@pidfile).to_i
+
+    tries = 0
+    while process_alive?(daemon_pid) && tries < 3
+      Process.kill('TERM', daemon_pid)
+      sleep 4 # default resque TERM timeout
+      tries += 1
+    end
+
+    if process_alive?(daemon_pid)
+      $stderr.puts "Oops. Daemon (pid #{daemon_pid}) is still running. Trying to halt it with SIGQUIT"
+      Process.kill('QUIT', daemon_pid)
+      sleep 1
+
+      # If it's still alive, someone will likely have to go kill the process manually. 
+      # Alternatively, we could kill -9 it, but I decided to err on the side of caution
+      if process_alive?(daemon_pid)
+        raise "Resque is zombified. You might have to clean up process #{daemon_pid} manually."
+      end
+    end
+  end
+
+  def process_alive?(pid)
+    Process.kill(0, pid)
+    return true
+  rescue Errno::ESRCH
+    return false
+  end
+
   def wait_for_jobs
     time_for_jobs = 5
-
     begin
       Timeout.timeout(time_for_jobs) { sleep(0.1) until Resque.info[:pending].zero? }
     rescue Timeout::Error => err
@@ -47,33 +91,47 @@ class ResqueTest < Test::Unit::TestCase
     end
   end
 
-  def teardown
-    $redis.set('index_key', 0)
-    $redis.del('queue:resque_test')
-    $collector.reset
+  def run_worker(opts={})
+    begin
+      start_worker(opts)
+      wait_for_jobs
+    ensure
+      stop_worker(opts)
+    end
   end
-
-  def test_all_jobs_ran
-    assert_equal JOB_COUNT, $redis.get('index_key').to_i
-  end
-
-  def test_agent_makes_only_one_metric_post
-    assert_equal(1, $collector.agent_data.select{|x| x.action == 'metric_data'}.size,
-                 "wrong number of metric_data posts in #{$collector.agent_data.inspect}")
-  end
-
 
   METRIC_VALUES_POSITION = 3
 
-  def test_agent_posts_correct_call_count
-    test_metric = 'OtherTransaction/ResqueJob/all'
-    metric_data = $collector.calls_for('metric_data').first
+  def assert_metric_and_call_count(name, expected_call_count)
+    metric_data = $collector.calls_for('metric_data')
+    assert_equal(1, metric_data.size, "expected exactly one metric_data post from agent")
 
-    metric_names = metric_data[METRIC_VALUES_POSITION].map{|m| m[0]['name']}
-    assert(metric_names.include?(test_metric),
-           "#{metric_names.inspect} should include '#{test_metric}'")
+    metric = metric_data.first[METRIC_VALUES_POSITION].find { |m| m[0]['name'] == name }
+    assert(metric, "could not find metric named #{name}")
 
-    call_count = metric_data[METRIC_VALUES_POSITION].find{|m| m[0]['name'] == test_metric}[1][0]
-    assert_equal JOB_COUNT, call_count
+    call_count = metric[1][0]
+    assert_equal(expected_call_count, call_count)
+  end
+
+  def test_all_jobs_ran
+    run_worker
+    assert_equal(JOB_COUNT, $redis.get('index_key').to_i)
+  end
+
+  def test_agent_posts_correct_metric_data
+    run_worker
+    assert_metric_and_call_count('OtherTransaction/ResqueJob/all', JOB_COUNT)
+  end
+
+  if RUBY_VERSION >= '1.9'
+    def test_all_jobs_ran_background
+      run_worker(:background => true)
+      assert_equal(JOB_COUNT, $redis.get('index_key').to_i)
+    end
+
+    def test_agent_posts_correct_metric_data_background
+      run_worker(:background => true)
+      assert_metric_and_call_count('OtherTransaction/ResqueJob/all', JOB_COUNT)
+    end
   end
 end
