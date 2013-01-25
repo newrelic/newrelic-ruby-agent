6dab22859908372b626be9a3a990d119506eca7b
Jason R. Clark <jclark@newrelic.com>
2013-01-25 09:45:23 -0800

    Reducing timing dependency in the GC tests. Check ratio of total to GC instead

--- test/multiverse/suites/rails/gc_instrumentation_test.rb
+++ test/multiverse/suites/rails/gc_instrumentation_test.rb
@@ -33,8 +33,7 @@ class GCRailsInstrumentationTest < ActionController::TestCase
 
     @controller = GcController.new
     NewRelic::Agent.instance.stats_engine.reset_stats
-    NewRelic::Agent.instance.transaction_sampler \
-      .instance_variable_set(:@samples, [])
+    NewRelic::Agent.instance.transaction_sampler.instance_variable_set(:@samples, [])
     NewRelic::Agent.manual_start
   end
 
@@ -45,24 +44,32 @@ class GCRailsInstrumentationTest < ActionController::TestCase
   def test_records_accurate_time_for_gc_activity
     get :gc_action
 
-    assert_in_delta(assigns[:duration],
-                    NewRelic::Agent.agent.stats_engine \
-                      .get_stats('GC/cumulative') \
-                      .total_call_time, 0.2,
-                    'problem with unscoped GC metric')
-    assert_in_delta(assigns[:duration],
-                    NewRelic::Agent.agent.stats_engine \
-                      .get_stats('GC/cumulative', true, false,
-                                 'Controller/gc/gc_action') \
-                      .total_call_time, 0.2,
-                    'problem with scoped GC metric')
+    assert_in_range(assigns[:duration], get_call_time('GC/cumulative'))
+    assert_in_range(assigns[:duration], get_call_time('GC/cumulative', 'Controller/gc/gc_action'))
   end
 
   def test_records_transaction_param_for_gc_activity
     get :gc_action
 
     trace = NewRelic::Agent.instance.transaction_sampler.last_sample
-    assert_in_delta(assigns[:duration], trace.params[:custom_params][:gc_time], 0.2)
+    assert_in_range(assigns[:duration], trace.params[:custom_params][:gc_time])
+  end
+
+  def assert_in_range(duration, gc_time)
+    assert gc_time > 0.0, "GC Time wasn't recorded!"
+
+    # This is a guess for a reasonable threshold here.
+    # Since these are timing based, we can revise or ditch as evidence ditacts
+    # One CI failure we saw at least had duration=0.314 and gc_time=0.088
+    ratio = gc_time / duration
+    assert(ratio > 0.1 && ratio < 1.0,
+      "Problem with GC/duration ratio. #{gc_time}/#{duration} = #{ratio} not between 0.1 and 1.0")
+  end
+
+  def get_call_time(name, scope=nil)
+    NewRelic::Agent.agent.stats_engine.
+      get_stats(name, true, false, scope).
+      total_call_time
   end
 
   def enable_gc_stats
