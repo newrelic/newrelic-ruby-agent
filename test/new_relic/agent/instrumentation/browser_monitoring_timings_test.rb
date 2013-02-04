7f9f5f7e58e492a3849d4256d3b99bd94849dbcd
Charlie Somerville <charlie@charliesomerville.com>
2013-02-04 13:04:02 +1100

    generally people expect Time.now to actually return a Time

--- test/new_relic/agent/instrumentation/browser_monitoring_timings_test.rb
+++ test/new_relic/agent/instrumentation/browser_monitoring_timings_test.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','test_h
 class NewRelic::Agent::Instrumentation::BrowserMonitoringTimingsTest < Test::Unit::TestCase
 
   def setup
-    Time.stubs(:now).returns(2000)
+    Time.stubs(:now).returns(Time.at(2000))
     @transaction = stub(
       :transaction_name => "Name",
       :start_time => 0
