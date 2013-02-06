d20b863511aa1b33c9d48ee1106688abc907c815
Jon Guymon <jon@newrelic.com>
2013-02-06 15:41:46 -0800

    RUBY-874 kill all white space

--- test/new_relic/agent/transaction_info_test.rb
+++ test/new_relic/agent/transaction_info_test.rb
@@ -9,46 +9,46 @@ class NewRelic::Agent::TransactionInfoTest < Test::Unit::TestCase
     @request_with_double_quotes = OpenStruct.new(:cookies => {'NRAGENT' => 'tk="""deadbeef"""'})
     @request_with_single_quotes = OpenStruct.new(:cookies => {'NRAGENT' => "tk='''deadbeef'''"})
     @request_with_multi_lt = OpenStruct.new(:cookies => {'NRAGENT' => 'tk=<<<deadbeef'})
-    @request_with_multi_gt = OpenStruct.new(:cookies => {'NRAGENT' => 'tk=>>>deadbeef'}) 
+    @request_with_multi_gt = OpenStruct.new(:cookies => {'NRAGENT' => 'tk=>>>deadbeef'})
     @request_with_bare_token = OpenStruct.new(:cookies => {'NRAGENT' => 0xdeadbeef})
     @request_with_nil_token = OpenStruct.new(:cookies => {'NRAGENT' => nil})
   end
-  
+
   def test_get_token_safe_token_returned_untouched
     assert_equal("12345678", NewRelic::Agent::TransactionInfo.get_token(@request))
   end
 
   def test_get_token_with_embedded_tags_sanitized
-  	assert_equal("",NewRelic::Agent::TransactionInfo.get_token(@request_with_embedded_tag))
+    assert_equal("",NewRelic::Agent::TransactionInfo.get_token(@request_with_embedded_tag))
   end
 
   def test_get_token_with_embedded_utf8_js_sanitized
-  	assert_equal("1234&amp;#34&amp;#93&amp;#41&amp;#595678",
-  		NewRelic::Agent::TransactionInfo.get_token(@request_with_embedded_utf8_encoded_js))	
+    assert_equal("1234&amp;#34&amp;#93&amp;#41&amp;#595678",
+                 NewRelic::Agent::TransactionInfo.get_token(@request_with_embedded_utf8_encoded_js))
   end
 
   def test_get_token_replaces_double_quoted_token_with_empty_string
-  	assert_equal("", NewRelic::Agent::TransactionInfo.get_token(@request_with_double_quotes))
+    assert_equal("", NewRelic::Agent::TransactionInfo.get_token(@request_with_double_quotes))
   end
 
   def test_get_token_replaces_single_quoted_toket_with_empty_string
-  	assert_equal("", NewRelic::Agent::TransactionInfo.get_token(@request_with_single_quotes))
+    assert_equal("", NewRelic::Agent::TransactionInfo.get_token(@request_with_single_quotes))
   end
 
   def test_get_token_replaces_token_started_with_multiple_Lt_with_empty_string
-  	assert_equal("", NewRelic::Agent::TransactionInfo.get_token(@request_with_multi_lt))
+    assert_equal("", NewRelic::Agent::TransactionInfo.get_token(@request_with_multi_lt))
   end
 
   def test_get_token_replaces_token_started_with_multiple_gt_with_empty_string
-  	assert_equal("", NewRelic::Agent::TransactionInfo.get_token(@request_with_multi_gt))
+    assert_equal("", NewRelic::Agent::TransactionInfo.get_token(@request_with_multi_gt))
   end
 
   def test_get_token_bare_value_replaced_with_nil
-  	assert_equal(nil,NewRelic::Agent::TransactionInfo.get_token(@request_with_bare_token))
+    assert_equal(nil,NewRelic::Agent::TransactionInfo.get_token(@request_with_bare_token))
   end
 
   def test_get_token_nil_token_returns_nil_token
-  	assert_equal(nil,NewRelic::Agent::TransactionInfo.get_token(@request_with_ni_token))
+    assert_equal(nil,NewRelic::Agent::TransactionInfo.get_token(@request_with_ni_token))
   end
 
   def test_has_correct_apdex_t_for_tansaction
