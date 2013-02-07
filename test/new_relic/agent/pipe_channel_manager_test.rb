42ddeba6a277cd61565a7867539a50f45baf82d3
Jason R. Clark <jclark@newrelic.com>
2013-02-06 16:23:54 -0800

    RUBY-1021 Apply error queue limit to Resque child errors
    The pipe communication between the Resque workers and the parent was side-stepping the
    limit we place on number of errors to return to the collector. Now we apply the same cap.

--- test/new_relic/agent/pipe_channel_manager_test.rb
+++ test/new_relic/agent/pipe_channel_manager_test.rb
@@ -77,9 +77,9 @@ class NewRelic::Agent::PipeChannelManagerTest < Test::Unit::TestCase
       sampler.notice_error(Exception.new("message"), :uri => '/myurl/',
                            :metric => 'path', :referer => 'test_referer',
                            :request_params => {:x => 'y'})
-      NewRelic::Agent.agent.merge_data_from([nil, nil, [sampler.errors]])
+      NewRelic::Agent.agent.merge_data_from([nil, nil, sampler.errors])
 
-      assert_equal(1, NewRelic::Agent.agent.unsent_errors_size)
+      assert_equal(1, NewRelic::Agent.agent.error_collector.errors.size)
 
       listener = start_listener_with_pipe(668)
 
@@ -94,7 +94,7 @@ class NewRelic::Agent::PipeChannelManagerTest < Test::Unit::TestCase
       Process.wait(pid)
       listener.stop
 
-      assert_equal(2, NewRelic::Agent.agent.unsent_errors_size)
+      assert_equal(2, NewRelic::Agent.agent.error_collector.errors.size)
     end
 
     def test_close_pipe_on_EOF_string
