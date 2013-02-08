a2dd438660779b2c9eb770e2a6ca826824096701
Jon Guymon <jon@newrelic.com>
2013-02-08 14:11:56 -0800

    RUBY-874 kill all white space

--- test/new_relic/agent/error_collector_test.rb
+++ test/new_relic/agent/error_collector_test.rb
@@ -162,10 +162,10 @@ class NewRelic::Agent::ErrorCollectorTest < Test::Unit::TestCase
 
   def test_exclude_block
     @error_collector.ignore_error_filter &wrapped_filter_proc
-        
+
     @error_collector.notice_error(IOError.new("message"), :metric => 'path', :request_params => {:x => 'y'})
     @error_collector.notice_error(StandardError.new("message"), :metric => 'path', :request_params => {:x => 'y'})
-    
+
     errors = @error_collector.harvest_errors([])
 
     assert_equal 1, errors.length
@@ -215,7 +215,7 @@ class NewRelic::Agent::ErrorCollectorTest < Test::Unit::TestCase
   def get_error_stats
     NewRelic::Agent.get_stats("Errors/all").call_count
   end
-  
+
   def wrapped_filter_proc
     Proc.new do |e|
       if e.is_a? IOError
@@ -225,7 +225,7 @@ class NewRelic::Agent::ErrorCollectorTest < Test::Unit::TestCase
       end
     end
   end
-  
+
   def silence_stream(*args)
     super
   rescue NoMethodError
