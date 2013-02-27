2935f06071d608365a39e2ed2e53a81ba776bf6d
Sam Goldstein <sam@newrelic.com>
2013-02-27 15:56:43 -0800

    Preliminary controller and error instrumentation for rails 4 beta

---
+++ lib/new_relic/agent/instrumentation/rails4/errors.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+module NewRelic
+  module Agent
+    module Instrumentation
+      module Rails4
+        module Errors
+          def newrelic_notice_error(exception, custom_params = {})
+            filtered_params = (respond_to? :filter_parameters) ? filter_parameters(params) : params
+            filtered_params.merge!(custom_params)
+            NewRelic::Agent::Instrumentation::MetricFrame.notice_error( \
+                exception, \
+                :request => request, \
+                :metric => newrelic_metric_path, \
+                :custom_params => filtered_params)
+          end
+        end
+      end
+    end
+  end
+end
+
+DependencyDetection.defer do
+  @name = :rails4_error
+  
+  depends_on do
+    defined?(::Rails) && ::Rails.respond_to?(:version) && ::Rails.version.to_i == 4
+  end
+
+  depends_on do
+    defined?(ActionController) && defined?(ActionController::Base)
+  end
+
+  executes do
+    ::NewRelic::Agent.logger.info 'Installing Rails4 Error instrumentation'
+  end
+
+  executes do
+    class ActionController::Base
+      include NewRelic::Agent::Instrumentation::Rails4::Errors
+    end
+  end
+end
