aa1d4b5e387bdc4cebf7522cb257f7f303e54bc7
Jason R. Clark <jclark@newrelic.com>
2013-05-03 10:23:08 -0700

    RUBY-1057 Sequel doesn't support 1.8.6

--- test/new_relic/agent/instrumentation/sequel_test.rb
+++ test/new_relic/agent/instrumentation/sequel_test.rb
@@ -2,6 +2,8 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
+if RUBY_VERSION >= "1.8.7"
+
 require 'sequel'
 
 require File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','test_helper'))
@@ -254,4 +256,4 @@ class NewRelic::Agent::Instrumentation::SequelInstrumentationTest < Test::Unit::
 
 end
 
-
+end
