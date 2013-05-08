808168996d835a7037c46e26a107075a7ad060ef
Jason R. Clark <jclark@newrelic.com>
2013-05-08 13:07:44 -0700

    RUBY-656 Better settings handling

--- lib/new_relic/agent/instrumentation/sinatra/ignorer.rb
+++ lib/new_relic/agent/instrumentation/sinatra/ignorer.rb
@@ -8,11 +8,9 @@ module NewRelic
       module Sinatra
         module Ignorer
 
-          def self.registered(app)
-            app.set :newrelic_ignores, Hash.new([]) unless app.respond_to?(:newrelic_ignores)
-          end
-
           def self.should_ignore?(app, type)
+            return false if !app.settings.respond_to?(:newrelic_ignores)
+
             app.settings.newrelic_ignores[type].any? do |pattern|
               pattern.match(app.request.path_info)
             end
@@ -33,12 +31,17 @@ module NewRelic
           private
 
           def set_newrelic_ignore(type, *routes)
+            # Important to default this in the context of the actual app
+            # If it's done at register time, ignores end up shared between apps.
+            set :newrelic_ignores, Hash.new([]) if !respond_to?(:newrelic_ignores)
+
             settings.newrelic_ignores[type] += routes.map do |r|
               # Ugly sending to private Base#compile, but we want to mimic
               # exactly Sinatra's mapping of route text to regex
               send(:compile, r).first
             end
           end
+
         end
       end
     end
