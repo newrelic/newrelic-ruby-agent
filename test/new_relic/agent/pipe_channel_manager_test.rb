2b9ddf43c65855bc6478c2c571800b869928b1cf
Ben Weintraub <ben@newrelic.com>
2013-05-16 10:56:34 -0700

    Fix flakiness in test_close_pipe_on_child_exit

--- test/new_relic/agent/pipe_channel_manager_test.rb
+++ test/new_relic/agent/pipe_channel_manager_test.rb
@@ -103,9 +103,13 @@ class NewRelic::Agent::PipeChannelManagerTest < Test::Unit::TestCase
       assert_equal(2, NewRelic::Agent.agent.error_collector.errors.size)
     end
 
+    def pipe_finished?(id)
+      (!NewRelic::Agent::PipeChannelManager.channels[id] ||
+        NewRelic::Agent::PipeChannelManager.channels[id].closed?)
+    end
+
     def assert_pipe_finished(id)
-      assert(!NewRelic::Agent::PipeChannelManager.channels[id] ||
-        NewRelic::Agent::PipeChannelManager.channels[id].closed?,
+      assert(pipe_finished?(id),
         "Expected pipe with ID #{id} to be nil or closed")
     end
 
@@ -126,6 +130,9 @@ class NewRelic::Agent::PipeChannelManagerTest < Test::Unit::TestCase
         exit!
       end
       Process.wait(pid)
+      until pipe_finished?(669) do
+        listener.wakeup
+      end
       listener.stop_listener_thread
       assert_pipe_finished(669)
     end
