a82381421c137a55b3fdb33c99cc0c93ad7efe61
Ben Weintraub <ben@newrelic.com>
2013-06-04 12:47:48 -0700

    RUBY-1122 assert_falsy is not yet available, so use refute

--- test/new_relic/agent/database_test.rb
+++ test/new_relic/agent/database_test.rb
@@ -206,6 +206,6 @@ class NewRelic::Agent::DatabaseTest < Test::Unit::TestCase
       NewRelic::Agent::Database::ConnectionManager.instance.get_connection(config, &connector)
     end
 
-    assert_falsy error_log.array.first.include? 'VOLDEMORT'
+    refute error_log.array.first.include? 'VOLDEMORT'
   end
 end
