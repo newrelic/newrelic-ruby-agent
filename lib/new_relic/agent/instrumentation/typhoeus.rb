8392ed304fcbd4964fbf6ce1d24578e8ce2c9a38
Ben Weintraub <ben@newrelic.com>
2013-06-14 16:28:32 -0700

    RUBY-1120 Avoid instrumenting requests made via Typhoeus Hydra mode
    Our current transaction tracing infrastructure doesn't deal well with
    concurrent operations within a single transaction. Until we come up
    with a more general solution, let's not even try to instrument Hydra
    mode.

--- lib/new_relic/agent/instrumentation/typhoeus.rb
+++ lib/new_relic/agent/instrumentation/typhoeus.rb
@@ -10,14 +10,14 @@ DependencyDetection.defer do
   end
 
   executes do
-    ::NewRelic::Agent.logger.info 'Installing Typhoeus instrumentation'
+    ::NewRelic::Agent.logger.info 'Installing Typhoeus instrumentation (without Hydra mode support)'
     require 'new_relic/agent/cross_app_tracing'
     require 'new_relic/agent/http_clients/typhoeus_wrappers'
   end
 
   executes do
     Typhoeus.before do |request|
-      if NewRelic::Agent.is_execution_traced?
+      if NewRelic::Agent.is_execution_traced? && request.hydra.nil?
         wrapped_request = ::NewRelic::Agent::HTTPClients::TyphoeusHTTPRequest.new(request)
         t0, segment = ::NewRelic::Agent::CrossAppTracing.start_trace(wrapped_request)
         request.on_complete do
@@ -25,6 +25,10 @@ DependencyDetection.defer do
           ::NewRelic::Agent::CrossAppTracing.finish_trace(t0, segment, wrapped_request, wrapped_response)
         end
       end
+
+      # Ensure that we always return a truthy value from the before block,
+      # otherwise Typhoeus will bail out of the instrumentation.
+      true
     end
   end
 end
