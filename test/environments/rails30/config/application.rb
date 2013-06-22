3ad880610f77b5442eaf4089dd0113663d070bb9
Jason R. Clark <jclark@newrelic.com>
2013-06-21 22:50:43 -0700

    Minimal Rails 3.0 app for unit testing

---
+++ test/environments/rails30/config/application.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+module RpmTestApp
+  class Application < Rails::Application
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    config.active_support.deprecation = :notify
+  end
+end
