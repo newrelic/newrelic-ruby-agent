e7e596fd22253e9157c68a17e0f9123d18debec2
Jonan Scheffler <jonanscheffler@gmail.com>
2013-06-27 11:17:21 -0700

    Use helper to compare versions correctly.

--- lib/new_relic/agent/instrumentation/httpclient.rb
+++ lib/new_relic/agent/instrumentation/httpclient.rb
@@ -12,7 +12,10 @@ DependencyDetection.defer do
   end
 
   depends_on do
-    HTTPClient::VERSION >= HTTPCLIENT_MIN_VERSION
+    minimum_supported_version = NewRelic::VersionNumber.new(HTTPCLIENT_MIN_VERSION)
+    current_version = NewRelic::VersionNumber.new(HTTPClient::VERSION)
+
+    current_version >= minimum_supported_version
   end
 
   executes do
