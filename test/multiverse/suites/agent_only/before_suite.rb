85400d1a2db24849d87fb679b94a87b90e9e65fc
Jason R. Clark <jclark@newrelic.com>
2013-06-26 17:04:11 -0700

    REVISE: Beginnings of not killing threads

--- test/multiverse/suites/agent_only/before_suite.rb
+++ test/multiverse/suites/agent_only/before_suite.rb
@@ -2,9 +2,8 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
-# Ugly, but we don't really want/need a public API to the agent's worker thread
-thread = NewRelic::Agent.instance.instance_variable_get(:@worker_thread)
-thread.kill if thread
+ENV["NEWRELIC_ENABLED"] = "false"
+require 'newrelic_rpm'
 
 # Redefine so we don't restart the thread accidentally
 module NewRelic
@@ -16,3 +15,6 @@ module NewRelic
     end
   end
 end
+
+ENV["NEWRELIC_ENABLED"] = "true"
+NewRelic::Agent.manual_start
