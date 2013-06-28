d4c2242f8a11a29bed591a9ee31ad64aa6b06ac7
Ben Weintraub <ben@newrelic.com>
2013-06-28 10:47:22 -0700

    Adding a startup/shutdown performance test

---
+++ test/performance/suites/startup.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+class StartupShutdown < Performance::TestCase
+  def test_startup_shutdown
+    (iterations / 100).times do
+      NewRelic::Agent.manual_start
+      NewRelic::Agent.shutdown
+    end
+  end
+end
