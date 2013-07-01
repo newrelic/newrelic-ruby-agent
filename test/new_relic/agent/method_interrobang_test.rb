b2d1a61a5872a24e71926454ee10362d80c55a26
Jason R. Clark <jclark@newrelic.com>
2013-07-01 12:17:56 -0700

    RUBY-1149 Moving to tests out of multiverse that don't belong

--- test/new_relic/agent/method_interrobang_test.rb
+++ test/new_relic/agent/method_interrobang_test.rb
@@ -2,11 +2,9 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
-
 require 'test/unit'
 require 'newrelic_rpm'
 require 'new_relic/agent/method_tracer'
-
 
 class MethodInterrobangTest < Test::Unit::TestCase
 	include NewRelic::Agent::MethodTracer
@@ -23,12 +21,12 @@ class MethodInterrobangTest < Test::Unit::TestCase
 	add_method_tracer :mutate!
 
 	def test_alias_method_ending_in_question_mark
-		assert_respond_to self,:interrogate?
+		assert_respond_to self, :interrogate?
 		assert_equal "say what?", interrogate?
 	end
 
 	def test_aliase_method_ending_in_exclamation_makr
-		assert_respond_to self,:mutate!
-		assert_equal "oh yeah!",mutate!
+		assert_respond_to self, :mutate!
+		assert_equal "oh yeah!", mutate!
 	end
 end
