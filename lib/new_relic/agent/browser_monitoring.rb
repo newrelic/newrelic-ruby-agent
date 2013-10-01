a19df9b46d617e8da8fe7cf79182d4c27cfaaada
Chase Douglas <chase@newrelic.com>
2013-10-01 10:14:06 -0700

    Guard against NREUM being undefined when adding params

--- lib/new_relic/agent/browser_monitoring.rb
+++ lib/new_relic/agent/browser_monitoring.rb
@@ -211,7 +211,7 @@ module NewRelic
             'agent' => NewRelic::Agent.config[:js_agent_file]
           }
 
-          html_safe_if_needed("\n<script type=\"text/javascript\">NREUM.info=#{NewRelic.json_dump(js_data)}</script>")
+          html_safe_if_needed("\n<script type=\"text/javascript\">window.NREUM||(NREUM={});NREUM.info=#{NewRelic.json_dump(js_data)}</script>")
         else
           obfuscated_transaction_name = obfuscate(config, browser_monitoring_transaction_name)
 
