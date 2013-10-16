3e96c04d3b318a26581b42c38d9cf94423d9075a
Ben Weintraub <ben@newrelic.com>
2013-10-16 14:23:47 -0700

    RUBY-1152 Re-enable special handling for EOFErrors in transmit_data

--- lib/new_relic/agent/agent.rb
+++ lib/new_relic/agent/agent.rb
@@ -1049,6 +1049,9 @@ module NewRelic
             check_for_and_handle_agent_commands
             harvest_and_send_for_agent_commands(disconnecting)
           end
+        rescue EOFError => e
+          ::NewRelic::Agent.logger.warn("EOFError after #{Time.now - now}s when transmitting data to New Relic Service.")
+          ::NewRelic::Agent.logger.debug(e)
         rescue => e
           retry_count ||= 0
           retry_count += 1
