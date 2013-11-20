fb7d8ce7037626d1568f41c5cb3714d6e4fd6ecf
Jonan Scheffler <jonanscheffler@gmail.com>
2013-11-20 12:06:05 -0800

    RUBY-1233 - Test that operations are recorded correctly.

--- test/multiverse/suites/mongo/mongo_instrumentation_test.rb
+++ test/multiverse/suites/mongo/mongo_instrumentation_test.rb
@@ -8,9 +8,39 @@ require File.join(File.dirname(__FILE__), '..', '..', '..', 'agent_helper')
 
 class NewRelic::Agent::Instrumentation::SequelInstrumentationTest < MiniTest::Unit::TestCase
 
-  def test_that_mongo_instrumentation_loaded
+  def test_mongo_instrumentation_loaded
     logging_methods = ::Mongo::Logging.instance_methods
     assert logging_methods.include?(:instrument_with_newrelic_trace), "Expected #{logging_methods.inspect}\n to include :instrument_with_newrelic_trace."
+  end
+
+  OPERATION_NAMES = {
+    'find' => Proc.new { Tribble.find },
+    'findOne' => Proc.new { Tribble.findOne },
+    'insert' => Proc.new { Tribble.insert },
+    'remove' => Proc.new { Tribble.remove },
+    'save' => Proc.new { Tribble.save },
+    'update' => Proc.new { Tribble.update },
+    'distinct' => Proc.new { Tribble.distinct },
+    'count' => Proc.new { Tribble.count },
+    'findAndModify' => Proc.new { Tribble.findAndModify },
+    'findAndRemove' => Proc.new { Tribble.findAndRemove },
+    'createIndex' => Proc.new { Tribble.createIndex },
+    'ensureIndex' => Proc.new { Tribble.ensureIndex },
+    'dropIndex' => Proc.new { Tribble.dropIndex },
+    'dropAllIndexes' => Proc.new { Tribble.dropAllIndexes },
+    'reIndex' => Proc.new { Tribble.reIndex }
+  }
+
+  def test_instrumentation_records_metrics
+    OPERATION_NAMES.each do |name, operation|
+      in_web_transaction { operation.call }
+
+      assert_metrics_recorded([
+        "Datastore/all",
+        "Datastore/operation/MongoDB/#{name}",
+        "Datastore/statement/MongoDB/tribbles/#{name}"
+      ])
+    end
   end
 
 end
