66d6f0c52ba1da760b9c3e833bf7d928919a4c69
Jason R. Clark <jclark@newrelic.com>
2013-11-21 11:12:55 -0800

    RUBY-1232 Fixing name, should have been capture_attributes.page_view_events

--- lib/new_relic/agent/browser_monitoring.rb
+++ lib/new_relic/agent/browser_monitoring.rb
@@ -194,7 +194,7 @@ module NewRelic
       def include_custom_parameters_in_extra?
         NewRelic::Agent.config[:'analytics_events.enabled'] &&
           NewRelic::Agent.config[:'analytics_events.transactions.enabled'] &&
-          NewRelic::Agent.config[:'capture_attributes.page_events']
+          NewRelic::Agent.config[:'capture_attributes.page_view_events']
       end
 
       # Format the props using semicolon separated pairs separated by '=':
