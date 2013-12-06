c13276087492c1623d5d39554fe758ade61ac9d9
Ben Weintraub <ben@newrelic.com>
2013-12-06 10:49:43 -0800

    FlakyProxy: use an options has to instantiate FlakyProxy::Proxy

--- test/flaky_proxy/lib/flaky_proxy/proxy.rb
+++ test/flaky_proxy/lib/flaky_proxy/proxy.rb
@@ -2,32 +2,29 @@ require 'socket'
 
 module FlakyProxy
   class Proxy
-    def initialize(bind_host, bind_port, target_host, target_port)
-      @bind_host = bind_host
-      @bind_port = bind_port
-      @listen_socket = nil
-      @backend_server = Server.new(target_host, target_port)
+    def initialize(options)
+      @listen_host = options[:listen_host]
+      @listen_port = options[:listen_port]
+      @backend_server = Server.new(options[:target_host], options[:target_port])
       @rules = RuleSet.build('')
-    end
-
-    def watch_rules_file(rules_file_path)
-      @rules_file_path = rules_file_path
+      @rules_path = options[:rules_path]
+      @listen_socket = nil
     end
 
     def reload_rules_file
-      if @rules_file_path
-        mtime = File.stat(@rules_file_path).mtime
+      if @rules_path
+        mtime = File.stat(@rules_path).mtime
         if @last_rules_mtime.nil? || mtime > @last_rules_mtime
-          FlakyProxy.logger.info("Reloading rules file at #{@rules_file_path}")
-          @rules = RuleSet.build(File.read(@rules_file_path))
+          FlakyProxy.logger.info("Reloading rules file at #{@rules_path}")
+          @rules = RuleSet.build(File.read(@rules_path))
           @last_rules_mtime = mtime
         end
       end
     end
 
     def run
-      FlakyProxy.logger.info("Starting FlakyProxy on #{@bind_host}:#{@bind_port} -> #{@backend_server.to_s}")
-      @listen_socket = TCPServer.new(@bind_host, @bind_port)
+      FlakyProxy.logger.info("Starting FlakyProxy on #{@listen_host}:#{@listen_port} -> #{@backend_server.to_s}")
+      @listen_socket = TCPServer.new(@listen_host, @listen_port)
       loop do
         client_socket = @listen_socket.accept
         reload_rules_file
