7ace7999d9ca17af6be10214fc018d7daa9ccae8
Jason R. Clark <jclark@newrelic.com>
2013-12-16 16:03:57 -0800

    RUBY-1233 Abide by record_sql settings for Mongo obfuscation

--- lib/new_relic/agent/datastores/mongo/statement_formatter.rb
+++ lib/new_relic/agent/datastores/mongo/statement_formatter.rb
@@ -2,6 +2,7 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
+require 'new_relic/agent/database'
 require 'new_relic/agent/datastores/mongo/obfuscator'
 
 module NewRelic
@@ -10,8 +11,23 @@ module NewRelic
       module Mongo
         module StatementFormatter
           def self.format(statement)
+            statement = statement.dup
+            modify_for_documents(statement)
+            modify_for_selector(statement)
+            statement
+          end
+
+          def self.modify_for_documents(statement)
             statement.delete(:documents)
-            NewRelic::Agent::Datastores::Mongo::Obfuscator.obfuscate_statement(statement)
+          end
+
+          def self.modify_for_selector(statement)
+            case NewRelic::Agent::Database.record_sql_method
+            when :obfuscated
+              Obfuscator.obfuscate_statement(statement)
+            when :off
+              statement.delete(:selector)
+            end
           end
         end
       end
--- test/new_relic/agent/datastores/mongo/statement_formatter_test.rb
+++ test/new_relic/agent/datastores/mongo/statement_formatter_test.rb
@@ -2,37 +2,69 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
-require 'new_relic/agent/datastores/mongo/statement_formatter'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','..','test_helper'))
+require 'new_relic/agent/datastores/mongo/statement_formatter'
 
-class NewRelic::Agent::Datastores::Mongo::StatementFormatterTest < Test::Unit::TestCase
-  def test_statement_formatter_removes_documents
-    statement = { :database   => "multiverse",
-                  :collection =>"tribbles",
-                  :operation => :insert,
-                  :documents  => [ { "name" => "soterios johnson",
-                                     :_id   => "BSON::ObjectId()" } ] }
+module NewRelic
+  module Agent
+    module Datastores
+      module Mongo
+        class StatementFormatterTest < Test::Unit::TestCase
+          DOC_STATEMENT = { :database   => "multiverse",
+                            :collection =>"tribbles",
+                            :operation  => :insert,
+                            :documents  => [ { "name" => "soterios johnson",
+                                               :_id   => "BSON::ObjectId()" } ] }.freeze
 
-    formatted = NewRelic::Agent::Datastores::Mongo::StatementFormatter.format(statement)
 
-    refute formatted.keys.include?(:documents), "Formatted statement should not include documents: #{formatted}"
-  end
+          SELECTOR_STATEMENT = { :database   => 'multiverse',
+                                 :collection => 'tribbles',
+                                 :selector   => { 'name'     => 'soterios johnson',
+                                                  :operation => :find,
+                                                  :_id       => "BSON::ObjectId('?')" } }.freeze
 
-  def test_statement_formatter_obfuscates_selectors
-    statement = { :database   => 'multiverse',
-                  :collection => 'tribbles',
-                  :selector   => { 'name'      => 'soterios johnson',
-                                    :operation => :find,
-                                    :_id       => "BSON::ObjectId('?')" } }
+          def test_doesnt_modify_incoming_statement
+            formatted = StatementFormatter.format(DOC_STATEMENT)
+            assert_not_same DOC_STATEMENT, formatted
+          end
 
-    expected = { :database   => 'multiverse',
-                  :collection => 'tribbles',
-                  :selector   => { 'name'      => '?',
-                                    :operation => :find,
-                                    :_id       => '?' } }
+          def test_statement_formatter_removes_documents
+            formatted = StatementFormatter.format(DOC_STATEMENT)
+            assert_not_includes(formatted.keys, :documents,
+                                "Formatted statement should not include documents: #{formatted}")
+          end
 
-    obfuscated = NewRelic::Agent::Datastores::Mongo::StatementFormatter.format(statement)
+          def test_statement_formatter_obfuscates_selectors
+            expected = { :database   => 'multiverse',
+                         :collection => 'tribbles',
+                         :selector   => { 'name'      => '?',
+                                          :operation => :find,
+                                          :_id       => '?' } }
 
-    assert_equal expected, obfuscated
+            with_config(:'transaction_tracer.record_sql' => "obfuscated") do
+              result = StatementFormatter.format(SELECTOR_STATEMENT)
+              assert_equal expected, result
+            end
+          end
+
+          def test_statement_formatter_raw_selectors
+            with_config(:'transaction_tracer.record_sql' => "raw") do
+              result = StatementFormatter.format(SELECTOR_STATEMENT)
+              assert_equal SELECTOR_STATEMENT, result
+            end
+          end
+
+          def test_statement_formatter_recording_off
+            expected = { :database   => 'multiverse',
+                         :collection => 'tribbles' }
+
+            with_config(:'transaction_tracer.record_sql' => "off") do
+              result = StatementFormatter.format(SELECTOR_STATEMENT)
+              assert_equal expected, result
+            end
+          end
+        end
+      end
+    end
   end
 end
