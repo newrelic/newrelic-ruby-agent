57ba498d3206c53138985fd6110645bb30ce826c
Jonan Scheffler <jonanscheffler@gmail.com>
2014-01-09 14:39:32 -0800

    Test drive mongo server.

---
+++ test/multiverse/suites/mongo/helpers/mongo_server.rb
@@ -0,0 +1,108 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'helpers', 'file_searching'))
+
+class MongoServer
+  include NewRelic::TestHelpers::FileSearching
+
+  attr_reader :type
+  attr_accessor :port
+
+  def initialize(type = :single)
+    @type = type
+    @port = next_available_port
+  end
+
+  def make_directories
+    directories = [
+      port_lock_directory,
+      pid_directory,
+      db_directory,
+      log_directory,
+      db_path
+    ]
+    FileUtils.mkdir_p(directories)
+  end
+
+  def tmp_directory
+    File.join(gem_root, 'tmp')
+  end
+
+  def port_lock_directory
+    File.join(tmp_directory, 'ports')
+  end
+
+  def port_lock_path
+    File.join(port_lock_directory, "#{self.port}.lock")
+  end
+
+  def pid_directory
+    File.join(tmp_directory, 'pids')
+  end
+
+  def pid_path
+    File.join(pid_directory, "#{self.port}.pid")
+  end
+
+  def db_directory
+    File.join(tmp_directory, 'db')
+  end
+
+  def db_path
+    File.join(db_directory, "data_#{self.port}")
+  end
+
+  def log_directory
+    File.join(tmp_directory, 'log')
+  end
+
+  def log_path
+    File.join(log_directory, "#{self.port}.log")
+  end
+
+  def start
+    puts startup_command
+    result = `#{startup_command}`
+    puts result
+  end
+
+  def startup_command
+    pid_file = "--pidfilepath #{pid_path}"
+    log_file = "--logpath #{log_path} "
+
+    dbpath = "--dbpath #{db_path}"
+    port_flag = "--port #{self.port}"
+    small_mongo = "--oplogSize 128 --smallfiles"
+    repl_set = "--fork --replSet multiverse"
+
+    base = "#{port_flag} #{pid_file} #{log_file} #{small_mongo} #{dbpath}"
+
+    if self.type == :single
+      "mongod #{base} &"
+    elsif self.type == :replica
+      "mongod #{repl_set} #{base} &"
+    end
+  end
+
+  def next_available_port
+    used_ports = all_port_lock_files.map do |filename|
+      File.read(filename).to_i
+    end
+
+    if used_ports.empty?
+      27017
+    else
+      used_ports.sort.last + 1
+    end
+  end
+
+  def all_port_lock_files
+    Dir.glob(File.join(port_lock_directory, '*.lock'))
+  end
+
+  def lock_port
+    File.write(port_lock_path, self.port)
+  end
+
+  def release_port
+    FileUtils.rm port_lock_path, force: true
+  end
+end
---
+++ test/multiverse/suites/mongo/helpers/mongo_server_test.rb
@@ -0,0 +1,51 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'mongo_server'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'helpers', 'file_searching'))
+require 'test/unit'
+
+class MongoServerTest < Test::Unit::TestCase
+  include NewRelic::TestHelpers::FileSearching
+
+  def setup
+    @server = MongoServer.new
+  end
+
+  def teardown
+    @server.release_port
+  end
+
+  def port_lock_path
+    File.join(gem_root, 'tmp', 'ports', "#{@server.port}.lock")
+  end
+
+  def test_creating_a_new_server_without_locking_port_uses_the_same_port
+    new_server = MongoServer.new
+    assert_equal @server.port, new_server.port
+  end
+
+  def test_lock_port_creates_a_lock_file
+    @server.lock_port
+    assert File.exists?(port_lock_path)
+  end
+
+  def test_creating_a_new_server_after_locking_port_uses_the_next_port
+    @server.lock_port
+    new_server = MongoServer.new
+    assert_equal @server.port + 1, new_server.port
+  end
+
+  def test_release_port_deletes_the_port_lock_file
+    @server.lock_port
+    @server.release_port
+    refute File.exists?(File.join(gem_root, 'tmp', 'ports', "#{@server.port}.lock"))
+  end
+
+  def test_all_port_lock_files_returns_all_file_names
+    File.write(port_lock_path, @server.port)
+    assert_equal [port_lock_path], @server.all_port_lock_files
+  end
+
+  def test_start_creates_a_mongod_process
+    @server.start
+    assert_equal 1, `ps aux | grep mongo[d]`.split("\n").length
+  end
+end
