6a108eb99f17dd022df2795b92a2e11deeec9191
Jason R. Clark <jclark@newrelic.com>
2014-01-14 10:31:31 -0800

    License header for new test file

--- test/multiverse/suites/agent_only/script/loading.rb
+++ test/multiverse/suites/agent_only/script/loading.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
 ENV["NEW_RELIC_LOG_FILE_PATH"] = "STDOUT"
 
