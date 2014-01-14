470d3aa39ee786523fe1e6324a7b9d2d509b4e53
Jason R. Clark <jclark@newrelic.com>
2014-01-14 15:38:45 -0800

    RUBY-1242 Reset transactions at start of requests
    For Rack-based web requests, we will force the transaction to be cleared out
    as early as possible in our request pipeline. This leans on our middlewares,
    all of which will attempt to reset if no one else has done in this request
    (tracked by the env).
    
    This makes it safe for us to use values from a completed transaction that are
    still present in the thread local after the transaction is done.

---
+++ lib/new_relic/rack/transaction_reset.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+require 'new_relic/agent/transaction_state'
+
+module NewRelic
+  module Rack
+    module TransactionReset
+      RESET_KEY = "newrelic.transaction_reset".freeze
+
+      def ensure_transaction_reset(env)
+        return if env.has_key?(RESET_KEY)
+
+        NewRelic::Agent::TransactionState.reset(nil)
+        env[RESET_KEY] = true
+      end
+    end
+  end
+end
