12a7b8a0213531629ef621b6d79423f12bbd166e
Jonan Scheffler <jonanscheffler@gmail.com>
2014-01-27 17:19:24 -0800

    Additional metrics callback for trace_execution_scoped.

--- lib/new_relic/agent/method_tracer.rb
+++ lib/new_relic/agent/method_tracer.rb
@@ -267,13 +267,20 @@ module NewRelic
       #
       def trace_execution_scoped(metric_names, options={})
         return yield if trace_disabled?(options)
+
         set_if_nil(options, :metric)
         set_if_nil(options, :deduct_call_time_from_parent)
+        additional_metrics_callback = options[:additional_metrics_callback]
+
         metric_names = Array(metric_names)
         first_name = metric_names.shift
+
         start_time, expected_scope = trace_execution_scoped_header(options)
+
         begin
-          yield
+          result = yield
+          metric_names += Array(additional_metrics_callback.call) if additional_metrics_callback
+          result
         ensure
           trace_execution_scoped_footer(start_time, first_name, metric_names, expected_scope, options)
         end
