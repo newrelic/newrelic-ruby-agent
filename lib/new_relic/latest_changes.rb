44723079d403df8d98de7483ff55b0e2ccbfd712
Jason R. Clark <jclark@newrelic.com>
2014-02-06 17:22:11 -0800

    Tighter regex for patch item title matching

--- lib/new_relic/latest_changes.rb
+++ lib/new_relic/latest_changes.rb
@@ -30,7 +30,7 @@ EOS
 
       current_item = nil
       latest.each do |line|
-        if line.match /\*.*/
+        if line.match /^\s*\*.*/
           if line.match /\(#{patch_level}\)/
             # Found a patch level item, so start tracking the lines!
             current_item = line
