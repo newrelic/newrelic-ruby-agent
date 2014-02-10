56ddb7a8bd8cc455766d50144a88f8abe1789060
Ben Weintraub <ben@newrelic.com>
2014-02-10 14:52:34 -0800

    RUBY-1271 Put our Typhoeus on_complete callback first, in case others raise
    If a user-specified Typhoeus on_complete callback raises, we want to be sure
    that we still are able to run our callback so that we can maintain integrity of
    the transaction trace stack.

--- lib/new_relic/agent/instrumentation/typhoeus.rb
+++ lib/new_relic/agent/instrumentation/typhoeus.rb
@@ -64,10 +64,13 @@ module NewRelic::Agent::Instrumentation::TyphoeusTracing
     if NewRelic::Agent.is_execution_traced? && !request_is_hydra_enabled?(request)
       wrapped_request = ::NewRelic::Agent::HTTPClients::TyphoeusHTTPRequest.new(request)
       t0, segment = ::NewRelic::Agent::CrossAppTracing.start_trace(wrapped_request)
-      request.on_complete do
-        wrapped_response = ::NewRelic::Agent::HTTPClients::TyphoeusHTTPResponse.new(request.response)
-        ::NewRelic::Agent::CrossAppTracing.finish_trace(t0, segment, wrapped_request, wrapped_response)
-      end if t0
+      if t0
+        callback = Proc.new do
+          wrapped_response = ::NewRelic::Agent::HTTPClients::TyphoeusHTTPResponse.new(request.response)
+          ::NewRelic::Agent::CrossAppTracing.finish_trace(t0, segment, wrapped_request, wrapped_response)
+        end
+        request.on_complete.unshift(callback)
+      end
     end
   end
 end
