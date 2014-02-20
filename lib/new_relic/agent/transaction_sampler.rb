a59abecac31afe857ff6fe6383e9f4520492ca66
Jason R. Clark <jclark@newrelic.com>
2014-02-19 17:13:24 -0800

    Revert "Revert "Merge pull request #389 from newrelic/RUBY-1273-dirac-guids""
    This reverts commit 3d32f47aa7bc062f134cd29a68f8f49ab0821dbd.

--- lib/new_relic/agent/transaction_sampler.rb
+++ lib/new_relic/agent/transaction_sampler.rb
@@ -121,6 +121,7 @@ module NewRelic
           @last_sample = last_builder.sample
           @last_sample.set_custom_param(:gc_time, gc_time) if gc_time
           store_sample(@last_sample)
+          @last_sample
         end
       end
 
