bf1bf2dbd523288717b739ee2a8174aac8835820
Ben Weintraub <ben@newrelic.com>
2014-02-23 14:17:30 -0800

    RUBY-1275 Move SQL Obfuscator class into its own file

--- lib/new_relic/agent/database.rb
+++ lib/new_relic/agent/database.rb
@@ -4,6 +4,7 @@
 
 require 'singleton'
 require 'new_relic/agent/database/obfuscation_helpers'
+require 'new_relic/agent/database/obfuscator'
 require 'new_relic/agent/database/explain_obfuscator'
 
 module NewRelic
@@ -241,60 +242,6 @@ module NewRelic
           end
 
           @connections = {}
-        end
-      end
-
-      class Obfuscator
-        include Singleton
-        include ObfuscationHelpers
-
-        attr_reader :obfuscator
-
-        def initialize
-          reset
-        end
-
-        def reset
-          @obfuscator = method(:default_sql_obfuscator)
-        end
-
-        # Sets the sql obfuscator used to clean up sql when sending it
-        # to the server. Possible types are:
-        #
-        # :before => sets the block to run before the existing
-        # obfuscators
-        #
-        # :after => sets the block to run after the existing
-        # obfuscator(s)
-        #
-        # :replace => removes the current obfuscator and replaces it
-        # with the provided block
-        def set_sql_obfuscator(type, &block)
-          if type == :before
-            @obfuscator = NewRelic::ChainedCall.new(block, @obfuscator)
-          elsif type == :after
-            @obfuscator = NewRelic::ChainedCall.new(@obfuscator, block)
-          elsif type == :replace
-            @obfuscator = block
-          else
-            fail "unknown sql_obfuscator type #{type}"
-          end
-        end
-
-        def default_sql_obfuscator(sql)
-          if sql[-3,3] == '...'
-            return "Query too large (over 16k characters) to safely obfuscate"
-          end
-
-          stmt = sql.kind_of?(Statement) ? sql : Statement.new(sql)
-          adapter = stmt.adapter
-          obfuscated = remove_escaped_quotes(stmt)
-          obfuscated = obfuscate_single_quote_literals(obfuscated)
-          if !(adapter.to_s =~ /postgres/ || adapter.to_s =~ /sqlite/)
-            obfuscated = obfuscate_double_quote_literals(obfuscated)
-          end
-          obfuscated = obfuscate_numeric_literals(obfuscated)
-          obfuscated.to_s # return back to a regular String
         end
       end
 
