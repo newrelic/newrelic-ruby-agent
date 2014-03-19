a29228a1f145f2211e2d0b5258b756c0df158df0
Jason R. Clark <jclark@newrelic.com>
2014-03-19 13:21:28 -0700

    RUBY-1282 ActiveMerchant instrumentation on 1.9.x
    Return from public_instance_methods was an array of strings on 1.8.x, but a
    list of symbols on 1.9.x+.
    
    Our comparison of the operations (#to_s'd) against the result from
    public_instance_methods ended up never passing on 1.9.x, so no instrumentation.

--- lib/new_relic/agent/instrumentation/active_merchant.rb
+++ lib/new_relic/agent/instrumentation/active_merchant.rb
@@ -18,10 +18,10 @@ DependencyDetection.defer do
   executes do
     ActiveMerchant::Billing::Gateway.implementations.each do |gateway|
       gateway.class_eval do
-        implemented_methods = public_instance_methods(false)
+        implemented_methods = public_instance_methods(false).map(&:to_sym)
         gateway_name = self.name.split('::').last
         [:authorize, :purchase, :credit, :void, :capture, :recurring].each do |operation|
-          if implemented_methods.include?(operation.to_s)
+          if implemented_methods.include?(operation)
             add_method_tracer operation, "ActiveMerchant/gateway/#{gateway_name}/#{operation}", :scoped_metric_only => true
             add_method_tracer operation, "ActiveMerchant/gateway/#{gateway_name}", :push_scope => false
             add_method_tracer operation, "ActiveMerchant/operation/#{operation}", :push_scope => false
