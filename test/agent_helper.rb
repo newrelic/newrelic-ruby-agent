49551f0b663a5df07813980607ab75db903596a1
Ben Weintraub <ben@newrelic.com>
2014-05-16 08:59:42 -0700

    Fix some bugs in the assert_metrics_recorded_exclusive helper leading to test failures on 1.8.x

--- test/agent_helper.rb
+++ test/agent_helper.rb
@@ -137,15 +137,16 @@ def assert_metrics_recorded_exclusive(expected, options={})
   recorded_metrics = NewRelic::Agent.instance.stats_engine.metric_specs
 
   if options[:filter]
-    recorded_metrics.select! { |m| m.name.match(options[:filter]) }
+    recorded_metrics = recorded_metrics.select { |m| m.name.match(options[:filter]) }
   end
   if options[:ignore_filter]
     recorded_metrics.reject! { |m| m.name.match(options[:ignore_filter]) }
   end
 
   expected_metrics   = expected.keys.map { |s| metric_spec_from_specish(s) }
-  unexpected_metrics = recorded_metrics.select { |m| m !~ /GC\/Transaction/ }
-  unexpected_metrics -= expected_metrics
+
+  unexpected_metrics = recorded_metrics - expected_metrics
+  unexpected_metrics.reject! { |m| m.name =~ /GC\/Transaction/ }
 
   assert_equal(0, unexpected_metrics.size, "Found unexpected metrics: #{format_metric_spec_list(unexpected_metrics)}")
 end
