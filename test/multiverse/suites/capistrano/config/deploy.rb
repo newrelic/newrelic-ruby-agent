68360c56c690022c7161a24076c4f8449ddc3501
Jason R. Clark <jclark@newrelic.com>
2014-05-20 13:47:41 -0700

    Comment and unnecessary boiler-plate cleanup

--- test/multiverse/suites/capistrano/config/deploy.rb
+++ test/multiverse/suites/capistrano/config/deploy.rb
@@ -1,7 +1,6 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
-
 set :application, 'test'
 
+# Since Capistrano 3 doesn't allow settings direct from command-line, add any
+# settings we want to conditionally toggle from tests in the following manner.
 set :newrelic_user, ENV["NEWRELIC_USER"] if ENV["NEWRELIC_USER"]
 set :newrelic_appname, ENV["NEWRELIC_APPNAME"] if ENV["NEWRELIC_APPNAME"]
