c1cf37a131b302d989bcd2b87047fe63067e4506
Ben Weintraub <ben@newrelic.com>
2014-06-27 17:16:23 -0700

    RUBY-1325 Add CAT Map tripId and pathHash to outgoing request headers

--- lib/new_relic/agent/cross_app_tracing.rb
+++ lib/new_relic/agent/cross_app_tracing.rb
@@ -161,8 +161,12 @@ module NewRelic
           raise NewRelic::Agent::CrossAppTracing::Error, "no cross app ID configured"
 
         state.is_cross_app_caller = true
-        txn_guid = state.request_guid
-        txn_data = NewRelic::JSONWrapper.dump([txn_guid, false])
+        txn_guid  = state.request_guid
+        if state.current_transaction
+          trip_id   = state.current_transaction.cat_trip_id(state)
+          path_hash = state.current_transaction.cat_path_hash(state)
+        end
+        txn_data  = NewRelic::JSONWrapper.dump([txn_guid, false, trip_id, path_hash])
 
         request[NR_ID_HEADER]  = obfuscator.obfuscate(cross_app_id)
         request[NR_TXN_HEADER] = obfuscator.obfuscate(txn_data)
