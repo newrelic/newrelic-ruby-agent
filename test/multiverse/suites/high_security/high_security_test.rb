61722c42860b2589523943d2418e01ee48ed7aa9
Jason R. Clark <jclark@newrelic.com>
2014-07-02 15:31:47 -0600

    RUBY-1319 Test for ssl on connect

--- test/multiverse/suites/high_security/high_security_test.rb
+++ test/multiverse/suites/high_security/high_security_test.rb
@@ -4,16 +4,33 @@
 
 require 'multiverse_helpers'
 
+# These tests are designed to work in conjunction with a local newrelic.yml
+# file set with "insecure" settings and the server returning "insecure" values,
+# and confirm that high security changes the actual agent behavior, not just
+# the settings in question.
 class HighSecurityTest < Minitest::Test
 
   include MultiverseHelpers
 
   setup_and_teardown_agent do |collector|
+    # Because our tests will default to using SSL, we need to stub that out
+    # on our HTTP connects to the fake collector which doesn't have SSL...
+    Net::HTTPSession.any_instance.expects('use_ssl=').with(true).at_least_once
+
     collector.stub('connect', {
       "agent_run_id" => 1,
       "listen_to_server_config" => true,
-      "capture_params"          => true,
+
+      "agent_config" => {
+        "capture_params" => true,
+      }
     }, 200)
+  end
+
+  def test_connects_via_ssl_no_matter_what
+    # Expectation is set in setup since we all need to sidestep the use_ssl=
+    # setting, and mocking gets weird if we call `stubs` then try to `expects`
+    NewRelic::Agent.manual_start(:ssl => false)
   end
 
   def test_disallows_server_config_from_overriding_high_security
