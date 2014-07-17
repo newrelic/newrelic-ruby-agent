284cb3f45391a43074b1b3e15db9c0eaa851fc90
Ben Weintraub <ben@newrelic.com>
2014-07-17 11:19:34 -0700

    RUBY-1325 Update pathHash algorithm to use 32 rather than 31 bits

--- test/script/path_hash.rb
+++ test/script/path_hash.rb
@@ -27,22 +27,20 @@ path.each do |hop|
   identifier = "#{app_name};#{transaction_name}"
   md5sum = Digest::MD5.digest(identifier)
   low4_of_md5 = md5sum.unpack("@12N").first
-  low31_of_md5 = low4_of_md5 & 0x7fffffff
 
-  rotated_path_hash = ((path_hash << 1) | (path_hash >> 30)) & 0x7fffffff
+  rotated_path_hash = ((path_hash << 1) | (path_hash >> 31)) & 0xffffffff
 
-  xor_result = rotated_path_hash ^ low31_of_md5
+  xor_result = rotated_path_hash ^ low4_of_md5
 
   puts '--'
   puts "A: txnIdentifier:       '#{identifier}'"
   puts "B: MD5(A):              #{Digest::MD5.hexdigest(identifier)}"
   puts "C: Low 4 bytes of B:    0x#{low4_of_md5.to_s(16).rjust(8, '0')} (0b#{low4_of_md5.to_s(2).rjust(32, '0')})"
-  puts "D: Low 31 bits of C:    0x#{low31_of_md5.to_s(16).rjust(8, '0')} (0b#{low31_of_md5.to_s(2).rjust(32, '0')})"
   puts ''
-  puts "E: Input path_hash:     0x#{path_hash.to_s(16).rjust(8, '0')} (0b#{path_hash.to_s(2).rjust(32, '0')})"
-  puts "F: Rotated E:           0x#{rotated_path_hash.to_s(16).rjust(8, '0')} (0b#{rotated_path_hash.to_s(2).rjust(32, '0')})"
+  puts "D: Input path_hash:     0x#{path_hash.to_s(16).rjust(8, '0')} (0b#{path_hash.to_s(2).rjust(32, '0')})"
+  puts "E: Rotated E:           0x#{rotated_path_hash.to_s(16).rjust(8, '0')} (0b#{rotated_path_hash.to_s(2).rjust(32, '0')})"
   puts ""
-  puts "G: XOR(D, F):           0x#{xor_result.to_s(16).rjust(8, '0')} (0b#{xor_result.to_s(2).rjust(32, '0')})"
+  puts "F: XOR(D, F):           0x#{xor_result.to_s(16).rjust(8, '0')} (0b#{xor_result.to_s(2).rjust(32, '0')})"
   puts '--'
 
   path_hash = xor_result
