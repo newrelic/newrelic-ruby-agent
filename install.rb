b87ad74dc3f4f674595b73f14eef446dff2a4d85
sanemat <o.gata.ken@gmail.com>
2014-07-21 18:41:46 +0900

    Adjust rails 4 rake task

--- install.rb
+++ install.rb
@@ -2,7 +2,7 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
-if __FILE__ == $0 || $0 =~ /script\/plugin/
+if __FILE__ == $0 || $0 =~ /script\/plugin/ || File.basename($0) == 'rake'
   $LOAD_PATH << File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
   require 'new_relic/cli/command'
   begin
