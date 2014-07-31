34ddd2890d6ba5a3ae9f400091d9bca2eed1b597
Chris Pine <cpine@newrelic.com>
2014-07-31 12:08:57 -0700

    EventLoop test cleanup. Tests are now more representative of actual use, and are easier to write. Also added a new test for a technique we might want to try.

--- test/new_relic/agent/event_loop_test.rb
+++ test/new_relic/agent/event_loop_test.rb
@@ -6,11 +6,11 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','..','test_helper
 
 class NewRelic::Agent::EventLoopTest < Minitest::Test
   def setup
+    freeze_time
     @loop = NewRelic::Agent::EventLoop.new
   end
 
   def test_fire_after
-    freeze_time
     call_count = 0
 
     @loop.on(:event) { call_count += 1 }
@@ -19,21 +19,17 @@ class NewRelic::Agent::EventLoopTest < Minitest::Test
     @loop.run_once(true)
     assert_equal(0, call_count)
 
-    advance_time(1)
-    @loop.run_once(true)
+    advance_loop(1)
     assert_equal(0, call_count)
 
-    advance_time(2)
-    @loop.run_once(true)
+    advance_loop(2)
     assert_equal(1, call_count)
 
-    advance_time(5)
-    @loop.run_once(true)
+    advance_loop(5)
     assert_equal(1, call_count)
   end
 
   def test_fire_every
-    freeze_time
     call_count = 0
 
     @loop.on(:event) do
@@ -45,69 +41,56 @@ class NewRelic::Agent::EventLoopTest < Minitest::Test
     @loop.run_once(true)
     assert_equal(0, call_count)
 
-    advance_time(2)
-    @loop.run_once(true)
+    advance_loop(2)
     assert_equal(0, call_count)
 
-    advance_time(1)
-    @loop.run_once(true)
+    advance_loop(1)
     assert_equal(1, call_count)
 
-    advance_time(4)
-    @loop.run_once(true)
+    advance_loop(4)
     assert_equal(2, call_count)
   end
 
   def test_fire_resets_associated_timers
-    freeze_time
     call_count = 0
 
     @loop.on(:event) { call_count += 1 }
     @loop.fire_every(10, :event)
 
-    advance_time(5)
+    advance_loop(5)
     @loop.fire(:event)
     @loop.run_once(true)
     assert_equal(1, call_count)
 
-    advance_time(5)
-    @loop.run_once(true)
+    advance_loop(5)
     assert_equal(1, call_count)
 
-    advance_time(5)
-    @loop.run_once(true)
+    advance_loop(5)
     assert_equal(2, call_count)
   end
 
   def test_adjust_period_larger
-    freeze_time
-
     call_count = 0
     @loop.on(:e) { call_count += 1 }
 
     @loop.fire_every(10, :e)
     @loop.run_once(true)
 
-    advance_time(9)
-    @loop.run_once(true)
+    advance_loop(9)
     assert_equal(0, call_count)
 
     @loop.fire_every(15, :e)
     @loop.run_once(true)
     assert_equal(0, call_count)
 
-    advance_time(1)
-    @loop.run_once(true)
+    advance_loop(1)
     assert_equal(0, call_count)
 
-    advance_time(5)
-    @loop.run_once(true)
+    advance_loop(5)
     assert_equal(1, call_count)
   end
 
   def test_adjust_period_smaller
-    freeze_time
-
     call_count = 0
     @loop.on(:e) { call_count += 1 }
     @loop.fire_every(10, :e)
@@ -115,20 +98,17 @@ class NewRelic::Agent::EventLoopTest < Minitest::Test
     @loop.run_once(true)
     assert_equal(0, call_count)
 
-    advance_time(5)
-    @loop.run_once(true)
+    advance_loop(5)
     assert_equal(0, call_count)
 
     @loop.fire_every(2, :e)
     @loop.run_once(true)
     assert_equal(1, call_count)
 
-    advance_time(1)
-    @loop.run_once(true)
+    advance_loop(1)
     assert_equal(1, call_count)
 
-    advance_time(1)
-    @loop.run_once(true)
+    advance_loop(1)
     assert_equal(2, call_count)
   end
 
@@ -153,26 +133,21 @@ class NewRelic::Agent::EventLoopTest < Minitest::Test
   end
 
   def test_manual_fire_resets_timers
-    freeze_time
-
     call_count = 0
     @loop.on(:e) { call_count += 1 }
     @loop.fire_every(4, :e)
 
-    advance_time(3)
-    @loop.run_once(true)
+    advance_loop(3)
     assert_equal(0, call_count)
 
     @loop.fire(:e)
     @loop.run_once(true)
     assert_equal(1, call_count)
 
-    advance_time(3)
-    @loop.run_once(true)
+    advance_loop(3)
     assert_equal(1, call_count)
 
-    advance_time(1)
-    @loop.run_once(true)
+    advance_loop(1)
     assert_equal(2, call_count)
   end
 
@@ -186,5 +161,42 @@ class NewRelic::Agent::EventLoopTest < Minitest::Test
 
     @loop.run_once(true)
     assert_equal(1, call_count)
+  end
+
+  def test_timer_period_reset_from_event
+    call_count = 0
+    @loop.on(:e           ) { call_count += 1          }
+    @loop.on(:reset_period) { @loop.fire_every(30, :e) }
+
+    @loop.fire_every( 5, :e           )
+    @loop.fire_after(31, :reset_period)
+
+    advance_loop(3)  # total time 3
+    assert_equal(0, call_count)
+
+    advance_loop(2)  # total time 5
+    assert_equal(1, call_count)
+
+    advance_loop(5)  # total time 10
+    assert_equal(2, call_count)
+
+    advance_loop(20)  # total time 30
+    assert_equal(6, call_count)
+
+    advance_loop(29)  # total time 59
+    assert_equal(6, call_count)
+
+    advance_loop(1)  # total time 60
+    assert_equal(7, call_count)
+
+    advance_loop(60)  # total time 120
+    assert_equal(9, call_count)
+  end
+
+  def advance_loop(n)
+    n.times do
+      advance_time(1)
+      @loop.run_once(true)
+    end
   end
 end
