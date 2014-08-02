fa5b61524d024d62eea1a147613538d016ae8e7a
Jonan Scheffler <jonan@newrelic.com>
2014-08-01 17:25:17 -0700

    Test Capistrano deploy settings.

--- test/multiverse/suites/capistrano/config/deploy.rb
+++ test/multiverse/suites/capistrano/config/deploy.rb
@@ -6,5 +6,9 @@ set :application, 'test'
 
 # Since Capistrano 3 doesn't allow settings direct from command-line, add any
 # settings we want to conditionally toggle from tests in the following manner.
-set :newrelic_user, ENV["NEWRELIC_USER"] if ENV["NEWRELIC_USER"]
-set :newrelic_appname, ENV["NEWRELIC_APPNAME"] if ENV["NEWRELIC_APPNAME"]
+ENV.keys.each do |key|
+  if key.match(/NEWRELIC_CAPISTRANO_/)
+    name = key.gsub('NEWRELIC_CAPISTRANO_', '').downcase
+    set "newrelic_#{name}".to_sym, ENV[key]
+  end
+end
