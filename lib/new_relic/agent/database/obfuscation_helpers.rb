7c8857c970e1193dc2511be2b846c1e2130a7ab6
Ben Weintraub <ben@newrelic.com>
2014-08-19 11:50:54 -0700

    RUBY-1355 Strip comments from SQL queries during obfuscation

--- lib/new_relic/agent/database/obfuscation_helpers.rb
+++ lib/new_relic/agent/database/obfuscation_helpers.rb
@@ -6,8 +6,6 @@ module NewRelic
   module Agent
     module Database
       module ObfuscationHelpers
-        NUMERICS = /\b\d+\b/
-
         # Note that the following two regexes are applied to a reversed version
         # of the query. This is why the backslash escape sequences (\' and \")
         # appear reversed within them.
@@ -23,35 +21,56 @@ module NewRelic
         # Reversing the query string before obfuscation allows us to get around
         # the fact that a \' appearing within a string may or may not terminate
         # the string, because we know that a string cannot *start* with a \'.
-        REVERSE_SINGLE_QUOTES = /'(?:''|'\\|[^'])*'/
-        REVERSE_ANY_QUOTES    = /'(?:''|'\\|[^'])*'|"(?:""|"\\|[^"])*"/
+        REVERSE_SINGLE_QUOTES_REGEX = /'(?:''|'\\|[^'])*'/
+        REVERSE_ANY_QUOTES_REGEX    = /'(?:''|'\\|[^'])*'|"(?:""|"\\|[^"])*"/
 
-        PLACEHOLDER = '?'
+        NUMERICS_REGEX = /\b\d+\b/
+
+        # We take a conservative, overly-aggressive approach to obfuscating
+        # comments, and drop everything from the query after encountering any
+        # character sequence that could be a comment initiator. We do this after
+        # removal of string literals to avoid accidentally over-obfuscating when
+        # a string literal contains a comment initiator.
+        SQL_COMMENT_REGEX = Regexp.new('(?:/\*|--|#).*', Regexp::MULTILINE).freeze
+
+        # We use these to check whether the query contains any quote characters
+        # after obfuscation. If so, that's a good indication that the original
+        # query was malformed, and so our obfuscation can't reliabily find
+        # literals. In such a case, we'll replace the entire query with a
+        # placeholder.
+        LITERAL_SINGLE_QUOTE = "'".freeze
+        LITERAL_DOUBLE_QUOTE = '"'.freeze
+
+        PLACEHOLDER = '?'.freeze
 
         def obfuscate_single_quote_literals(sql)
           obfuscated = sql.reverse
-          obfuscated.gsub!(REVERSE_SINGLE_QUOTES, PLACEHOLDER)
+          obfuscated.gsub!(REVERSE_SINGLE_QUOTES_REGEX, PLACEHOLDER)
           obfuscated.reverse!
           obfuscated
         end
 
         def obfuscate_quoted_literals(sql)
           obfuscated = sql.reverse
-          obfuscated.gsub!(REVERSE_ANY_QUOTES, PLACEHOLDER)
+          obfuscated.gsub!(REVERSE_ANY_QUOTES_REGEX, PLACEHOLDER)
           obfuscated.reverse!
           obfuscated
         end
 
         def obfuscate_numeric_literals(sql)
-          sql.gsub(NUMERICS, PLACEHOLDER)
+          sql.gsub(NUMERICS_REGEX, PLACEHOLDER)
+        end
+
+        def remove_comments(sql)
+          sql.gsub(SQL_COMMENT_REGEX, PLACEHOLDER)
         end
 
         def contains_single_quotes?(str)
-          str.include?("'")
+          str.include?(LITERAL_SINGLE_QUOTE)
         end
 
         def contains_quotes?(str)
-          str.include?('"') || str.include?("'")
+          str.include?(LITERAL_SINGLE_QUOTE) || str.include?(LITERAL_DOUBLE_QUOTE)
         end
       end
     end
