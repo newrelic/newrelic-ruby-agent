63cf0ee999c370c1d42e185cba718e3ffc13effb
Jason R. Clark <jclark@newrelic.com>
2014-09-25 11:45:35 -0700

    Another rev on grouping

--- test/multiverse/lib/multiverse/runner.rb
+++ test/multiverse/lib/multiverse/runner.rb
@@ -82,15 +82,16 @@ module Multiverse
     end
 
     GROUPS = {
-      "agent"       => ["agent_only", "bare", "config_file_loading",
-                        "deferred_instrumentation", "high_security", "json",
-                        "marshalling"],
-      "background"  => ["resque", "sidekiq"],
-      "database"    => ["datamapper", "mongo", "sequel"],
-      "httpclients" => ["curb", "excon", "httpclient", "typhoeus", "net_http"],
-      "rails"       => ["active_record", "rails"],
+      "agent"         => ["agent_only", "bare", "config_file_loading",
+                          "deferred_instrumentation", "high_security"],
+      "background"    => ["resque", "sidekiq"],
+      "database"      => ["datamapper", "mongo", "sequel"],
+      "httpclients"   => ["curb", "excon", "httpclient", "typhoeus", "net_http"],
+      "rails"         => ["active_record", "rails"],
+      "serialization" => ["json", "marshalling", "yajl"],
+      "sinatra"       => ["sinatra", "padrino"],
 
-      "rest"        => []  # Specially handled below
+      "rest"          => []  # Specially handled below
     }
 
     def passes_filter?(dir, filter)
