70f52ea80367bc603ea90f42265ee609cee95c6b
Jason R. Clark <jclark@newrelic.com>
2014-10-09 10:28:17 -0700

    RUBY-1389 Use DJ's migration directly

--- test/multiverse/suites/delayed_job/before_suite.rb
+++ test/multiverse/suites/delayed_job/before_suite.rb
@@ -2,22 +2,12 @@
 $database_name = "testdb.#{ENV["MULTIVERSE_ENV"]}.sqlite3"
 $db_connection = ActiveRecord::Base.establish_connection(:adapter => 'sqlite3',
                                                          :database => $database_name)
-ActiveRecord::Migration.class_eval do
-  @connection = $db_connection
-  create_table :delayed_jobs, :force => true do |table|
-    table.integer  :priority, :default => 0      # Allows some jobs to jump to the front of the queue
-    table.integer  :attempts, :default => 0      # Provides for retries, but still fail eventually.
-    table.text     :handler                      # YAML-encoded string of the object that will do work
-    table.text     :last_error                   # reason for last failure (See Note below)
-    table.datetime :run_at                       # When to run. Could be Time.zone.now for immediately, or sometime in the future.
-    table.datetime :locked_at                    # Set when a client is working on this object
-    table.datetime :failed_at                    # Set when all retries have failed (actually, by default, the record is deleted instead)
-    table.string   :locked_by                    # Who is working on this object (if locked)
-    table.string   :queue                        # The name of the queue this job is in
-    table.timestamps
-  end
 
-  add_index :delayed_jobs, [:priority, :run_at], :name => 'delayed_jobs_priority'
+# TODO: This will almost certainly have to get more version/gem aware as wel
+# test other backends and versions, but for now it does the job!
+require 'generators/delayed_job/templates/migration'
+class CreateDelayedJobs
+  @connection = $db_connection
 end
 
-#drop_table :delayed_jobs
+CreateDelayedJobs.up
