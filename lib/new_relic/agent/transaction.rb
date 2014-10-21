7083cdbf7e8181bcf05c1b2078cb7691211e9623
Ben Weintraub <ben@newrelic.com>
2014-10-21 11:07:18 -0700

    RUBY-1391 Ensure we include GUID for analytics events triggered by synthetics

--- lib/new_relic/agent/transaction.rb
+++ lib/new_relic/agent/transaction.rb
@@ -481,7 +481,7 @@ module NewRelic
       end
 
       def include_guid?(state, duration)
-        state.is_cross_app?
+        state.is_cross_app? || is_synthetics_request?
       end
 
       def cat_trip_id(state)
