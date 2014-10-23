16007ce1f599e7d126c6592ec7077a4f34c2104a
Ben Weintraub <ben@newrelic.com>
2014-10-23 11:21:51 -0700

    RUBY-1396 files called 'manifest' are git ignored. Doh.

--- test/new_relic/agent/system_info_test.rb
+++ test/new_relic/agent/system_info_test.rb
@@ -41,16 +41,16 @@ class NewRelic::Agent::SystemInfoTest < Minitest::Test
     end
   end
 
-  cgroup_test_dir   = File.join(cross_agent_tests_dir, 'docker_container_id')
-  cgroup_test_cases = load_cross_agent_test(File.join('docker_container_id', 'manifest'))
+  docker_id_test_dir   = File.join(cross_agent_tests_dir, 'docker_container_id')
+  docker_id_test_cases = load_cross_agent_test(File.join('docker_container_id', 'cases'))
 
-  cgroup_test_cases.each do |test_case|
+  docker_id_test_cases.each do |test_case|
     filename = test_case['filename']
     basename = File.basename(filename, '.txt')
     test_name = "test_docker_container_id_#{basename}"
 
     define_method(test_name) do
-      input = File.read(File.join(cgroup_test_dir, filename))
+      input = File.read(File.join(docker_id_test_dir, filename))
       container_id = @sysinfo.parse_docker_container_id(input)
 
       message = "Parsed incorrect Docker container ID from #{filename}"
