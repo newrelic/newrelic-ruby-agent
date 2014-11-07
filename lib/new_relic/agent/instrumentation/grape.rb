a4758da06f4978998e697e7e06338b6de64b2d6f
Chris Pine <cpine@newrelic.com>
2014-11-07 15:06:12 -0800

    RUBY-1404: Pull string literals out into separate module.

--- lib/new_relic/agent/instrumentation/grape.rb
+++ lib/new_relic/agent/instrumentation/grape.rb
@@ -15,7 +15,19 @@ DependencyDetection.defer do
   end
 
   def install_grape_instrumentation
+    install_constants
     instrument_call
+  end
+
+  def install_constants
+    grape_constants = Module.new
+
+    grape_constants.const_set(:API_ENDPOINT, 'api.endpoint'.freeze)
+    grape_constants.const_set(:ROUTE_INFO  ,   'route_info'.freeze)
+    grape_constants.const_set(:FORMAT      ,   '(.:format)'.freeze)
+    grape_constants.const_set(:EMPTY_STRING,             ''.freeze)
+
+    ::NewRelic::Agent.const_set(:GrapeConstants, grape_constants)
   end
 
   def instrument_call
@@ -27,11 +39,12 @@ DependencyDetection.defer do
           exception = e
         end
 
-        endpoint = env['api.endpoint']
+        endpoint = env[::NewRelic::Agent::GrapeConstants::API_ENDPOINT]
 
         if endpoint
-          route_obj   = endpoint.params['route_info']
-          action_name = route_obj.route_path.gsub('(.:format)','')
+          route_obj   = endpoint.params[::NewRelic::Agent::GrapeConstants::ROUTE_INFO]
+          action_name = route_obj.route_path.gsub(::NewRelic::Agent::GrapeConstants::FORMAT,
+                                                  ::NewRelic::Agent::GrapeConstants::EMPTY_STRING)
           method_name = route_obj.route_method
 
           txn_name = "#{self.class.name}#{action_name} (#{method_name})"
