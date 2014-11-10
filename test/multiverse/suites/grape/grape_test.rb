dd482d36a799b9ed9ca2ef0871030b93bb47fe92
Jonan Scheffler <jonan@newrelic.com>
2014-11-10 14:23:52 -0800

    RUBY-1404: Test setting Grape transaction names.

--- test/multiverse/suites/grape/grape_test.rb
+++ test/multiverse/suites/grape/grape_test.rb
@@ -8,10 +8,27 @@ require 'multiverse_helpers'
 
 require File.join(File.dirname(__FILE__), "..", "..", "..", "agent_helper")
 
-class TestAPI < Grape::API
-  get :test do
-    'Test!'
-  end
+class TestApi < Grape::API
+  namespace :grape_ape
+    get do
+      'List grape apes!'
+    end
+
+    get ':id' do
+      'Show grape ape!'
+    end
+
+    post do
+      'Create grape ape!'
+    end
+
+    put ':id' do
+      'Update grape ape!'
+    end
+
+    delete ':id' do
+      'Destroy grape ape!'
+    end
 end
 
 class GrapeTest < Minitest::Test
@@ -19,10 +36,31 @@ class GrapeTest < Minitest::Test
   include MultiverseHelpers
 
   def app
-    TestAPI.new
+    TestApi.new
   end
 
-  def test_truthiness
-    get '/test'
+  def test_getting_a_list_of_grape_apes
+    NewRelic::Agent.expects(:set_transaction_name).with('TestApi/grape_ape (GET)')
+    get '/grape_ape'
+  end
+
+  def test_showing_a_grape_ape
+    NewRelic::Agent.expects(:set_transaction_name).with('TestApi/grape_ape/:id (GET)')
+    get '/grape_ape/1'
+  end
+
+  def test_creating_a_grape_ape
+    NewRelic::Agent.expects(:set_transaction_name).with('TestApi/grape_ape (POST)')
+    post '/grape_ape', {}
+  end
+
+  def test_updating_a_grape_ape
+    NewRelic::Agent.expects(:set_transaction_name).with('TestApi/grape_ape/:id (PUT)')
+    put '/grape_ape/1', {}
+  end
+
+  def test_deleting_a_grape_ape
+    NewRelic::Agent.expects(:set_transaction_name).with('TestApi/grape_ape/:id (DELETE)')
+    delete '/grape_ape/1'
   end
 end
