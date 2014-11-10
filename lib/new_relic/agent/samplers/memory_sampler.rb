687489ec946bb5ed6916b526f30e049d089c5f7d
Jonathan del Strother <jon.delStrother@audioboo.fm>
2014-11-10 14:55:14 +0000

    Add support for /system/lxproc on Illumos

--- lib/new_relic/agent/samplers/memory_sampler.rb
+++ lib/new_relic/agent/samplers/memory_sampler.rb
@@ -20,10 +20,7 @@ module NewRelic
           elsif platform =~ /linux/
             @sampler = ProcStatus.new
             if !@sampler.can_run?
-              ::NewRelic::Agent.logger.debug "Error attempting to use /proc/#{$$}/status file for reading memory. Using ps command instead."
               @sampler = ShellPS.new("ps -o rsz")
-            else
-              ::NewRelic::Agent.logger.debug "Using /proc/#{$$}/status for reading process memory."
             end
           elsif platform =~ /darwin9/ # 10.5
             @sampler = ShellPS.new("ps -o rsz")
@@ -32,11 +29,15 @@ module NewRelic
           elsif platform =~ /freebsd/
             @sampler = ShellPS.new("ps -o rss")
           elsif platform =~ /solaris/
-            @sampler = ShellPS.new("/usr/bin/ps -o rss -p")
+            @sampler = ProcStatus.new("/system/lxproc")
+            if !@sampler.can_run?
+              @sampler = ShellPS.new("/usr/bin/ps -o rss -p")
+            end
           end
 
           raise Unsupported, "Unsupported platform for getting memory: #{platform}" if @sampler.nil?
-          raise Unsupported, "Unable to run #{@sampler}" unless @sampler.can_run?
+          raise Unsupported, "Unable to run <#{@sampler}>" unless @sampler.can_run?
+          ::NewRelic::Agent.logger.debug "Using <#{@sampler}> for reading process memory."
         end
 
         def self.supported_on_this_platform?
@@ -126,6 +127,10 @@ module NewRelic
         # A class that samples memory by reading the file /proc/$$/status, which is specific to linux
         #
         class ProcStatus < Base
+          def initialize(proc_prefix="/proc")
+            super()
+            @proc_prefix = proc_prefix
+          end
           # Returns the amount of resident memory this process is using in MB
           def get_memory
             proc_status = File.open(proc_status_file, "r") {|f| f.read_nonblock(4096).strip }
@@ -136,7 +141,7 @@ module NewRelic
           end
 
           def proc_status_file
-            "/proc/#{$$}/status"
+            "#{@proc_prefix}/#{$$}/status"
           end
 
           def to_s
