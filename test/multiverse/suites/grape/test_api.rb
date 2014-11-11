8f9488a7a4bae7350136bc09ff406d70ac7d9cff
Jonan Scheffler <jonan@newrelic.com>
2014-11-11 15:06:32 -0800

    RUBY-1404: Use all the namespace aliases in case they change.

--- test/multiverse/suites/grape/test_api.rb
+++ test/multiverse/suites/grape/test_api.rb
@@ -1,28 +1,35 @@
-# encoding: utf-8
-# This file is distributed under New Relic's license terms.
-# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
-
 require 'grape'
 
 class TestApi < Grape::API
-  namespace :grape_ape
+  # namespace, group, resource, and resources all do the same thing.
+  # They are aliases for namespace.
+  namespace :grape_ape do
     get do
       'List grape apes!'
     end
+  end
 
+  group :grape_ape do
     get ':id' do
       'Show grape ape!'
     end
+  end
 
+  resource :grape_ape do
     post do
       'Create grape ape!'
     end
+  end
 
+  resources :grape_ape do
     put ':id' do
       'Update grape ape!'
     end
+  end
 
+  namespace :grape_ape do
     delete ':id' do
       'Destroy grape ape!'
     end
+  end
 end
