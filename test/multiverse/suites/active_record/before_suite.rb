903ed8f482cfbbe5a0b5b10d540de7986e138ed1
Jason R. Clark <jclark@newrelic.com>
2014-11-12 14:38:44 -0800

    Better error messages if failure during before_suite

--- test/multiverse/suites/active_record/before_suite.rb
+++ test/multiverse/suites/active_record/before_suite.rb
@@ -2,10 +2,15 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
-load 'Rakefile'
-
-Rake::Task['db:create'].invoke
-Rake::Task['db:migrate'].invoke
+begin
+  load 'Rakefile'
+  Rake::Task['db:create'].invoke
+  Rake::Task['db:migrate'].invoke
+rescue => e
+  puts e
+  puts e.backtrace.join("\n\t")
+  raise
+end
 
 class Minitest::Test
   def after_teardown
