ac0ca86fa542a69a1ffd8cd7aa1c26343b39170a
Ben Weintraub <ben@newrelic.com>
2014-11-19 22:06:24 -0800

    RUBY-1387 Allow passing of args to simulated Resque hooks

--- test/multiverse/suites/resque/resque_marshalling_test.rb
+++ test/multiverse/suites/resque/resque_marshalling_test.rb
@@ -18,21 +18,27 @@ class ResqueMarshallingTest < Minitest::Test
     extend Resque::Plugins::NewRelicInstrumentation
   end
 
-  def invoke_hook(name)
+  def invoke_hook(name, *args)
     hooks = Array(Resque.send(name))
-    hooks.each { |h| h.call }
+    hooks.each { |h| h.call(*args) }
   end
 
   def around_each
     invoke_hook(:before_first_fork)
-    invoke_hook(:before_fork)
+
+    # We just need something that responds to object_id here, because our
+    # Resque instrumentation uses that to assign a unique identifier to the pipe
+    # that will be used to send data about this job.
+    job = Object.new
+
+    invoke_hook(:before_fork, job)
 
     child_pid = Process.fork
 
     if child_pid
       Process.wait(child_pid)
     else
-      invoke_hook(:after_fork)
+      invoke_hook(:after_fork, job)
       DummyJob.around_perform_with_monitoring do
         yield
       end
