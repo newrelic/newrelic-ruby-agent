19716beb2678149dcde497a2f53e7631205a0cc5
Ben Weintraub <ben@newrelic.com>
2015-01-22 15:52:58 -0800

    RUBY-1404 Only filter params if we are going to capture them from grape instrumentation

--- lib/new_relic/agent/instrumentation/grape.rb
+++ lib/new_relic/agent/instrumentation/grape.rb
@@ -19,7 +19,7 @@ module NewRelic
         def handle_transaction(endpoint, class_name)
           return unless endpoint && route = endpoint.route
           name_transaction(route, class_name)
-          capture_params(endpoint)
+          capture_params(endpoint) if Agent.config[:capture_params]
         end
 
         def name_transaction(route, class_name)
