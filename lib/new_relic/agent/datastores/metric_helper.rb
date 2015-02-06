02df1ad80432a4f46f30a3667a5f829ea8a187e7
Jonan Scheffler <jonan@newrelic.com>
2015-02-06 14:00:23 -0800

    WIP

--- lib/new_relic/agent/datastores/metric_helper.rb
+++ lib/new_relic/agent/datastores/metric_helper.rb
@@ -9,6 +9,27 @@ module NewRelic
         def self.statement_metric_for(product, collection, operation)
           "Datastore/statement/#{product}/#{collection}/#{operation}"
         end
+
+        def self.operation_metric_for(product, operation)
+          "Datastore/operation/#{product}/#{operation}"
+        end
+
+        def self.context_metric
+          if NewRelic::Agent::Transaction.recording_web_transaction?
+            "Datastore/allWeb"
+          else
+            "Datastore/allOther"
+          end
+        end
+
+        def self.metrics_for(product, collection, operation)
+          [
+            "Datastore/all",
+            context_metric,
+            statement_metric_for(product, collection, operation),
+            operation_metric_for(product, operation)
+          ]
+        end
       end
     end
   end
--- test/new_relic/agent/datastores/metric_helper_test.rb
+++ test/new_relic/agent/datastores/metric_helper_test.rb
@@ -6,13 +6,82 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','test_h
 require 'new_relic/agent/datastores/metric_helper'
 
 class NewRelic::Agent::Datastores::MetricHelperTest < Minitest::Test
-  def test_statement_metric_for
-    product = "JonanDB"
-    collection = "wiggles"
-    operation = "select"
+  def setup
+    @product = "JonanDB"
+    @collection = "wiggles"
+    @operation = "select"
+  end
 
+  def test_statement_metric_for
     expected = "Datastore/statement/JonanDB/wiggles/select"
-    result = NewRelic::Agent::Datastores::MetricHelper.statement_metric_for(product, collection, operation)
+    result = NewRelic::Agent::Datastores::MetricHelper.statement_metric_for(@product, @collection, @operation)
     assert_equal expected, result
+  end
+
+  def test_operation_metric_for
+    expected = "Datastore/operation/JonanDB/select"
+    result = NewRelic::Agent::Datastores::MetricHelper.operation_metric_for(@product, @operation)
+    assert_equal expected, result
+  end
+
+  def test_context_metric_returns_web_for_web_context
+    NewRelic::Agent::Transaction.stubs(:recording_web_transaction?).returns(true)
+    expected = "Datastore/allWeb"
+    result = NewRelic::Agent::Datastores::MetricHelper.context_metric
+    assert_equal expected, result
+  end
+
+  def test_context_metric_returns_other_for_non_web_context
+    NewRelic::Agent::Transaction.stubs(:recording_web_transaction?).returns(false)
+    expected = "Datastore/allOther"
+    result = NewRelic::Agent::Datastores::MetricHelper.context_metric
+    assert_equal expected, result
+  end
+
+  def test_metrics_for_in_web_context
+    NewRelic::Agent::Transaction.stubs(:recording_web_transaction?).returns(true)
+    expected = [
+      "Datastore/all",
+      "Datastore/allWeb",
+      "Datastore/statement/JonanDB/wiggles/select",
+      "Datastore/operation/JonanDB/select"
+    ]
+
+    result = NewRelic::Agent::Datastores::MetricHelper.metrics_for(@product, @collection, @operation)
+    assert_equal expected, result
+  end
+
+  def test_metrics_for_outside_web_context
+    NewRelic::Agent::Transaction.stubs(:recording_web_transaction?).returns(false)
+    expected = [
+      "Datastore/all",
+      "Datastore/allOther",
+      "Datastore/statement/JonanDB/wiggles/select",
+      "Datastore/operation/JonanDB/select"
+    ]
+
+    result = NewRelic::Agent::Datastores::MetricHelper.metrics_for(@product, @collection, @operation)
+    assert_equal expected, result
+  end
+
+  def test_metric_from_activerecord_name
+    names = {
+      "Wiggles Load" => "Datastore/Wiggles/find",
+      "Wiggles Count" => "Datastore/Wiggles/find",
+      "Wiggles Exists" => "Datastore/Wiggles/find",
+      "Wiggles Indexes" => nil,
+      "Wiggles Columns" => nil,
+      "Wiggles Destroy" => "Datastore/Wiggles/destroy",
+      "Wiggles Find" => "Datastore/Wiggles/find",
+      "Wiggles Save" => "Datastore/Wiggles/save",
+      "Wiggles Create" => "Datastore/Wiggles/create",
+      "Wiggles Update" => "Datastore/Wiggles/save",
+      "Join Find" => "Datastore/Wiggles/find"
+    }
+
+    names.each do |name, metric|
+      result = ARHelperTest.
+      assert_equal
+    end
   end
 end
