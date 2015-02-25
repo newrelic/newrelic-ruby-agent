287c341e6d2d365c1edb0e7df0b001e8d9527817
Matthew Wear <mwear@newrelic.com>
2015-02-25 09:23:13 -0800

    RUBY-1440 Remove test for no longer needed method to convert sequel method names to sql operations

--- test/new_relic/agent/instrumentation/sequel_helper_test.rb
+++ test/new_relic/agent/instrumentation/sequel_helper_test.rb
@@ -26,27 +26,6 @@ module NewRelic
         default_result = NewRelic::Agent::Instrumentation::SequelHelper.product_name_from_adapter("YouDontKnowThisAdapter")
         assert_equal expected_default, default_result
       end
-
-      def test_operation_from_method_name
-        methods_to_operations = {
-          'all' => 'select',
-          'first' => 'select',
-          'get' => 'select',
-          'update' => 'update',
-          'update_all' => 'update',
-          'update_except' => 'update',
-          'update_fields' => 'update',
-          'update_only' => 'update',
-          'create' => 'insert',
-          'save' => 'insert',
-          'delete' => 'delete',
-          'destroy' => 'delete'
-        }
-
-        methods_to_operations.each_pair do |method_name, operation|
-          assert_equal operation, NewRelic::Agent::Instrumentation::SequelHelper.operation_from_method_name(method_name)
-        end
-      end
     end
   end
 end
