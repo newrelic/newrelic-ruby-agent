669ffb81440294986f3e79079ff8ac9115771404
Matthew Wear <mwear@newrelic.com>
2015-03-19 10:37:43 -0700

    RUBY-1415 Add stringified args to agent attributes for Resque and Sidekiq

--- lib/new_relic/agent/instrumentation/resque.rb
+++ lib/new_relic/agent/instrumentation/resque.rb
@@ -28,6 +28,7 @@ DependencyDetection.defer do
                 :category => 'OtherTransaction/ResqueJob') do
 
                 if NewRelic::Agent.config[:'resque.capture_params']
+                  NewRelic::Agent::Transaction.add_agent_attribute(:'job.resque.arguments', args.inspect)
                   NewRelic::Agent.add_custom_parameters(:job_arguments => args)
                 end
 
