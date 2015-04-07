642664b98c2052eb2af7c20b4e48df3e7f7a131d
Jason R. Clark <jclark@newrelic.com>
2015-04-07 09:59:22 -0700

    Don't let Sidekiq's server usurp our exit code

--- test/multiverse/suites/sidekiq/after_suite.rb
+++ test/multiverse/suites/sidekiq/after_suite.rb
@@ -2,6 +2,15 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
+module Sidekiq
+  class CLI
+    def exit(*args)
+      # No-op Sidekiq's exit since we don't want it shutting us down and eating
+      # our exit code
+    end
+  end
+end
+
 if defined?(SidekiqServer)
   SidekiqServer.instance.stop
 end
