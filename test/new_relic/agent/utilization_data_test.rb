396843380e61e46375df759947ae0f415032542f
Matthew Wear <mwear@newrelic.com>
2015-05-11 15:34:05 -0700

    RUBY-1472 Introduce cross agent tests for utilization data

--- test/new_relic/agent/utilization_data_test.rb
+++ test/new_relic/agent/utilization_data_test.rb
@@ -7,12 +7,37 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','data_container_t
 
 module NewRelic::Agent
   class UtilizationDataTest < Minitest::Test
-    # We don't behave like a normal container, but we need to match the
-    # interface at least!
-    include NewRelic::BasicDataContainerMethodTests
+    def setup
+      NewRelic::Agent.drop_buffered_data
+    end
 
-    def create_container
-      UtilizationData.new
+    test_cases = load_cross_agent_test("aws")
+    test_cases.each do |test_case|
+      define_method("test_#{test_case['testname'].gsub(/\W/, "_")}") do
+
+        Net::HTTP.stubs(:get).with(URI(test_case['uri'])).returns(test_case['response'])
+        utilization_data = NewRelic::Agent::UtilizationData.new
+        assert_equal test_case['expected'], utilization_data.instance_type
+
+        if test_case['metric']
+          test_case['metric'].each_pair do |metric_name, incoming_attributes|
+            expected_attributes = incoming_attributes.inject({}) do |memo, (k,v)|
+              memo[translate_method_name(k)] = v
+              memo
+            end
+
+            assert_metrics_recorded(metric_name => expected_attributes)
+          end
+        end
+      end
+    end
+
+    METHOD_LOOKUP = {
+      "callCount" => :call_count
+    }
+
+    def translate_method_name(method_name)
+      METHOD_LOOKUP[method_name]
     end
   end
 end
