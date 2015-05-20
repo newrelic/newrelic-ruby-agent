cb993690268c5ebbb37dfc8f5b5085fd38d361e7
Jonan Scheffler <jonan@newrelic.com>
2015-05-19 17:22:10 -0700

    RUBY-1478: Start Redis instrumentation.

---
+++ lib/new_relic/agent/instrumentation/redis.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+module NewRelic
+  module Agent
+    module Instrumentation
+      module Redis
+      end
+    end
+  end
+end
