99443e050f17206c40a2d36bb5ff39e05099c371
Matthew Wear <mwear@newrelic.com>
2015-08-17 17:13:18 -0700

    RUBY-1498 Update tests to fail
    In the initial pass for the mongo multiverse I adjusted assertions
    or used the skip method to make tests pass. In this commit I've
    changed the assertions to reflect what we ultimately want from
    the instrumentation. Many of these tests are failing, but making
    them pass will drive the instrumentation in the proper direction.

--- test/multiverse/suites/mongo/mongo2_instrumentation_test.rb
+++ test/multiverse/suites/mongo/mongo2_instrumentation_test.rb
@@ -36,121 +36,85 @@ if NewRelic::Agent::Datastores::Mongo.is_supported_version? &&
             @collection.drop
           end
 
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-675670a5c62e5a5db76ebb544a02fdef/insert
-          # expected: Datastore/statement/MongoDB/tribbles-675670a5c62e5a5db76ebb544a02fdef/insertOne
           def test_records_metrics_for_insert_one
             @collection.insert_one(@tribbles.first)
 
-            metrics = build_test_metrics(:insert)
-            #metrics = build_test_metrics(:insertOne)
+            metrics = build_test_metrics(:insertOne)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-675670a5c62e5a5db76ebb544a02fdef/insert
-          # expected: Datastore/statement/MongoDB/tribbles-675670a5c62e5a5db76ebb544a02fdef/insertMany
           def test_records_metrics_for_insert_many
             @collection.insert_many(@tribbles)
 
-            metrics = build_test_metrics(:insert)
-            #metrics = build_test_metrics(:insertMany)
+            metrics = build_test_metrics(:insertMany)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-675670a5c62e5a5db76ebb544a02fdef/delete
-          # expected: Datastore/statement/MongoDB/tribbles-675670a5c62e5a5db76ebb544a02fdef/deleteOne
           def test_records_metrics_for_delete_one
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
             @collection.delete_one(@tribbles.first)
 
-            metrics = build_test_metrics(:delete)
-            #metrics = build_test_metrics(:deleteOne)
+            metrics = build_test_metrics(:deleteOne)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-675670a5c62e5a5db76ebb544a02fdef/delete
-          # expected: Datastore/statement/MongoDB/tribbles-675670a5c62e5a5db76ebb544a02fdef/deleteMany
           def test_records_metrics_for_delete_many
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
             @collection.delete_many(@tribbles.first)
 
-            metrics = build_test_metrics(:delete)
-            #metrics = build_test_metrics(:deleteMany)
+            metrics = build_test_metrics(:deleteMany)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-abd925b0c0002184373d853a01e44b33/update
-          # expected: Datastore/statement/MongoDB/tribbles-abd925b0c0002184373d853a01e44b33/replaceOne
           def test_records_metrics_for_replace_one
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
             @collection.replace_one(@tribbles[0], @tribbles[1])
 
-            metrics = build_test_metrics(:update)
-            #metrics = build_test_metrics(:replaceOne)
-
+            metrics = build_test_metrics(:replaceOne)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-abd925b0c0002184373d853a01e44b33/update
-          # expected: Datastore/statement/MongoDB/tribbles-abd925b0c0002184373d853a01e44b33/updateOne
           def test_records_metrics_for_update_one
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
             @collection.update_one(@tribbles[0], "$set" => @tribbles[1])
 
-            metrics = build_test_metrics(:update)
-            #metrics = build_test_metrics(:updateOne)
-
+            metrics = build_test_metrics(:updateOne)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-abd925b0c0002184373d853a01e44b33/update
-          # expected: Datastore/statement/MongoDB/tribbles-abd925b0c0002184373d853a01e44b33/updateMany
           def test_records_metrics_for_update_many
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
             @collection.update_many(@tribbles[0], "$set" => @tribbles[1])
 
-            metrics = build_test_metrics(:update)
-            #metrics = build_test_metrics(:updateMany)
-
+            metrics = build_test_metrics(:updateMany)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-          # this test fails:
-          # metric recorded: Datastore/statement/MongoDB/{"name"=>"soterios johnson"}/find
-          # metric expected: Datastore/statement/MongoDB/tribbles-781f3c395d787b38400db0dd3bc96a05/find
           def test_records_metrics_for_find
-            skip "expected failure"
-
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
@@ -162,53 +126,37 @@ if NewRelic::Agent::Datastores::Mongo.is_supported_version? &&
             assert_metrics_recorded(expected)
           end
 
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-27ff948395d959eb0912a65537f7bac8/findandmodify
-          # expected: Datastore/statement/MongoDB/tribbles-27ff948395d959eb0912a65537f7bac8/findOneAndDelete
           def test_records_metrics_for_find_one_and_delete
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
             @collection.find_one_and_delete(@tribbles.first)
 
-            metrics = build_test_metrics(:findandmodify)
-            #metrics = build_test_metrics(:findOneAndDelete)
-
+            metrics = build_test_metrics(:findOneAndDelete)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-
-          # result is unexpected
-          # recorded: Datastore/statement/MongoDB/tribbles-211b12982e437ddf57028d5b76713156/findandmodify
-          # expected: Datastore/statement/MongoDB/tribbles-211b12982e437ddf57028d5b76713156/findOneAndReplace
           def test_records_metrics_for_find_one_and_replace
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
             @collection.find_one_and_replace(@tribbles[0], @tribbles[1])
 
-            metrics = build_test_metrics(:findandmodify)
-            #metrics = build_test_metrics(:findOneAndReplace)
-
+            metrics = build_test_metrics(:findOneAndReplace)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
           end
 
-          # result is unexpected
-          # recorded:  Datastore/statement/MongoDB/tribbles-6e9236b8e6a43e9b0a4e81b4f1119bf3/findandmodify
-          # expected: Datastore/statement/MongoDB/tribbles-6e9236b8e6a43e9b0a4e81b4f1119bf3/findOneAndUpdate
           def test_records_metrics_for_find_one_and_update
             @collection.insert_one(@tribbles.first)
             NewRelic::Agent.drop_buffered_data
 
             @collection.find_one_and_update(@tribbles[0], "$set" => @tribbles[1])
 
-            metrics = build_test_metrics(:findandmodify)
-            #metrics = build_test_metrics(:findOneAndUpdate)
-
+            metrics = build_test_metrics(:findOneAndUpdate)
             expected = metrics_with_attributes(metrics)
 
             assert_metrics_recorded(expected)
@@ -264,13 +212,7 @@ if NewRelic::Agent::Datastores::Mongo.is_supported_version? &&
             assert_metrics_recorded([[metric, "backed-up"]])
           end
 
-          # test fails
-          # expected: {:database=>"multiverse", :collection=>"tribbles-32fee7720f615346945a28fb0efc0ad6", :operation=>:insert}
-          # actual: "admin.insert {:insert=>\"tribbles-32fee7720f615346945a28fb0efc0ad6\", :documents=>[{\"name\"=>\"soterios johnson\", :_id=>BSON::ObjectId('55d21e63481a820761000003')}], :writeConcern=>{:w=>1}, :ordered=>true}"
-
           def test_notices_nosql
-            skip "expected failure"
-
             node = nil
 
             in_transaction do
@@ -290,11 +232,7 @@ if NewRelic::Agent::Datastores::Mongo.is_supported_version? &&
             assert_equal expected, result
           end
 
-          # test fails due to formatting of statement passed to
-          # NewRelic::Agent.instance.transaction_sampler.notice_nosql_statement
-
           def test_noticed_nosql_includes_operation
-           skip "expected failure"
             node = nil
 
             in_transaction do
@@ -307,12 +245,7 @@ if NewRelic::Agent::Datastores::Mongo.is_supported_version? &&
             assert_equal :insert, query[:operation]
           end
 
-          # test fails due to formatting of statement passed to
-          # NewRelic::Agent.instance.transaction_sampler.notice_nosql_statement
-
           def test_noticed_nosql_includes_update_one_operation
-            skip "expected failure"
-
             node = nil
 
             in_transaction do
@@ -326,11 +259,7 @@ if NewRelic::Agent::Datastores::Mongo.is_supported_version? &&
             assert_equal :update, query[:operation]
           end
 
-          # test fails due to formatting of statement passed to
-          # NewRelic::Agent.instance.transaction_sampler.notice_nosql_statement
-
           def test_noticed_nosql_includes_find_operation
-            skip "expected failure"
             node = nil
 
             in_transaction do
@@ -344,13 +273,7 @@ if NewRelic::Agent::Datastores::Mongo.is_supported_version? &&
             assert_equal :find, query[:operation]
           end
 
-          # test fails because the statement passed to
-          # NewRelic::Agent.instance.transaction_sampler.notice_nosql_statement
-          # is not a hash
-
           def test_noticed_nosql_does_not_contain_documents
-            skip "expected failure"
-
             node = nil
 
             in_transaction do
@@ -363,15 +286,7 @@ if NewRelic::Agent::Datastores::Mongo.is_supported_version? &&
             refute statement.keys.include?(:documents), "Noticed NoSQL should not include documents: #{statement}"
           end
 
-          # test fails because of the current formatting of the the statement passed to
-          # NewRelic::Agent.instance.transaction_sampler.notice_nosql_statement
-          # With previous versions of the instrumentation statement would appear as:
-          # {:operation=>:delete, :database=>"multiverse", :collection=>"tribbles-0d5370c959ce28e3673a4a2b87565c2c", :selector=>{"password"=>"?"}}
-          # with the subscriber it is:
-          # "admin.delete {:delete=>\"tribbles-5f586294aebe1fb82210854f58adc2ac\", :deletes=>[{:q=>{\"password\"=>\"$ecret\"}, :limit=>1}], :writeConcern=>{:w=>1}, :ordered=>true}"
           def test_noticed_nosql_does_not_contain_selector_values
-            skip "expected failure"
-
             @collection.insert_one({'password' => '$ecret'})
             node = nil
 
