dd1d432eade6ca60b2b842fd3a43b39947ec41bf
Matthew Wear <mwear@newrelic.com>
2015-08-21 14:00:47 -0700

    RUBY-1501 Populate transaction error event with fields from noticed error

--- lib/new_relic/agent/error_event_aggregator.rb
+++ lib/new_relic/agent/error_event_aggregator.rb
@@ -8,22 +8,33 @@ require 'new_relic/agent/event_buffer'
 module NewRelic
   module Agent
     class ErrorEventAggregator
+      EVENT_TYPE = "TransactionError".freeze
+
       def initialize
-        #capacity will come from config
         @lock = Mutex.new
+        #capacity will come from config
         @error_event_buffer = SampledBuffer.new(100)
       end
 
       def append_event noticed_error, transaction
         @lock.synchronize do
           @error_event_buffer.append_event do
-            generate_event(noticed_error, transaction)
+            event_for_collector(noticed_error, transaction)
           end
         end
       end
 
-      def generate_event noticed_error, transaction
-        [noticed_error, transaction]
+      def event_for_collector noticed_error, transaction
+        [
+          {
+            :type => EVENT_TYPE,
+            :errorClass => noticed_error.exception_class_name,
+            :errorMessage => noticed_error.message,
+            :timestamp => noticed_error.timestamp.to_f
+          },
+          {},
+          {}
+        ]
       end
 
       def harvest!
--- test/new_relic/agent/error_event_aggregrator_test.rb
+++ test/new_relic/agent/error_event_aggregrator_test.rb
@@ -11,6 +11,7 @@ module NewRelic
     class ErrorEventAggregatorTest < Minitest::Test
       def setup
         @error_event_aggregator = NewRelic::Agent::ErrorEventAggregator.new
+        freeze_time
       end
 
       def create_container
@@ -19,11 +20,25 @@ module NewRelic
 
       def populate_container(sampler, n)
         n.times do |i|
-          @error_event_aggregator.append_event i, i
+          error = NewRelic::NoticedError.new "Controller/blogs/index", RuntimeError.new("Big Controller")
+          transaction = nil
+          @error_event_aggregator.append_event error, transaction
         end
       end
 
       include NewRelic::DataContainerTests
+
+      def test_generates_event_from_error
+        error = NewRelic::NoticedError.new "Controller/blogs/index", RuntimeError.new("Big Controller")
+        @error_event_aggregator.append_event error, nil
+        errors = @error_event_aggregator.harvest!
+        intrinsics, *_ = errors.first
+
+        assert_equal "TransactionError", intrinsics[:type]
+        assert_equal Time.now.to_f, intrinsics[:timestamp]
+        assert_equal "RuntimeError", intrinsics[:errorClass]
+        assert_equal "Big Controller", intrinsics[:errorMessage]
+      end
     end
   end
 end
