53eb54c7460f58a12d7277178c0ed80640055b51
Matthew Wear <mwear@newrelic.com>
2015-09-10 16:43:55 -0700

    RUBY-1501 Add harvest_container! method to shared data container tests
    Add harvest_container! method to data container tests so that buffers
    that return samples mixed with metadata, such as the error event buffer,
    can override to harvest the container and return samples with the metadata
    extracted.

--- test/new_relic/data_container_tests.rb
+++ test/new_relic/data_container_tests.rb
@@ -29,10 +29,16 @@ module NewRelic
       self.respond_to?(:max_data_items) ? max_data_items : 5
     end
 
+    # containers containing data that is a mix of metadata and samples can override this
+    # method to return the samples harvested. Data containers that buffer
+    def harvest_container! c
+      c.harvest!
+    end
+
     def test_harvest_should_return_all_data_items
       c = create_container
       populate_container(c, num_data_items)
-      results = c.harvest!
+      results = harvest_container! c
       assert_equal(num_data_items, results.size)
     end
 
@@ -40,18 +46,20 @@ module NewRelic
       c = create_container
       populate_container(c, num_data_items)
 
-      c.harvest! # clears container
-      results = c.harvest!
+      harvest_container! c # clears container
+      results = harvest_container! c
       assert_equal(0, results.size)
     end
 
     def test_calling_harvest_after_re_populating_works
       container = create_container
       populate_container(container, num_data_items)
-      assert_equal(num_data_items, container.harvest!.size)
+      results = harvest_container! container
+      assert_equal(num_data_items, results.size)
 
       populate_container(container, num_data_items)
-      assert_equal(num_data_items, container.harvest!.size)
+      results = harvest_container! container
+      assert_equal(num_data_items, results.size)
     end
   end
 
@@ -62,14 +70,15 @@ module NewRelic
       c = create_container
       populate_container(c, 5)
       c.reset!
-      assert_equal(0, c.harvest!.size)
+      results = harvest_container! c
+      assert_equal(0, results.size)
     end
 
     def test_merge_should_re_integrate_items
       c = create_container
       populate_container(c, 5)
-      c.merge!(c.harvest!)
-      results = c.harvest!
+      c.merge!(harvest_container! c)
+      results = harvest_container! c
       assert_equal(5, results.size)
     end
   end
