b107748408b34920e01c5cece8d1db86262d083d
Karl Sandwich <ksandwich@newrelic.com>
2015-11-20 16:58:53 -0800

    RUBY-1482 Fix typo in legacy naming.

--- lib/new_relic/agent/instrumentation/delayed_job_instrumentation.rb
+++ lib/new_relic/agent/instrumentation/delayed_job_instrumentation.rb
@@ -31,7 +31,7 @@ module NewRelic
           # Older versions of Delayed Job use a semicolon-delimited string to stash the class name.
           # The format of this string is "LOAD;<class name>;<ORM ID>"
           def legacy_performable_method?(payload_object)
-            payload_object.object.is_a?(String) && payload_object.start_with?(LEGACY_DJ_FORMAT_PREFIX)
+            payload_object.object.is_a?(String) && payload_object.object.start_with?(LEGACY_DJ_FORMAT_PREFIX)
           end
 
           # If parsing for the class name fails, return a sensible default
