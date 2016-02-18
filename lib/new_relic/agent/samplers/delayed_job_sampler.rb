newrelic/newrelic-ruby-agent

commit 1e457bb7bd4875005ac8a7b15773a02f62b0f6cc

RUBY-1389 Avoid deprecation warnings on AR 4.0

lib/new_relic/agent/samplers/delayed_job_sampler.rb
@@ -31,12 +31,23 @@ module NewRelic
           NewRelic::Agent.record_metric("Workers/DelayedJob/locked_jobs", value)
         end
 
+        FAILED_QUERY = 'failed_at is not NULL'.freeze
+        LOCKED_QUERY = 'locked_by is not NULL'.freeze
+
         def failed_jobs
-          Delayed::Job.count(:conditions => 'failed_at is not NULL')
+          count(FAILED_QUERY)
         end
 
         def locked_jobs
-          Delayed::Job.count(:conditions => 'locked_by is not NULL')
+          count(LOCKED_QUERY)
+        end
+
+        def count(query)
+          if ActiveRecord::VERSION::MAJOR.to_i < 4
+            Delayed::Job.count(query)
+          else
+            Delayed::Job.where(query).count
+          end
         end
 
         def self.supported_on_this_platform?
@@ -75,7 +86,7 @@ module NewRelic
           # here that's valid on 2.x through 4.1, so split it up.
           result = if ActiveRecord::VERSION::MAJOR.to_i < 4
             Delayed::Job.count(:group => column,
-                                        :conditions => [QUEUE_QUERY_CONDITION, Time.now])
+                               :conditions => [QUEUE_QUERY_CONDITION, Time.now])
           else
             Delayed::Job.where(QUEUE_QUERY_CONDITION, Time.now).
                          group(column).

commit 630095cc8e011e05e689f209fb32917fa16e5960

Fix delayed jobs sampler query in AR3
This fixes a regression introduced by 1e457bb7

At least in postgresql `count(false)` returns 1, so the query that was
generated was always counting everything in the `delayed_jobs` table and
not just the non-null records as it is meant to do.

lib/new_relic/agent/samplers/delayed_job_sampler.rb
@@ -51,7 +51,7 @@ module NewRelic
 
         def count(query)
           if ::ActiveRecord::VERSION::MAJOR.to_i < 4
-            ::Delayed::Job.count(query)
+            ::Delayed::Job.count(conditions: query)
           else
             ::Delayed::Job.where(query).count
           end