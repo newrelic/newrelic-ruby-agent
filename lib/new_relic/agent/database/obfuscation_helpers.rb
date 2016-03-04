2d2dfeb25c9a96375d4e9e8dac12154751335d62
Katherine Wu <kwu@newrelic.com>
2016-03-03 18:41:54 -0800

    Only obfuscate single quote literal if needed, to avoid unneeded allocation

--- lib/new_relic/agent/database/obfuscation_helpers.rb
+++ lib/new_relic/agent/database/obfuscation_helpers.rb
@@ -50,6 +50,7 @@ module NewRelic
         FAILED_TO_OBFUSCATE_MESSAGE = "Failed to obfuscate SQL query - quote characters remained after obfuscation".freeze
 
         def obfuscate_single_quote_literals(sql)
+          return sql unless sql =~ COMPONENTS_REGEX_MAP[:single_quotes]
           sql.gsub(COMPONENTS_REGEX_MAP[:single_quotes], PLACEHOLDER)
         end
 
