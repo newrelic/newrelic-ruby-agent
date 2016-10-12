78f406fee276a51e0869e16763616b5180643dec
Katherine Wu <kwu@newrelic.com>
2016-10-12 15:30:05 -0700

    RUBY-1638 Update Redis instrumentation to use start_datastore_segment API

--- lib/new_relic/agent/instrumentation/redis.rb
+++ lib/new_relic/agent/instrumentation/redis.rb
@@ -32,14 +32,12 @@ DependencyDetection.defer do
         operation = args[0][0]
         statement = ::NewRelic::Agent::Datastores::Redis.format_command(args[0])
 
-        if statement
-          callback = Proc.new do |result, _, elapsed|
-            NewRelic::Agent::Datastores.notice_statement(statement, elapsed)
-          end
-        end
-
-        NewRelic::Agent::Datastores.wrap(NewRelic::Agent::Datastores::Redis::PRODUCT_NAME, operation, nil, callback) do
+        segment = NewRelic::Agent::Transaction.start_datastore_segment NewRelic::Agent::Datastores::Redis::PRODUCT_NAME, operation
+        begin
           call_without_new_relic(*args, &block)
+          segment.notice_nosql_statement statement if statement
+        ensure
+          segment.finish
         end
       end
 
@@ -50,20 +48,23 @@ DependencyDetection.defer do
         operation = pipeline.is_a?(::Redis::Pipeline::Multi) ? NewRelic::Agent::Datastores::Redis::MULTI_OPERATION : NewRelic::Agent::Datastores::Redis::PIPELINE_OPERATION
         statement = ::NewRelic::Agent::Datastores::Redis.format_pipeline_commands(pipeline.commands)
 
-        callback = Proc.new do |result, _, elapsed|
-          NewRelic::Agent::Datastores.notice_statement(statement, elapsed)
-        end
-
-        NewRelic::Agent::Datastores.wrap(NewRelic::Agent::Datastores::Redis::PRODUCT_NAME, operation, nil, callback) do
+        segment = NewRelic::Agent::Transaction.start_datastore_segment NewRelic::Agent::Datastores::Redis::PRODUCT_NAME, operation
+        begin
           call_pipeline_without_new_relic(*args, &block)
+          segment.notice_nosql_statement statement
+        ensure
+          segment.finish
         end
       end
 
       alias_method :connect_without_new_relic, :connect
 
       def connect(*args, &block)
-        NewRelic::Agent::Datastores.wrap(NewRelic::Agent::Datastores::Redis::PRODUCT_NAME, NewRelic::Agent::Datastores::Redis::CONNECT) do
+        segment = NewRelic::Agent::Transaction.start_datastore_segment NewRelic::Agent::Datastores::Redis::PRODUCT_NAME, NewRelic::Agent::Datastores::Redis::CONNECT
+        begin
           connect_without_new_relic(*args, &block)
+        ensure
+          segment.finish
         end
       end
     end
