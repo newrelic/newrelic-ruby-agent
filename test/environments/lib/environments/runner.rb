b909e4945b6b59068f8d47f7d0889d3323065717
Kenichi Nakamura <knakamura@newrelic.com>
2016-11-10 14:01:56 -0800

    back to with_original_env with ENV.delete_if added

--- test/environments/lib/environments/runner.rb
+++ test/environments/lib/environments/runner.rb
@@ -43,7 +43,8 @@ module Environments
 
       puts yellow("Tests to run:\n\t#{tests_to_run.map{|s|s.gsub(env_root + "/", "")}.join("\n\t")}")
       tests_to_run.each do |dir|
-        Bundler.with_clean_env do
+        Bundler.with_original_env do
+          ENV.delete_if {|k, _| k[0, 7] == "BUNDLE_" }
           dir = File.expand_path(dir)
           puts "", yellow("Running tests for #{dir}")
           status = bundle(dir)
