dd2c573267bec6eb269505d768974c5df0e2916e
Matthew Wear <mwear@newrelic.com>
2016-12-21 17:50:41 -0800

    RUBY-1689 Use in_transaction blocks for mongo test cases
    We are making changes to the agent so that agent-based instrumentation
    will not record metrics outside of a tranaction. This commit updates
    mongo multiverse tests that expect metrics to be recorded in
    in_transaction blocks.

--- test/multiverse/suites/mongo/helpers/mongo_operation_tests.rb
+++ test/multiverse/suites/mongo/helpers/mongo_operation_tests.rb
@@ -4,7 +4,9 @@
 
 module MongoOperationTests
   def test_records_metrics_for_insert
-    @collection.insert(@tribble)
+    in_transaction do
+      @collection.insert(@tribble)
+    end
 
     metrics = build_test_metrics(:insert)
     expected = metrics_with_attributes(metrics)
@@ -16,7 +18,9 @@ module MongoOperationTests
     @collection.insert(@tribble)
     NewRelic::Agent.drop_buffered_data
 
-    @collection.find(@tribble).to_a
+    in_transaction do
+      @collection.find(@tribble).to_a
+    end
 
     metrics = build_test_metrics(:find)
     expected = metrics_with_attributes(metrics)
@@ -28,7 +32,9 @@ module MongoOperationTests
     @collection.insert(@tribble)
     NewRelic::Agent.drop_buffered_data
 
-    @collection.find_one
+    in_transaction do
+      @collection.find_one
+    end
 
     metrics = build_test_metrics(:findOne)
     expected = metrics_with_attributes(metrics)
@@ -40,7 +46,9 @@ module MongoOperationTests
     @collection.insert(@tribble)
     NewRelic::Agent.drop_buffered_data
 
-    @collection.remove(@tribble).to_a
+    in_transaction do
+      @collection.remove(@tribble).to_a
+    end
 
     metrics = build_test_metrics(:remove)
     expected = metrics_with_attributes(metrics)
@@ -49,7 +57,9 @@ module MongoOperationTests
   end
 
   def test_records_metrics_for_save
-    @collection.save(@tribble)
+    in_transaction do
+      @collection.save(@tribble)
+    end
 
     metrics = build_test_metrics(:save)
     expected = metrics_with_attributes(metrics)
@@ -58,7 +68,9 @@ module MongoOperationTests
   end
 
   def test_save_does_not_record_insert
-    @collection.save(@tribble)
+    in_transaction do
+      @collection.save(@tribble)
+    end
 
     metrics = build_test_metrics(:save)
     metrics_with_attributes(metrics)
@@ -70,7 +82,9 @@ module MongoOperationTests
     updated = @tribble.dup
     updated['name'] = 'codemonkey'
 
-    @collection.update(@tribble, updated)
+    in_transaction do
+      @collection.update(@tribble, updated)
+    end
 
     metrics = build_test_metrics(:update)
     expected = metrics_with_attributes(metrics)
@@ -79,7 +93,9 @@ module MongoOperationTests
   end
 
   def test_records_metrics_for_distinct
-    @collection.distinct('name')
+    in_transaction do
+      @collection.distinct('name')
+    end
 
     metrics = build_test_metrics(:distinct)
     expected = metrics_with_attributes(metrics)
@@ -88,7 +104,9 @@ module MongoOperationTests
   end
 
   def test_records_metrics_for_count
-    @collection.count
+    in_transaction do
+      @collection.count
+    end
 
     metrics = build_test_metrics(:count)
     expected = metrics_with_attributes(metrics)
@@ -98,9 +116,11 @@ module MongoOperationTests
 
   def test_records_metrics_for_group
     begin
-      @collection.group({:key => "name",
-                        :initial => {:count => 0},
-                        :reduce => "function(k,v) { v.count += 1; }" })
+      in_transaction do
+        @collection.group({:key => "name",
+                           :initial => {:count => 0},
+                           :reduce => "function(k,v) { v.count += 1; }" })
+      end
     rescue Mongo::OperationFailure
       # We get occasional group failures, but should still record metrics
     end
@@ -114,7 +134,10 @@ module MongoOperationTests
   def test_records_metrics_for_find_and_modify
     updated = @tribble.dup
     updated['name'] = 'codemonkey'
-    @collection.find_and_modify(:query => @tribble, :update => updated)
+
+    in_transaction do
+      @collection.find_and_modify(:query => @tribble, :update => updated)
+    end
 
     metrics = build_test_metrics(:findAndModify)
     expected = metrics_with_attributes(metrics)
@@ -123,7 +146,9 @@ module MongoOperationTests
   end
 
   def test_records_metrics_for_find_and_remove
-    @collection.find_and_modify(:query => @tribble, :remove =>true)
+    in_transaction do
+      @collection.find_and_modify(:query => @tribble, :remove =>true)
+    end
 
     metrics = build_test_metrics(:findAndRemove)
     expected = metrics_with_attributes(metrics)
@@ -132,7 +157,9 @@ module MongoOperationTests
   end
 
   def test_records_metrics_for_create_index
-    @collection.create_index([[unique_field_name, Mongo::ASCENDING]])
+    in_transaction do
+      @collection.create_index([[unique_field_name, Mongo::ASCENDING]])
+    end
 
     # The createIndexes command was added to the mongo server in version 2.6.
     # As of version 1.10.0 of the Ruby driver, the driver will attempt to
@@ -163,7 +190,9 @@ module MongoOperationTests
   end
 
   def test_records_metrics_for_ensure_index
-    @collection.ensure_index([[unique_field_name, Mongo::ASCENDING]])
+    in_transaction do
+      @collection.ensure_index([[unique_field_name, Mongo::ASCENDING]])
+    end
 
     metrics = build_test_metrics(:ensureIndex)
     expected = metrics_with_attributes(metrics)
@@ -172,7 +201,9 @@ module MongoOperationTests
   end
 
   def test_records_metrics_for_ensure_index_with_symbol
-    @collection.ensure_index(unique_field_name.to_sym)
+    in_transaction do
+      @collection.ensure_index(unique_field_name.to_sym)
+    end
 
     metrics = build_test_metrics(:ensureIndex)
     expected = metrics_with_attributes(metrics)
@@ -181,7 +212,9 @@ module MongoOperationTests
   end
 
   def test_records_metrics_for_ensure_index_with_string
-    @collection.ensure_index(unique_field_name)
+    in_transaction do
+      @collection.ensure_index(unique_field_name)
+    end
 
     metrics = build_test_metrics(:ensureIndex)
     expected = metrics_with_attributes(metrics)
@@ -190,23 +223,28 @@ module MongoOperationTests
   end
 
   def test_ensure_index_does_not_record_insert
-    @collection.ensure_index([[unique_field_name, Mongo::ASCENDING]])
+    in_transaction do
+      @collection.ensure_index([[unique_field_name, Mongo::ASCENDING]])
+    end
 
     assert_metrics_not_recorded(['Datastore/operation/MongoDB/insert'])
   end
 
   def test_ensure_index_does_call_ensure_index
     options = [[unique_field_name, Mongo::ASCENDING]]
-
-    @collection.expects(:ensure_index_without_new_relic_trace).with(options, any_parameters).once
-    @collection.ensure_index(options)
+    in_transaction do
+      @collection.expects(:ensure_index_without_new_relic_trace).with(options, any_parameters).once
+      @collection.ensure_index(options)
+    end
   end
 
   def test_records_metrics_for_drop_index
     name =  @collection.create_index([[unique_field_name, Mongo::ASCENDING]])
     NewRelic::Agent.drop_buffered_data
 
-    @collection.drop_index(name)
+    in_transaction do
+      @collection.drop_index(name)
+    end
 
     metrics = build_test_metrics(:dropIndex)
     expected = metrics_with_attributes(metrics)
@@ -218,7 +256,9 @@ module MongoOperationTests
     @collection.create_index([[unique_field_name, Mongo::ASCENDING]])
     NewRelic::Agent.drop_buffered_data
 
-    @collection.drop_indexes
+    in_transaction do
+      @collection.drop_indexes
+    end
 
     metrics = build_test_metrics(:dropIndexes)
     expected = metrics_with_attributes(metrics)
@@ -230,7 +270,9 @@ module MongoOperationTests
     @collection.create_index([[unique_field_name, Mongo::ASCENDING]])
     NewRelic::Agent.drop_buffered_data
 
-    @database.command({ :reIndex => @collection_name })
+    in_transaction do
+      @database.command({ :reIndex => @collection_name })
+    end
 
     metrics = build_test_metrics(:reIndex)
     expected = metrics_with_attributes(metrics)
@@ -241,7 +283,9 @@ module MongoOperationTests
   def test_rename_collection
     ensure_collection_exists
 
-    @collection.rename("renamed_#{@collection_name}")
+    in_transaction do
+      @collection.rename("renamed_#{@collection_name}")
+    end
 
     metrics = build_test_metrics(:renameCollection)
     expected = metrics_with_attributes(metrics)
@@ -254,7 +298,9 @@ module MongoOperationTests
   def test_rename_collection_via_db
     ensure_collection_exists
 
-    @database.rename_collection(@collection_name, "renamed_#{@collection_name}")
+    in_transaction do
+      @database.rename_collection(@collection_name, "renamed_#{@collection_name}")
+    end
 
     metrics = build_test_metrics(:renameCollection)
     expected = metrics_with_attributes(metrics)
@@ -267,7 +313,9 @@ module MongoOperationTests
   def test_drop_collection
     ensure_collection_exists
 
-    @database.drop_collection(@collection_name)
+    in_transaction do
+      @database.drop_collection(@collection_name)
+    end
 
     metrics = build_test_metrics(:drop)
     expected = metrics_with_attributes(metrics)
@@ -278,7 +326,10 @@ module MongoOperationTests
   def test_collstats
     @collection.insert(@tribble)
     NewRelic::Agent.drop_buffered_data
-    @collection.stats
+
+    in_transaction do
+      @collection.stats
+    end
 
     metrics = build_test_metrics(:collstats)
     expected = metrics_with_attributes(metrics)
@@ -435,7 +486,9 @@ module MongoOperationTests
 
   def test_web_requests_record_all_web_metric
     NewRelic::Agent::Transaction.stubs(:recording_web_transaction?).returns(true)
-    @collection.insert(@tribble)
+    in_web_transaction do
+      @collection.insert(@tribble)
+    end
 
     metrics = build_test_metrics(:insert)
     expected = metrics_with_attributes(metrics)
@@ -445,14 +498,20 @@ module MongoOperationTests
 
   def test_web_requests_do_not_record_all_other_metric
     NewRelic::Agent::Transaction.stubs(:recording_web_transaction?).returns(true)
-    @collection.insert(@tribble)
+
+    in_web_transaction do
+      @collection.insert(@tribble)
+    end
 
     assert_metrics_not_recorded(['Datastore/allOther'])
   end
 
   def test_other_requests_record_all_other_metric
     NewRelic::Agent::Transaction.stubs(:recording_web_transaction?).returns(false)
-    @collection.insert(@tribble)
+
+    in_background_transaction do
+      @collection.insert(@tribble)
+    end
 
     metrics = build_test_metrics(:insert)
     expected = metrics_with_attributes(metrics)
@@ -462,7 +521,10 @@ module MongoOperationTests
 
   def test_other_requests_do_not_record_all_web_metric
     NewRelic::Agent::Transaction.stubs(:recording_web_transaction?).returns(false)
-    @collection.insert(@tribble)
+
+    in_background_transaction do
+      @collection.insert(@tribble)
+    end
 
     assert_metrics_not_recorded(['Datastore/allWeb'])
   end
