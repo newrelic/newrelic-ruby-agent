9b804c4c59c12e090f393feb175b8020f191d229
Matthew Wear <mwear@newrelic.com>
2017-01-05 16:18:16 -0800

    RUBY-1690 Update #! to use /usr/bin/env ruby

--- test/script/external_server.rb
+++ test/script/external_server.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 # encoding: utf-8
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
