6b6daf030ed4bafbaea851d54ca7774296a1e442
Kenichi Nakamura <knakamura@newrelic.com>
2017-02-21 13:57:13 -0800

    RUBY-1669 reintegrate dependency detection "gem"

--- vendor/gems/dependency_detection-0.0.1.build/lib/dependency_detection/version.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-# This file is distributed under New Relic's license terms.
-# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
-
-module DependencyDetection
-  VERSION='0.0.1.build'
-end
