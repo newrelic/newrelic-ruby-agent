25cfe1efbbf50a292887b184b38c3f2402cd6ded
Matthew Wear <mwear@newrelic.com>
2017-03-07 15:48:41 -0800

    RUBY-1724 Defer recording metrics for initial_segment

--- lib/new_relic/agent/transaction.rb
+++ lib/new_relic/agent/transaction.rb
@@ -413,6 +413,7 @@ module NewRelic
       def create_initial_segment name
         @initial_segment = create_segment @default_name
         @initial_segment.record_scoped_metric = false
+        @initial_segment.record_metrics = false
         @initial_segment
       end
 
@@ -488,7 +489,7 @@ module NewRelic
           outermost_frame.name = @frozen_name
         end
 
-        outermost_frame.finish
+        @initial_segment.record_metrics
 
         NewRelic::Agent::BusyCalculator.dispatcher_finish(end_time)
 
