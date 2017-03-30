4f356dace49bcde8a277381d0bdedf621d448544
Katherine Wu <kwu@newrelic.com>
2017-03-30 14:05:55 -0700

    Add comments to look RUBY-1749 [ci skip]

--- test/multiverse/lib/multiverse/runner.rb
+++ test/multiverse/lib/multiverse/runner.rb
@@ -96,12 +96,15 @@ module Multiverse
       "rest"          => []  # Specially handled below
     }
 
+    # Would like to reinstate but requires investigation, see RUBY-1749
     unless RUBY_VERSION >= '2.1' and RUBY_VERSION < '2.3'
       GROUPS['background_2'] << 'rake'
     end
 
     def passes_filter?(dir, filter)
       return true if filter.nil?
+
+      # Would like to reinstate but requires investigation, see RUBY-1749
       return false if dir == 'rake' and RUBY_VERSION >= '2.1' and RUBY_VERSION < '2.3'
 
       if filter.include?("group=")
