446194938666074762630d6a273708d09c824dac
Matthew Wear <mwear@newrelic.com>
2017-05-12 17:02:25 -0700

    RUBY-1742 Migrations are versioned in Rails 5+

--- test/multiverse/suites/active_record/db/migrate/20141105131800_create_users_and_aliases.rb
+++ test/multiverse/suites/active_record/db/migrate/20141105131800_create_users_and_aliases.rb
@@ -2,7 +2,7 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
-class CreateUsersAndAliases < ActiveRecord::Migration
+class CreateUsersAndAliases < ActiveRecord::VERSION::STRING >= "5.0.0" ? ActiveRecord::Migration[5.0] : ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string  :name
