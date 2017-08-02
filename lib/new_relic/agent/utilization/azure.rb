f12e60a9566a32871880ceeff2609a4f41f0cbe5
Dana Scheider <dscheider@newrelic.com>
2017-08-02 13:02:45 -0700

    RUBY-1794: Fix boolean literal true to String 'true'

--- lib/new_relic/agent/utilization/azure.rb
+++ lib/new_relic/agent/utilization/azure.rb
@@ -8,7 +8,7 @@ module NewRelic
       class Azure < Vendor
         vendor_name "azure"
         endpoint "http://169.254.169.254/metadata/instance/compute?api-version=2017-03-01"
-        headers "Metadata" => true
+        headers "Metadata" => "true"
         keys ["vmId", "name", "vmSize", "location"]
         key_transforms :to_sym
       end
