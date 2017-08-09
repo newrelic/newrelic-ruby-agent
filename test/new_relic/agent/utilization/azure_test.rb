a6734c3b510db566746f4748c44a904c423c6843
Kenichi Nakamura <knakamura@newrelic.com>
2017-08-09 11:02:43 -0700

    RUBY-1802 add Azure specific cross agent tests and driver

--- test/new_relic/agent/utilization/azure_test.rb
+++ test/new_relic/agent/utilization/azure_test.rb
@@ -3,7 +3,7 @@
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
 require File.expand_path('../../../../test_helper', __FILE__)
-require 'new_relic/agent/utilization/gcp'
+require 'new_relic/agent/utilization/azure'
 
 module NewRelic
   module Agent
@@ -16,6 +16,8 @@ module NewRelic
         def teardown
           NewRelic::Agent.drop_buffered_data
         end
+
+        # ---
 
         def test_generates_expected_collector_hash_for_valid_response
           fixture = File.read File.join(azure_fixture_path, "valid.json")
@@ -66,8 +68,52 @@ module NewRelic
           refute_metrics_recorded "Supportability/utilization/azure/error"
         end
 
+        # ---
+
         def azure_fixture_path
           File.expand_path('../../../../fixtures/utilization/azure', __FILE__)
+        end
+
+        # ---
+
+        load_cross_agent_test("utilization_vendor_specific/azure").each do |test_case|
+          test_case = symbolize_keys_in_object test_case
+
+          define_method("test_#{test_case[:testname]}".gsub(" ", "_")) do
+            uri_obj = test_case[:uri][:'http://169.254.169.254/metadata/instance/compute?api-version=2017-03-01']
+            if uri_obj[:timeout]
+              @vendor.stubs(:request_metadata).returns(nil)
+            else
+              response = mock code: '200', body: ::JSON.dump(uri_obj[:response])
+              @vendor.stubs(:request_metadata).returns(response)
+            end
+
+            # TravisCI may run these tests in a docker environment, which means we get an unexpected docker
+            # id in the vendors hash.
+            with_config :'utilization.detect_docker' => false do
+              detection = @vendor.detect
+
+              expected = test_case[:expected_vendors_hash].nil? ? {azure: {}} : test_case[:expected_vendors_hash]
+              assert_equal expected, {azure: @vendor.metadata}
+
+              if test_case[:expected_metrics]
+                test_case[:expected_metrics].each do |metric,v|
+                  if v[:call_count] == 0
+                    if uri_obj[:timeout]
+                      refute detection, '@vendor.detect should have returned false'
+                    else
+                      assert detection, '@vendor.detect should have returned truthy'
+                    end
+                    assert_metrics_not_recorded [metric.to_s]
+                  else
+                    refute detection, '@vendor.detect should have returned false'
+                    assert_metrics_recorded [metric.to_s]
+                  end
+                end
+              end
+            end
+          end
+
         end
       end
     end
