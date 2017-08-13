f7ddd9799e298ce70b7678e41a3d0595fdf6cbd6
Matthew Wear <mwear@newrelic.com>
2017-08-12 20:17:55 -0700

    RUBY-1806 Multiverse test for ThroughputMonitor

---
+++ test/multiverse/suites/agent_only/throughput_monitor_test.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+
+module NewRelic
+  module Agent
+    class ThroughputMonitorTest < Minitest::Test
+
+      include MultiverseHelpers
+
+      setup_and_teardown_agent
+
+      def test_throughput_monitor_valid_stats_and_reset_after_harvest
+        sampled_count = 0
+        20.times do |i|
+          in_transaction("test_txn_#{i}") do |txn|
+            sampled_count += 1 if txn.collect_sample?
+          end
+        end
+
+        stats = NewRelic::Agent.instance.throughput_monitor.stats
+        assert_equal 0, stats[:seen_last]
+        assert_equal 20, stats[:seen]
+        assert_equal sampled_count, stats[:sampled_count]
+
+        run_harvest
+
+        stats = NewRelic::Agent.instance.throughput_monitor.stats
+        assert_equal 0, stats[:seen]
+        assert_equal 20, stats[:seen_last]
+      end
+    end
+  end
+end
