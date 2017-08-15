f930fcd71396f59fb8c1ee4a099f7cc7de6f47c6
Matthew Wear <mwear@newrelic.com>
2017-08-15 16:12:32 -0700

    RUBY-1806 Channel sampled events through append_sampled for priority treatment

--- lib/new_relic/agent/transaction_event_recorder.rb
+++ lib/new_relic/agent/transaction_event_recorder.rb
@@ -22,7 +22,9 @@ module NewRelic
       def record payload
         return unless NewRelic::Agent.config[:'analytics_events.enabled']
 
-        if synthetics_event? payload
+        if sampled_event? payload
+          transaction_event_aggregator.append_sampled { create_event(payload) }
+        elsif synthetics_event? payload
           event = create_event payload
           _, rejected = synthetics_event_aggregator.append_or_reject event
           transaction_event_aggregator.append event if rejected
@@ -37,6 +39,12 @@ module NewRelic
 
       def synthetics_event? payload
         payload.key? :synthetics_resource_id
+      end
+
+      SAMPLED_KEY = 'nr.sampled'
+
+      def sampled_event? payload
+        !!payload[SAMPLED_KEY]
       end
 
       def drop_buffered_data
