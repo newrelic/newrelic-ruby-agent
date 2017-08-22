4368eaec9f5f8ab2e5ae7a8251d5d56e6d19a4df
Dana Scheider <dscheider@newrelic.com>
2017-08-22 13:25:04 -0700

    RUBY-1787: Add needed require

--- lib/new_relic/agent/instrumentation/active_record_4.rb
+++ lib/new_relic/agent/instrumentation/active_record_4.rb
@@ -2,6 +2,7 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 require 'new_relic/agent/instrumentation/active_record_subscriber'
+require 'new_relic/agent/prepend_supportability'
 
 DependencyDetection.defer do
   named :active_record_4
