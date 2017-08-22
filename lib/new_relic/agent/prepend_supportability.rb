c4aef97ef0b95658798b4574fac8386e56265813
Dana Scheider <dscheider@newrelic.com>
2017-08-22 13:25:23 -0700

    RUBY-1787: Fix NoMethodError

--- lib/new_relic/agent/prepend_supportability.rb
+++ lib/new_relic/agent/prepend_supportability.rb
@@ -3,7 +3,7 @@ module NewRelic
     module PrependSupportability
       def self.record_metrics_for *classes
         classes.each do |klass|
-          count = klass.send(:ancestors).index_of(klass)
+          count = klass.send(:ancestors).index(klass)
           ::NewRelic::Agent.record_metric("Supportability/PrependedModules/#{klass}", count)
         end
       end
