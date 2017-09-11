e2373cebd013eb13225f0da2462d2652384c93b1
Kenichi Nakamura <knakamura@newrelic.com>
2017-09-11 13:24:55 -0700

    RUBY-1787 fix for private prepend on < 2.1

--- test/multiverse/suites/rails_prepend/newrelic_prepender/lib/newrelic_prepender.rb
+++ test/multiverse/suites/rails_prepend/newrelic_prepender/lib/newrelic_prepender.rb
@@ -6,35 +6,42 @@ require "newrelic_prepender/version"
 
 module NewRelic
   module Prepender
+    def self.do_prepend base
+      if RUBY_VERSION >= '2.1.0'
+        base.prepend NewRelic::Prepender
+      else
+        base.__send__ :prepend, NewRelic::Prepender
+      end
+    end
   end
 end
 
 require 'action_controller'
 
-::ActionController::Base.prepend NewRelic::Prepender
+NewRelic::Prepender.do_prepend ::ActionController::Base
 
 if ::Rails::VERSION::MAJOR.to_i == 5
-  ::ActionController::API.prepend NewRelic::Prepender
+  NewRelic::Prepender.do_prepend ::ActionController::API
 end
 
 require 'action_view'
 
-::ActionView::Base.prepend NewRelic::Prepender
-::ActionView::Template.prepend NewRelic::Prepender
-::ActionView::Renderer.prepend NewRelic::Prepender
+NewRelic::Prepender.do_prepend ::ActionView::Base
+NewRelic::Prepender.do_prepend ::ActionView::Template
+NewRelic::Prepender.do_prepend ::ActionView::Renderer
 
 if ::Rails::VERSION::MAJOR.to_i == 5
   require 'action_cable/engine'
 
-  ::ActionCable::Engine.prepend NewRelic::Prepender
-  ::ActionCable::RemoteConnections.prepend NewRelic::Prepender
+  NewRelic::Prepender.do_prepend ::ActionCable::Engine
+  NewRelic::Prepender.do_prepend ::ActionCable::RemoteConnections
 
   require 'active_job'
 
-  ::ActiveJob::Base.prepend NewRelic::Prepender
+  NewRelic::Prepender.do_prepend ::ActiveJob::Base
 end
 
 require 'active_record'
 
-::ActiveRecord::Base.prepend NewRelic::Prepender
-::ActiveRecord::Relation.prepend NewRelic::Prepender
+NewRelic::Prepender.do_prepend ::ActiveRecord::Base
+NewRelic::Prepender.do_prepend ::ActiveRecord::Relation
