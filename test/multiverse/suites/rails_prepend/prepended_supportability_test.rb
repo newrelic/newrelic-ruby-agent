426c3a9627161f56ff3073a779d693b8d2c30fe9
Kenichi Nakamura <knakamura@newrelic.com>
2017-09-13 11:11:00 -0700

    RUBY-1787 fix for rails 5.0 prepending anon mods

--- test/multiverse/suites/rails_prepend/prepended_supportability_test.rb
+++ test/multiverse/suites/rails_prepend/prepended_supportability_test.rb
@@ -10,108 +10,54 @@ class PrependedSupportabilityMetricsTest < Minitest::Test
   def test_action_view_prepended_metrics
     assert_metrics_recorded({
 
-      # these values are different because haml prepends a module on
-      # ActionView::Base as well.
+      # haml prepends a module on ActionView::Base
       #
-      "Supportability/PrependedModules/ActionView::Base" => {
-        call_count: 1,
-        max_call_time: 2,
-        min_call_time: 2,
-        sum_of_squares: 4.0,
-        total_call_time: 2.0,
-        total_exclusive_time: 2.0
-      },
+      "Supportability/PrependedModules/ActionView::Base" => metric_values_for(2),
 
-      "Supportability/PrependedModules/ActionView::Template" => {
-        call_count: 1,
-        max_call_time: 1,
-        min_call_time: 1,
-        sum_of_squares: 1.0,
-        total_call_time: 1.0,
-        total_exclusive_time: 1.0
-      },
-      "Supportability/PrependedModules/ActionView::Renderer" => {
-        call_count: 1,
-        max_call_time: 1,
-        min_call_time: 1,
-        sum_of_squares: 1.0,
-        total_call_time: 1.0,
-        total_exclusive_time: 1.0
-      }
+      "Supportability/PrependedModules/ActionView::Template" => metric_values_for(1),
+      "Supportability/PrependedModules/ActionView::Renderer" => metric_values_for(1)
     })
   end
 
   def test_action_contoller_prepended_metrics
     metrics = ["Supportability/PrependedModules/ActionController::Base"]
     metrics << "Supportability/PrependedModules/ActionController::API" if ::Rails::VERSION::MAJOR.to_i == 5
-    assert_metrics_recorded(metrics.reduce({}) do |h,m|
-      h[m] = {
-        call_count: 1,
-        max_call_time: 1,
-        min_call_time: 1,
-        sum_of_squares: 1.0,
-        total_call_time: 1.0,
-        total_exclusive_time: 1.0
-      }
-      h
-    end)
+    assert_metrics_recorded(metrics.reduce({}) {|h,m| h[m] = metric_values_for(1); h})
   end
 
   if ::Rails::VERSION::MAJOR.to_i == 5
     def test_action_cable_prepended_metrics
       assert_metrics_recorded({
-        "Supportability/PrependedModules/ActionCable::Engine" => {
-          call_count: 1,
-          max_call_time: 1,
-          min_call_time: 1,
-          sum_of_squares: 1.0,
-          total_call_time: 1.0,
-          total_exclusive_time: 1.0
-        },
-        "Supportability/PrependedModules/ActionCable::RemoteConnections" => {
-          call_count: 1,
-          max_call_time: 1,
-          min_call_time: 1,
-          sum_of_squares: 1.0,
-          total_call_time: 1.0,
-          total_exclusive_time: 1.0
-        }
+        "Supportability/PrependedModules/ActionCable::Engine" => metric_values_for(1),
+        "Supportability/PrependedModules/ActionCable::RemoteConnections" => metric_values_for(1)
       })
     end
 
     def test_active_job_prepended_metrics
-      assert_metrics_recorded({
-        "Supportability/PrependedModules/ActiveJob::Base" => {
-        call_count: 1,
-        max_call_time: 1,
-        min_call_time: 1,
-        sum_of_squares: 1.0,
-        total_call_time: 1.0,
-        total_exclusive_time: 1.0
-      }
-      })
+      assert_metrics_recorded({ "Supportability/PrependedModules/ActiveJob::Base" => metric_values_for(1) })
     end
   end
 
   def test_active_record_prepended_metrics
+
+    # rails 5.0 prepends an anonymous module on to AR::Relation
+    #
+    val = 1
+    val += 1 if ::Rails::VERSION::MAJOR.to_i == 5 and ::Rails::VERSION::MINOR.to_i == 0
+
     assert_metrics_recorded({
-      "Supportability/PrependedModules/ActiveRecord::Base" => {
-        call_count: 1,
-        max_call_time: 1,
-        min_call_time: 1,
-        sum_of_squares: 1.0,
-        total_call_time: 1.0,
-        total_exclusive_time: 1.0
-      },
-      "Supportability/PrependedModules/ActiveRecord::Relation" => {
-        call_count: 1,
-        max_call_time: 1,
-        min_call_time: 1,
-        sum_of_squares: 1.0,
-        total_call_time: 1.0,
-        total_exclusive_time: 1.0
-      }
+      "Supportability/PrependedModules/ActiveRecord::Base" => metric_values_for(1),
+      "Supportability/PrependedModules/ActiveRecord::Relation" => metric_values_for(val)
     })
+  end
+
+  def metric_values_for val
+    { call_count: 1,
+      max_call_time: val,
+      min_call_time: val,
+      sum_of_squares: val**2.to_f,
+      total_call_time: val.to_f,
+      total_exclusive_time: val.to_f }
   end
 
 end
