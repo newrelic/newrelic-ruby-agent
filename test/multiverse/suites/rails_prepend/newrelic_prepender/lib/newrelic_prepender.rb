b5e3616a3eaaf04c2bf172905f3833d3ec167604
Kenichi Nakamura <knakamura@newrelic.com>
2017-09-14 10:52:45 -0700

    RUBY-1787 tidy up newrelic_prepender

--- test/multiverse/suites/rails_prepend/newrelic_prepender/lib/newrelic_prepender.rb
+++ test/multiverse/suites/rails_prepend/newrelic_prepender/lib/newrelic_prepender.rb
@@ -4,34 +4,40 @@
 
 require "newrelic_prepender/version"
 
-module NewRelic; module Prepender; end; end
+module NewRelic
+  module Prepender
+    def self.do_prepend *bases
+      bases.each {|b| b.__send__ :prepend, self}
+    end
+  end
+end
 
 require 'action_controller'
 
-::ActionController::Base.__send__ :prepend, NewRelic::Prepender
+NewRelic::Prepender.do_prepend ::ActionController::Base
 
 if ::Rails::VERSION::MAJOR.to_i == 5
-  ::ActionController::API.__send__ :prepend, NewRelic::Prepender
+  NewRelic::Prepender.do_prepend ::ActionController::API
 end
 
 require 'action_view'
 
-::ActionView::Base.__send__ :prepend, NewRelic::Prepender
-::ActionView::Template.__send__ :prepend, NewRelic::Prepender
-::ActionView::Renderer.__send__ :prepend, NewRelic::Prepender
+NewRelic::Prepender.do_prepend ::ActionView::Base,
+                               ::ActionView::Template,
+                               ::ActionView::Renderer
 
 if ::Rails::VERSION::MAJOR.to_i == 5
   require 'action_cable/engine'
 
-  ::ActionCable::Engine.__send__ :prepend, NewRelic::Prepender
-  ::ActionCable::RemoteConnections.__send__ :prepend, NewRelic::Prepender
+  NewRelic::Prepender.do_prepend ::ActionCable::Engine,
+                                 ::ActionCable::RemoteConnections
 
   require 'active_job'
 
-  ::ActiveJob::Base.__send__ :prepend, NewRelic::Prepender
+  NewRelic::Prepender.do_prepend ::ActiveJob::Base
 end
 
 require 'active_record'
 
-::ActiveRecord::Base.__send__ :prepend, NewRelic::Prepender
-::ActiveRecord::Relation.__send__ :prepend, NewRelic::Prepender
+NewRelic::Prepender.do_prepend ::ActiveRecord::Base,
+                               ::ActiveRecord::Relation
