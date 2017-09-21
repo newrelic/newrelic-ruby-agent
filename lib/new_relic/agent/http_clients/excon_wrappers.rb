bffa1c2fcff71bf89246249355341b0f54d33dd3
Dana Scheider <dscheider@newrelic.com>
2017-09-21 15:58:38 -0700

    RUBY-1821: Add requires

--- lib/new_relic/agent/http_clients/excon_wrappers.rb
+++ lib/new_relic/agent/http_clients/excon_wrappers.rb
@@ -2,6 +2,8 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
 
+require 'new_relic/agent/http_clients/abstract_request'
+
 module NewRelic
   module Agent
     module HTTPClients
