newrelic/newrelic-ruby-agent

commit 585f07e28d539bd4897dec04c739c5c61bd27beb

RUBY-1787: Change metric names in response to metric review

lib/new_relic/agent/instrumentation/active_record_5.rb
@@ -88,7 +88,7 @@ DependencyDetection.defer do
       count = 0
       [::ActiveRecord::Base, ::ActiveRecord::Relation].each {|klass| count += klass.send(:ancestors).index(klass) }
 
-      ::NewRelic::Agent.record_metric('Supportability/ActiveRecord/PrependedModules/count', count)
+      ::NewRelic::Agent.record_metric('Supportability/PrependedModules/ActiveRecord', count)
     end
   end
 end

lib/new_relic/agent/instrumentation/active_record_4.rb
@@ -29,7 +29,7 @@ DependencyDetection.defer do
       count = 0
       [::ActiveRecord::Base, ::ActiveRecord::Relation].each {|klass| count += klass.send(:ancestors).index(klass) }
 
-      ::NewRelic::Agent.record_metric('Supportability/ActiveRecord/PrependedModules/count', count)
+      ::NewRelic::Agent.record_metric('Supportability/PrependedModules/ActiveRecord', count)
 
       ::NewRelic::Agent::Instrumentation::ActiveRecordHelper.instrument_additional_methods
     end

lib/new_relic/agent/instrumentation/rails4/action_controller.rb
@@ -29,5 +29,5 @@ DependencyDetection.defer do
 
   count = ::ActionController::Base.ancestors.index(::ActionController::Base)
 
-  ::NewRelic::Agent.record_metric('Supportability/ActionController/PrependedModules/count', count)
+  ::NewRelic::Agent.record_metric('Supportability/PrependedModules/ActionController', count)
 end

lib/new_relic/agent/instrumentation/rails4/action_view.rb
@@ -27,6 +27,6 @@ DependencyDetection.defer do
       count += klass.ancestors.index(klass)
     end
 
-    NewRelic::Agent.record_metric('Supportability/ActionView/PrependedModules/count', count)
+    NewRelic::Agent.record_metric('Supportability/PrependedModules/ActionView', count)
   end
 end

lib/new_relic/agent/instrumentation/rails5/action_cable.rb
@@ -31,6 +31,6 @@ DependencyDetection.defer do
       count += klass.ancestors.index(klass)
     end
 
-    NewRelic::Agent.record_metric('Supportability/ActionCable/PrependedModules/count', count)
+    NewRelic::Agent.record_metric('Supportability/PrependedModules/ActionCable', count)
   end
 end

lib/new_relic/agent/instrumentation/rails5/action_controller.rb
@@ -31,5 +31,5 @@ DependencyDetection.defer do
   count = 0
   [::ActionController::Base, ::ActionController::API].each {|klass| count += klass.ancestors.index(klass) }
 
-  NewRelic::Agent.record_metric('Supportability/ActionController/PrependedModules/count', count)
+  NewRelic::Agent.record_metric('Supportability/PrependedModules/ActionController', count)
 end

lib/new_relic/agent/instrumentation/rails5/action_view.rb
@@ -27,6 +27,6 @@ DependencyDetection.defer do
       count += klass.ancestors.index(klass)
     end
 
-    NewRelic::Agent.record_metric('Supportability/ActionView/PrependedModules/count', count)
+    NewRelic::Agent.record_metric('Supportability/PrependedModules/ActionView', count)
   end
 end

commit fe3cf494c04b29a7ec6dc121f3c9f566954c081e

Fix test errors caused by 585f07e

lib/new_relic/agent/instrumentation/active_record_5.rb
@@ -79,12 +79,11 @@ DependencyDetection.defer do
   end
 
   executes do
-    ::NewRelic::Agent::PrependSupportability.record_metrics_for(::ActiveRecord::Base, ::ActiveRecord::Relation)
-
     ActiveSupport::Notifications.subscribe('sql.active_record',
       NewRelic::Agent::Instrumentation::ActiveRecordSubscriber.new)
 
     ActiveSupport.on_load(:active_record) do
+      ::NewRelic::Agent::PrependSupportability.record_metrics_for(::ActiveRecord::Base, ::ActiveRecord::Relation)
       ::ActiveRecord::Base.prepend ::NewRelic::Agent::Instrumentation::ActiveRecord::BaseExtensions
       ::ActiveRecord::Relation.prepend ::NewRelic::Agent::Instrumentation::ActiveRecord::RelationExtensions
     end