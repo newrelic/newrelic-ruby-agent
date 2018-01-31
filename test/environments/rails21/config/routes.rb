fce1ee965f96f636a4d106c325393e68164a5019
Matthew Wear <mwear@newrelic.com>
2018-01-31 15:38:28 -0800

    RUBY-1861 Remove unused unit test environments

--- test/environments/rails21/config/routes.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-# This file is distributed under New Relic's license terms.
-# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
-
-# File is required to exist by Rails
