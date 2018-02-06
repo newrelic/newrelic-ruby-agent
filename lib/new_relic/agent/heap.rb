13d2df1d377ef7569fc57f8520239bfe74d84a82
Matthew Wear <mwear@newrelic.com>
2018-02-05 16:55:10 -0800

    Add basic heap implementation

---
+++ lib/new_relic/agent/heap.rb
@@ -0,0 +1,83 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+module NewRelic
+  module Agent
+    # This class implements a min Heap. The first element is always the one with the
+    # lowest priority. It is a tree structure that is represented as an array. The
+    # relationship between between nodes in the tree and indices in the array are as
+    # follows:
+    #
+    # parent_index      = (child_index -  1) / 2
+    # left_child_index  = parent_index * 2 + 1
+    # right_child_index = parent_index * 2 + 2
+    #
+    # the root node is at index 0
+    # a node is a leaf node when its index >= length / 2
+    #
+
+    class Heap
+
+      # @param [Callable] priority_fn an optional priority function used to
+      #   to compute the priority for an item. If it's not supplied priority
+      #   will be computed using Comparable
+      def initialize(&priority_fn)
+        @items = []
+        @priority_fn = priority_fn || ->(x) { x }
+      end
+
+      def push(item)
+        @items << item
+        heapify_up(@items.size - 1)
+      end
+
+      def pop
+        swap(0, @items.size - 1)
+        item = @items.pop
+        heapify_down(0)
+        item
+      end
+
+      def to_a
+        @items
+      end
+
+      private
+
+      def priority(index)
+        @priority_fn.call(@items[index])
+      end
+
+      def heapify_up(child_index)
+        return if child_index == 0
+
+        parent_index = (child_index - 1) / 2
+
+        if priority(child_index) < priority(parent_index)
+          swap(child_index, parent_index)
+          heapify_up(parent_index)
+        end
+      end
+
+      def heapify_down(parent_index)
+        child_index = 2 * parent_index + 1
+        return if child_index > @items.size - 1
+
+        if(child_index < @items.size - 1 && priority(child_index) > priority(child_index + 1))
+          child_index += 1
+        end
+
+        if(priority(child_index) < priority(parent_index))
+          swap(parent_index, child_index)
+          heapify_down(child_index)
+        end
+      end
+
+      def swap(i, j)
+        @items[i], @items[j] = @items[j], @items[i]
+      end
+    end
+
+  end
+end
---
+++ test/new_relic/agent/heap_test.rb
@@ -0,0 +1,109 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+require File.expand_path('../../../test_helper', __FILE__)
+require 'new_relic/agent/heap'
+
+module NewRelic
+  module Agent
+    class HeapTest < Minitest::Test
+      def test_items_inserted_in_proper_order
+        heap = Heap.new
+
+        heap.push(12)
+        heap.push(5)
+        heap.push(4)
+        heap.push(8)
+
+        assert_equal [4, 8, 5, 12], heap.to_a
+      end
+
+      def test_tree_rebalanced_on_pop
+        heap = Heap.new
+
+        heap.push(12)
+        heap.push(5)
+        heap.push(4)
+        heap.push(8)
+
+        heap.pop
+
+        assert_equal [5, 8, 12], heap.to_a
+      end
+
+      def test_items_are_popped_in_ascending_order
+        heap = Heap.new
+        heap.push(12)
+        heap.push(5)
+        heap.push(4)
+        heap.push(8)
+        heap.push(30)
+        heap.push(7)
+
+        ordered_items = []
+
+        6.times do
+          ordered_items << heap.pop
+        end
+
+        assert_equal [4, 5, 7, 8, 12, 30], ordered_items
+        assert_equal [], heap.to_a
+      end
+
+      def test_items_are_popped_in_ascending_order_with_priority_function
+        heap = Heap.new {|x| x[:priority] }
+        heap.push({priority: 12})
+        heap.push({priority: 5})
+        heap.push({priority: 4})
+        heap.push({priority: 8})
+        heap.push({priority: 30})
+        heap.push({priority: 7})
+
+        ordered_items = []
+
+        6.times do
+          ordered_items << heap.pop
+        end
+
+        expected = [
+          {priority: 4},
+          {priority: 5},
+          {priority: 7},
+          {priority: 8},
+          {priority: 12},
+          {priority: 30}
+        ]
+
+        assert_equal expected, ordered_items
+        assert_equal [], heap.to_a
+      end
+
+      def test_large_heap_even_number_of_items
+        heap = Heap.new
+
+        input = (0..1000).to_a
+        input.shuffle.each { |i| heap.push(i) }
+
+        output = []
+        input.size.times { output << heap.pop }
+
+        assert_equal input, output
+        assert_equal [], heap.to_a
+      end
+
+      def test_large_heap_odd_number_of_items
+        heap = Heap.new
+
+        input = (0..1001).to_a
+        input.shuffle.each { |i| heap.push(i) }
+
+        output = []
+        input.size.times { output << heap.pop }
+
+        assert_equal input, output
+        assert_equal [], heap.to_a
+      end
+    end
+  end
+end
