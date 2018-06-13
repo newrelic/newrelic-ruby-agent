60ef61c512a29107de5015b1d574efc4566d08f6
Matthew Wear <mwear@newrelic.com>
2018-06-13 15:45:37 -0700

    RUBY-1960 Make guids consistent size
    Our transaction guids are 8 bytes, however, we are generating 16 byte
    guids for segments. This commit updates segment guid generation to be
    8 bytes to match our transaction guids.

--- lib/new_relic/agent/transaction/abstract_segment.rb
+++ lib/new_relic/agent/transaction/abstract_segment.rb
@@ -27,7 +27,7 @@ module NewRelic
         def initialize name=nil, start_time=nil
           @name = name
           @transaction = nil
-          @guid = SecureRandom.hex(16)
+          @guid = SecureRandom.hex(8)
           @parent = nil
           @params = nil
           @start_time = start_time if start_time
