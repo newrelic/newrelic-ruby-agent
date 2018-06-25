5b13972596ef957abcbc0796dd7218a1ba14d694
Matthew Wear <mwear@newrelic.com>
2018-06-25 15:57:50 -0700

    RUBY-1968 Move obfuscation logic into Database::Statement
    This commit adds a safe_statement method to the database class
    that will return the database statement in the format most permissable
    by the config, obfuscating it and memoizing the value if necessary.

--- lib/new_relic/agent/database.rb
+++ lib/new_relic/agent/database.rb
@@ -195,6 +195,20 @@ module NewRelic
           @host = host
           @port_path_or_id = port_path_or_id
           @database_name = database_name
+          @safe_statement = nil
+        end
+
+        # Returns a statement that will be in the form most permissable by the
+        # config. The format will be safe for transmission to New Relic.
+        def safe_statement
+          @safe_statement ||= case Database.record_sql_method
+            when :obfuscated
+              Database.obfuscate_sql(sql)
+            when :raw
+              sql.to_s
+            else
+              nil
+            end
         end
 
         # This takes a connection config hash from ActiveRecord or Sequel and
