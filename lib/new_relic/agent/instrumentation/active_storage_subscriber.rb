7af50990204060d667d6065d44e9921bc958105b
Rachel Klein <rklein@newrelic.com>
2018-11-29 16:33:28 -0800

    RUBY-2061 Add basic metric recording to subscriber

--- lib/new_relic/agent/instrumentation/active_storage_subscriber.rb
+++ lib/new_relic/agent/instrumentation/active_storage_subscriber.rb
@@ -7,12 +7,38 @@ module NewRelic
   module Agent
     module Instrumentation
       class ActiveStorageSubscriber < EventedSubscriber
-        def start(name, id, payload)
-          NewRelic::Agent.logger.debug "ActiveStorageSubscriber#start: name: #{name}, id: #{id}, payload: #{payload}"
+        def start name, id, payload
+          return unless state.is_execution_traced?
+          start_recording_metrics(name, payload)
+        rescue => e
+          log_notification_error e, name, 'start'
         end
 
-        def finish(name, id, payload)
-          NewRelic::Agent.logger.debug "ActiveStorageSubscriber#finish: name: #{name}, id: #{id}, payload: #{payload}"
+        def finish name, id, payload
+          return unless state.is_execution_traced?
+          stop_recording_metrics @segment
+        end
+
+        def start_recording_metrics name, payload
+          NewRelic::Agent.logger.debug "Recorded segment with name: #{metric_name(name, payload)}"
+          @segment = Transaction.start_segment name: metric_name(name, payload)
+        end
+
+        def stop_recording_metrics segment
+          @segment.finish if @segment
+        end
+
+        def metric_name name, payload
+          service = payload[:service]
+          method = method_from_name name
+          "Ruby/ActiveStorage/#{service}/#{method}"
+        end
+
+        DOT_ACTIVE_STORAGE = ".active_storage".freeze
+        EMPTY_STRING = "".freeze
+
+        def method_from_name name
+          name.gsub DOT_ACTIVE_STORAGE, EMPTY_STRING
         end
       end
     end
