affd6068e1fcdf7b3fb69b760488251af7b217a0
Matthew Wear <mwear@newrelic.com>
2018-12-03 16:16:21 -0800

    RUBY-2061 Store segments on event_stack and add test for ActiveStorageSubscriber

--- lib/new_relic/agent/instrumentation/active_storage_subscriber.rb
+++ lib/new_relic/agent/instrumentation/active_storage_subscriber.rb
@@ -9,36 +9,45 @@ module NewRelic
       class ActiveStorageSubscriber < EventedSubscriber
         def start name, id, payload
           return unless state.is_execution_traced?
-          start_recording_metrics(name, payload)
+          start_segment(name, id, payload)
         rescue => e
           log_notification_error e, name, 'start'
         end
 
         def finish name, id, payload
           return unless state.is_execution_traced?
-          stop_recording_metrics @segment
+
+          finish_segment id
         end
 
-        def start_recording_metrics name, payload
-          NewRelic::Agent.logger.debug "Recorded segment with name: #{metric_name(name, payload)}"
-          @segment = Transaction.start_segment name: metric_name(name, payload)
+        def start_segment name, id, payload
+          segment = Transaction.start_segment name: metric_name(name, payload)
+          event_stack[id].push segment
         end
 
-        def stop_recording_metrics segment
-          @segment.finish if @segment
+        def finish_segment id
+          segment = event_stack[id].pop
+          segment.finish if segment
         end
 
         def metric_name name, payload
           service = payload[:service]
           method = method_from_name name
-          "Ruby/ActiveStorage/#{service}/#{method}"
+          "Ruby/ActiveStorage/#{service}Service/#{method}"
         end
 
-        DOT_ACTIVE_STORAGE = ".active_storage".freeze
-        EMPTY_STRING = "".freeze
+        METHOD_NAME_MAPPING = {
+          "service_upload.active_storage"             => "upload".freeze,
+          "service_streaming_download.active_storage" => "streaming_download".freeze,
+          "service_download.active_storage"           => "download".freeze,
+          "service_delete.active_storage"             => "delete".freeze,
+          "service_delete_prefixed.active_storage"    => "delete_prefixed".freeze,
+          "service_exist.active_storage"              => "exist".freeze,
+          "service_url.active_storage"                => "url".freeze
+        }
 
         def method_from_name name
-          name.gsub DOT_ACTIVE_STORAGE, EMPTY_STRING
+          METHOD_NAME_MAPPING[name]
         end
       end
     end
---
+++ test/new_relic/agent/instrumentation/active_storage_subscriber_test.rb
@@ -0,0 +1,49 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+require File.expand_path('../../../../test_helper', __FILE__)
+require 'new_relic/agent/instrumentation/active_storage_subscriber'
+require 'securerandom'
+
+module NewRelic
+  module Agent
+    module Instrumentation
+      class ActiveStorageSubscriberTest < Minitest::Test
+        def setup
+          nr_freeze_time
+          @subscriber = ActiveStorageSubscriber.new
+          @id = SecureRandom.hex
+
+          NewRelic::Agent.drop_buffered_data
+        end
+
+        def teardown
+          NewRelic::Agent.drop_buffered_data
+        end
+
+        def test_metrics_recorded_for_known_methods
+          in_transaction 'test' do
+            @subscriber.class::METHOD_NAME_MAPPING.each do |event_name, _|
+              generate_event event_name
+            end
+          end
+
+          @subscriber.class::METHOD_NAME_MAPPING.each do |_, method_name|
+            assert_metrics_recorded "Ruby/ActiveStorage/DiskService/#{method_name}"
+          end
+        end
+
+        private
+
+        def generate_event(event_name, attributes = {})
+          defaults = {key: SecureRandom.hex, service: "Disk"}
+          payload = defaults.merge(attributes)
+          @subscriber.start event_name, @id, payload
+          yield if block_given?
+          @subscriber.finish event_name, @id, payload
+        end
+      end
+    end
+  end
+end
