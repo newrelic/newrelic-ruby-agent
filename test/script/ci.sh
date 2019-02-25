062af1bf4b346c601293f0f3201bf0ec5d982029
Matthew Wear <mwear@newrelic.com>
2019-02-25 14:53:33 -0800

    Revert "RUBY-2101 Use old Bundler version for testing"
    This reverts commit 9a16a3b7199a378f113ae1bc87eed9a0d8b02f30.

--- test/script/ci.sh
+++ test/script/ci.sh
@@ -7,9 +7,9 @@ unset RACK_ENV
 unset RAILS_ENV
 
 if [ "$TYPE" = "UNIT" ]; then
-  bundle _1.17.3_ exec rake test:env[$ENVIRONMENT]
+  bundle exec rake test:env[$ENVIRONMENT]
 elif [ "$TYPE" = "FUNCTIONAL" ]; then
-  bundle _1.17.3_ exec rake test:multiverse[group=$GROUP,verbose,nocache]
+  bundle exec rake test:multiverse[group=$GROUP,verbose,nocache]
 elif [ "$TYPE" = "NULLVERSE" ]; then
-  bundle _1.17.3_ exec rake test:nullverse
+  bundle exec rake test:nullverse
 fi
