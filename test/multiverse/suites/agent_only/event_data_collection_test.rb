1d3fe20d0132bff701d8e8e25e25142b6f0b4269
jfoote <jfoote@newrelic.com>
2019-03-21 15:16:45 -0700

    RUBY-2163 Add test to verify that event_report_period is updated on reconnect

--- test/multiverse/suites/agent_only/event_data_collection_test.rb
+++ test/multiverse/suites/agent_only/event_data_collection_test.rb
@@ -21,7 +21,7 @@ class EventDataCollectionTest < Minitest::Test
     assert_equal expected, single_connect_posted['event_data']
   end
 
-  def test_adjusts_event_report_period
+  def test_sets_event_report_period_on_connect_repsonse
     connect_response = {
       "agent_run_id" => 1,
       "event_data" => {
@@ -39,5 +39,31 @@ class EventDataCollectionTest < Minitest::Test
     trigger_agent_reconnect
 
     assert_equal 5, NewRelic::Agent.config[:event_report_period]
+  end
+
+  def test_resets_event_report_period_on_reconnect
+    connect_response = {
+      "agent_run_id" => 1,
+      "event_data" => {
+        "report_period_ms" => 5000,
+        "harvest_limits" => {
+          "analytic_event_data" => 1200,
+          "custom_event_data" => 1000,
+          "error_event_data" => 100
+        }
+      }
+    }
+
+    setup_agent
+    $collector.stub('connect', connect_response)
+    trigger_agent_reconnect
+
+    assert_equal 5, NewRelic::Agent.config[:event_report_period]
+
+    connect_response['event_data']['report_period_ms'] = 1000000
+    $collector.stub('connect', connect_response)
+    trigger_agent_reconnect
+
+    assert_equal 1000, NewRelic::Agent.config[:event_report_period]
   end
 end
