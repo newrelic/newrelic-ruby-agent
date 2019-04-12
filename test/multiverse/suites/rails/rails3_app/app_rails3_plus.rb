14dbc1250cb763b5133622f19889c45956e98957
Rachel Klein <rklein@newrelic.com>
2019-04-12 10:34:50 -0700

    RUBY-2181 Add workaround to avoid development_secret bug in Rails 5.2.2.1 and above

--- test/multiverse/suites/rails/rails3_app/app_rails3_plus.rb
+++ test/multiverse/suites/rails/rails3_app/app_rails3_plus.rb
@@ -68,6 +68,7 @@ if !defined?(MyApp)
     config.secret_token = "49837489qkuweoiuoqwehisuakshdjksadhaisdy78o34y138974xyqp9rmye8yrpiokeuioqwzyoiuxftoyqiuxrhm3iou1hrzmjk"
     config.eager_load = false
     config.filter_parameters += [:secret]
+    config.secret_key_base = SecureRandom.hex(64)
     initializer "install_error_middleware" do
       config.middleware.use ErrorMiddleware
     end
