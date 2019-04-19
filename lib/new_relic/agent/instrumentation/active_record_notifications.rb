7a778a8c698bd97149c95ed3ec0807227a2cd678
jfoote <jfoote@newrelic.com>
2019-04-19 11:55:17 -0700

    RUBY-2178 Merge instrumentation of Active Record 4 and 5

--- lib/new_relic/agent/instrumentation/active_record_notifications.rb
+++ lib/new_relic/agent/instrumentation/active_record_notifications.rb
@@ -11,7 +11,7 @@ DependencyDetection.defer do
   depends_on do
     defined?(::ActiveRecord) && defined?(::ActiveRecord::Base) &&
       defined?(::ActiveRecord::VERSION) &&
-      ::ActiveRecord::VERSION::MAJOR.to_i >= 5
+      ::ActiveRecord::VERSION::MAJOR.to_i >= 4
   end
 
   depends_on do
@@ -26,16 +26,35 @@ DependencyDetection.defer do
   executes do
     ActiveSupport::Notifications.subscribe('sql.active_record',
       NewRelic::Agent::Instrumentation::ActiveRecordSubscriber.new)
+  end
 
+  executes do
     ActiveSupport.on_load(:active_record) do
-      ::NewRelic::Agent::PrependSupportability.record_metrics_for(::ActiveRecord::Base, ::ActiveRecord::Relation)
-      ::ActiveRecord::Base.prepend ::NewRelic::Agent::Instrumentation::ActiveRecordPrepend::BaseExtensions
-      ::ActiveRecord::Relation.prepend ::NewRelic::Agent::Instrumentation::ActiveRecordPrepend::RelationExtensions
+      ::NewRelic::Agent::PrependSupportability.record_metrics_for(
+          ::ActiveRecord::Base,
+          ::ActiveRecord::Relation)
 
-      if ::ActiveRecord::VERSION::MINOR.to_i == 1 &&
-         ::ActiveRecord::VERSION::TINY.to_i >= 6
-        ::ActiveRecord::Base.prepend ::NewRelic::Agent::Instrumentation::ActiveRecordPrepend::BaseExtensions516
+      # Default to .prepending, unless the ActiveRecord version is <=4 
+      # **AND** the :prepend_active_record_instrumentation config is false
+      if ::ActiveRecord::VERSION::MAJOR > 4 \
+          || ::NewRelic::Agent.config[:prepend_active_record_instrumentation]
+
+        ::ActiveRecord::Base.send(:prepend,
+            ::NewRelic::Agent::Instrumentation::ActiveRecordPrepend::BaseExtensions)
+        ::ActiveRecord::Relation.send(:prepend,
+            ::NewRelic::Agent::Instrumentation::ActiveRecordPrepend::RelationExtensions)
+      else
+        ::NewRelic::Agent::Instrumentation::ActiveRecordHelper.instrument_additional_methods
       end
+    end
+  end
+
+  executes do
+    if ::ActiveRecord::VERSION::MAJOR == 5 \
+        && ::ActiveRecord::VERSION::MINOR.to_i == 1 \
+        && ::ActiveRecord::VERSION::TINY.to_i >= 6
+
+      ::ActiveRecord::Base.prepend ::NewRelic::Agent::Instrumentation::ActiveRecordPrepend::BaseExtensions516
     end
   end
 end
