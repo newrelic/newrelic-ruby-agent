cd402843fae6de92ac70ad5eae71365505587489
Rachel Klein <rklein@newrelic.com>
2019-10-07 17:15:29 -0700

    RUBY-2309 Add benchmarks for insert, parse, external request segment

--- test/performance/suites/external_segment.rb
+++ test/performance/suites/external_segment.rb
@@ -7,7 +7,7 @@ require 'new_relic/agent/obfuscator'
 
 class ExternalSegment < Performance::TestCase
 
-  CONFIG = {
+  CAT_CONFIG = {
       :license_key     => 'a' * 40,
       :'cross_application_tracer.enabled' => true,
       :cross_process_id                   => "1#1884",
@@ -15,8 +15,14 @@ class ExternalSegment < Performance::TestCase
       :trusted_account_ids                => [1]
   }
 
+  TRACE_CONTEXT_CONFIG = {
+      :'trace_context.enabled' => true,
+      :account_id => "190",
+      :primary_application_id => "46954",
+      :disable_harvest_thread => true
+  }
+
   def setup
-    NewRelic::Agent.config.add_config_for_testing(CONFIG)
 
     NewRelic::Agent.manual_start(
       :monitor_mode   => false
@@ -36,8 +42,23 @@ class ExternalSegment < Performance::TestCase
   end
 
   def test_external_cat_request
+    NewRelic::Agent.config.add_config_for_testing(CAT_CONFIG)
+
     io_server = start_server
-    reply_with_cat_headers io_server
+    reply_with_headers io_server, cat_response_headers
+    measure do
+      in_transaction do
+        Net::HTTP.get(TEST_URI)
+      end
+    end
+    stop_server io_server
+  end
+
+  def test_external_trace_context_request
+    NewRelic::Agent.config.add_config_for_testing(TRACE_CONTEXT_CONFIG)
+
+    io_server = start_server
+    reply_with_headers io_server, trace_context_response_headers
     measure do
       in_transaction do
         Net::HTTP.get(TEST_URI)
@@ -49,7 +70,7 @@ class ExternalSegment < Performance::TestCase
   SERVER_SCRIPT_PATH = File.expand_path('../../../script/external_server.rb', __FILE__)
 
   def start_server
-    io = IO.popen(SERVER_SCRIPT_PATH, 'r+')
+  io = IO.popen(SERVER_SCRIPT_PATH, 'r+')
     response = JSON.parse(io.gets)
     if response["message"] == "started"
       io
@@ -58,10 +79,10 @@ class ExternalSegment < Performance::TestCase
     end
   end
 
-  def reply_with_cat_headers io_server
+  def reply_with_headers io_server, headers
     message = {
       :command => "add_headers",
-      :payload => cat_response_headers
+      :payload => headers
     }.to_json
     io_server.puts message
   end
@@ -76,5 +97,12 @@ class ExternalSegment < Performance::TestCase
     obfuscator = NewRelic::Agent::Obfuscator.new NewRelic::Agent.config[:encoding_key]
     app_data = obfuscator.obfuscate(["1#1884", "txn-name", 2, 8, 0, 'BEC1BC64675138B9'].to_json) + "\n"
     {'X-NewRelic-App-Data' => app_data}
+  end
+
+  def trace_context_response_headers
+    {
+      'traceparent' =>'00-da8bc8cc6d062849b0efcf3c169afb5a-7d3efb1b173fecfa-01',
+      'tracestate' => '33@nr=0-0-33-2827902-7d3efb1b173fecfa-e8b91a159289ff74-1-1.234567-1518469636035'
+    }
   end
 end
---
+++ test/performance/suites/trace_context.rb
@@ -0,0 +1,63 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+
+require 'net/http'
+require 'new_relic/agent/trace_context'
+require 'new_relic/agent/transaction/trace_context'
+
+class TraceContext < Performance::TestCase
+
+  CONFIG = {
+      :'trace_context.enabled' => true,
+      :account_id => "190",
+      :primary_application_id => "46954",
+      :disable_harvest_thread => true
+  }
+
+  def test_parse
+    carrier = {
+        'traceparent' =>'00-da8bc8cc6d062849b0efcf3c169afb5a-7d3efb1b173fecfa-01',
+        'tracestate' => '33@nr=0-0-33-2827902-7d3efb1b173fecfa-e8b91a159289ff74-1-1.234567-1518469636035'
+    }
+
+    measure do
+      ::NewRelic::Agent::TraceContext.parse carrier: carrier,
+                                            trace_state_entry_key: "33@nr"
+    end
+  end
+
+  def test_insert
+    carrier = {}
+    trace_id = 'a8e67265afe2773a3c611b94306ee5c2'
+    parent_id = 'fb1010463ea28a38'
+    trace_flags = 0x1
+    trace_state = 'k1=asdf,k2=qwerty'
+
+    measure do
+      ::NewRelic::Agent::TraceContext.insert carrier: carrier,
+                                             trace_id: trace_id,
+                                             parent_id: parent_id,
+                                             trace_flags: trace_flags,
+                                             trace_state: trace_state
+    end
+  end
+
+  def test_insert_trace_context
+    NewRelic::Agent.agent.stubs(:connected?).returns(true)
+
+    carrier = {}
+
+    with_config CONFIG do
+      in_transaction do |txn|
+        measure do
+          txn.insert_trace_context carrier: carrier
+        end
+      end
+    end
+  end
+
+  def test_trace_context_monitor_call
+
+  end
+end
