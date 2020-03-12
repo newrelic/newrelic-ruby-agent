b55849024dbb6330b5c761b48d282bb1dd56e6ff
Andreas Eger <dev@eger-andreas.de>
2020-03-12 19:27:56 +0100

    fix DelayedClass

--- lib/new_relic/agent/instrumentation/sidekiq.rb
+++ lib/new_relic/agent/instrumentation/sidekiq.rb
@@ -64,7 +64,7 @@ DependencyDetection.defer do
         }
       rescue => e
         NewRelic::Agent.logger.error("Failure during deserializing YAML for Sidekiq::Extensions::DelayedClass", e)
-        NewRelic::SidekiqInstrumentation.default_trace_args(msg)
+        NewRelic::SidekiqInstrumentation::Server.default_trace_args(msg)
       end
     end
 
