f0d324516bd47cf0f54e49d92a98351f2a92f57e
Michael Lang <mlang@newrelic.com>
2020-05-18 09:23:40 -0400

    RUBY-2407 tests now work with dependency detection

--- infinite_tracing/lib/newrelic/infinite_tracing.rb
+++ infinite_tracing/lib/newrelic/infinite_tracing.rb
@@ -1,3 +1,8 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/rpm/blob/master/LICENSE for complete details.
+# frozen_string_literal: true
+
 unless defined? NewRelic::Agent::InfiniteTracing
   require_relative '../new_relic/infinite_tracing'
 end
--- infinite_tracing/test/test_helper.rb
+++ infinite_tracing/test/test_helper.rb
@@ -20,18 +20,21 @@ require 'mocha/setup'
 require 'newrelic_rpm'
 require 'new_relic/infinite_tracing'
 
+# Load the agent's test helpers for use in infinite tracing tests
 agent_helper_path = File.join(agent_test_path, 'helpers')
 require File.join(agent_helper_path, 'file_searching.rb')
 require File.join(agent_helper_path, 'config_scanning.rb')
 require File.join(agent_helper_path, 'misc.rb')
+require File.join(agent_helper_path, 'exceptions.rb')
 
 Dir[File.expand_path('../support/*', __FILE__)].each { |f| require f }
-
-require File.expand_path(File.join(File.dirname(__FILE__), '..',  '..', 'test', 'helpers', 'exceptions'))
 
 # This is the public method recommended for plugin developers to share our
 # agent helpers. Use it so we don't accidentally break it.
 NewRelic::Agent.require_test_helper
+
+# Activates Infinite Tracing so we can test
+DependencyDetection.detect!
 
 def timeout_cap duration=3
   Timeout::timeout(duration) { yield }
