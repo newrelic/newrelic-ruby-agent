15f4009d33b331046088ecd19e3b7a227c3db3a7
Michael Lang <mlang@newrelic.com>
2020-05-27 17:11:26 -0400

    RUBY-2421 sane channel defaults selected

--- infinite_tracing/lib/infinite_tracing/channel.rb
+++ infinite_tracing/lib/infinite_tracing/channel.rb
@@ -33,25 +33,10 @@ module NewRelic::Agent
         Config.trace_observer_host_and_port
       end
 
-      # FOREVER = (2**(4 * 8 -2) -1)
-      # THIRTY_SECONDS = 30_000
- 
-      # TODO: decide what defaults we want to use
-
       def settings
         {
           'grpc.minimal_stack' => 1,
           'grpc.enable_deadline_checking' => 0,
-          # 'grpc.arg_max_connection_idle_ms' => FOREVER,
-          # 'grpc.keepalive_time_ms' => THIRTY_SECONDS,
-          # 'grpc.keepalive_timeout_ms' => THIRTY_SECONDS,
-          # 'grpc.keepalive_permit_without_calls' => THIRTY_SECONDS,
-          # 'grpc.arg_max_concurrent_streams' => 10,
-          # 'grpc.max_concurrent_streams' => 10,
-          # 'grpc.max_connection_age_ms' => FOREVER,
-          # 'grpc.http2.max_pings_without_data' => 0,
-          # 'grpc.http2.min_time_between_pings_ms' => 10000,
-          # 'grpc.http2.min_ping_interval_without_data_ms' => 5000,
         }
       end
 
