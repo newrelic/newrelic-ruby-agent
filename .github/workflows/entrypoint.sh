63ab59563ea511575c4d2755fe9c8547252a638f
Michael Lang <mlang@newrelic.com>
2020-07-17 11:08:46 -0400

    greeter example

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/sh -l
 
-# `$*` expands the `args` supplied in an `array` individually 
-# or splits `args` in a string separated by whitespace.
-sh -c "echo $*"
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
