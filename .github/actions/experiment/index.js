5739985ddba0c34d5d469d769b26b4c828d89ae1
Michael Lang <mlang@newrelic.com>
2020-07-27 13:01:40 -0400

    /usr/bin/ruby for cryin' out loud

--- .github/actions/experiment/index.js
+++ .github/actions/experiment/index.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("ruby ./.github/actions/experiment/index.rb", (error, stdout, stderr) => {
+exec("/usr/bin/ruby ./.github/actions/experiment/index.rb", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
