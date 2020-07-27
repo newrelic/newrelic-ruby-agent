f5f90f868a86b379f650317dca19d8cacda67db2
Michael Lang <mlang@newrelic.com>
2020-07-27 12:55:56 -0400

    js bridge to ruby

---
+++ .github/actions/experiment/index.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+exec("ruby index.rb", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
