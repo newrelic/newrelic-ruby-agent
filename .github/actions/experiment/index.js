cebf00a238ad316b99363dc976f16646d10c7865
Michael Lang <mlang@newrelic.com>
2020-07-27 14:56:45 -0400

    awaiting for realz

--- .github/actions/experiment/index.js
+++ .github/actions/experiment/index.js
@@ -2,27 +2,28 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-// const { exec } = require("child_process");
+async function run() {
+  try {
+    const rubyVersion = core.getInput('ruby-version');
+    console.log(`Using ${rubyVersion}`);
+    
+    await exec.exec("/usr/bin/ruby ./.github/actions/experiment/index.rb", (error, stdout, stderr) => {
+      if (error) {
+          core.setFailed(error.message);
+          return;
+      }
+      if (stderr) {
+          core.setFailed(stderr);
+          return;
+      }
+      // output = $stdout;
+      // console.log(output);
+      core.setOutput('results', 'outputVal');
+    });
 
-
-try {
-  const rubyVersion = core.getInput('ruby-version');
-  console.log(`Using ${rubyVersion}`);
-  
-  await exec.exec("/usr/bin/ruby ./.github/actions/experiment/index.rb", (error, stdout, stderr) => {
-    if (error) {
-        core.setFailed(error.message);
-        return;
-    }
-    if (stderr) {
-        core.setFailed(stderr);
-        return;
-    }
-    // output = $stdout;
-    // console.log(output);
-    core.setOutput('results', 'outputVal');
-  });
-
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
