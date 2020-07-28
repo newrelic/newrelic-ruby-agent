bec8837bcc26201ffa9c7098296ad3f7973a7599
Michael Lang <mlang@newrelic.com>
2020-07-28 14:22:40 -0400

    shot in the dark

--- .github/actions/build-ruby/index.js
+++ .github/actions/build-ruby/index.js
@@ -130,7 +130,7 @@ async function installBundler(rubyVersion) {
   const bundlePath = `${process.env.HOME}/.rubies/ruby-${rubyVersion}/bin`
 
   if (!fs.existsSync(`${bundlePath}/bundle`)) {
-    await exec.exec("sudo gem install bundler", ['-v', "\'~> 1\'", "--no-document", "-​-bindir", bundlePath])
+    await exec.exec("gem install bundler", ['-v', "\'~> 1\'"])
   }
 
   core.endGroup()
