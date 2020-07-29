1755a1953ab0b2d4138d6f8b80daf406dfdb5e14
Michael Lang <mlang@newrelic.com>
2020-07-29 16:47:07 -0400

    commenting on index.js

--- .github/actions/build-ruby/dist/index.js
+++ .github/actions/build-ruby/dist/index.js
@@ -2965,10 +2965,13 @@ const core = __webpack_require__(968)
 const github = __webpack_require__(887)
 const exec = __webpack_require__(198)
 
-function chomp(raw_text) {
-  return raw_text.replace(/(\n|\r)+$/, '')
+// removes trailing newlines and linefeeds from the given text string
+function chomp(text) {
+  return text.replace(/(\n|\r)+$/, '')
 }
 
+// invokes the @actions/exec exec function with listeners to capture the 
+// output stream as the return result.
 async function execute(command) {
  try {
    let outputStr = ''
@@ -2988,6 +2991,8 @@ async function execute(command) {
  }
 }
 
+// installs system dependencies needed to successfully build the ruby executables
+// NOTE: Ubuntu specific!
 async function installSystemDependencies() {
   core.startGroup(`Installing system dependencies`)
 
@@ -3001,6 +3006,10 @@ async function installSystemDependencies() {
   core.endGroup()
 }
 
+// ruby-build is used to compile Ruby and it's various executables
+// rbenv's ruby-build doesn't fully support EOL rubies (< 2.4 at time of this writing).
+// setup-ruby also doesn't correctly build the older rubies with openssl support.
+// long story short, openssl library for these older ruby need to be 1.0 variant.
 async function installRubyBuild(rubyVersion) {
   core.startGroup(`Installing ruby-build`)
 
@@ -3024,13 +3033,23 @@ async function installRubyBuild(rubyVersion) {
   core.endGroup()
 }
 
+// Add the environment variables needed to correctly build ruby and later run ruby tests.
+// this function is invoked even if ruby is cached and compile step is skipped.
 function setupRubyEnvironment() {
+
+  // LANG environment must be set or Ruby will default external_encoding to US-ASCII i
+  // instead of UTF-8 and this will fail many tests.
   core.exportVariable('LANG', 'C.UTF-8')
+
+  // enable-shared prevents native extension gems from breaking if they're cached
+  // independently of the ruby binaries
   core.exportVariable('RUBY_CONFIGURE_OPTS', '--enable-shared --disable-install-doc')
+
   // https://github.com/actions/virtual-environments/issues/267
   core.exportVariable('CPPFLAGS', '-DENABLE_PATH_CHECK=0')
 }
 
+// Shows some version love!
 async function showVersions() {
   core.startGroup("Show Versions")
 
@@ -3042,20 +3061,26 @@ async function showVersions() {
   core.endGroup()
 }
 
+// Just one place to define the ruby binaries path and that is here
+// NOTE: the cache step in the workflow .yml file must match this path
 function rubyPath(rubyVersion) {
   return `${process.env.HOME}/.rubies/ruby-${rubyVersion}`
 }
 
+// This "activates" our newly built ruby so it comes first in the path
 function addRubyToPath(rubyVersion) {
   core.addPath(`${rubyPath(rubyVersion)}/bin`)
 }
 
+// kicks off the ruby build process.
 async function buildRuby(rubyVersion) {
   core.startGroup(`Build Ruby ${rubyVersion}`)
   await exec.exec(`ruby-build --verbose ${rubyVersion} ${rubyPath(rubyVersion)}`) 
   core.endGroup()
 }
 
+// Older rubies come with Ruby gems 2.5.x and we need 3.0.6 minimum to 
+// correctly install Bundler and do anything else within the multiverse test suite
 async function upgradeRubyGems(rubyVersion) {
   core.startGroup(`Upgrade RubyGems`)
 
@@ -3090,6 +3115,7 @@ async function upgradeRubyGems(rubyVersion) {
   core.endGroup()
 }
 
+// install Bundler 1.17.3 (or thereabouts)
 async function installBundler(rubyVersion) {
   core.startGroup(`Install bundler`)
 
@@ -3102,6 +3128,19 @@ async function installBundler(rubyVersion) {
   core.endGroup()
 }
 
+// Will set up the Ruby environment so the desired Ruby binaries are used in the unit tests
+// If Ruby hasn't been built and cached, yet, we also compile the Ruby binaries.
+// 
+// The binaries, once built, can be cached with the following in the workflow .yml file:
+//
+// - uses: actions/cache@v2
+//   id: ruby-cache
+//   with:
+//     path: ~/.rubies/ruby-${{ matrix.ruby-version }}
+//     key: ruby-cache-${{ matrix.ruby-version }}
+//     restore-keys: |
+//       ruby-cache-${{ matrix.ruby-version }}
+//
 async function main() {
   const rubyVersion = core.getInput('ruby-version')
   const rubyBinPath = `${rubyPath(rubyVersion)}/bin`
--- .github/actions/build-ruby/index.js
+++ .github/actions/build-ruby/index.js
@@ -6,10 +6,13 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const exec = require('@actions/exec')
 
-function chomp(raw_text) {
-  return raw_text.replace(/(\n|\r)+$/, '')
+// removes trailing newlines and linefeeds from the given text string
+function chomp(text) {
+  return text.replace(/(\n|\r)+$/, '')
 }
 
+// invokes the @actions/exec exec function with listeners to capture the 
+// output stream as the return result.
 async function execute(command) {
  try {
    let outputStr = ''
@@ -29,6 +32,8 @@ async function execute(command) {
  }
 }
 
+// installs system dependencies needed to successfully build the ruby executables
+// NOTE: Ubuntu specific!
 async function installSystemDependencies() {
   core.startGroup(`Installing system dependencies`)
 
@@ -42,6 +47,10 @@ async function installSystemDependencies() {
   core.endGroup()
 }
 
+// ruby-build is used to compile Ruby and it's various executables
+// rbenv's ruby-build doesn't fully support EOL rubies (< 2.4 at time of this writing).
+// setup-ruby also doesn't correctly build the older rubies with openssl support.
+// long story short, openssl library for these older ruby need to be 1.0 variant.
 async function installRubyBuild(rubyVersion) {
   core.startGroup(`Installing ruby-build`)
 
@@ -65,13 +74,23 @@ async function installRubyBuild(rubyVersion) {
   core.endGroup()
 }
 
+// Add the environment variables needed to correctly build ruby and later run ruby tests.
+// this function is invoked even if ruby is cached and compile step is skipped.
 function setupRubyEnvironment() {
+
+  // LANG environment must be set or Ruby will default external_encoding to US-ASCII i
+  // instead of UTF-8 and this will fail many tests.
   core.exportVariable('LANG', 'C.UTF-8')
+
+  // enable-shared prevents native extension gems from breaking if they're cached
+  // independently of the ruby binaries
   core.exportVariable('RUBY_CONFIGURE_OPTS', '--enable-shared --disable-install-doc')
+
   // https://github.com/actions/virtual-environments/issues/267
   core.exportVariable('CPPFLAGS', '-DENABLE_PATH_CHECK=0')
 }
 
+// Shows some version love!
 async function showVersions() {
   core.startGroup("Show Versions")
 
@@ -83,20 +102,26 @@ async function showVersions() {
   core.endGroup()
 }
 
+// Just one place to define the ruby binaries path and that is here
+// NOTE: the cache step in the workflow .yml file must match this path
 function rubyPath(rubyVersion) {
   return `${process.env.HOME}/.rubies/ruby-${rubyVersion}`
 }
 
+// This "activates" our newly built ruby so it comes first in the path
 function addRubyToPath(rubyVersion) {
   core.addPath(`${rubyPath(rubyVersion)}/bin`)
 }
 
+// kicks off the ruby build process.
 async function buildRuby(rubyVersion) {
   core.startGroup(`Build Ruby ${rubyVersion}`)
   await exec.exec(`ruby-build --verbose ${rubyVersion} ${rubyPath(rubyVersion)}`) 
   core.endGroup()
 }
 
+// Older rubies come with Ruby gems 2.5.x and we need 3.0.6 minimum to 
+// correctly install Bundler and do anything else within the multiverse test suite
 async function upgradeRubyGems(rubyVersion) {
   core.startGroup(`Upgrade RubyGems`)
 
@@ -131,6 +156,7 @@ async function upgradeRubyGems(rubyVersion) {
   core.endGroup()
 }
 
+// install Bundler 1.17.3 (or thereabouts)
 async function installBundler(rubyVersion) {
   core.startGroup(`Install bundler`)
 
@@ -143,6 +169,19 @@ async function installBundler(rubyVersion) {
   core.endGroup()
 }
 
+// Will set up the Ruby environment so the desired Ruby binaries are used in the unit tests
+// If Ruby hasn't been built and cached, yet, we also compile the Ruby binaries.
+// 
+// The binaries, once built, can be cached with the following in the workflow .yml file:
+//
+// - uses: actions/cache@v2
+//   id: ruby-cache
+//   with:
+//     path: ~/.rubies/ruby-${{ matrix.ruby-version }}
+//     key: ruby-cache-${{ matrix.ruby-version }}
+//     restore-keys: |
+//       ruby-cache-${{ matrix.ruby-version }}
+//
 async function main() {
   const rubyVersion = core.getInput('ruby-version')
   const rubyBinPath = `${rubyPath(rubyVersion)}/bin`
