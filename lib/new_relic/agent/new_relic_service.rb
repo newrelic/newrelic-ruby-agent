d462dc571a02597f069ba643d16269e84bce12bc
Tanna McClure <tmcclure@newrelic.com>
2020-08-10 14:18:59 -0700

    changed method used to record metric

--- lib/new_relic/agent/new_relic_service.rb
+++ lib/new_relic/agent/new_relic_service.rb
@@ -359,7 +359,7 @@ module NewRelic
           NewRelic::Agent.logger.warn("Couldn't find CA bundle from configured ca_bundle_path: #{path_override}") unless File.exist? path_override
           path_override
         else
-          Agent.record_metric("Supportability/Ruby/BundledCertsRequired", 0.0)
+          ::NewRelic::Agent.increment_metric("Supportability/Ruby/BundledCertsRequired")
           File.expand_path(File.join(control.newrelic_root, 'cert', 'cacert.pem'))
         end
       end
