c884b58a5deaa8d6a8f618eb703bc65382867632
Michael Lang <mlang@newrelic.com>
2020-09-16 18:17:04 -0400

    printing out gem push command

--- .github/workflows/scripts/rubygems-publish.rb
+++ .github/workflows/scripts/rubygems-publish.rb
@@ -5,7 +5,13 @@ version = ENV["VERSION"]
 mfa_identifier = Base32.encode ENV["RUBY_GEMS_MFA_KEY"]
 
 puts "Publshing the newrelic_rpm-#{version}.gem file..."
-puts `gem push --otp #{totp = ROTP::TOTP.new(mfa_identifier)} newrelic_rpm-#{version}.gem`
+totp = ROTP::TOTP.new(mfa_identifier)
+cmd = "gem push --otp #{totp} newrelic_rpm-#{version}.gem"
+puts "executing: #{cmd}"
+puts `#{cmd}`
 
 puts "Publshing the newrelic-infinite_tracing-#{version}.gem file..."
-puts `gem push --otp #{totp = ROTP::TOTP.new(mfa_identifier)} newrelic-infinite_tracing-#{version}.gem`
+totp = ROTP::TOTP.new(mfa_identifier)
+cmd = "gem push --otp #{totp} newrelic-infinite_tracing-#{version}.gem"
+puts "executing: #{cmd}"
+puts `#{cmd}`
