21a9ee0fc9f6eb3996c2475593e72ac53f5ea98e
Michael Lang <mlang@newrelic.com>
2020-09-16 18:23:57 -0400

    trying with -k flag

--- .github/workflows/scripts/rubygems-publish.rb
+++ .github/workflows/scripts/rubygems-publish.rb
@@ -6,11 +6,11 @@ mfa_identifier = Base32.encode ENV["RUBY_GEMS_MFA_KEY"]
 totp = ROTP::TOTP.new(mfa_identifier)
 
 puts "Publshing the newrelic_rpm-#{version}.gem file..."
-cmd = "gem push --otp #{totp.now} newrelic_rpm-#{version}.gem"
+cmd = "gem push --otp #{totp.now} -k GEM_HOST_API_KEY newrelic_rpm-#{version}.gem"
 puts "executing: #{cmd}"
 puts `#{cmd}`
 
 puts "Publshing the newrelic-infinite_tracing-#{version}.gem file..."
-cmd = "gem push --otp #{totp.now} newrelic-infinite_tracing-#{version}.gem"
+cmd = "gem push --otp #{totp.now} -k GEM_HOST_API_KEY infinite_tracing/newrelic-infinite_tracing-#{version}.gem"
 puts "executing: #{cmd}"
 puts `#{cmd}`
