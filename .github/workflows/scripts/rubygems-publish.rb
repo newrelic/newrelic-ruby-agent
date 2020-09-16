69ff9f98fd88fc964bd9a03d87d4549075a501d1
Michael Lang <mlang@newrelic.com>
2020-09-16 18:20:13 -0400

    use now with rotp

--- .github/workflows/scripts/rubygems-publish.rb
+++ .github/workflows/scripts/rubygems-publish.rb
@@ -3,15 +3,14 @@ require 'base32'
 
 version = ENV["VERSION"]
 mfa_identifier = Base32.encode ENV["RUBY_GEMS_MFA_KEY"]
+totp = ROTP::TOTP.new(mfa_identifier)
 
 puts "Publshing the newrelic_rpm-#{version}.gem file..."
-totp = ROTP::TOTP.new(mfa_identifier)
-cmd = "gem push --otp #{totp} newrelic_rpm-#{version}.gem"
+cmd = "gem push --otp #{totp.now} newrelic_rpm-#{version}.gem"
 puts "executing: #{cmd}"
 puts `#{cmd}`
 
 puts "Publshing the newrelic-infinite_tracing-#{version}.gem file..."
-totp = ROTP::TOTP.new(mfa_identifier)
-cmd = "gem push --otp #{totp} newrelic-infinite_tracing-#{version}.gem"
+cmd = "gem push --otp #{totp.now} newrelic-infinite_tracing-#{version}.gem"
 puts "executing: #{cmd}"
 puts `#{cmd}`
