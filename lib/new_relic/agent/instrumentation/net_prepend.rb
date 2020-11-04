cde7d105cb669f6a9b2afa93076c9b1dd88b1bd5
Tanna McClure <tmcclure@newrelic.com>
2020-11-03 16:36:22 -0800

    update net http instrumentation to use prepend when enabled

--- lib/new_relic/agent/instrumentation/net_prepend.rb
+++ lib/new_relic/agent/instrumentation/net_prepend.rb
@@ -2,28 +2,15 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 
-DependencyDetection.defer do
-  named :net_http
+module NewRelic 
+  module Agent 
+    module Instrumentation
+      module NetPrepend
 
-  depends_on do
-    defined?(Net) && defined?(Net::HTTP)
-  end
-
-  executes do
-    ::NewRelic::Agent.logger.info 'Installing Net instrumentation'
-    require 'new_relic/agent/distributed_tracing/cross_app_tracing'
-    require 'new_relic/agent/http_clients/net_http_wrappers'
-  end
-
-  executes do
-    if ::NewRelic::Agent.config[:prepend_net_instrumentation]
-      ::Net::HTTP.prepend ::NewRelic::Agent::Instrumentation::NetPrepend
-    else 
-      class Net::HTTP
         if RUBY_VERSION < "2.7.0"
-          def request_with_newrelic_trace(request, *args, &block)
+          def request(request, *args, &block)
             wrapped_request = NewRelic::Agent::HTTPClients::NetHTTPRequest.new(self, request)
-    
+  
             segment = NewRelic::Agent::Tracer.start_external_request_segment(
               library: wrapped_request.type,
               uri: wrapped_request.uri,
@@ -38,7 +25,7 @@ DependencyDetection.defer do
               # counting if connection wasn't started (which calls request again).
               NewRelic::Agent.disable_all_tracing do
                 response = NewRelic::Agent::Tracer.capture_segment_error segment do
-                  request_without_newrelic_trace(request, *args, &block)
+                  super
                 end
               end
     
@@ -48,11 +35,11 @@ DependencyDetection.defer do
             ensure
               segment.finish
             end
-          end  
+          end
         else
-          def request_with_newrelic_trace(request, *args, **kwargs, &block)
+          def request(request, *args, **kwargs, &block)
             wrapped_request = NewRelic::Agent::HTTPClients::NetHTTPRequest.new(self, request)
-    
+  
             segment = NewRelic::Agent::Tracer.start_external_request_segment(
               library: wrapped_request.type,
               uri: wrapped_request.uri,
@@ -67,7 +54,7 @@ DependencyDetection.defer do
               # counting if connection wasn't started (which calls request again).
               NewRelic::Agent.disable_all_tracing do
                 response = NewRelic::Agent::Tracer.capture_segment_error segment do
-                  request_without_newrelic_trace(request, *args, **kwargs, &block)
+                  super
                 end
               end
     
@@ -79,11 +66,7 @@ DependencyDetection.defer do
             end
           end
         end
-  
-        alias request_without_newrelic_trace request
-        alias request request_with_newrelic_trace
       end
     end
-
   end
 end
