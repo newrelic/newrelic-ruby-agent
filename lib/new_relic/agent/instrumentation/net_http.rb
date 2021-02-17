63f91034397f574404ce75adc1f09de557e5c71c
Michael Lang <mlang@newrelic.com>
2021-02-17 13:56:05 -0500

    support chaining and prepending instrumentation in the dependency manager; support new config options

--- lib/new_relic/agent/instrumentation/net_http.rb
+++ lib/new_relic/agent/instrumentation/net_http.rb
@@ -32,7 +32,7 @@ DependencyDetection.defer do
   executes do
     if use_prepend?
       prepend_instrument ::Net::HTTP, ::NewRelic::Agent::Instrumentation::NetHTTP::Prepend
-    else 
+    else
       chain_instrument ::NewRelic::Agent::Instrumentation::NetHTTP::Chain
     end
   end
--- lib/new_relic/agent/instrumentation/net_http/chain.rb
+++ lib/new_relic/agent/instrumentation/net_http/chain.rb
@@ -9,17 +9,17 @@ module NewRelic::Agent::Instrumentation
         Net::HTTP.class_eval do
           def request_with_newrelic_trace(request, *args, &block)
             wrapped_request = NewRelic::Agent::HTTPClients::NetHTTPRequest.new(self, request)
-    
+
             segment = NewRelic::Agent::Tracer.start_external_request_segment(
               library: wrapped_request.type,
               uri: wrapped_request.uri,
               procedure: wrapped_request.method
             )
-    
+
             begin
               response = nil
               segment.add_request_headers wrapped_request
-    
+
               # RUBY-1244 Disable further tracing in request to avoid double
               # counting if connection wasn't started (which calls request again).
               NewRelic::Agent.disable_all_tracing do
@@ -27,15 +27,15 @@ module NewRelic::Agent::Instrumentation
                   request_without_newrelic_trace(request, *args, &block)
                 end
               end
-    
+
               wrapped_response = NewRelic::Agent::HTTPClients::NetHTTPResponse.new response
               segment.process_response_headers wrapped_response
               response
             ensure
               segment.finish
             end
-          end  
-    
+          end
+
           alias request_without_newrelic_trace request
           alias request request_with_newrelic_trace
         end
