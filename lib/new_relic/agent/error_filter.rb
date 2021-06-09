51f86ee97757d03b584738fab822b6b722bad80b
Aaron Huntsman <aaron.m.huntsman@gmail.com>
2021-06-09 16:59:26 -0400

    add skeleton tests and ErrorFilter class

---
+++ lib/new_relic/agent/error_filter.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+
+module NewRelic
+  module Agent
+
+    # Handles loading of ignored and expected errors from the agent configuration, and
+    # determining at runtime whether an exception is ignored or expected.
+    class ErrorFilter
+    end
+  end
+end
