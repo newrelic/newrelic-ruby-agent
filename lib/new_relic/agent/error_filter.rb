c2aed02937e3085a8c0dd0e473a91f3785f88f6c
Aaron Huntsman <aaron.m.huntsman@gmail.com>
2021-06-14 21:22:39 -0400

    more fixes; add collector configs to default source

--- lib/new_relic/agent/error_filter.rb
+++ lib/new_relic/agent/error_filter.rb
@@ -8,84 +8,131 @@ module NewRelic
     # Handles loading of ignored and expected errors from the agent configuration, and
     # determining at runtime whether an exception is ignored or expected.
     class ErrorFilter
+
       def initialize
-        reload
+        @ignore_errors, @ignore_classes, @expected_classes = [], [], []
+        @ignore_messages, @expected_messages = {}, {}
+        @ignore_status_codes, @expected_status_codes = [], []
       end
 
-      # Load ignored/expected errors from current agent config
-      def reload
-        @ignored_classes      = fetch_agent_config(:ignore_classes) || []
-        @ignored_messages     = fetch_agent_config(:ignore_messages) || {}
-        @ignored_status_codes = fetch_agent_config(:ignore_status_codes) || ''
+      def load_all
+        %i(
+          ignore_errors ignore_classes ignore_messages ignore_status_codes
+          expected_classes expected_messages expected_status_codes
+        ).each { |setting| load_from_config(setting) }
+      end
 
-        @expected_classes      = fetch_agent_config(:expected_classes) || []
-        @expected_messages     = fetch_agent_config(:expected_messages) || {}
-        @expected_status_codes = fetch_agent_config(:expected_status_codes) || ''
-
-        # error_collector.ignore_errors is deprecated, but we still support it
-        if @ignored_classes.empty? && ignore_errors = fetch_agent_config(:ignore_errors)
-          @ignored_classes << ignore_errors.split(',').map!(&:strip)
-          @ignored_classes.flatten!
+      def load_from_config(setting, value = nil)
+        errors = nil
+        new_value = value || fetch_agent_config(setting.to_sym)
+        case setting.to_sym
+        when :ignore_errors  # Deprecated; only use if ignore_classes isn't present
+          errors = @ignore_errors = new_value.to_s.split(',').map!(&:strip)
+        when :ignore_classes
+          errors = @ignore_classes = new_value || []
+        when :ignore_messages
+          errors = @ignore_messages = new_value || {}
+        when :ignore_status_codes
+          errors = @ignore_status_codes = parse_status_codes(new_value) || []
+        when :expected_classes
+          errors = @expected_classes = new_value || []
+        when :expected_messages
+          errors = @expected_messages = new_value || {}
+        when :expected_status_codes
+          errors = @expected_status_codes = parse_status_codes(new_value) || []
         end
-
-        log_filters
-      end
-
-      # Takes an Exception object. Depending on whether the Agent is configured
-      # to treat the exception as Ignored, Expected or neither, returns :ignored,
-      # :expected or nil, respectively.
-      def type_for_exception(ex)
-        return nil unless ex.is_a?(Exception)
-        return :ignored if _ignored?(ex)
-        return :expected if _expected?(ex)
-        nil
+        log_filter(setting, errors)
       end
 
       # Define #ignored? and #expected? in this way so that any given exception
-      # cannot be both ignored and expected. Ignoring takes priority.
+      # cannot be both ignored and expected when using type_for_exception.
+      # Ignoring takes priority.
 
-      def ignored?(ex)
-        type_for_exception(ex) == :ignored
+      def ignore?(ex)
+        @ignore_classes.include?(ex.class.name) || 
+          (@ignore_classes.empty? && @ignore_errors.include?(ex.class.name)) ||
+          @ignore_messages.keys.include?(ex.class.name) &&
+          @ignore_messages[ex.class.name].any? { |m| ex.message.include?(m) }
       end
 
       def expected?(ex)
-        type_for_exception(ex) == :expected
+        @expected_classes.include?(ex.class.name) ||
+        @expected_messages.keys.include?(ex.class.name) &&
+        @expected_messages[ex.class.name].any? { |m| ex.message.include?(m) }
       end
 
       def fetch_agent_config(cfg)
         NewRelic::Agent.config[:"error_collector.#{cfg}"]
       end
 
+      def ignore(errors)
+        case errors
+        when Array
+          @ignore_classes += errors
+          log_filter(:ignore_classes, errors)
+        when Hash
+          @ignore_messages.update(errors)
+          log_filter(:ignore_messages, errors)
+        when String
+          if errors.match(/^[\d\,\-]+$/)
+            @ignore_status_codes += parse_status_codes(errors)  # TODO: convert this value to a Hash
+          else
+            new_ignore_classes = errors.split(',').map!(&:strip)
+            @ignore_classes += new_ignore_classes
+            log_filter(:ignore_classes, new_ignore_classes)
+          end
+        end
+      end
+
+      def expect(errors)
+        case errors
+        when Array
+          @expected_classes += errors
+          log_filter(:expected_classes, errors)
+        when Hash
+          @expected_messages.update(errors)
+          log_filter(:expected_messages, errors)
+        when String
+          if errors.match(/^[\d\,\-]+$/)
+            @expected_status_codes += parse_status_codes(errors)
+          else
+            new_expected_classes = errors.split(',').map!(&:strip)
+            @expected_classes += new_expected_classes
+            log_filter(:expected_classes, new_expected_classes)
+          end
+        end
+      end
+
       private
 
-      def _ignored?(ex)
-        @ignored_classes.include?(ex.class.name) ||
-          @ignored_messages.keys.include?(ex.class.name) &&
-          @ignored_messages[ex.class.name].any? { |m| ex.message.include?(m) }
+      def log_filter(setting, errors)
+        case setting
+        when :ignore_errors, :ignore_classes
+          errors.each do |error|
+            ::NewRelic::Agent.logger.debug("Ignoring errors of type '#{error}'")
+          end
+        when :ignore_messages
+          errors.each do |error,messages|
+            ::NewRelic::Agent.logger.debug("Ignoring errors of type '#{error}' with messages: #{messages.join(',')}")
+          end
+        when :ignore_status_codes
+          ::NewRelic::Agent.logger.debug("Ignoring errors associated with status codes: #{errors}")
+        when :expected_classes
+          errors.each do |error|
+            ::NewRelic::Agent.logger.debug("Expecting errors of type '#{error}'")
+          end
+        when :expected_messages
+          errors.each do |error,messages|
+            ::NewRelic::Agent.logger.debug("Expecting errors of type '#{error}' with messages: #{messages.join(',')}")
+          end
+        when :expected_status_codes
+          ::NewRelic::Agent.logger.debug("Expecting errors associated with status codes: #{errors}")
+        end
       end
 
-      def _expected?(ex)
-        @expected_classes.include?(ex.class.name) ||
-          @expected_messages.keys.include?(ex.class.name) &&
-          @expected_messages[ex.class.name].any? { |m| ex.message.include?(m) }
-      end
-
-      def log_filters
-        @ignored_classes.each do |c|
-          ::NewRelic::Agent.logger.debug("Ignoring errors of type '#{c}'")
-        end
-        @ignored_messages.each do |k,v|
-          ::NewRelic::Agent.logger.debug("Ignoring errors of type '#{k}' with messages: " + v.join(', '))
-        end
-        ::NewRelic::Agent.logger.debug("Ignoring status codes #{@ignored_status_codes}") unless @ignored_status_codes.empty?
-
-        @expected_classes.each do |c|
-          ::NewRelic::Agent.logger.debug("Expecting errors of type '#{c}'")
-        end
-        @expected_messages.each do |k,v|
-          ::NewRelic::Agent.logger.debug("Expecting errors of type '#{k}' with messages: " + v.join(', '))
-        end
-        ::NewRelic::Agent.logger.debug("Expecting status codes #{@expected_status_codes}") unless @expected_status_codes.empty?
+      def parse_status_codes(code_string)
+        # TODO: implement
+        []
       end
     end
   end
