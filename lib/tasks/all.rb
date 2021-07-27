a2bca56e43f5b9f55028bca5a25cc3eeade95578
Kayla Reopelle <kreopelle@newrelic.com>
2021-07-27 14:07:51 -0700

    Update requirement comment

--- lib/tasks/all.rb
+++ lib/tasks/all.rb
@@ -2,7 +2,7 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 
-# This is required to load in task definitions from merb
+# This is required to load in task definitions
 Dir.glob(File.join(File.dirname(__FILE__),'*.rake')) do |file|
   load file
 end
