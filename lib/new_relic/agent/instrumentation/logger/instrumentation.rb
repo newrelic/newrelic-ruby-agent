b4bc0a87a27b39c095a6885756981135069cee50
Jason R. Clark <jclark@newrelic.com>
2021-09-27 17:06:46 +0200

    Gather size after formatting, more tests

--- lib/new_relic/agent/instrumentation/logger/instrumentation.rb
+++ lib/new_relic/agent/instrumentation/logger/instrumentation.rb
@@ -10,25 +10,34 @@ module NewRelic
           defined?(@skip_instrumenting) && @skip_instrumenting
         end
 
+        def mark_skip_instrumenting
+          @skip_instrumenting = true
+        end
+
+        def clear_skip_instrumenting
+          @skip_instrumenting = false
+        end
+
         def format_message_with_tracing(severity, datetime, progname, msg)
-          return yield if skip_instrumenting?
+          formatted_message = yield
+          return formatted_message if skip_instrumenting?
 
           begin
             # It's critical we don't instrumention further logging from our
             # metric recording or we'll stack overflow!!
-            @skip_instrumenting = true
+            mark_skip_instrumenting
 
             sev = severity || UNKNOWN
             NewRelic::Agent.increment_metric("Logging/lines")
             NewRelic::Agent.increment_metric("Logging/lines/#{sev}")
 
-            size = msg.nil? ? 0 : msg.length
+            size = formatted_message.nil? ? 0 : formatted_message.length
             NewRelic::Agent.record_metric("Logging/size", size)
             NewRelic::Agent.record_metric("Logging/size/#{sev}", size)
 
-            yield
+            return formatted_message
           ensure
-            @skip_instrumenting = false
+            clear_skip_instrumenting
           end
         end
       end
--- test/new_relic/agent/instrumentation/logger/instrumentation_test.rb
+++ test/new_relic/agent/instrumentation/logger/instrumentation_test.rb
@@ -16,10 +16,6 @@ class NewRelic::Agent::Instrumentation::LoggerTest < Minitest::Test
     NewRelic::Agent.instance.stats_engine.reset!
   end
 
-  def test_doesnt_touch_construction
-    assert_metrics_recorded_exclusive []
-  end
-
   LEVELS = [
     ['debug', Logger::DEBUG],
     ['error', Logger::ERROR],
@@ -34,6 +30,24 @@ class NewRelic::Agent::Instrumentation::LoggerTest < Minitest::Test
       @logger.send(name, "A message")
       assert_equal(1, @written.string.lines.count)
       assert_match(/#{name.upcase}.*A message/, @written.string)
+      assert_logging_metrics(name.upcase)
+    end
+
+    define_method("test_records_multiple_calls_#{name}") do
+      @logger.send(name, "A message")
+      @logger.send(name, "Another")
+
+      assert_equal(2, @written.string.lines.count)
+      assert_match(/#{name.upcase}.*A message/, @written.string)
+      assert_match(/#{name.upcase}.*Another/, @written.string)
+      assert_logging_metrics(name.upcase, 2)
+    end
+
+    # logger#debug(Object.new)
+    define_method("test_records_not_a_string_#{name}") do
+      @logger.send(name, Object.new)
+      assert_equal(1, @written.string.lines.count)
+      assert_match(/#{name.upcase}.*<Object.*>/, @written.string)
       assert_logging_metrics(name.upcase)
     end
 
@@ -73,6 +87,13 @@ class NewRelic::Agent::Instrumentation::LoggerTest < Minitest::Test
     end
   end
 
+  def test_still_skips_levels
+    @logger.level = ::Logger::INFO
+    @logger.debug("Won't see this")
+    assert_equal(0, @written.string.lines.count)
+    refute_any_logging_metrics()
+  end
+
   def test_unknown
     @logger.unknown("A message")
     assert_equal(1, @written.string.lines.count)
@@ -94,14 +115,27 @@ class NewRelic::Agent::Instrumentation::LoggerTest < Minitest::Test
     assert_logging_metrics("ANY")
   end
 
-  def assert_logging_metrics(label)
-    assert_metrics_recorded_exclusive [
-      "Logging/lines",
-      "Logging/lines/#{label}",
-      "Logging/size",
-      "Logging/size/#{label}",
-      "Supportability/API/increment_metric",
-      "Supportability/API/record_metric",
-    ]
+  def test_skips_when_set
+    @logger.mark_skip_instrumenting
+    @logger.log(1_000_000, "A message", "progname")
+
+    assert_equal(1, @written.string.lines.count)
+    assert_match(/ANY.*progname.*A message/, @written.string)
+    refute_any_logging_metrics()
+  end
+
+  def refute_any_logging_metrics
+    assert_metrics_recorded_exclusive([])
+  end
+
+  def assert_logging_metrics(label, count = 1)
+    assert_metrics_recorded_exclusive({
+      "Logging/lines"          => { :call_count => count },
+      "Logging/lines/#{label}" => { :call_count => count },
+      "Logging/size" => {},
+      "Logging/size/#{label}" => {},
+      "Supportability/API/increment_metric" => {},
+      "Supportability/API/record_metric" => {}
+    })
   end
 end
