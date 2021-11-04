2cc897580223536003b05ed50369a70ef486a2b4
Kayla Reopelle <kreopelle@newrelic.com>
2021-11-04 14:14:29 -0700

    Create structure for tilt instrumentation

---
+++ lib/new_relic/agent/instrumentation/tilt/chain.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+
+module NewRelic::Agent::Instrumentation
+  module Tilt
+    def self.instrument!
+      # no-op
+    end
+  end
+end
