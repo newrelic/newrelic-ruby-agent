5b04c2afa0f7fe4eb139b520a7fa9f0dcaacaff4
Kayla Reopelle <kreopelle@newrelic.com>
2021-11-17 16:43:22 -0800

    Add missing license statement

--- test/environments/rails70a/config/boot.rb
+++ test/environments/rails70a/config/boot.rb
@@ -1,3 +1,7 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
