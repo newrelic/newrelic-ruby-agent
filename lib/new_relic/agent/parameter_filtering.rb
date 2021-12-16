4978f4155bdab9790d569d546a19eff96b41a7a9
Kayla Reopelle <kreopelle@newrelic.com>
2021-12-15 16:13:11 -0800

    Add condition to check Rails version

--- lib/new_relic/agent/parameter_filtering.rb
+++ lib/new_relic/agent/parameter_filtering.rb
@@ -9,7 +9,7 @@ module NewRelic
 
       ACTION_DISPATCH_PARAMETER_FILTER = "action_dispatch.parameter_filter".freeze
 
-      if defined?(Rails)
+      if defined?(Rails) && Gem::Version.new(::Rails::VERSION::STRING) >= Gem::Version.new('5.0.0')
         Rails.application.config.to_prepare do
           RAILS_FILTER_CLASS = if defined?(ActiveSupport::ParameterFilter)
              ActiveSupport::ParameterFilter
@@ -20,7 +20,13 @@ module NewRelic
           end
         end
       else
-        RAILS_FILTER_CLASS = nil
+        RAILS_FILTER_CLASS = if defined?(ActiveSupport::ParameterFilter)
+           ActiveSupport::ParameterFilter
+        elsif defined?(ActionDispatch::Http::ParameterFilter)
+          ActionDispatch::Http::ParameterFilter
+        else
+          nil
+        end
       end
 
       def apply_filters(env, params)
