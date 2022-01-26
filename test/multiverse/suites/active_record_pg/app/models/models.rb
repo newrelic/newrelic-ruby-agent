8872d80f0e7df09d0c50fdcd75b4a6413735681d
Tanna McClure <tmcclure@newrelic.com>
2022-01-26 15:50:31 -0800

    added require for 2.2.10

--- test/multiverse/suites/active_record_pg/app/models/models.rb
+++ test/multiverse/suites/active_record_pg/app/models/models.rb
@@ -3,6 +3,7 @@
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 
 require 'new_relic/agent/datastores'
+require 'pg'
 
 class User < ActiveRecord::Base
   has_many :aliases, :dependent => :destroy
