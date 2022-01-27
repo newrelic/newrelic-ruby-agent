14cf731356ccb99329cea89010e0bb50bb17046f
Tanna McClure <tmcclure@newrelic.com>
2022-01-26 16:23:21 -0800

    update active record migrations to use 6 or 5 depending on version

--- test/multiverse/suites/active_record_pg/db/migrate/20150413011200_add_timestamps_to_orders.rb
+++ test/multiverse/suites/active_record_pg/db/migrate/20150413011200_add_timestamps_to_orders.rb
@@ -2,7 +2,7 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 
-class AddTimestampsToOrders < ActiveRecord::VERSION::STRING >= "5.0.0" ? ActiveRecord::Migration[6.0] : ActiveRecord::Migration
+class CreateUsersAndAliases < ActiveRecord::VERSION::STRING >= "5.0.0" ? ActiveRecord::Migration["#{ActiveRecord::VERSION::STRING[0]}.0"] : ActiveRecord::Migration
   def self.up
     change_table(:orders) do |t|
       t.timestamps
