2c29e2cceedcfbc1a2456b6504bb79ff86f3c195
fallwith <fallwith@gmail.com>
2022-02-10 17:17:38 -0800

    .-.

--- .github/actions/exec-tester/dist/index.js
+++ .github/actions/exec-tester/dist/index.js
@@ -21,20 +21,8 @@ const io = __nccwpck_require__(7436)
 
 async function main() {
   try {
+    await exec.exec('sudo' [`${process.env.GITHUB_WORKSPACE}/test/script/install_mysql55`])
     const mysql55Path = '/usr/local/mysql1138'
-    await exec.exec('wget', ['https://github.com/mysql/mysql-server/archive/refs/tags/mysql-5.5.63.tar.gz'])
-    await exec.exec('tar', ['xzf', 'mysql-5.5.63.tar.gz'])
-    await exec.exec('cd', ['mysql-server-mysql-5.5.63/'])
-    await exec.exec('cmake', ['.', `-DCMAKE_INSTALL_PREFIX=${mysql55Path}`,
-                                   `-DMYSQL_DATADIR=${mysql55Path}/data`,
-                                   '-DDOWNLOAD_BOOST=1',
-                                   '-DWITH_BOOST=/tmp/boost',
-                                   '-DWITH_SSL=bundled'])
-    await exec.exec('make')
-    await exec.exec('make', ['install'])
-    await exec.exec('cd', ['..'])
-    await exec.exec('rm', ['-rf', 'mysql-5.5.63.tar.gz', 'mysql-server-mysql-5.5.63'])
-
     await exec.exec(`ls -l ${mysql55Path}`)
   }
   catch (error) {
