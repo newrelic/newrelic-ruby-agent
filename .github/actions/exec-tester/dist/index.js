dc7c63a346a8e3f899017a8881f0593390264fa0
fallwith <fallwith@gmail.com>
2022-02-10 16:52:19 -0800

    array

--- .github/actions/exec-tester/dist/index.js
+++ .github/actions/exec-tester/dist/index.js
@@ -22,18 +22,18 @@ const io = __nccwpck_require__(7436)
 async function main() {
   try {
     const mysql55Path = '/usr/local/mysql1138'
-    await exec.exec('wget', `https://github.com/mysql/mysql-server/archive/refs/tags/mysql-5.5.63.tar.gz \
-               && tar xzf mysql-5.5.63.tar.gz \
-               && cd mysql-server-mysql-5.5.63/ \
-               && cmake . -DCMAKE_INSTALL_PREFIX=${mysql55Path} \
-               -DMYSQL_DATADIR=${mysql55Path}/data \
-               -DDOWNLOAD_BOOST=1 \
-               -DWITH_BOOST=/tmp/boost \
-               -DWITH_SSL=bundled \
-               && make \
-               && make install \
-               && cd .. \
-               && rm -rf mysql-5.5.63.tar.gz mysql-server-mysql-5.5.63`)
+    await exec.exec('wget', ['https://github.com/mysql/mysql-server/archive/refs/tags/mysql-5.5.63.tar.gz',
+                             '&&', 'tar', 'xzf', 'mysql-5.5.63.tar.gz',
+                             '&&', 'cd', 'mysql-server-mysql-5.5.63/',
+                             '&&', 'cmake', '.', `-DCMAKE_INSTALL_PREFIX=${mysql55Path}`,
+                             `-DMYSQL_DATADIR=${mysql55Path}/data`,
+                             '-DDOWNLOAD_BOOST=1',
+                             '-DWITH_BOOST=/tmp/boost',
+                             '-DWITH_SSL=bundled',
+                             '&&', 'make',
+                             '&&', 'make', 'install',
+                             '&&', 'cd', '..',
+                             '&&', 'rm', '-rf', 'mysql-5.5.63.tar.gz', 'mysql-server-mysql-5.5.63'])
 
     await exec.exec(`ls -l ${mysql55Path}`)
   }
