ca1ac5032ae305298d57e0cd103f88d5e76bb434
fallwith <fallwith@gmail.com>
2022-02-10 16:31:05 -0800

    await

--- .github/actions/exec-tester/dist/index.js
+++ .github/actions/exec-tester/dist/index.js
@@ -22,7 +22,7 @@ const io = __nccwpck_require__(7436)
 async function main() {
   try {
     const mysql55Path = '/usr/local/mysql1138'
-    exec.exec(`wget https://github.com/mysql/mysql-server/archive/refs/tags/mysql-5.5.63.tar.gz \
+    await exec.exec(`wget https://github.com/mysql/mysql-server/archive/refs/tags/mysql-5.5.63.tar.gz \
                && tar xzf mysql-5.5.63.tar.gz \
                && cd mysql-server-mysql-5.5.63/ \
                && cmake . -DCMAKE_INSTALL_PREFIX=${mysql55Path} \
@@ -35,7 +35,7 @@ async function main() {
                && cd .. \
                && rm -rf mysql-5.5.63.tar.gz mysql-server-mysql-5.5.63`)
 
-    exec.exec(`ls -l ${mysql55Path}`)
+    await exec.exec(`ls -l ${mysql55Path}`)
   }
   catch (error) {
     core.setFailed(`Action failed with error ${error}`)
--- .github/actions/exec-tester/index.js
+++ .github/actions/exec-tester/index.js
@@ -11,7 +11,7 @@ const io = require('@actions/io')
 async function main() {
   try {
     const mysql55Path = '/usr/local/mysql1138'
-    exec.exec(`wget https://github.com/mysql/mysql-server/archive/refs/tags/mysql-5.5.63.tar.gz \
+    await exec.exec(`wget https://github.com/mysql/mysql-server/archive/refs/tags/mysql-5.5.63.tar.gz \
                && tar xzf mysql-5.5.63.tar.gz \
                && cd mysql-server-mysql-5.5.63/ \
                && cmake . -DCMAKE_INSTALL_PREFIX=${mysql55Path} \
@@ -24,7 +24,7 @@ async function main() {
                && cd .. \
                && rm -rf mysql-5.5.63.tar.gz mysql-server-mysql-5.5.63`)
 
-    exec.exec(`ls -l ${mysql55Path}`)
+    await exec.exec(`ls -l ${mysql55Path}`)
   }
   catch (error) {
     core.setFailed(`Action failed with error ${error}`)
