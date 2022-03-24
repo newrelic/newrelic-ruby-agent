deb109e1515c89005e07c56b19f23bf25cfc87c7
Kayla Reopelle (she/her) <87386821+kaylareopelle@users.noreply.github.com>
2022-03-24 08:43:16 -0700

    Return magic comment to file

--- lib/new_relic/agent/local_log_decorator.rb
+++ lib/new_relic/agent/local_log_decorator.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+# frozen_string_literal: true
 
 module NewRelic
   module Agent
-    #
     # This module contains helper methods related to decorating log messages
     module LocalLogDecorator
       extend self
