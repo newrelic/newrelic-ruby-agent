72c04f1bb4a315357ce99bc8df34715ed869f6b1
Tanna McClure <tmcclure@newrelic.com>
2022-04-13 15:50:54 -0700

    update NewRelic::TracedThread documentation

--- lib/new_relic/traced_thread.rb
+++ lib/new_relic/traced_thread.rb
@@ -6,14 +6,20 @@
 module NewRelic
   #
   # This class allows the state of a transaction to be passed to a Thread so that nested segments can be created from the operations performed within the Thread's block.
-  # In order to have this functionality inserted into all threads automatically,
+  # To have the New Relic Ruby agent automatically trace all of your applications threads,
   # enable the `instrumentation.thread.tracing` configuration option in your newrelic.yml
+  #
+  # Note: disabling the configuration option `instrumentation.thread` while using this class can cause incorrectly nested spans
   #
   # @api public
   class TracedThread < Thread
     #
-    # Creates a new thread that will be traced by the agent.
-    # Use this class the same as the Thread class
+    # Creates a new Thread whose work will be traced by New Relic.
+    # Use this class as a replacement for the native Thread class.
+    # Example: instead of using `Thread.new`, use
+    # ```ruby
+    #   NewRelic::TracedThread.new { execute_some_code }
+    # ```
     #
     # @api public
     def initialize(*args, &block)
