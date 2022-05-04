7753eac797e55271bc616031da29b11d3ba80247
Tanna McClure <tmcclure@newrelic.com>
2022-05-04 15:13:51 -0700

    removed expect_mock: false when it shouldn't be there

--- infinite_tracing/test/infinite_tracing/agent_integrations/segment_test.rb
+++ infinite_tracing/test/infinite_tracing/agent_integrations/segment_test.rb
@@ -73,7 +73,7 @@ module NewRelic
         end
 
         def test_ignored_transaction_does_not_record_span_event
-          span_events = generate_and_stream_segments(expect_mock: false) do
+          span_events = generate_and_stream_segments do
             in_transaction('wat') do |txn|
               txn.stubs(:ignore?).returns(true)
 
