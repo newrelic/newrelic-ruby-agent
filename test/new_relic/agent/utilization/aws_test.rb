590993706242828105fadfcdb9b7182cc2232890
fallwith <fallwith@gmail.com>
2022-05-09 15:19:47 -0700

    Ruby < 2.4 doesn't support String#match?
    use `=~` instead of `#match?` for older Rubies

--- test/new_relic/agent/utilization/aws_test.rb
+++ test/new_relic/agent/utilization/aws_test.rb
@@ -86,10 +86,7 @@ module NewRelic
           define_method("test_#{test_case[:testname]}".gsub(" ", "_")) do
             NewRelic::Agent::Utilization::AWS.stubs(:imds_token).returns('John Howe')
             uri_obj_key = test_case[:uri].keys.detect do |key|
-              # TODO: OLD RUBIES - RUBY_VERSION < 2.4
-              # Ruby 2.4 added Symbol#match? Earlier versions need to convert
-              # the symbol to a String first
-              key.to_s.match?(%r{http://169.254.169.254/(?:2016-09-02|latest)/dynamic/instance-identity/document})
+              key =~ %r{http://169.254.169.254/(?:2016-09-02|latest)/dynamic/instance-identity/document}
             end
             uri_obj = test_case[:uri][uri_obj_key]
             if uri_obj[:timeout]
