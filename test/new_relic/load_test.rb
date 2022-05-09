327ab258133bebcec95fae61a25579dfa96d4367
hramadan <hramadan@newrelic.com>
2022-05-09 14:48:14 -0700

    Update 'require_relative' test/nullverse

--- test/new_relic/load_test.rb
+++ test/new_relic/load_test.rb
@@ -2,9 +2,9 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 
+require_relative '../test_helper'
 require 'resolv'
 require 'mocha/setup'
-require_relative '../test_helper'
 
 class LoadTest < Minitest::Test
   def test_loading_agent_when_disabled_does_not_resolv_addresses
