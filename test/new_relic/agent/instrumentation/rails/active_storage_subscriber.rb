5e32d58a2da6eb73796dc3f3ed9d05dd83d93e18
hramadan <hramadan@newrelic.com>
2022-05-11 16:11:40 -0700

    Test fix

--- test/new_relic/agent/instrumentation/rails/active_storage_subscriber.rb
+++ test/new_relic/agent/instrumentation/rails/active_storage_subscriber.rb
@@ -2,7 +2,7 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 
-require_relative '../../../../../../test_helper'
+require_relative '../../../../../test_helper'
 
 module NewRelic
   module Agent
