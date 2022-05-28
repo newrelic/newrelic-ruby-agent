954628aee853e83a511e9228ff3e076d18d59537
fallwith <fallwith@gmail.com>
2022-05-27 18:32:33 -0700

    remove Hometown
    * Remove Hometown - its implementation as a Thread watcher is especially
      problematic with Rails 7
    * AR connection pool handler: make sure #spec if defined before invoking
      it

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,11 +19,6 @@ require 'minitest/pride' unless ENV['CI']
 require 'minitest/stub_const'
 require 'mocha/setup'
 
-require 'hometown'
-require_relative 'helpers/hometown_monkey_patch'
-
-Hometown.watch(::Thread)
-
 Dir[File.expand_path('../helpers/*', __FILE__)].each { |f| require f.sub(/.*test\//, '') }
 
 # We can speed things up in tests that don't need to load rails.
