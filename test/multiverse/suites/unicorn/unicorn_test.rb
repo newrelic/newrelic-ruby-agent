81c6a022dc8bef5669ced96c5fb03e9519f8ca55
Kayla Reopelle <kreopelle@newrelic.com>
2022-06-17 12:04:00 -0700

    Create Unicorn multiverse suite

---
+++ test/multiverse/suites/unicorn/unicorn_test.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+# frozen_string_literal: true
+
+require 'unicorn'
+require 'newrelic_rpm'
+
+class UnicornTest < Minitest::Test
+  include MultiverseHelpers
+  attr_accessor :server
+
+  def setup
+    File.truncate('log/newrelic_agent.log', 0)
+    @server = ::Unicorn::HttpServer.new(
+      Rack::Builder.app(lambda { [200, {'Content-Type' => 'text/html'}, ['OK']] })
+    )
+    @server.start
+    trigger_agent_reconnect
+  end
+
+  def teardown
+    @server.stop
+  end
+
+  def test_unicorn_set_as_discovered_dispatcher
+    assert_logged('Dispatcher: unicorn')
+  end
+
+  def test_defer_message_logged_when_unicorn_in_use
+    assert_logged('Deferring startup of agent reporting thread because unicorn may fork.')
+  end
+
+  def assert_logged(expected_message)
+    messages = File.readlines('log/newrelic_agent.log')
+    messages.each { |msg| puts msg } # Remove this before merge
+    assert(
+      messages.any? { |message| message.include?(expected_message) },
+      "Expected message not found: #{expected_message}"
+    )
+  end
+end
