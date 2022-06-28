bf1797660136479e41f1baa1956280a18d96c94d
hramadan <hramadan@newrelic.com>
2022-06-28 13:17:54 -0700

    Add context comments for SimpleCov version requirements

--- test/simplecov_test_helper.rb
+++ test/simplecov_test_helper.rb
@@ -2,7 +2,12 @@
 # This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 
-require 'simplecov' if RUBY_VERSION >= '2.7.0'
+# The minimum version of Ruby required (by New Relic) to run SimpleCov
+# NOTE: At the time that SimpleCov was introduced to this code base, it
+# required Ruby >= 2.5.0 and Ruby 2.6.0 was marked for EOL
+SIMPLECOV_MIN_RUBY_VERSION = '2.7.0'
+
+require 'simplecov' if RUBY_VERSION >= SIMPLECOV_MIN_RUBY_VERSION
 
 module SimpleCovHelper
   def self.command_name suite_name
