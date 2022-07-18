253083c13efc66316ef8fa67c1f64e040f3fed8a
Kayla Reopelle <kreopelle@newrelic.com>
2022-07-18 15:27:08 -0700

    Start transaction or segment in the gRPC Server

--- lib/new_relic/agent/instrumentation/grpc/server/instrumentation.rb
+++ lib/new_relic/agent/instrumentation/grpc/server/instrumentation.rb
@@ -9,13 +9,32 @@ module NewRelic
         module Server
           def handle_with_tracing(active_call, mth, inter_ctx)
             return yield unless trace_with_newrelic?
-
             trace_headers = active_call.metadata.delete(NewRelic::NEWRELIC_KEY)
             ::NewRelic::Agent::DistributedTracing::accept_distributed_trace_headers(trace_headers, 'Other') if ::NewRelic::Agent.config[:'distirbuted_tracing.enabled']
+
+            finishable = NewRelic::Agent::Tracer.start_transaction_or_segment(
+              name: mth.original_name,
+              category: :web,
+              options: server_options(active_call.metadata, mth.original_name)
+            )
             yield
+          ensure
+            finishable.finish
           end
 
           private
+
+          def server_options(headers, method)
+            host = 'host'
+            port = 'port'
+            {
+              request: {
+                headers: headers,
+                uri: "grpc://#{host}:#{port}/#{method}",
+                method: method
+              }
+            }
+          end
 
           def trace_with_newrelic?(host = nil)
             # TODO: check hostname against the configured denylist
