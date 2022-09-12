3528d6005081f92af1a86c7e4460b3c51dd7e721
Tanna McClure <tmcclure@newrelic.com>
2022-09-12 13:43:51 -0700

    return true

--- test/helpers/misc.rb
+++ test/helpers/misc.rb
@@ -35,7 +35,7 @@ def fixture_tcp_socket(response)
     s.stubs(:closed?).returns(false)
     s.stubs(:close)
     s.stubs(:setsockopt)
-    s.stubs(:sync).returns(String.new("meow"))
+    s.stubs(:sync).returns(true)
 
     # Simulate a bunch of socket-ey stuff since Mocha doesn't really
     # provide any other way to do it
