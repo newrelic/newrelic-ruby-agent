e557e84aa0c8f3661cd9e5f95b92f79768da8d76
Hannah Ramadan <76922290+hannahramadan@users.noreply.github.com>
2022-09-28 14:43:28 -0700

    Fix order of assert_equal

--- test/new_relic/agent/error_trace_aggregator_test.rb
+++ test/new_relic/agent/error_trace_aggregator_test.rb
@@ -139,7 +139,7 @@ module NewRelic
         end
 
         errors = error_trace_aggregator.harvest!
-        assert_equal(errors.length, max_q_length)
+        assert_equal(max_q_length, errors.length)
         errors.each_index do |i|
           error = errors.shift
           actual = error.to_collector_array.last["userAttributes"]["x"]
