b721d223cec4480f604d556868495f10eda0c401
Kayla Reopelle <kreopelle@newrelic.com>
2022-10-03 17:13:45 -0700

    Create datastore segment on #perform_request call
    This first pass at the data segment finds dynamic values
    for some attributes and hard codes others.
    
    This misses any reporting on the arguments passed to perform_request
    We should find a way to include those arguments in the segment

--- lib/new_relic/agent/instrumentation/elasticsearch/instrumentation.rb
+++ lib/new_relic/agent/instrumentation/elasticsearch/instrumentation.rb
@@ -4,8 +4,45 @@
 
 module NewRelic::Agent::Instrumentation
   module Elasticsearch
+    PRODUCT_NAME = 'Elasticsearch'
+    OPERATION = 'query'
+
     def perform_request_with_tracing(*args)
-      # add instrumentation content here
+      # args = method, path, params = {}, body = nil
+      # does updating your indicies hit perform_request?
+      segment = NewRelic::Agent::Tracer.start_datastore_segment(
+        product: PRODUCT_NAME,
+        operation: OPERATION,
+        host: host,
+        port_path_or_id: port,
+        database_name: cluster_name
+      )
+      begin
+        # add attributes for all method args?
+        # right now, no query data/arguments are preserved
+        NewRelic::Agent::Tracer.capture_segment_error(segment) { yield }
+      ensure
+        segment.finish if segment
+      end
+    end
+
+    private
+
+    def cluster_name
+      # this makes a call to perform_request, so not ideal
+      cluster.stats['cluster_name']
+    end
+
+    def hosts
+      (transport.hosts.first || NewRelic::EMPTY_HASH)
+    end
+
+    def host
+      hosts[:host]
+    end
+
+    def port
+      hosts[:port]
     end
   end
 end
