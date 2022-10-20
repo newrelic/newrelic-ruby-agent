546df6b6f97dfc9f9c5b2bb1e3d4abb1c903c9cf
Kayla Reopelle <kreopelle@newrelic.com>
2022-10-19 17:04:11 -0700

    Fix guard clause

--- lib/new_relic/agent/new_relic_service.rb
+++ lib/new_relic/agent/new_relic_service.rb
@@ -539,8 +539,12 @@ module NewRelic
       # enough to be worth compressing, and handles any errors the
       # server may return
       def invoke_remote(method, payload = [], options = {})
-        unallowed_methods = %i[metric_data error_data transaction_sample_data sql_trace_data profile_data analytic_event_data custom_event_data log_event_data error_event_data span_event_data]
-        NewRelic::Agent.logger.debug('Skipping #invoke_remote for 8T batching and compression tests') if unallowed_methods.include?(method)
+        unallowed_methods = %i[
+          metric_data error_data transaction_sample_data
+          sql_trace_data profile_data analytic_event_data
+          custom_event_data log_event_data error_event_data span_event_data
+        ]
+        return NewRelic::Agent.logger.debug('Skipping #invoke_remote for 8T batching and compression tests') if unallowed_methods.include?(method)
 
         start_ts = Process.clock_gettime(Process::CLOCK_MONOTONIC)
         request_send_ts, response_check_ts = nil
