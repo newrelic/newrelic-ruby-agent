88e393ba3c79787e0011a7b03233aa4a0b7fbd67
fallwith <fallwith@gmail.com>
2022-10-27 21:28:51 -0700

    custom 8t protobuf encode/decode logic
    hoc non operatur, sed iterum officialis exsecutionem non facit

--- infinite_tracing/lib/infinite_tracing/channel.rb
+++ infinite_tracing/lib/infinite_tracing/channel.rb
@@ -5,53 +5,14 @@
 module NewRelic::Agent
   module InfiniteTracing
     class Channel
-      COMPRESSION_LEVELS = %i[none low medium high].freeze
-      DEFAULT_COMPRESSION_LEVEL = :none
-
       def stub
         NewRelic::Agent.logger.debug("Infinite Tracer Opening Channel to #{host_and_port}")
 
-        Com::Newrelic::Trace::V1::IngestService::Stub.new( \
-          host_and_port,
-          credentials,
-          channel_override: channel,
-          channel_args: channel_args
-        )
+        Com::Newrelic::Trace::V1::IngestService::Stub.new(host_and_port, credentials, channel_override: channel)
       end
 
       def channel
         GRPC::Core::Channel.new(host_and_port, settings, credentials)
-      end
-
-      def channel_args
-        return NewRelic::EMPTY_HASH unless compression_enabled?
-
-        GRPC::Core::CompressionOptions.new(compression_options).to_channel_arg_hash
-      end
-
-      def compression_enabled?
-        compression_level != :none
-      end
-
-      def compression_level
-        @compression_level ||= begin
-          level = if valid_compression_level?(configured_compression_level)
-            configured_compression_level
-          else
-            DEFAULT_COMPRESSION_LEVEL
-          end
-          NewRelic::Agent.logger.debug("Infinite Tracer compression level set to #{level}")
-          level
-        end
-      end
-
-      def compression_options
-        {default_algorithm: :gzip,
-         default_level: compression_level}
-      end
-
-      def configured_compression_level
-        NewRelic::Agent.config[:'infinite_tracing.compression_level']
       end
 
       def credentials
@@ -64,19 +25,7 @@ module NewRelic::Agent
       end
 
       def settings
-        {
-          'grpc.minimal_stack' => 1,
-          'grpc.enable_deadline_checking' => 0
-        }
-      end
-
-      def valid_compression_level?(level)
-        return true if COMPRESSION_LEVELS.include?(level)
-
-        NewRelic::Agent.logger.error("Invalid compression level '#{level}' specified! Must be one of " \
-          "#{COMPRESSION_LEVELS.join('|')}. Using default level of '#{DEFAULT_COMPRESSION_LEVEL}'")
-
-        false
+        {'grpc.minimal_stack' => 1, 'grpc.enable_deadline_checking' => 0}
       end
     end
   end
--- infinite_tracing/lib/infinite_tracing/proto.rb
+++ infinite_tracing/lib/infinite_tracing/proto.rb
@@ -2,8 +2,10 @@
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 # frozen_string_literal: true
 
+require 'base64'
 require 'grpc'
 require 'google/protobuf'
+require 'zlib'
 
 require_relative 'proto/infinite_tracing_pb'
 require_relative 'proto/infinite_tracing_services_pb'
@@ -14,4 +16,40 @@ module NewRelic::Agent::InfiniteTracing
   SpanBatch = Com::Newrelic::Trace::V1::SpanBatch
   AttributeValue = Com::Newrelic::Trace::V1::AttributeValue
   RecordStatus = Com::Newrelic::Trace::V1::RecordStatus
+end
+
+# Monkeypath IngestService to use custom encode/decode routines
+class Com::Newrelic::Trace::V1::IngestService
+  self.marshal_class_method = :nr_encode_gzip
+  self.unmarshal_class_method = :nr_decode_gzip
+end
+
+# Define custom base64/gzip based encode/decode methods
+module NrInfiniteTracingCoders
+  def nr_decode_gzip(payload)
+    Base64.decode64(Zlib.gunzip(self.class.decode(payload)))
+  end
+
+  def nr_encode_gzip(payload)
+    Base64.encode64(Zlib.gzip(self.class.encode(payload)))
+  end
+end
+
+# Bring the encode/decode methods into all protobug classes
+module Com::Newrelic::Trace::V1
+  class Span
+    include NrInfiniteTracingCoders
+  end
+
+  class SpanBatch
+    include NrInfiniteTracingCoders
+  end
+
+  class AttributeValue
+    include NrInfiniteTracingCoders
+  end
+
+  class RecordStatus
+    include NrInfiniteTracingCoders
+  end
 end
