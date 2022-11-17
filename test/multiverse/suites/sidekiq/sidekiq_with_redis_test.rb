516dd6f4603da7609cc2b476f95b9892ce92eac4
fallwith <fallwith@gmail.com>
2022-11-17 11:22:22 -0800

    CI: improve Sidekiq/Redis combo test skipping
    improve the skipping logic
    
    - don't perform a Ruby version check to artificially limit the number of
      environments that will conduct the test. the test is not Ruby version
      dependent, but is also not particularly slow. let it run everywhere.
    - make the skip logic more clear, both in how the code itself flows and
      the reasoning behind it as it pertains to informing readers about the
      reproduction of the bug

--- test/multiverse/suites/sidekiq/sidekiq_with_redis_test.rb
+++ test/multiverse/suites/sidekiq/sidekiq_with_redis_test.rb
@@ -34,7 +34,7 @@ class SidekiqWithRedisTest < MiniTest::Test
   # NOTE: Because Sidekiq v7.0+ can use `redis-client` without `redis`, this
   #       test brings in the `redis` gem directly via `bundler/inline`
   def test_redis_client_pipelined_calls_work
-    skip unless run_these_tests?
+    skip 'Testing conducted only using Sidekiq v7.0+ with redis not yet bundled' unless sidekiq_without_redis?
 
     gemfile do
       source 'https://rubygems.org'
@@ -42,7 +42,6 @@ class SidekiqWithRedisTest < MiniTest::Test
       gem 'redis'
     end
 
-    require 'sidekiq'
     require 'newrelic_rpm'
 
     conn = Sidekiq::RedisConnection.create
@@ -62,11 +61,24 @@ class SidekiqWithRedisTest < MiniTest::Test
 
   private
 
-  def run_these_tests?
-    # these tests aren't impacted by Ruby version
-    return false unless RUBY_VERSION >= '3.1.2'
+  # Sidekiq v7.0 depends on the redis-client gem and by default will not have
+  # the redis gem present. Issue #1639 arose from having both Sidekiq v7.0 and
+  # the redis gem. For this testing, we want to identify an environment that
+  # has Sidekiq v7.0 but does not have the redis gem present. We will then
+  # introduce the redis gem to the environment and ensure that things still
+  # work as expected.
+  def sidekiq_without_redis?
+    require 'sidekiq'
+    return false unless Gem::Version.new(Sidekiq::VERSION) >= Gem::Version.new('7.0.0')
+    return false if defined?(RedisClient).nil?
 
-    # only test with environments that have not already bundled the redis gem
-    !Gem::Specification.all.any? { |s| s.name == 'redis' }
+    # NOTE: introspection through Bundler/RubyGems methods produces warnings,
+    #       so we just use a begin/rescue to test for the presence of redis
+    begin
+      require 'redis'
+      false # redis is already bundled - do not test
+    rescue LoadError
+      true # redis is not yet bundled, proceed to test
+    end
   end
 end
