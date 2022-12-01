63e3a50522418224f5b796e7891fa58b8b33f233
hramadan <hramadan@newrelic.com>
2022-12-01 14:06:11 -0800

    concurrent_ruby scaffold

--- lib/new_relic/agent/configuration/default_source.rb
+++ lib/new_relic/agent/configuration/default_source.rb
@@ -1569,6 +1569,14 @@ If `true`, disables agent middleware for Sinatra. This middleware is responsible
           :allowed_from_server => false,
           :description => 'Controls auto-instrumentation of bunny at start up.  May be one of [auto|prepend|chain|disabled].'
         },
+        :'instrumentation.concurrent_ruby' => {
+          :default => 'auto',
+          :public => true,
+          :type => String,
+          :dynamic_name => true,
+          :allowed_from_server => false,
+          :description => 'Controls auto-instrumentation of the concurrent_ruby library at start up. May be one of [auto|prepend|chain|disabled].'
+        },
         :'instrumentation.curb' => {
           :default => instrumentation_value_of(:disable_curb),
           :documentation_default => 'auto',
