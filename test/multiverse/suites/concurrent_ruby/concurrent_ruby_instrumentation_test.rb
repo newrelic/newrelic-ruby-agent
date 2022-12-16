47c56eb8ac57c6488e3c477bd3f31b528d1b6963
Tanna McClure <tmcclure@newrelic.com>
2022-12-16 14:44:26 -0800

    resolve intermittent failures
    Concurrent ruby will reuse threads created before a transaction
    began, which causes the thread creation segment to not appear in the
    expected transaction.

--- test/multiverse/suites/concurrent_ruby/concurrent_ruby_instrumentation_test.rb
+++ test/multiverse/suites/concurrent_ruby/concurrent_ruby_instrumentation_test.rb
@@ -4,17 +4,18 @@
 
 class ConcurrentRubyInstrumentationTest < Minitest::Test
   def test_promises_future_creates_segment_with_default_name
-    skip
     txn = in_transaction do
-      Concurrent::Promises.future { 'hi' }
+      future = Concurrent::Promises.future { 'hi' }
+      future.wait!
     end
-    segment = txn.segments[1]
 
-    assert_equal segment.name, NewRelic::Agent::Instrumentation::ConcurrentRuby::DEFAULT_NAME
+    expected_segments = ['Concurrent::ThreadPoolExecutor#post', 'Concurrent/task']
+
+    assert_equal(3, txn.segments.length)
+    assert expected_segments.to_set.subset?(txn.segments.map { |s| s.name }.to_set)
   end
 
   def test_promises_future_creates_segments_for_nested_instrumented_calls
-    skip
     with_config(:'instrumentation.thread.tracing' => false) do
       future = nil
       txn = in_transaction do
@@ -22,12 +23,14 @@ class ConcurrentRubyInstrumentationTest < Minitest::Test
         future.wait!
       end
 
+      expected_segments = ['Concurrent::ThreadPoolExecutor#post', 'Concurrent/task', 'External/www.example.com/Net::HTTP/GET']
+
       assert_equal(4, txn.segments.length)
+      assert expected_segments.to_set.subset?(txn.segments.map { |s| s.name }.to_set)
     end
   end
 
   def test_promises_future_creates_segments_for_nested_instrumented_calls_with_thread_tracing_enabled
-    # skip
     with_config(:'instrumentation.thread.tracing' => true) do
       future = nil
       txn = in_transaction do
@@ -35,20 +38,11 @@ class ConcurrentRubyInstrumentationTest < Minitest::Test
         future.wait!
       end
 
-      # intermittent failure with thread segment missing?  i think it might be fixed now tho
-      #   dummy
-      #   Concurrent::ThreadPoolExecutor#post
-      #   Ruby/Thread/2640
-      #   Ruby/Inner_concurrent_ruby
-      #   External/www.example.com/Net::HTTP/GET
-      # txn.segments.each do |s|
-      #   puts s.name
-      # end
-
-      # concurrent ruby doesn't use ruby threads when running on java, so that segment will not be present
-      expected_segments = RUBY_PLATFORM == 'java' ? 4 : 5
-
-      assert_equal(expected_segments, txn.segments.length)
+      expected_segments = ['Concurrent::ThreadPoolExecutor#post', 'Concurrent/task', 'External/www.example.com/Net::HTTP/GET']
+      # We can't check the number of segments when thread tracing is enabled because we can not rely on concurrent
+      # ruby creating threads during this transaction, as it can reuse threads that were created previously.
+      # Instead, we check to make sure the segments that should be present are.
+      assert expected_segments.to_set.subset?(txn.segments.map { |s| s.name }.to_set)
     end
   end
 
