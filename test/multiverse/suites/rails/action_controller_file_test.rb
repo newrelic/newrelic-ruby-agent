37f501098949ed94cdbe8fa2c9d611f2e444d013
Tanna McClure <tmcclure@newrelic.com>
2023-01-19 14:10:59 -0800

    add test for halted_callback

--- test/multiverse/suites/rails/action_controller_file_test.rb
+++ test/multiverse/suites/rails/action_controller_file_test.rb
@@ -7,43 +7,24 @@ require './app'
 if defined?(ActionController::Live)
 
   class DataController < ApplicationController
-    # RESPONSE_BODY = "<html><head></head><body>Brains!</body></html>"
-
+    # send_file
     def send_test_file
       send_file('/Users/tmcclure/projects/ruby-agent/THIRD_PARTY_NOTICES.md')
     end
 
+    # send_data
     def send_test_data
       send_data("wow its a adata")
     end
 
-    def do_a_redirect
-      # redirect_to action: "index"
-      # redirect_to action: 'index', status: 303
-    end
-
-    before_action :otherthing, only: :halt_my_callback
-    # before_action :set_newsletter_email, only: [:show, :edit]
-
-    def otherthing
-      puts "**1**"
-      throw(:abort)
-    end
+    # halted_callback
+    before_action :halter, only: :halt_my_callback
 
     def halt_my_callback
-      puts "**2**"
-      # throw(:abort)
     end
 
-    def index
-      # puts "*"* 30
-
-      NewRelic::Agent.record_metric("wow_here")
-    end
-
-    def secondthing
-      # puts "*"*30
-      # NewRelic::Agent.record_metric("wow_here")
+    def halter
+      redirect_to("http://foo.bar/")
     end
   end
 
@@ -66,18 +47,18 @@ if defined?(ActionController::Live)
       assert_metrics_recorded(['Controller/data/send_test_data', 'Nested/Controller/data/send_test_data/send_data'])
     end
 
-    # TODO: add ignore tests
-
-    # def test_redirect_to
-    #   get('/data/do_a_redirect')
-
-    #   assert_metrics_recorded(['Controller/data/do_a_redirect', 'Nested/Controller/data/do_a_redirect/redirect_to', "wow_here"])
-    # end
-
     def test_halted_callback
       get('/data/halt_my_callback')
 
       assert_metrics_recorded(['Controller/data/halt_my_callback', 'Nested/Controller/data/halt_my_callback/halted_callback'])
     end
+
+    # TODO: add ignore tests
+    # def test_redirect_to
+    #   get('/data/do_a_redirect')
+    #   assert_metrics_recorded(['Controller/data/do_a_redirect', 'Nested/Controller/data/do_a_redirect/redirect_to', "wow_here"])
+    # end
+
+    # unpermitted_parameters
   end
 end
