56f193bc33e9cb44194702f435fbc042cbb71722
Tanna McClure <tmcclure@newrelic.com>
2023-02-07 14:45:29 -0800

    fix segment starting id reference

--- lib/new_relic/agent/transaction/tracing.rb
+++ lib/new_relic/agent/transaction/tracing.rb
@@ -35,7 +35,7 @@ module NewRelic
 
         def segment_complete(segment)
           # if parent was in another thread, remove the current_segment entry for this thread
-          if segment.parent && segment.parent.starting_thread_id != ::Thread.current.object_id
+          if segment.parent && segment.parent.starting_segment_key != ::Thread.current.object_id
             # removing doesn't work well when fibers
             # remove_current_segment_by_thread_id(::Thread.current.object_id)
           else
