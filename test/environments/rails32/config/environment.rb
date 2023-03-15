4ad9f5ad4e27e7e544f65600700604bb8f6c8066
Tanna McClure <tmcclure@newrelic.com>
2023-03-15 09:48:42 -0700

    remove unsupported version testing

--- test/environments/rails32/config/environment.rb
+++
@@ -1,6 +0,0 @@
-# This file is distributed under New Relic's license terms.
-# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
-# frozen_string_literal: true
-
-require_relative 'application'
-RpmTestApp::Application.initialize!
