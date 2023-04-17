b3f4d6c0871c84dabbc5ab37592a5e57deddec71
Kayla Reopelle <kreopelle@newrelic.com>
2023-04-17 14:48:17 -0700

    Update release callout
    This commit incorporates changes made by a docs writer

--- .github/workflows/scripts/generate_release_notes.rb
+++ .github/workflows/scripts/generate_release_notes.rb
@@ -10,7 +10,7 @@ class GenerateReleaseNotes
   DIVIDER = '---'
   SUPPORT_STATEMENT = <<~SUPPORT_STATEMENT
   <Callout variant="important">
-    We recommend updating to the latest agent version as soon as it's available. If you can't upgrade to the latest version, update your agents to a version at most 90 days old. Read more about [keeping agents up to date](/docs/new-relic-solutions/new-relic-one/install-configure/update-new-relic-agent/).
+    We recommend updating to the latest agent version as soon as it's available. If you can't upgrade to the latest version, update your agents to a version no more than 90 days old. Read more about [keeping agents up to date](/docs/new-relic-solutions/new-relic-one/install-configure/update-new-relic-agent/).
 
     See the New Relic Ruby agent [EOL policy](https://docs.newrelic.com/docs/apm/agents/ruby-agent/getting-started/ruby-agent-eol-policy/) for information about agent releases and support dates.
   </Callout>
