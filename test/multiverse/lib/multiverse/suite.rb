96763dd790157715ac3860781dd6086f3fac43a7
Hannah Ramadan <76922290+hannahramadan@users.noreply.github.com>
2023-05-01 11:58:40 -0700

    Comment correction

--- test/multiverse/lib/multiverse/suite.rb
+++ test/multiverse/lib/multiverse/suite.rb
@@ -413,7 +413,7 @@ module Multiverse
       puts yellow("Starting tests in child PID #{Process.pid} at #{Time.now}\n")
     end
 
-    # active_record_pg test suite runs in serialize to prevent database conflicts
+    # active_record_pg test suite runs in serial to prevent database conflicts
     def should_serialize?
       ENV['SERIALIZE'] || debug || self.directory.include?('active_record_pg')
     end
