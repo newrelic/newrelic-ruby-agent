2689f8e1165f28326ae336226c41073dc62d00b3
hramadan <hramadan@newrelic.com>
2023-05-23 10:49:26 -1000

    Webrick only if ruby version  > 3

--- test/multiverse/lib/multiverse/suite.rb
+++ test/multiverse/lib/multiverse/suite.rb
@@ -285,8 +285,8 @@ module Multiverse
         f.puts "gem 'minitest-stub-const', '~> 0.6', require: false"
 
         # pin webrick until we investigate why 1.8.1 breaks things
-        f.puts "gem 'webrick'"
-        # f.puts ruby3_gem_webrick
+        # f.puts "gem 'webrick', '< 1.8.0'"
+        f.puts ruby3_gem_webrick
 
         f.puts "gem 'warning'"
 
