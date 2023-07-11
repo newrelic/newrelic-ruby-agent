bbc96511fb7608d8e0a8b74058a5dda39d672380
fallwith <fallwith@gmail.com>
2023-07-11 12:42:49 -0700

    CI gemspec test: skip 'env' tests incl. norails
    We were previously skipping the `rails<version>` env tests, but not the
    `norails` ones. Skip those too.

--- test/new_relic/gemspec_files_test.rb
+++ test/new_relic/gemspec_files_test.rb
@@ -6,7 +6,7 @@ require 'minitest/autorun'
 
 class GemspecFilesTest < Minitest::Test
   def test_the_test_agent_helper_is_shipped_in_the_gem_files
-    skip if defined?(Rails::VERSION)
+    skip if defined?(Rails::VERSION) || File.basename(Dir.pwd).match?('rails')
 
     gem_spec_file_path = File.expand_path('../../../newrelic_rpm.gemspec', __FILE__)
     gem_spec = eval(Gem.open_file(gem_spec_file_path, 'r:UTF-8:-', &:read))
