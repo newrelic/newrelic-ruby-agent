cc2cc4b49f95cd1e2cd17e69af3cb9a01a6d808d
fallwith <fallwith@gmail.com>
2023-07-11 11:44:40 -0700

    CI: gemspec files test - bypass cache
    effectively fork `Gem::Specification.load` to bypass its internal
    caching mechanism, and catch parse failures with an assertion.

--- test/new_relic/gemspec_files_test.rb
+++ test/new_relic/gemspec_files_test.rb
@@ -8,13 +8,11 @@ class GemspecFilesTest < Minitest::Test
   def test_the_test_agent_helper_is_shipped_in_the_gem_files
     skip if defined?(Rails::VERSION)
 
-    agent_helper_file = 'test/agent_helper.rb'
-
     gem_spec_file_path = File.expand_path('../../../newrelic_rpm.gemspec', __FILE__)
+    gem_spec = eval(Gem.open_file(gem_spec_file_path, 'r:UTF-8:-', &:read))
 
-    gem_spec = Gem::Specification.load(gem_spec_file_path)
-
+    assert gem_spec, "Failed to parse '#{gem_spec_file_path}'"
     assert_equal('newrelic_rpm', gem_spec.name)
-    assert_includes(gem_spec.files, agent_helper_file)
+    assert_includes(gem_spec.files, 'test/agent_helper.rb')
   end
 end
