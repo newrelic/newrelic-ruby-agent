7dcc6659947fe8d0602a193e8c134ca9ea01ce12
fallwith <fallwith@gmail.com>
2023-07-21 16:14:47 -0700

    perf test RuboCop fixes
    autocorrect the autogenerated Rails content to align with the project's
    linter config

--- test/performance/rails_app/config/environments/production.rb
+++ test/performance/rails_app/config/environments/production.rb
@@ -53,16 +53,16 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new($stdout)
+    logger = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
