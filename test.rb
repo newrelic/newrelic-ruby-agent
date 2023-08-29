429d03d164f6e3eea1789ac080abae2ca8fc4f9b
fallwith <fallwith@gmail.com>
2023-08-29 15:27:45 -0700

    remove junk files
    remove junk that was accidentally added

--- test.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-string = 'hello string'
-array = %w[hello array]
-symbol = :hello_symbol
-regexp = /hello regex/
-
-[string, array, symbol, regexp].each { |o| puts "#{o} (#{o.class}) frozen? => #{o.frozen?}" }
