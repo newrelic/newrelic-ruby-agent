82a100c22ca5b7ffd364a94c9de404c52de4da5a
fallwith <fallwith@gmail.com>
2023-09-01 15:58:12 -0700

    CI: GHA Sidekiq test fixes
    GHA is having issues that cannot be reproduced elsewhere

--- test/multiverse/suites/sidekiq/sidekiq_test_helpers.rb
+++ test/multiverse/suites/sidekiq/sidekiq_test_helpers.rb
@@ -70,9 +70,18 @@ module SidekiqTestHelpers
       cli = Sidekiq::CLI.instance
       cli.parse(['--require', File.absolute_path(__FILE__), '--queue', 'default,1'])
       cli.logger.instance_variable_get(:@logdev).instance_variable_set(:@dev, File.new('/dev/null', 'w'))
-      cli.instance_variable_set(:@config, Sidekiq::Config.new) unless cli.respond_to?(:config) && cli.config
+      ensure_sidekiq_config(cli)
       cli
     end
+  end
+
+  def ensure_sidekiq_config(cli)
+    return unless Sidekiq::VERSION.split('.').first.to_i >= 7
+    return unless cli.respond_to?(:config)
+    return unless cli.config.nil?
+
+    require 'sidekiq/config'
+    cli.instance_variable_set(:@config, ::Sidekiq::Config.new)
   end
 
   def flatten(object)
