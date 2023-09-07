6128868e853a4314af882dae176564586ed6eeae
hramadan <hramadan@newrelic.com>
2023-09-07 12:14:37 -0700

    Test updates

--- test/multiverse/suites/stripe/stripe_instrumentation_test.rb
+++ test/multiverse/suites/stripe/stripe_instrumentation_test.rb
@@ -15,6 +15,7 @@ class StripeInstrumentation < Minitest::Test
     # later, helps us get around needing to provide a valid API key
     @connection = Stripe::ConnectionManager.new
     @response = Net::HTTPResponse.new('1.1', '200', 'OK')
+    # Bypass #stream_check ("attempt to read body out of block")
     @response.instance_variable_set(:@read, true)
     @response.body = {
       object: 'list',
@@ -55,9 +56,10 @@ class StripeInstrumentation < Minitest::Test
     end
   end
 
-  def test_agent_collects_user_data_attributes
+  def test_agent_collects_user_data_attributes_when_configured
     Stripe::Instrumentation.subscribe(:request_begin) do |events|
       events.user_data[:cat] = 'meow'
+      events.user_data[:dog] = 'woof'
     end
 
     with_config(:'stripe.user_data.include' => '.') do
@@ -71,6 +73,33 @@ class StripeInstrumentation < Minitest::Test
             stripe_attributes = stripe_segment.attributes.agent_attributes_for(NewRelic::Agent::AttributeFilter::DST_SPAN_EVENTS)
 
             assert_equal('meow', stripe_attributes['stripe_user_data_cat'])
+            assert_equal('woof', stripe_attributes['stripe_user_data_dog'])
+          end
+        end
+      end
+    end
+  end
+
+  def test_agent_collects_select_user_data_attributes
+    Stripe::Instrumentation.subscribe(:request_begin) do |events|
+      events.user_data[:frog] = 'ribbit'
+      events.user_data[:sheep] = 'baa'
+      events.user_data[:cow] = 'moo'
+    end
+
+    with_config(:'stripe.user_data.include' => 'frog, sheep') do
+      Stripe::StripeClient.stub(:default_connection_manager, @connection) do
+        @connection.stub(:execute_request, @response) do
+          in_transaction do |txn|
+            Stripe::Customer.list({limit: 3}) # Start a Stripe event
+            stripe_segment = txn.segments.detect { |s| s.name == 'Stripe/v1/customers get' }
+
+            assert(stripe_segment)
+            stripe_attributes = stripe_segment.attributes.agent_attributes_for(NewRelic::Agent::AttributeFilter::DST_SPAN_EVENTS)
+
+            assert_equal('ribbit', stripe_attributes['stripe_user_data_frog'])
+            assert_equal('baa', stripe_attributes['stripe_user_data_sheep'])
+            assert_nil(stripe_attributes['stripe_user_data_cow'])
           end
         end
       end
@@ -79,10 +108,10 @@ class StripeInstrumentation < Minitest::Test
 
   def test_agent_ignores_user_data_attributes
     Stripe::Instrumentation.subscribe(:request_begin) do |events|
-      events.user_data[:dog] = 'woof'
+      events.user_data[:bird] = 'tweet'
     end
 
-    with_config(:'stripe.user_data.exclude' => 'dog') do
+    with_config(:'stripe.user_data.exclude' => 'bird') do
       Stripe::StripeClient.stub(:default_connection_manager, @connection) do
         @connection.stub(:execute_request, @response) do
           in_transaction do |txn|
@@ -92,7 +121,7 @@ class StripeInstrumentation < Minitest::Test
             assert(stripe_segment)
             stripe_attributes = stripe_segment.attributes.agent_attributes_for(NewRelic::Agent::AttributeFilter::DST_SPAN_EVENTS)
 
-            assert_nil(stripe_attributes['stripe_user_data_dog'])
+            assert_nil(stripe_attributes['stripe_user_data_bird'])
           end
         end
       end
@@ -112,5 +141,20 @@ class StripeInstrumentation < Minitest::Test
         end
       end
     end
+  end
+
+  def test_start_segment_records_error
+    NewRelic::Agent.stub(:logger, NewRelic::Agent::MemoryLogger.new) do
+      bad_event = OpenStruct.new(path: 'v1/charges', method: 'get')
+      newrelic_subscriber = NewRelic::Agent::Instrumentation::StripeSubscriber.new.start_segment(bad_event)
+
+      assert_logged(/Error starting New Relic Stripe segment/m)
+    end
+  end
+
+  def assert_logged(expected)
+    found = NewRelic::Agent.logger.messages.any? { |m| m[1][0].match?(expected) }
+
+    assert(found, "Didn't see log message: '#{expected}'")
   end
 end
