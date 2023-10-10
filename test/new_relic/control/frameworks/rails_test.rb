c7b1f3d740dbd647720cca1defb2300981b77dc2
fallwith <fallwith@gmail.com>
2023-10-10 11:42:47 -0700

    rails_test.rb: new instance var location
    The browser monitoring 'installed?' instance var has moved

--- test/new_relic/control/frameworks/rails_test.rb
+++ test/new_relic/control/frameworks/rails_test.rb
@@ -5,12 +5,19 @@
 require_relative '../../../test_helper'
 
 class NewRelic::Control::Frameworks::RailsTest < Minitest::Test
+  def setup
+    reset_installed_instance_variable
+  end
+
+  def teardown
+    reset_installed_instance_variable
+  end
+
   def test_install_browser_monitoring
     require 'new_relic/rack/browser_monitoring'
     middleware = stub('middleware config')
     config = stub('rails config', :middleware => middleware)
     middleware.expects(:use).with(NewRelic::Rack::BrowserMonitoring)
-    NewRelic::Control.instance.instance_eval { @browser_monitoring_installed = false }
     with_config(:'browser_monitoring.auto_instrument' => true) do
       NewRelic::Control.instance.install_browser_monitoring(config)
     end
@@ -20,10 +27,27 @@ class NewRelic::Control::Frameworks::RailsTest < Minitest::Test
     middleware = stub('middleware config')
     config = stub('rails config', :middleware => middleware)
     middleware.expects(:use).never
-    NewRelic::Control.instance.instance_eval { @browser_monitoring_installed = false }
-
+    set_installed_instance_variable
     with_config(:'browser_monitoring.auto_instrument' => false) do
       NewRelic::Control.instance.install_browser_monitoring(config)
     end
+  end
+
+  private
+
+  def reset_installed_instance_variable
+    return unless NewRelic::Control::Frameworks::Rails::INSTALLED_SINGLETON.instance_variable_defined?(
+      NewRelic::Control::Frameworks::Rails::INSTALLED
+    )
+
+    NewRelic::Control::Frameworks::Rails::INSTALLED_SINGLETON.remove_instance_variable(
+      NewRelic::Control::Frameworks::Rails::INSTALLED
+    )
+  end
+
+  def set_installed_instance_variable
+    NewRelic::Control::Frameworks::Rails::INSTALLED_SINGLETON.instance_variable_set(
+      NewRelic::Control::Frameworks::Rails::INSTALLED, true
+    )
   end
 end
