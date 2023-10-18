8d0b0cbb014f8a24dbbcb59db9e72b74df4fcd2a
Tanna McClure <tmcclure@newrelic.com>
2023-10-18 13:44:01 -0700

    delete comments

--- lib/new_relic/agent/instrumentation/async_http/instrumentation.rb
+++ lib/new_relic/agent/instrumentation/async_http/instrumentation.rb
@@ -4,14 +4,6 @@
 
 module NewRelic::Agent::Instrumentation
   module AsyncHttp
-    # from the async http doumentation:
-    # @parameter method [String] The request method, e.g. `GET`.
-    # @parameter url [String] The URL to request, e.g. `https://www.codeotaku.com`.
-    # @parameter headers [Hash | Protocol::HTTP::Headers] The headers to send with the request.
-    # @parameter body [String | Protocol::HTTP::Body] The body to send with the request.
-
-    # but their example has headers being an array??? weird, ig lets make sure we work ok with that
-    # like [[:content_type, "application/json"], [:accept, "application/json"]
     def call_with_new_relic(method, url, headers = nil, body = nil)
       headers ||= {} # if it is nil, we need to make it a hash so we can insert headers
       wrapped_request = NewRelic::Agent::HTTPClients::AsyncHTTPRequest.new(self, method, url, headers)
