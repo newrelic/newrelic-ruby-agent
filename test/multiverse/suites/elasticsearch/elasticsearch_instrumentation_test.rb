6743a7381c28e00b8e00eb9170c03ebf3a47ae62
Hannah Ramadan <hannahr315@gmail..com>
2023-12-05 11:00:35 -0800

    rubocop

--- test/multiverse/suites/elasticsearch/elasticsearch_instrumentation_test.rb
+++ test/multiverse/suites/elasticsearch/elasticsearch_instrumentation_test.rb
@@ -1,4 +1,4 @@
-#2. This file is distributed under New Relic's license terms.
+# This file is distributed under New Relic's license terms.
 # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
 # frozen_string_literal: true
 
