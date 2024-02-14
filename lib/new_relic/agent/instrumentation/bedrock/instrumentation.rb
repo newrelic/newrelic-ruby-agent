fbadd90bbbf7cb52b85f0d78d1a9b29f439ffbe1
Tanna McClure <tmcclure@newrelic.com>
2024-02-14 14:05:30 -0800

    separate out embedded vs completion

--- lib/new_relic/agent/instrumentation/bedrock/instrumentation.rb
+++ lib/new_relic/agent/instrumentation/bedrock/instrumentation.rb
@@ -10,50 +10,32 @@ module NewRelic::Agent::Instrumentation
       NewRelic::Agent.record_metric("Supportability/Ruby/ML/Bedrock/#{Aws::BedrockRuntime::GEM_VERSION}", 0.0)
       NewRelic::Agent::Llm::LlmEvent.set_llm_agent_attribute_on_transaction
 
-      segment = NewRelic::Agent::Tracer.start_segment(name: 'Llm/completion or embed/Bedrock/invoke_model')
-      segment.llm_event ||= {}
+      segment_name = "Llm/#{is_embed_model?(params[:model_id]) ? 'Embedding' : 'Completion'}/Bedrock/invoke_model"
+      segment = NewRelic::Agent::Tracer.start_segment(name: segment_name)
+      segment.llm_event = {} if segment
 
       response = NewRelic::Agent::Tracer.capture_segment_error(segment) { yield }
     ensure
       segment&.finish
-      create_llm_events(segment, params, options, response)
-      # binding.irb
+      create_llm_events(segment, params, response)
+      binding.irb
     end
 
     ##################################################################
 
-    def create_llm_events(segment, params, options, response)
-      body = JSON.parse(params[:body])
-      response_body = JSON.parse(response.body.read)
-      response.body.rewind # put the response back
+    def is_embed_model?(model)
+      model.start_with?('amazon.titan-embed-', 'cohere.embed-')
+    end
 
-      model = params[:model_id]
-
-      puts model
-      puts 'params: ' + params.to_s
-      puts 'body: ' + body.to_s
-      puts 'options:' + options.to_s
-      puts 'response_body:' + response_body.to_s
-
+    def create_llm_events(segment, params, response)
+      model, body, response_body = json_parse_params(params, response)
       shared_attributes = create_shared_attributes(model, segment)
 
-      all_attributes = if model.start_with?('amazon.titan-text-')
-        titan_attributes(body, response_body)
-      elsif model.start_with?('anthropic.claude-')
-        anthropic_attributes(body, response_body)
-      elsif model.start_with?('cohere.command-')
-        cohere_command_attributes(body, response_body)
-      elsif model.start_with?('meta.llama2-')
-        llama2_attributes(body, response_body)
-      elsif model.start_with?('ai21.j2-')
-        jurassic_attributes(body, response_body)
-        # elsif model.start_with?('amazon.titan-embed-')
-        #   titan_embed_attributes(params, options, result)
-        # elsif model.start_with?('cohere.embed-')
-        #   cohere_embed_attributes(params, options, result)
+      if is_embed_model?(model)
+        create_embed_completion_events(model, body, response_body, shared_attributes, segment)
+      else
+        create_chat_completion_events(model, body, response_body, shared_attributes, segment)
       end
-
-      create_chat_events(shared_attributes, *all_attributes, segment)
     rescue => e
       # log something
       puts 'oop'
@@ -63,17 +45,32 @@ module NewRelic::Agent::Instrumentation
 
     ##################################################################
 
-    def create_chat_events(shared, summary, messages, segment)
-      summary_event = create_chat_completion_summary_event(shared, summary, segment)
-      messages.each_with_index do |message, index|
-        create_chat_completion_message_event(index, summary_event.id, shared, message)
+    def create_chat_completion_events(model, body, response_body, shared_attributes, segment)
+      summary_attributes, messages_attributes = if model.start_with?('amazon.titan-text-')
+        titan_attributes(body, response_body)
+      elsif model.start_with?('anthropic.claude-')
+        anthropic_attributes(body, response_body)
+      elsif model.start_with?('cohere.command-')
+        cohere_command_attributes(body, response_body)
+      elsif model.start_with?('meta.llama2-')
+        llama2_attributes(body, response_body)
+      elsif model.start_with?('ai21.j2-')
+        ajurassic_attributes(body, response_body)
+      else
+        # log something?
+        return
+      end
+
+      summary_event = create_chat_completion_summary_event(shared_attributes, summary_attributes, segment)
+      messages_attributes.each_with_index do |message, index|
+        create_chat_completion_message_event(index, summary_event.id, shared_attributes, message)
       end
     end
 
     def create_chat_completion_summary_event(shared, attributes, segment)
       summary_event = NewRelic::Agent::Llm::ChatCompletionSummary.new(shared)
 
-      @nr_events << summary_event # todo tmp
+      @nr_events << summary_event # TODO: tmp
 
       summary_event.id = NewRelic::Agent::GuidGenerator.generate_guid
       # summary_event.api_key_last_four_digits = config&.credentials&.access_key_id[-4..-1] # todo maybe dont do this
@@ -97,7 +94,7 @@ module NewRelic::Agent::Instrumentation
 
     def create_chat_completion_message_event(index, completion_id, shared, attributes)
       message_event = NewRelic::Agent::Llm::ChatCompletionMessage.new(shared)
-      @nr_events << message_event # todo tmp
+      @nr_events << message_event # TODO: tmp
       message_event.id = "#{shared[:request_id]}-#{index}"
       message_event.completion_id = completion_id
       message_event.sequence = index
@@ -108,11 +105,24 @@ module NewRelic::Agent::Instrumentation
       message_event.record
     end
 
+    ##################################################################
+
+    def create_embed_completion_events(model, body, response_body, shared_attributes, segment)
+      ###
+
+      # elsif model.start_with?('amazon.titan-embed-')
+      #   titan_embed_attributes(params, options, result)
+      # elsif model.start_with?('cohere.embed-')
+      #   cohere_embed_attributes(params, options, result)
+      # end
+    end
+
+    ##################################################################
+
     def create_shared_attributes(model, segment)
       shared_attributes = {}
 
       shared_attributes[:request_id] = segment&.llm_event&.[](:request_id)
-
       shared_attributes[:response_model] = model
       shared_attributes[:vendor] = 'bedrock'
 
@@ -120,6 +130,15 @@ module NewRelic::Agent::Instrumentation
       shared_attributes[:conversation_id] = conversation_id if conversation_id
 
       shared_attributes
+    end
+
+    def json_parse_params(params, response)
+      model = params[:model_id]
+      body = JSON.parse(params[:body])
+      response_body = JSON.parse(response.body.read)
+      response.body.rewind # put the response back
+
+      return model, body, response_body
     end
 
     ##################################################################
@@ -148,7 +167,7 @@ module NewRelic::Agent::Instrumentation
         }
       end
 
-      [summary_attributes, messages_attributes]
+      return summary_attributes, messages_attributes
     end
 
     def anthropic_attributes(body, response_body)
@@ -172,7 +191,7 @@ module NewRelic::Agent::Instrumentation
         is_response: true
       }
 
-      [summary_attributes, messages_attributes]
+      return summary_attributes, messages_attributes
     end
 
     def cohere_command_attributes(body, response_body)
@@ -198,7 +217,7 @@ module NewRelic::Agent::Instrumentation
         }
       end
 
-      [summary_attributes, messages_attributes]
+      return summary_attributes, messages_attributes
     end
 
     def llama2_attributes(body, response_body)
@@ -216,13 +235,13 @@ module NewRelic::Agent::Instrumentation
         role: 'user'
       }
 
-        messages_attributes << {
-          content: response_body['generation'],
-          role: 'assistant',
-          is_response: true
-        }
+      messages_attributes << {
+        content: response_body['generation'],
+        role: 'assistant',
+        is_response: true
+      }
 
-      [summary_attributes, messages_attributes]
+      return summary_attributes, messages_attributes
     end
 
     def jurassic_attributes(body, response_body)
@@ -248,7 +267,7 @@ module NewRelic::Agent::Instrumentation
         }
       end
 
-      [summary_attributes, messages_attributes]
+      return summary_attributes, messages_attributes
     end
 
     # Embedding:
