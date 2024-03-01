ce0ee2d8818c149a649fda49aebc8cc60615f3c9
Hannah Ramadan <hannahr315@gmail.com>
2024-03-01 11:24:36 -0800

    Code refactor

--- test/new_relic/agent/llm/feedback_test.rb
+++ test/new_relic/agent/llm/feedback_test.rb
@@ -8,16 +8,17 @@ module NewRelic::Agent::Llm
   class FeedbackTest < Minitest::Test
     def setup
       NewRelic::Agent.drop_buffered_data
+      @trace_id = '01234567890'
     end
 
     def test_record_llm_feedback_event_records_required_attributes
       in_transaction do |txn|
-        NewRelic::Agent.record_llm_feedback_event(trace_id: '01234567890', rating: 5)
+        NewRelic::Agent.record_llm_feedback_event(trace_id: @trace_id, rating: 5)
         _, events = NewRelic::Agent.agent.custom_event_aggregator.harvest!
         type, attributes = events[0]
 
         assert_equal 'LlmFeedbackMessage', type['type']
-        assert_equal '01234567890', attributes['trace_id']
+        assert_equal @trace_id, attributes['trace_id']
         assert_equal 5, attributes['rating']
         assert_equal 'Ruby', attributes['ingest_source']
         assert attributes['id']
@@ -26,7 +27,7 @@ module NewRelic::Agent::Llm
 
     def test_record_llm_feedback_event_records_optional_attributes
       in_transaction do |txn|
-        NewRelic::Agent.record_llm_feedback_event(trace_id: '01234567890', rating: 5,
+        NewRelic::Agent.record_llm_feedback_event(trace_id: @trace_id, rating: 5,
           category: 'Helpful', message: 'Looks good!', metadata: {'pop' => 'tart', 'toaster' => 'strudel'})
         _, events = NewRelic::Agent.agent.custom_event_aggregator.harvest!
         type, attributes = events[0]
@@ -39,17 +40,22 @@ module NewRelic::Agent::Llm
     end
 
     def test_record_llm_feedback_event_raises_missing_required_parameter
-      assert_raises(ArgumentError) { NewRelic::Agent.record_llm_feedback_event(trace_id: '01234567890') }
+      assert_raises(ArgumentError) { NewRelic::Agent.record_llm_feedback_event(trace_id: @trace_id) }
     end
 
     def test_record_llm_feedback_event_invalid_param
-      assert_raises(ArgumentError) { NewRelic::Agent.record_llm_feedback_event(trace_id: '01234567890', rating: 5, food: 'blueberry') }
+      assert_raises(ArgumentError) { NewRelic::Agent.record_llm_feedback_event(
+        trace_id: @trace_id,
+        rating: 5,
+        food: 'blueberry'
+        )
+      }
     end
 
     def test_record_llm_feedback_event_rescues_exception
       NewRelic::Agent.stub(:logger, NewRelic::Agent::MemoryLogger.new) do
         NewRelic::Agent.stub(:record_custom_event, proc { |*_args| raise 'kaboom' }) do
-          NewRelic::Agent.record_llm_feedback_event(trace_id: '01234567890', rating: 5)
+          NewRelic::Agent.record_llm_feedback_event(trace_id: @trace_id, rating: 5)
         end
 
         assert_logged(/record_llm_feedback_event/)
