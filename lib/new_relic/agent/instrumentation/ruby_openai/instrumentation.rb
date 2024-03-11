3ce45f9882a99ee600561c855285a29a63b07c67
Kayla Reopelle <kreopelle@newrelic.com>
2024-03-11 10:12:58 -0700

    Remove extraneous return

--- lib/new_relic/agent/instrumentation/ruby_openai/instrumentation.rb
+++ lib/new_relic/agent/instrumentation/ruby_openai/instrumentation.rb
@@ -147,7 +147,7 @@ module NewRelic::Agent::Instrumentation
       return unless NewRelic::Agent.llm_token_count_callback
 
       count = NewRelic::Agent.llm_token_count_callback.call({model: model, content: content})
-      return count if count.is_a?(Integer) && count > 0
+      count if count.is_a?(Integer) && count > 0
     end
 
     def record_content_enabled?
