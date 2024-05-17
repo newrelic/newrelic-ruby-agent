a9102c7a793323a38910e0d7e9d9edeef44871d8
Tanna McClure <tmcclure@newrelic.com>
2024-05-17 14:16:33 -0500

    add aws decoding test

---
+++ test/new_relic/agent/aws_test.rb
@@ -0,0 +1,23 @@
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+
+class AwsTest < Minitest::Test
+  def test_create_arn
+    config = mock
+    config.stubs(:region).returns('us-test-region-1')
+    mock_credentials = mock
+    mock_credentials.stubs(:access_key_id).returns('AKIAIOSFODNN7EXAMPLE') # this is a fake access key id from aws docs
+    config.stubs(:credentials).returns(mock_credentials)
+
+    service = 'test-service'
+    resource = 'test/test-resource'
+    arn = NewRelic::Agent::Aws.create_arn(service, resource, config)
+
+    expected = 'arn:aws:test-service:us-test-region-1:36315003739:test/test-resource'
+
+    assert_equal expected, arn
+  end
+end
