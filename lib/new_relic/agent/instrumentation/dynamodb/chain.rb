6c0633e086abbaf0a3234f708fd63e341731adde
Tanna McClure <tmcclure@newrelic.com>
2024-05-17 14:01:35 -0500

    remove unnecessary to_sym

--- lib/new_relic/agent/instrumentation/dynamodb/chain.rb
+++ lib/new_relic/agent/instrumentation/dynamodb/chain.rb
@@ -18,7 +18,7 @@ module NewRelic::Agent::Instrumentation
           update_item].each do |method_name|
           alias_method("#{method_name}_without_new_relic".to_sym, method_name.to_sym)
 
-          define_method(method_name.to_sym) do |*args|
+          define_method(method_name) do |*args|
             instrument_method_with_new_relic(method_name, *args) { send("#{method_name}_without_new_relic".to_sym, *args) }
           end
         end
