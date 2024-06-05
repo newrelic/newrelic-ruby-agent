ed173125a3f287b2fd5efdc990ea754ebe248f00
fallwith <fallwith@gmail.com>
2024-06-04 18:01:37 -0700

    outputs

---
+++ .github/workflows/scripts/gather_action_versions.sh
@@ -0,0 +1,23 @@
+# setup-ruby, tag v1.177.1
+echo SETUP_RUBY_SHA=943103cae7d3f1bb1e4951d5fcc7928b40e4b742 >> $GITHUB_OUTPUT
+
+# checkout, tag v4.1.2
+echo CHECKOUT_SHA=9bb56186c3b09b4f86b1c65136769dd318469633 >> $GITHUB_OUTPUT
+
+# kanga333/variable-mapper, tag v0.3.0
+echo VARIABLE_MAPPER_SHA=3681b75f5c6c00162721168fb91ab74925eaebcb >> $GITHUB_OUTPUT
+
+# actions/cache, tag v3.3.1
+echo CACHE_SHA=88522ab9f39a2ea568f7027eddc7d8d8bc9d59c8 >> $GITHUB_OUTPUT
+
+# nick-fields/retry, tag v2.8.3
+echo RETRY_SHA=943e742917ac94714d2f408a0e8320f2d1fcafcd >> $GITHUB_OUTPUT
+
+# technote-space/workflow-conclusion-action, tag v3.0.3
+echo WORKFLOW_CONCLUSION_SHA=45ce8e0eb155657ab8ccf346ade734257fd196a5a >> $GITHUB_OUTPUT
+
+# voxmedia/github-action-slack-notify-build, v1.6.0
+echo SLACK_NOTIFY_BUILD_SHA=3665186a8c1a022b28a1dbe0954e73aa9081ea9e >> $GITHUB_OUTPUT
+
+# Mercymeilya/last-workflow-status, tag v0.3.3
+echo LAST_WORKFLOW_STATUS_SHA=3418710aefe8556d73b6f173a0564d38bcfd9a43 >> $GITHUB_OUTPUT
