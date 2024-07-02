ee711e00c48c6a7f0a7df63d2769d8ac3d5ccf6c
Kayla Reopelle <kreopelle@newrelic.com>
2024-07-02 08:48:37 -0700

    Add comment to ClusterMiddleware

--- lib/new_relic/agent/instrumentation/redis/cluster_middleware.rb
+++ lib/new_relic/agent/instrumentation/redis/cluster_middleware.rb
@@ -7,6 +7,13 @@ module NewRelic::Agent::Instrumentation
     module ClusterMiddleware
       include NewRelic::Agent::Instrumentation::Redis
 
+      # Until we decide to move our Redis instrumentation entirely off patches
+      # keep the middleware instrumentation for the call and connect methods
+      # limited to the redis-clustering instrumentation.
+      #
+      # Redis's middleware option does not capture errors as high in the stack
+      # as our patches. Leaving the patches for call and connect on the main
+      # Redis gem limits the feature disparity our customers experience.
       def call(*args, &block)
         call_with_tracing(args[0]) { super }
       end
