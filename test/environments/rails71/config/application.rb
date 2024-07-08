8ea9cef0132f714ee5ef7e146ed6090800edd2b0
fallwith <fallwith@gmail.com>
2024-07-08 16:29:36 -0700

    have the Rails v7.1 test app use v7.1 defaults
    v7.0 copy/paste fix: use v7.1 defaults with Rails v7.1

--- test/environments/rails71/config/application.rb
+++ test/environments/rails71/config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 module RpmTestApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults(7.0)
+    config.load_defaults(7.1)
     config.eager_load = false
     config.filter_parameters += [:password]
   end
