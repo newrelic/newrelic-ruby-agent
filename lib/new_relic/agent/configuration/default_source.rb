7c0b4757d2894212f39157df24a3a5d492aaa355
Prateek Sen <33506953+prateeksen@users.noreply.github.com>
2024-07-22 22:59:41 +0530

    Update lib/new_relic/agent/configuration/default_source.rb
    Co-authored-by: Kayla Reopelle <87386821+kaylareopelle@users.noreply.github.com>

--- lib/new_relic/agent/configuration/default_source.rb
+++ lib/new_relic/agent/configuration/default_source.rb
@@ -2621,7 +2621,7 @@ module NewRelic
           :public => true,
           :type => Boolean,
           :allowed_from_server => false,
-          :description => 'If `true`, enables RXSS(Reflected Cross-site Scripting) detection'
+          :description => 'If `true`, enables RXSS (reflected cross-site scripting) detection'
         },
         :'security.detection.deserialization.enabled' => {
           :default => true,
