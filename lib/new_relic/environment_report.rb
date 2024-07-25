57e4ecaace6d5099ef31344a4f687a95ce730daa
prateek-ap <psen@newrelic.com>
2024-07-25 15:05:17 +0530

    Updated os distribution identification logic

--- lib/new_relic/environment_report.rb
+++ lib/new_relic/environment_report.rb
@@ -67,7 +67,7 @@ module NewRelic
     report_on('Physical Cores') { ::NewRelic::Agent::SystemInfo.num_physical_cores }
     report_on('Arch') { ::NewRelic::Agent::SystemInfo.processor_arch }
     report_on('OS version') { ::NewRelic::Agent::SystemInfo.os_version }
-    report_on('OS') { ::NewRelic::Agent::SystemInfo.ruby_os_identifier }
+    report_on('OS') { ::NewRelic::Agent::SystemInfo.os_distribution }
     report_on('Database adapter') { ::NewRelic::Agent::DatabaseAdapter.value }
     report_on('Framework') { Agent.config[:framework].to_s }
     report_on('Dispatcher') { Agent.config[:dispatcher].to_s }
