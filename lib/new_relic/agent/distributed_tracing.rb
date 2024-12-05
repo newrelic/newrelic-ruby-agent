816eb8f3f75d0047a0bdcac7b64faf88025afd03
Hannah Ramadan <hannahr315@gmail.com>
2024-12-05 13:47:52 -0800

    Only warn if debug level

--- lib/new_relic/agent/distributed_tracing.rb
+++ lib/new_relic/agent/distributed_tracing.rb
@@ -99,7 +99,7 @@ module NewRelic
         record_api_supportability_metric(:accept_distributed_trace_headers)
 
         unless Agent.config[:'distributed_tracing.enabled']
-          NewRelic::Agent.logger.warn('Not configured to accept distributed trace headers')
+          NewRelic::Agent.logger.debug('Not configured to accept distributed trace headers')
           return nil
         end
 
