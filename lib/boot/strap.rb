b4c86e3983aecb1fb34ab266cee8717a067dbed3
Hannah Ramadan <hannahr315@gmail.com>
2025-01-15 15:06:00 -0800

    Test push v unshift

--- lib/boot/strap.rb
+++ lib/boot/strap.rb
@@ -55,7 +55,7 @@ module NRBundlerPatch
   def require_newrelic
     lib = File.expand_path('../..', __FILE__)
     $LOAD_PATH.reject! { |path| path.include?('newrelic_rpm') }
-    $LOAD_PATH.unshift(lib)
+    $LOAD_PATH.push(lib)
     Kernel.require NR_AGENT_GEM
   end
 end
