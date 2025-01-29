c5ef75e42709ae07c6ad082c31c9b38869e6f2fb
Hannah Ramadan <hannahr315@gmail.com>
2025-01-29 11:08:31 -0800

    Require in boot.rb

--- test/performance/rails_app/config/boot.rb
+++ test/performance/rails_app/config/boot.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
