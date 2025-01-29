4efaea1bc7cfc0ca3e0a91f1c2e9c934e5bde40d
Hannah Ramadan <hannahr315@gmail.com>
2025-01-29 11:20:39 -0800

    Fix it all!

--- test/performance/rails_app/config/boot.rb
+++ test/performance/rails_app/config/boot.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'logger'
-
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
