e2478b347099be38f01821c1626f5c0d8997c51a
Hannah Ramadan <hannahr315@gmail.com>
2025-01-29 09:24:42 -0800

    Add Logger for perf test suite

--- test/performance/rails_app/config/application.rb
+++ test/performance/rails_app/config/application.rb
@@ -15,6 +15,7 @@ require 'action_controller/railtie'
 require 'action_view/railtie'
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
+require 'logger'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
