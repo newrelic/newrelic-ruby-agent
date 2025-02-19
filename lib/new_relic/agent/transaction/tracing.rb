8469ffb139e161ef56b99e93be78a82b005450db
Tanna McClure <tmcclure@newrelic.com>
2025-02-19 11:49:53 -0600

    add &. to segment.finalize

--- lib/new_relic/agent/transaction/tracing.rb
+++ lib/new_relic/agent/transaction/tracing.rb
@@ -64,7 +64,7 @@ module NewRelic
         private
 
         def finalize_segments
-          segments.each { |s| s.finalize }
+          segments.each { |s| s&.finalize }
         end
 
         WEB_TRANSACTION_TOTAL_TIME = 'WebTransactionTotalTime'.freeze
