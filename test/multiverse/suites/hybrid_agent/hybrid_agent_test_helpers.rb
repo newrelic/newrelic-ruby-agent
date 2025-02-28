69d3d26786596aba79b2cb904c1f3b78bb7ff21f
Kayla Reopelle <kreopelle@newrelic.com>
2025-02-28 12:26:09 -0800

    remove helper module

--- test/multiverse/suites/hybrid_agent/hybrid_agent_test_helpers.rb
+++
@@ -1,9 +0,0 @@
-# require 'opentelemetry'
-# require 'newrelic_rpm'
-
-module HybridAgentTestHelpers
-  def do_work_in_span(span_kind:, span_name:)
-    # create an OpenTelemetry Span
-    OpenTelemetry::SDK.
-  end
-end
