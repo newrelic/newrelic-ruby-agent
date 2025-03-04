665718132334e68ed125ef9f227612e79cdb6415
Kayla Reopelle <kreopelle@newrelic.com>
2025-03-04 12:07:05 -0800

    Remove the TODO workarounds for eval with files

--- test/new_relic/gemspec_files_test.rb
+++ test/new_relic/gemspec_files_test.rb
@@ -11,12 +11,6 @@ class GemspecFilesTest < Minitest::Test
 
     gem_spec_file_path = File.expand_path('../../../newrelic_rpm.gemspec', __FILE__)
     gem_spec_content = Gem.open_file(gem_spec_file_path, 'r:UTF-8:-', &:read)
-    # TODO: Test this behavior against Ruby 3.3.0 when it is out of preview
-    #       to see if this behavior persists. Remove the gsub if not.
-    # With Ruby 3.3.0-preview2, eval() yields '(eval ...' as the String value
-    # when __FILE__ is used so swap out __FILE__ for the known agent root path
-    # Related to Issue #2430
-    gem_spec_content.gsub!('__FILE__', "'#{gem_spec_file_path}'")
 
     Dir.chdir(File.dirname(gem_spec_file_path)) do
       gem_spec = eval(gem_spec_content)
