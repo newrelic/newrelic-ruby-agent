f14a45a9df922a54753982f6b974a7d9f8df807a
Hannah Ramadan <hannahr315@gmail.com>
2025-03-28 16:10:57 -0700

    Automate slacked gems

--- .github/workflows/scripts/slack_notifications/gem_notifier.rb
+++ .github/workflows/scripts/slack_notifications/gem_notifier.rb
@@ -7,19 +7,53 @@
 require 'time'
 require 'httparty'
 require_relative 'slack_notifier'
+require_relative '../../../../lib/new_relic/agent/configuration/default_source'
 
 class GemNotifier < SlackNotifier
+  GEM_NAME_MAPPING = {
+    'active_support_broadcast_logger' => 'activesupport-broadcast_logger',
+    'active_support_logger' => 'activesupport-logger',
+    'async_http' => 'async-http',
+    'aws_sdk_firehose' => 'aws-sdk-firehose',
+    'aws_sdk_kinesis' => 'aws-sdk-kinesis',
+    'aws_sdk_lambda' => 'aws-sdk-lambda',
+    'ruby_kafka' => 'kafka',
+    'aws_sqs' => 'aws-sdk-sqs',
+    'concurrent_ruby' => 'concurrent-ruby',
+    'grpc_client' => 'grpc',
+    'memcache_client' => 'memcache-client',
+    'net_http' => 'net-http',
+    'ruby_openai' => 'ruby-openai'
+  }
+
+  NOT_A_GEM = ['grpc.host_denylist', 'puma_rack', 'puma_rack_urlmap', 'rack_urlmap', 'thread.tracing']
+
   def self.check_for_updates(watched_gems)
     return if verify_gem_list(watched_gems)
+
     watched_gems.each do |gem_name|
       gem_info = verify_gem(gem_name)
       versions = gem_versions(gem_info)
       send_slack_message(gem_message(gem_name, versions)) if gem_updated?(versions)
     end
+
     report_errors
   end
 
   private
+
+  def self.instrumented_gems
+    valid_gems = []
+    NewRelic::Agent::Configuration::DEFAULTS.keys.each do |key|
+      if key.start_with?('instrumentation.')
+        gem_name = key.to_s.gsub('instrumentation.', '')
+        gem_name = GEM_NAME_MAPPING[gem_name] if GEM_NAME_MAPPING.include?(gem_name)
+        valid_gems << gem_name unless NOT_A_GEM.include?(gem_name)
+      end
+    end
+
+    valid_gems
+  end
 
   def self.verify_gem_list(watched_gems)
     abort("Nothing to see here! The 'watched_gems' array cannot be empty") if watched_gems.empty?
@@ -92,5 +126,5 @@ class GemNotifier < SlackNotifier
 end
 
 if $PROGRAM_NAME == __FILE__
-  GemNotifier.check_for_updates(ARGV)
+  GemNotifier.check_for_updates(GemNotifier.instrumented_gems)
 end
