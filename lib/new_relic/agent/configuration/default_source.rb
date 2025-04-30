c349be30369fdeddaff1d6c9d0e2e1c3ef36b33e
Kayla Reopelle <kreopelle@newrelic.com>
2025-04-30 09:55:36 -0700

    Update desc spacing to match docs-eng changes
    These changes were made to support auto-translation tools.

--- lib/new_relic/agent/configuration/default_source.rb
+++ lib/new_relic/agent/configuration/default_source.rb
@@ -387,9 +387,9 @@ module NewRelic
           :allowed_from_server => false,
           :description => <<~DESCRIPTION
             An array of ActiveSupport custom event names to subscribe to and instrument. For example,
-            - one.custom.event
-            - another.event
-            - a.third.event
+              - one.custom.event
+              - another.event
+              - a.third.event
           DESCRIPTION
         },
         :'ai_monitoring.enabled' => {
@@ -407,11 +407,11 @@ module NewRelic
           :description => <<~DESCRIPTION
             If `false`, LLM instrumentation (OpenAI only for now) will not capture input and output content on specific LLM events.
 
-            The excluded attributes include:
-            - `content` from LlmChatCompletionMessage events
-            - `input` from LlmEmbedding events
+              The excluded attributes include:
+              - `content` from LlmChatCompletionMessage events
+              - `input` from LlmEmbedding events
 
-            This is an optional security setting to prevent recording sensitive data sent to and received from your LLMs.
+              This is an optional security setting to prevent recording sensitive data sent to and received from your LLMs.
           DESCRIPTION
         },
         # this is only set via server side config
@@ -487,10 +487,10 @@ module NewRelic
           :allowed_from_server => false,
           :description => <<~DESC
             Path to `newrelic.yml`. If undefined, the agent checks the following directories (in order):
-            - `config/newrelic.yml`
-            - `newrelic.yml`
-            - `$HOME/.newrelic/newrelic.yml`
-            - `$HOME/newrelic.yml`
+              - `config/newrelic.yml`
+              - `newrelic.yml`
+              - `$HOME/.newrelic/newrelic.yml`
+              - `$HOME/newrelic.yml`
           DESC
         },
         :'exclude_newrelic_header' => {
@@ -676,10 +676,10 @@ module NewRelic
           :allowed_from_server => true,
           :description => <<~DESC
             Obfuscation level for SQL queries reported in transaction trace nodes.
-            By default, this is set to `obfuscated`, which strips out the numeric and string literals.
-            - If you do not want the agent to capture query information, set this to `none`.
-            - If you want the agent to capture all query information in its original form, set this to `raw`.
-            - When you enable [high security mode](/docs/agents/manage-apm-agents/configuration/high-security-mode), this is automatically set to `obfuscated`.
+              By default, this is set to `obfuscated`, which strips out the numeric and string literals.
+              - If you do not want the agent to capture query information, set this to `none`.
+              - If you want the agent to capture all query information in its original form, set this to `raw`.
+              - When you enable [high security mode](/docs/agents/manage-apm-agents/configuration/high-security-mode), this is automatically set to `obfuscated`.
           DESC
         },
 
@@ -858,19 +858,19 @@ module NewRelic
           :description => <<~DESCRIPTION
             Sets the minimum level a log event must have to be forwarded to New Relic.
 
-            This is based on the integer values of [Ruby's `Logger::Severity` constants](https://github.com/ruby/logger/blob/113b82a06b3076b93a71cd467e1605b23afb3088/lib/logger/severity.rb).
+              This is based on the integer values of [Ruby's `Logger::Severity` constants](https://github.com/ruby/logger/blob/113b82a06b3076b93a71cd467e1605b23afb3088/lib/logger/severity.rb).
 
-            The intention is to forward logs with the level given to the configuration, as well as any logs with a higher level of severity.
+              The intention is to forward logs with the level given to the configuration, as well as any logs with a higher level of severity.
 
-            For example, setting this value to "debug" will forward all log events to New Relic. Setting this value to "error" will only forward log events with the levels "error", "fatal", and "unknown".
+              For example, setting this value to "debug" will forward all log events to New Relic. Setting this value to "error" will only forward log events with the levels "error", "fatal", and "unknown".
 
-            Valid values (ordered lowest to highest):
-            - "debug"
-            - "info"
-            - "warn"
-            - "error"
-            - "fatal"
-            - "unknown"
+              Valid values (ordered lowest to highest):
+              - "debug"
+              - "info"
+              - "warn"
+              - "error"
+              - "fatal"
+              - "unknown"
           DESCRIPTION
         },
         :'application_logging.forwarding.custom_attributes' => {
@@ -1153,7 +1153,7 @@ module NewRelic
           :public => true,
           :type => Boolean,
           :allowed_from_server => true,
-          :description => "If `true`, the agent will report source code level metrics for traced methods.\nSee: " \
+          :description => "If `true`, the agent will report source code level metrics for traced methods.\n  See: " \
                           'https://docs.newrelic.com/docs/apm/agents/ruby-agent/features/ruby-codestream-integration/'
         },
         # Cross application tracer
@@ -1165,7 +1165,7 @@ module NewRelic
           :deprecated => true,
           :description => deprecated_description(
             :'distributed_tracing.enabled',
-            'If `true`, enables [cross-application tracing](/docs/agents/ruby-agent/features/cross-application-tracing-ruby/) when `distributed_tracing.enabled` is set to `false`.'
+            '  If `true`, enables [cross-application tracing](/docs/agents/ruby-agent/features/cross-application-tracing-ruby/) when `distributed_tracing.enabled` is set to `false`.'
           )
         },
         # Custom attributes
@@ -1185,45 +1185,45 @@ module NewRelic
           :description => <<~DESCRIPTION
             An array of `CLASS#METHOD` (for instance methods) and/or `CLASS.METHOD` (for class methods) strings representing Ruby methods that the agent can automatically add custom instrumentation to. This doesn't require any modifications of the source code that defines the methods.
 
-            Use fully qualified class names (using the `::` delimiter) that include any module or class namespacing.
+              Use fully qualified class names (using the `::` delimiter) that include any module or class namespacing.
 
-            Here is some Ruby source code that defines a `render_png` instance method for an `Image` class and a `notify` class method for a `User` class, both within a `MyCompany` module namespace:
+              Here is some Ruby source code that defines a `render_png` instance method for an `Image` class and a `notify` class method for a `User` class, both within a `MyCompany` module namespace:
 
-            ```rb
-            module MyCompany
-              class Image
-                def render_png
-                  # code to render a PNG
+              ```rb
+              module MyCompany
+                class Image
+                  def render_png
+                    # code to render a PNG
+                  end
+                end
+
+                class User
+                  def self.notify
+                    # code to notify users
+                  end
                 end
               end
+              ```
 
-              class User
-                def self.notify
-                  # code to notify users
-                end
-              end
-            end
-            ```
+              Given that source code, the `newrelic.yml` config file might request instrumentation for both of these methods like so:
 
-            Given that source code, the `newrelic.yml` config file might request instrumentation for both of these methods like so:
+              ```yaml
+              automatic_custom_instrumentation_method_list:
+                - MyCompany::Image#render_png
+                - MyCompany::User.notify
+              ```
 
-            ```yaml
-            automatic_custom_instrumentation_method_list:
-              - MyCompany::Image#render_png
-              - MyCompany::User.notify
-            ```
+              That configuration example uses YAML array syntax to specify both methods. Alternatively, you can use a comma-delimited string:
 
-            That configuration example uses YAML array syntax to specify both methods. Alternatively, you can use a comma-delimited string:
+              ```yaml
+              automatic_custom_instrumentation_method_list: 'MyCompany::Image#render_png, MyCompany::User.notify'
+              ```
 
-            ```yaml
-            automatic_custom_instrumentation_method_list: 'MyCompany::Image#render_png, MyCompany::User.notify'
-            ```
+              Whitespace around the comma(s) in the list is optional. When configuring the agent with a list of methods via the `NEW_RELIC_AUTOMATIC_CUSTOM_INSTRUMENTATION_METHOD_LIST` environment variable, use this comma-delimited string format:
 
-            Whitespace around the comma(s) in the list is optional. When configuring the agent with a list of methods via the `NEW_RELIC_AUTOMATIC_CUSTOM_INSTRUMENTATION_METHOD_LIST` environment variable, use this comma-delimited string format:
-
-            ```sh
-            export NEW_RELIC_AUTOMATIC_CUSTOM_INSTRUMENTATION_METHOD_LIST='MyCompany::Image#render_png, MyCompany::User.notify'
-            ```
+              ```sh
+              export NEW_RELIC_AUTOMATIC_CUSTOM_INSTRUMENTATION_METHOD_LIST='MyCompany::Image#render_png, MyCompany::User.notify'
+              ```
           DESCRIPTION
         },
         # Custom events
@@ -1426,18 +1426,18 @@ module NewRelic
           :description => <<~DESCRIPTION
             If `true`, disables agent middleware for Sinatra. This middleware is responsible for advanced feature support such as [cross application tracing](/docs/apm/transactions/cross-application-traces/cross-application-tracing), [page load timing](/docs/browser/new-relic-browser/getting-started/new-relic-browser), and [error collection](/docs/apm/applications-menu/events/view-apm-error-analytics).
 
-            <Callout variant="important">
-            Cross application tracing is deprecated in favor of [distributed tracing](/docs/apm/distributed-tracing/getting-started/introduction-distributed-tracing). Distributed tracing is on by default for Ruby agent versions 8.0.0 and above. Middlewares are not required to support distributed tracing.
+              <Callout variant="important">
+              Cross application tracing is deprecated in favor of [distributed tracing](/docs/apm/distributed-tracing/getting-started/introduction-distributed-tracing). Distributed tracing is on by default for Ruby agent versions 8.0.0 and above. Middlewares are not required to support distributed tracing.
 
-            To continue using cross application tracing, update the following options in your `newrelic.yml` configuration file:
+              To continue using cross application tracing, update the following options in your `newrelic.yml` configuration file:
 
-            ```yaml
-            cross_application_tracer:
-              enabled: true
-            distributed_tracing:
-              enabled: false
-            ```
-            </Callout>
+              ```yaml
+              cross_application_tracer:
+                enabled: true
+              distributed_tracing:
+                enabled: false
+              ```
+              </Callout>
           DESCRIPTION
         },
         :disable_view_instrumentation => {
@@ -1961,10 +1961,10 @@ module NewRelic
           :transform => DefaultSource.method(:convert_to_list),
           :description => <<~DESCRIPTION
             An array of strings to specify which keys and/or values inside a Stripe event's `user_data` hash should
-            not be reported to New Relic. Each string in this array will be turned into a regular expression via
-            `Regexp.new` to permit advanced matching. For each hash pair, if either the key or value is matched the pair
-            isn't reported. By default, no `user_data` is reported. Use this option only if the
-            `stripe.user_data.include` option is also used.
+              not be reported to New Relic. Each string in this array will be turned into a regular expression via
+              `Regexp.new` to permit advanced matching. For each hash pair, if either the key or value is matched the pair
+              isn't reported. By default, no `user_data` is reported. Use this option only if the
+              `stripe.user_data.include` option is also used.
           DESCRIPTION
         },
         :'instrumentation.thread' => {
