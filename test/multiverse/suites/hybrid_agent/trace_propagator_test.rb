d74f4de237cc3f3af1d8abe327d30aab6f2d80cd
Kayla Reopelle <kreopelle@newrelic.com>
2025-06-11 16:25:22 -0700

    Add trace propagator tests

--- test/multiverse/suites/hybrid_agent/trace_propagator_test.rb
+++ test/multiverse/suites/hybrid_agent/trace_propagator_test.rb
@@ -10,13 +10,53 @@ module NewRelic
           class TracePropagatorTest < Minitest::Test
             class FakePropError < StandardError; end
 
+            def setup
+              @propagator = OpenTelemetry::Context::Propagation::TracePropagator.new
+            end
+
             def test_inject_calls_distributed_tracing_api
-              @propagator = NewRelic::Agent::OpenTelemetry::Context::Propagation::TracePropagator.new
               fake_carrier = {}
 
-              NewRelic::Agent::DistributedTracing.stub(:insert_distributed_trace_headers, ->(carrier) { raise FakePropError.new }) do
+              DistributedTracing.stub(:insert_distributed_trace_headers, ->(carrier) { raise FakePropError.new }) do
                 assert_raises(FakePropError) { @propagator.inject(fake_carrier) }
               end
+            end
+
+            def test_extract_returns_context_with_standard_headers
+
+            end
+
+            def test_extract_rescues_errors_and_returns_empty_context
+              carrier = {}
+              result = @propagator.extract(carrier)
+              assert_raises
+              assert_instance_of(::OpenTelemetry::Context, result)
+            end
+
+            def test_extract_handles_rack_getter
+              # This getter class is deprecated and may go away someday
+              # The other getter class in the code is from a different library
+              # If we want to test with it, we need to add opentelemetry-common to
+              # the Envfile
+              with_config(:account_id => '190', primary_application_id: '46954') do
+                rack_carrier = {
+                  'HTTP_TRACEPARENT' => "00-da8bc8cc6d062849b0efcf3c169afb5a-7d3efb1b173fecfa-01",
+                  'HTTP_TRACESTATE' => "190@nr=0-0-1-46954-7d3efb1b173fecfa-e8b91a159289ff74-01-01.23456-1518469636035"
+                }
+                result = @propagator.extract(rack_carrier, getter: ::OpenTelemetry::Context::Propagation.rack_env_getter)
+
+                # it's difficult to access the spancontext information
+                # from a context object. parsing the string will help us
+                # check the values with fewer shenanigans
+                context_string = result.instance_variable_get(:@entries).to_s
+                assert_match(/trace_id=\"da8bc8cc6d062849b0efcf3c169afb5a"/, context_string, 'trace_id does not match')
+                assert_match(/span_id=\"7d3efb1b173fecfa"/, context_string, "span_id doesn't match")
+                assert_match(/trace_flags=\"00\"/, context_string, "trace_flags doesn't match")
+                assert_match(/@transaction_id=\"e8b91a159289ff74\"/, context_string, "transaction_id doesn't match")
+                assert_match(/@sampled=true/, context_string, "sampled doesn't match")
+                assert_match(/@parent_app_id=\"46954/, context_string, "parent_app_id doesn't match")
+              end
+
             end
           end
         end
