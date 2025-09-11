c916e08b5ce95e7bcd1475461511bfd4b0ff5923
Hannah Ramadan <hannahr315@gmail.com>
2025-09-11 11:59:05 -0700

    remove logger from suite.rb

--- test/multiverse/lib/multiverse/suite.rb
+++ test/multiverse/lib/multiverse/suite.rb
@@ -11,7 +11,7 @@ require_relative '../../../warning_test_helper'
 require_relative '../../../simplecov_test_helper'
 require_relative '../../../../lib/new_relic/base64'
 
-require 'logger'
+# require 'logger'
 require 'rubygems'
 require 'fileutils'
 require 'digest'
