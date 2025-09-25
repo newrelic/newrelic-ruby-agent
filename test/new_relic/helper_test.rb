da287768ef9073b6534854861c987d499babba18
Hannah Ramadan <hannahr315@gmail.com>
2025-09-25 11:39:55 -0700

    nil -> ''

--- test/new_relic/helper_test.rb
+++ test/new_relic/helper_test.rb
@@ -87,7 +87,7 @@ class HelperTest < Minitest::Test
     assert(NewRelic::Helper.version_satisfied?(1, '<', 2))
     assert(NewRelic::Helper.version_satisfied?(1.2, '<', 1.3))
     assert(NewRelic::Helper.version_satisfied?(Gem::Version.new('1.2'), '<', Gem::Version.new('1.3')))
-    assert(NewRelic::Helper.version_satisfied?(nil, '<', 1))
+    assert(NewRelic::Helper.version_satisfied?('', '<', 1))
     assert(NewRelic::Helper.version_satisfied?('', '<', 1))
     assert(NewRelic::Helper.version_satisfied?('1.2', '<', 1.3))
   end
@@ -98,7 +98,7 @@ class HelperTest < Minitest::Test
     assert(NewRelic::Helper.version_satisfied?(1, '<=', 2))
     assert(NewRelic::Helper.version_satisfied?(1.2, '<=', 1.3))
     assert(NewRelic::Helper.version_satisfied?(Gem::Version.new('1.2'), '<=', Gem::Version.new('1.3')))
-    assert(NewRelic::Helper.version_satisfied?(nil, '<=', 1))
+    assert(NewRelic::Helper.version_satisfied?('', '<=', 1))
     assert(NewRelic::Helper.version_satisfied?('', '<=', 1))
     assert(NewRelic::Helper.version_satisfied?('1.2', '<=', 1.3))
   end
@@ -109,7 +109,7 @@ class HelperTest < Minitest::Test
     assert(NewRelic::Helper.version_satisfied?(2, '>', 1))
     assert(NewRelic::Helper.version_satisfied?(1.3, '>', 1.2))
     assert(NewRelic::Helper.version_satisfied?(Gem::Version.new('1.3'), '>', Gem::Version.new('1.2')))
-    assert(NewRelic::Helper.version_satisfied?(1, '>', nil))
+    assert(NewRelic::Helper.version_satisfied?(1, '>', ''))
     assert(NewRelic::Helper.version_satisfied?(1, '>', ''))
     assert(NewRelic::Helper.version_satisfied?(1.3, '>', '1.2'))
   end
@@ -120,7 +120,7 @@ class HelperTest < Minitest::Test
     assert(NewRelic::Helper.version_satisfied?(2, '>=', 1))
     assert(NewRelic::Helper.version_satisfied?(1.3, '>=', 1.2))
     assert(NewRelic::Helper.version_satisfied?(Gem::Version.new('1.3'), '>=', Gem::Version.new('1.2')))
-    assert(NewRelic::Helper.version_satisfied?(1, '>=', nil))
+    assert(NewRelic::Helper.version_satisfied?(1, '>=', ''))
     assert(NewRelic::Helper.version_satisfied?(1, '>=', ''))
     assert(NewRelic::Helper.version_satisfied?(1.3, '>=', '1.2'))
   end
