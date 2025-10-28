6c326fa2a3013e300578e83b0d30136e2cc60b34
Giulio Giraldi <dongiulio@gmail.com>
2025-10-28 13:13:06 +0100

    rubocop

--- test/multiverse/suites/sidekiq_notice_only_once/sidekiq_notice_only_once_enabled_test.rb
+++ test/multiverse/suites/sidekiq_notice_only_once/sidekiq_notice_only_once_enabled_test.rb
@@ -17,16 +17,16 @@ class SidekiqNoticeOnlyOnceEnabledTest < Minitest::Test
     skip 'Test requires Sidekiq v6+' unless Sidekiq::VERSION.split('.').first.to_i >= 6
 
     config = if Sidekiq::VERSION.split('.').first.to_i >= 7
-               Sidekiq.default_configuration
-             else
-               Sidekiq
-             end
+      Sidekiq.default_configuration
+    else
+      Sidekiq
+    end
 
     death_handlers = if config.respond_to?(:death_handlers)
-                       config.death_handlers
-                     else
-                       config[:death_handlers] || []
-                     end
+      config.death_handlers
+    else
+      config[:death_handlers] || []
+    end
 
     nr_death_handler_found = death_handlers.any? do |handler|
       handler.is_a?(Proc) && handler.source_location&.first&.include?('newrelic')
@@ -41,16 +41,16 @@ class SidekiqNoticeOnlyOnceEnabledTest < Minitest::Test
     skip 'Test requires Sidekiq v6+' unless Sidekiq::VERSION.split('.').first.to_i >= 6
 
     config = if Sidekiq::VERSION.split('.').first.to_i >= 7
-               Sidekiq.default_configuration
-             else
-               Sidekiq
-             end
+      Sidekiq.default_configuration
+    else
+      Sidekiq
+    end
 
     error_handlers = if config.respond_to?(:error_handlers)
-                       config.error_handlers
-                     else
-                       config[:error_handlers] || []
-                     end
+      config.error_handlers
+    else
+      config[:error_handlers] || []
+    end
 
     nr_error_handler_found = error_handlers.any? do |handler|
       handler.is_a?(Proc) && handler.source_location&.first&.include?('newrelic')
