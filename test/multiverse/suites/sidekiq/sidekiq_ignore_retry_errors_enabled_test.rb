d20bd30f164a23e3bcaf133c15554792f9152536
Hannah Ramadan <hannahr315@gmail.com>
2025-11-06 15:07:36 -0800

    rubocop

--- test/multiverse/suites/sidekiq/sidekiq_ignore_retry_errors_enabled_test.rb
+++ test/multiverse/suites/sidekiq/sidekiq_ignore_retry_errors_enabled_test.rb
@@ -1,6 +1,6 @@
-# # This file is distributed under New Relic's license terms.
-# # See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
-# # frozen_string_literal: true
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+# frozen_string_literal: true
 
 # require_relative 'sidekiq_test_helpers'
 
