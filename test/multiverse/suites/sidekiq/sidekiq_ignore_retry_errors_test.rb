ecd871ae41a8c6cb18e06bdcd4371facd870d2f5
Hannah Ramadan <hannahr315@gmail.com>
2025-11-06 10:20:35 -0800

    instrumentation edit

--- test/multiverse/suites/sidekiq/sidekiq_ignore_retry_errors_test.rb
+++ test/multiverse/suites/sidekiq/sidekiq_ignore_retry_errors_test.rb
@@ -50,6 +50,32 @@ class SidekiqIgnoreRetryErrorsTest < Minitest::Test
       'Expected NewRelic error_handler to be registered when sidekiq.ignore_retry_errors is false'
   end
 
+  def test_error_handlers_not_registered_when_sidekiq_ignore_retry_errors_is_true
+    # TODO: MAJOR VERSION - remove this when Sidekiq v5 is no longer supported
+    skip 'Test requires Sidekiq v6+' unless Sidekiq::VERSION.split('.').first.to_i >= 6
+
+    with_config(:'sidekiq.ignore_retry_errors' => true) do
+      config = if Sidekiq::VERSION.split('.').first.to_i >= 7
+        Sidekiq.default_configuration
+      else
+        Sidekiq
+      end
+
+      error_handlers = if config.respond_to?(:error_handlers)
+        config.error_handlers
+      else
+        config[:error_handlers] || []
+      end
+
+      nr_error_handler_found = error_handlers.any? do |handler|
+        handler.is_a?(Proc) && handler.source_location&.first&.include?('newrelic')
+      end
+
+      refute nr_error_handler_found,
+        'Expected NewRelic error_handler to NOT be registered when sidekiq.ignore_retry_errors is true'
+    end
+  end
+
   def test_death_handlers_registered_when_sidekiq_ignore_retry_errors_is_true
     # TODO: MAJOR VERSION - remove this when Sidekiq v5 is no longer supported
     skip 'Test requires Sidekiq v6+' unless Sidekiq::VERSION.split('.').first.to_i >= 6
@@ -98,32 +124,6 @@ class SidekiqIgnoreRetryErrorsTest < Minitest::Test
 
     refute nr_death_handler_found,
       'Expected NewRelic death_handler to NOT be registered when sidekiq.ignore_retry_errors is false'
-  end
-
-  def test_error_handlers_not_registered_when_sidekiq_ignore_retry_errors_is_true
-    # TODO: MAJOR VERSION - remove this when Sidekiq v5 is no longer supported
-    skip 'Test requires Sidekiq v6+' unless Sidekiq::VERSION.split('.').first.to_i >= 6
-
-    with_config(:'sidekiq.ignore_retry_errors' => true) do
-      config = if Sidekiq::VERSION.split('.').first.to_i >= 7
-        Sidekiq.default_configuration
-      else
-        Sidekiq
-      end
-
-      error_handlers = if config.respond_to?(:error_handlers)
-        config.error_handlers
-      else
-        config[:error_handlers] || []
-      end
-
-      nr_error_handler_found = error_handlers.any? do |handler|
-        handler.is_a?(Proc) && handler.source_location&.first&.include?('newrelic')
-      end
-
-      refute nr_error_handler_found,
-        'Expected NewRelic error_handler to NOT be registered when sidekiq.ignore_retry_errors is true'
-    end
   end
 
   def test_basic_job_execution_still_works
