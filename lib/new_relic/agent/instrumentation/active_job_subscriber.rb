bf15249c8b49234ff9783eb6f181164b340ad6ce
Hannah Ramadan <76922290+hannahramadan@users.noreply.github.com>
2025-11-12 10:44:22 -0800

    Apply suggestions from code review
    Co-authored-by: Kayla Reopelle <87386821+kaylareopelle@users.noreply.github.com>

--- lib/new_relic/agent/instrumentation/active_job_subscriber.rb
+++ lib/new_relic/agent/instrumentation/active_job_subscriber.rb
@@ -22,7 +22,8 @@ module NewRelic
           job = payload[:job] || payload[:jobs].first
 
           queue = job.queue_name
-          job_class = job.class.name.split('::').last
+          job_class = class_name.include?('::') ?
+  class_name[class_name.rindex('::')+2..-1] : class_name
           method = method_from_name(name)
           "Ruby/ActiveJob/#{job_class}/#{queue}/#{method}"
         end
