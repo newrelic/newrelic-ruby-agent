69b681aa68fec47a0214a3b182a91b310e91da55
Tanna McClure <tmcclure@newrelic.com>
2026-01-16 15:45:50 -0600

    remove simeplcov forking stuff

--- test/simplecov_test_helper.rb
+++ test/simplecov_test_helper.rb
@@ -14,30 +14,6 @@ begin
     # Enable subprocess tracking IMMEDIATELY after loading SimpleCov
     # This must happen before SimpleCov.start is called
     SimpleCov.enable_for_subprocesses(true) if defined?(SimpleCov)
-
-    # Use external at_exit so we can control when coverage is finalized
-    SimpleCov.external_at_exit = true if defined?(SimpleCov)
-
-    # Store the parent process PID to detect forks
-    SIMPLECOV_PARENT_PID = Process.pid
-
-    # Register parent process finalization FIRST (runs LAST due to LIFO)
-    # This ensures parent processes finalize coverage at the very end
-    at_exit do
-      next unless defined?(SimpleCov) && SimpleCov.running
-
-      # Only finalize in parent process at the very end
-      SimpleCov.result if Process.pid == SIMPLECOV_PARENT_PID
-    end
-
-    # Register forked child finalization SECOND (runs FIRST due to LIFO)
-    # This ensures forked children finalize before they exit
-    at_exit do
-      next unless defined?(SimpleCov) && SimpleCov.running
-
-      # Only finalize in forked child processes
-      SimpleCov.result if Process.pid != SIMPLECOV_PARENT_PID
-    end
   end
 rescue LoadError => e
   puts
