a5301a470d0621e507491c51f1c13fdf3638a869
Kayla Reopelle <kreopelle@newrelic.com>
2026-02-09 10:49:04 -0800

    Update tests to use instrumentation not on exclude
    We changed what instrumentation was on the exclude list and the tests
    referenced some newly excluded instrumentation modules.

--- test/new_relic/agent/opentelemetry_bridge_test.rb
+++ test/new_relic/agent/opentelemetry_bridge_test.rb
@@ -90,28 +90,28 @@ module NewRelic
 
       def test_install_instrumentation_filters_excluded_instrumentation
         with_config(
-          :'opentelemetry.traces.exclude' => 'OpenTelemetry::Instrumentation::Redis,OpenTelemetry::Instrumentation::PG',
+          :'opentelemetry.traces.exclude' => 'OpenTelemetry::Instrumentation::Que,OpenTelemetry::Instrumentation::PG',
           :'opentelemetry.traces.include' => ''
         ) do
           mock_registry = Minitest::Mock.new
           mock_lock = Mutex.new
 
           # Create simple objects that respond to to_s
-          redis_inst = Object.new
+          que_inst = Object.new
           pg_inst = Object.new
-          rails_inst = Object.new
-          redis_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Redis' }
+          faraday_inst = Object.new
+          que_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Que' }
           pg_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::PG' }
-          rails_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Rails' }
+          faraday_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Faraday' }
 
-          mock_instrumentation = [redis_inst, pg_inst, rails_inst]
+          mock_instrumentation = [que_inst, pg_inst, faraday_inst]
 
           mock_registry.expect(:instance_variable_get, mock_lock, [:@lock])
           mock_registry.expect(:instance_variable_get, mock_instrumentation, [:@instrumentation])
           mock_registry.expect(:instance_variable_set, nil) do |var, value|
             assert_equal :@instrumentation, var
             assert_equal 1, value.length
-            assert_equal 'OpenTelemetry::Instrumentation::Rails', value.first.to_s
+            assert_equal 'OpenTelemetry::Instrumentation::Faraday', value.first.to_s
             true
           end
           mock_registry.expect(:install_all, nil)
@@ -136,18 +136,18 @@ module NewRelic
 
       def test_install_instrumentation_handles_whitespace_in_config
         with_config(
-          :'opentelemetry.traces.exclude' => ' OpenTelemetry::Instrumentation::Redis , OpenTelemetry::Instrumentation::PG ',
+          :'opentelemetry.traces.exclude' => ' OpenTelemetry::Instrumentation::Que , OpenTelemetry::Instrumentation::PG ',
           :'opentelemetry.traces.include' => ''
         ) do
           mock_registry = Minitest::Mock.new
           mock_lock = Mutex.new
 
-          redis_inst = Object.new
+          que_inst = Object.new
           pg_inst = Object.new
-          redis_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Redis' }
+          que_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Que' }
           pg_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::PG' }
 
-          mock_instrumentation = [redis_inst, pg_inst]
+          mock_instrumentation = [que_inst, pg_inst]
 
           mock_registry.expect(:instance_variable_get, mock_lock, [:@lock])
           mock_registry.expect(:instance_variable_get, mock_instrumentation, [:@instrumentation])
@@ -178,20 +178,20 @@ module NewRelic
 
       def test_install_instrumentation_configured_exclude_has_priority_over_include
         with_config(
-          :'opentelemetry.traces.exclude' => 'OpenTelemetry::Instrumentation::Redis,OpenTelemetry::Instrumentation::PG',
-          :'opentelemetry.traces.include' => 'OpenTelemetry::Instrumentation::Redis'
+          :'opentelemetry.traces.exclude' => 'OpenTelemetry::Instrumentation::Que,OpenTelemetry::Instrumentation::PG',
+          :'opentelemetry.traces.include' => 'OpenTelemetry::Instrumentation::Que'
         ) do
           mock_registry = Minitest::Mock.new
           mock_lock = Mutex.new
 
-          redis_inst = Object.new
+          que_inst = Object.new
           pg_inst = Object.new
-          rails_inst = Object.new
-          redis_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Redis' }
+          faraday_inst = Object.new
+          que_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Que' }
           pg_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::PG' }
-          rails_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Rails' }
+          faraday_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Faraday' }
 
-          mock_instrumentation = [redis_inst, pg_inst, rails_inst]
+          mock_instrumentation = [que_inst, pg_inst, faraday_inst]
 
           mock_registry.expect(:instance_variable_get, mock_lock, [:@lock])
           mock_registry.expect(:instance_variable_get, mock_instrumentation, [:@instrumentation])
@@ -200,8 +200,8 @@ module NewRelic
             # Configured exclude has priority, so Redis should be excluded even
             # though it's in include list
             assert_equal 1, value.length
-            assert_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Rails'
-            refute_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Redis'
+            assert_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Faraday'
+            refute_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Que'
             refute_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::PG'
             true
           end
@@ -233,14 +233,14 @@ module NewRelic
           mock_registry = Minitest::Mock.new
           mock_lock = Mutex.new
 
-          redis_inst = Object.new
+          que_inst = Object.new
           elasticsearch_inst = Object.new
           dalli_inst = Object.new
-          redis_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Redis' }
+          que_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Que' }
           elasticsearch_inst.define_singleton_method(:to_s) { 'elasticsearch-api' }
           dalli_inst.define_singleton_method(:to_s) { 'dalli' }
 
-          mock_instrumentation = [redis_inst, elasticsearch_inst, dalli_inst]
+          mock_instrumentation = [que_inst, elasticsearch_inst, dalli_inst]
 
           # Default excludes should be applied
           mock_registry.expect(:instance_variable_get, mock_lock, [:@lock])
@@ -248,7 +248,7 @@ module NewRelic
           mock_registry.expect(:instance_variable_set, nil) do |var, value|
             assert_equal :@instrumentation, var
             assert_equal 1, value.length
-            assert_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Redis'
+            assert_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Que'
             refute_includes value.map(&:to_s), 'elasticsearch-api'
             refute_includes value.map(&:to_s), 'dalli'
             true
@@ -281,14 +281,14 @@ module NewRelic
           mock_registry = Minitest::Mock.new
           mock_lock = Mutex.new
 
-          redis_inst = Object.new
+          que_inst = Object.new
           elasticsearch_inst = Object.new
           dalli_inst = Object.new
-          redis_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Redis' }
+          que_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Que' }
           elasticsearch_inst.define_singleton_method(:to_s) { 'elasticsearch-api' }
           dalli_inst.define_singleton_method(:to_s) { 'dalli' }
 
-          mock_instrumentation = [redis_inst, elasticsearch_inst, dalli_inst]
+          mock_instrumentation = [que_inst, elasticsearch_inst, dalli_inst]
 
           # elasticsearch-api is in default exclude list but also in configured include list
           # Configured include should override default exclude
@@ -297,7 +297,7 @@ module NewRelic
           mock_registry.expect(:instance_variable_set, nil) do |var, value|
             assert_equal :@instrumentation, var
             assert_equal 2, value.length
-            assert_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Redis'
+            assert_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Que'
             assert_includes value.map(&:to_s), 'elasticsearch-api'
             refute_includes value.map(&:to_s), 'dalli'
             true
@@ -330,14 +330,14 @@ module NewRelic
           mock_registry = Minitest::Mock.new
           mock_lock = Mutex.new
 
-          redis_inst = Object.new
+          que_inst = Object.new
           elasticsearch_inst = Object.new
           dalli_inst = Object.new
-          redis_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Redis' }
+          que_inst.define_singleton_method(:to_s) { 'OpenTelemetry::Instrumentation::Que' }
           elasticsearch_inst.define_singleton_method(:to_s) { 'elasticsearch-api' }
           dalli_inst.define_singleton_method(:to_s) { 'dalli' }
 
-          mock_instrumentation = [redis_inst, elasticsearch_inst, dalli_inst]
+          mock_instrumentation = [que_inst, elasticsearch_inst, dalli_inst]
 
           # elasticsearch-api is in configured exclude list,
           # so it should be excluded even though it's also in configured include
@@ -347,7 +347,7 @@ module NewRelic
           mock_registry.expect(:instance_variable_set, nil) do |var, value|
             assert_equal :@instrumentation, var
             assert_equal 2, value.length
-            assert_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Redis'
+            assert_includes value.map(&:to_s), 'OpenTelemetry::Instrumentation::Que'
             assert_includes value.map(&:to_s), 'dalli'
             refute_includes value.map(&:to_s), 'elasticsearch-api'
             true
