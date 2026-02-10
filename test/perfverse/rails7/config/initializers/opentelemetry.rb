06736f38e00f024ae4becfc5f77e8a170d98a159
Kayla Reopelle <kreopelle@newrelic.com>
2026-02-10 07:45:07 -0800

    Add license header to initializer

--- test/perfverse/rails7/config/initializers/opentelemetry.rb
+++ test/perfverse/rails7/config/initializers/opentelemetry.rb
@@ -1,3 +1,7 @@
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+# frozen_string_literal: true
+
 require 'opentelemetry-api'
 
 APP_TRACER = OpenTelemetry.tracer_provider.tracer('agent-app', '0.0.0')
