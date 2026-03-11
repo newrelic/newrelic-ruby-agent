0e15dddffce5141c79ce28184441754d74a8c581
Hannah Ramadan <hannahr315@gmail.com>
2026-03-11 10:43:06 -0700

    Update OTel patch

--- lib/new_relic/agent/opentelemetry/trace_patch.rb
+++ lib/new_relic/agent/opentelemetry/trace_patch.rb
@@ -9,21 +9,31 @@ module NewRelic
         def current_span(context = nil)
           return super if context
 
-          thread = Thread.current
-          return super if thread[:nr_otel_recursion_guard]
+          storage = otel_current_storage
+          return super if storage[:nr_otel_recursion_guard]
 
-          nr_span = thread[:nr_otel_current_span]
+          nr_span = storage[:nr_otel_current_span]
           return nr_span if nr_span
 
           # Fallback with recursion protection
-          thread[:nr_otel_recursion_guard] = true
+          storage[:nr_otel_recursion_guard] = true
           result = super
-          thread[:nr_otel_recursion_guard] = nil
+          storage[:nr_otel_recursion_guard] = nil
 
           result
         rescue => e
           NewRelic::Agent.logger.debug("Error in OpenTelemetry.current_span override, falling back to original implementation: #{e}")
           super
+        end
+
+        private
+
+        def otel_current_storage
+          if NewRelic::Agent.config[:dispatcher] == :falcon
+            Fiber.current
+          else
+            Thread.current
+          end
         end
       end
     end
--- lib/new_relic/agent/opentelemetry/transaction_patch.rb
+++ lib/new_relic/agent/opentelemetry/transaction_patch.rb
@@ -10,7 +10,7 @@ module NewRelic
           @current_segment_lock.synchronize do
             if new_segment&.respond_to?(:transaction) && new_segment.transaction
               span = find_or_create_span(new_segment)
-              Thread.current[:nr_otel_current_span] = span
+              otel_current_storage[:nr_otel_current_span] = span
             end
           end
 
@@ -18,20 +18,28 @@ module NewRelic
         end
 
         def remove_current_segment_by_thread_id(id)
-          if id == Thread.current.object_id
-            Thread.current[:nr_otel_current_span] = nil
+          if id == NewRelic::Agent::TransactionTimeAggregator.current_execution_context_id
+            otel_current_storage[:nr_otel_current_span] = nil
           end
 
           super
         end
 
         def finish
-          Thread.current[:nr_otel_current_span] = nil
+          otel_current_storage[:nr_otel_current_span] = nil
 
           super
         end
 
         private
+
+        def otel_current_storage
+          if NewRelic::Agent.config[:dispatcher] == :falcon
+            Fiber.current
+          else
+            Thread.current
+          end
+        end
 
         def find_or_create_span(segment)
           if segment.instance_variable_defined?(:@otel_span)
