cc215b8fdf6ea505e1815afb8bc58585375b74ab
Kayla Reopelle <kreopelle@newrelic.com>
2026-03-17 17:05:13 -0700

    wip better tests

--- test/multiverse/suites/sidekiq/sidekiq_force_new_transaction_test.rb
+++ test/multiverse/suites/sidekiq/sidekiq_force_new_transaction_test.rb
@@ -16,10 +16,6 @@ class SidekiqForceNewTransactionTest < Minitest::Test
     mocha_teardown
   end
 
-  # TODO: Do we need to add active job to reproduce the behavior this feature intends to fix?
-  # TODO: Do we need to add tests related to DT headers? Recheck what the customer's concern about that was
-  # TODO: Are we able to use perform_action_with_newrelic_trace or do we need to use start_new_transaction?
-
   def test_sidekiq_force_new_transaction_default_value
     refute NewRelic::Agent.config[:'sidekiq.force_new_transaction'],
       'Expected sidekiq.force_new_transaction default to be false'
@@ -55,7 +51,6 @@ class SidekiqForceNewTransactionTest < Minitest::Test
         NRDeadEndJob.perform_inline
       end
 
-      # Do I need these if I harvest?
       segment_names = t.segments.map(&:name)
 
       assert_equal %w[web Nested/OtherTransaction/SidekiqJob/NRDeadEndJob/perform], segment_names
@@ -66,17 +61,16 @@ class SidekiqForceNewTransactionTest < Minitest::Test
       assert_equal 1, transactions[1].size
 
       first_transaction = transactions[1][0]
-      # assert_equal 'web', first_transaction['name']
+      assert_equal 'web', first_transaction[0]['name']
 
-      spans = harvest_span_events!
+      _, spans = harvest_span_events!
 
       assert_equal 2, spans[1].size
+      assert_equal 'web', spans[1][0]['name']
+      assert spans[1][0]['nr.entryPoint']
 
-      # other possible span assertions:
-      # make sure the nr.entryPoint attribute is present on the web span
-      # make sure it isn't on the sidekiq span
-      # make sure the sidekiq span is the child of the web span
-      # could make sure they all have the same transaction id
+      assert 'Nested/OtherTransaction/SidekiqJob/NRDeadEndJob/perform', spans[1][0]['name']
+      assert_equal spans[1][0]['transactionId'], spans[1][1]['transactionId']
     end
   end
 
@@ -97,35 +91,25 @@ class SidekiqForceNewTransactionTest < Minitest::Test
         NRDeadEndJob.perform_inline
       end
 
-      segment_names = t.segments.map(&:name)
-
-      assert_equal %w[web], segment_names
-      # TODO: ADD MORE ASSERTIONS
-    end
-  end
-
-  def test_sidekiq_job_part_of_new_transaction_when_true
-    # Sidekiq version 6.x's perform_inline invokes String#constantize, which is only
-    # delivered by ActiveSupport, which this test suite doesn't currently include.
-    skip 'Test requires Sidekiq v7+' unless NewRelic::Helper.version_satisfied?(Sidekiq::VERSION, '>=', '7.0.0')
-
-    with_config(:'sidekiq.force_new_transaction' => true) do
-      config = if Sidekiq::VERSION.split('.').first.to_i >= 7
-        Sidekiq.default_configuration
-      else
-        Sidekiq
-      end
-
-      t = in_web_transaction('web') do |txn|
-        txn.stubs(:sampled?).returns(true)
-        NRDeadEndJob.perform_inline
-      end
-
-      # Do I need to harvest?
-      segment_names = t.segments.map(&:name)
-
       transactions = harvest_transaction_events!
-      # TODO: ADD MORE ASSERTIONS
+
+      assert_equal 2, transactions[0][:events_seen]
+      assert_equal 2, transactions[1].size
+
+      first_transaction = transactions[1][0]
+      assert_equal 'web', first_transaction['name']
+
+      second_transaction = transactions[1][1]
+      assert_equal 'Nested/OtherTransaction/SidekiqJob/NRDeadEndJob/perform', second_transaction['name']
+
+      _, spans = harvest_span_events!
+
+      assert_equal 2, spans[1].size
+      assert_equal 'web', spans[1][1]['name']
+      assert spans[1][1]['nr.entryPoint']
+
+      assert 'Nested/OtherTransaction/SidekiqJob/NRDeadEndJob/perform', spans[1][0]['name']
+      assert_equal spans[1][0]['transactionId'], spans[1][1]['transactionId']
     end
   end
 end
