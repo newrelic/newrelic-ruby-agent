bc1d595cf79b22103c30152dd85bde7b14eb2a4a
Hannah Ramadan <hannahr315@gmail.com>
2026-04-06 14:25:35 -0700

    Remove comment

--- lib/new_relic/agent/transaction/transaction_sample_buffer.rb
+++ lib/new_relic/agent/transaction/transaction_sample_buffer.rb
@@ -78,9 +78,6 @@ module NewRelic
         # Our default truncation strategy is to keep max_capacity
         # worth of the longest samples. Override this method for alternate
         # behavior.
-        #
-        # This doesn't use the more convenient #last and #sort_by to avoid
-        # additional array allocations (and abundant alliteration)
         def truncate_samples
           @samples.sort_by!(&:duration)
           @samples.slice!(0..-(max_capacity + 1))
