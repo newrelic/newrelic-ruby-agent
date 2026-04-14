59d75b9ce17c6a9a8fa1d97d15b9579ff5ac0de3
Kayla Reopelle <kreopelle@newrelic.com>
2026-04-14 11:52:27 +0200

    Reduce method calls by caching log level values
    Previously, upcase.to_sym was being called at least twice on every log
    recorded -- once on the configured log level and once on the severity
    of the log event. Now, these method calls are reduced through caching.
    
    * Memoize the configured_log_level_constant method
    * Create a hash cache for severity constants seen by the agent

--- lib/new_relic/agent/log_event_aggregator.rb
+++ lib/new_relic/agent/log_event_aggregator.rb
@@ -53,6 +53,7 @@ module NewRelic
         @high_security = NewRelic::Agent.config[:high_security]
         @instrumentation_logger_enabled = NewRelic::Agent::Instrumentation::Logger.enabled?
         @attributes = NewRelic::Agent::LogEventAttributes.new
+        @severity_constant_cache = {}
 
         register_for_done_configuring(events)
       end
@@ -320,6 +321,8 @@ module NewRelic
           @seen = 0
           @seen_by_severity.clear
         end
+        @severity_constant_cache.clear
+        @configured_log_level_constant = nil
 
         super
       end
@@ -462,11 +465,11 @@ module NewRelic
       end
 
       def configured_log_level_constant
-        format_log_level_constant(NewRelic::Agent.config[LOG_LEVEL_KEY])
+        @configured_log_level_constant ||= format_log_level_constant(NewRelic::Agent.config[LOG_LEVEL_KEY])
       end
 
       def format_log_level_constant(log_level)
-        log_level.upcase.to_sym
+        @severity_constant_cache[log_level] ||= log_level.upcase.to_sym
       end
 
       def severity_too_low?(severity)
