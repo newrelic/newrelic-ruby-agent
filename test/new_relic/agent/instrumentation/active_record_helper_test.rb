99652dc649d43ce1e41dc28165e3253249347dad
Hannah Ramadan <hannahr315@gmail.com>
2026-04-21 12:19:01 -0700

    AR helper tests

--- test/new_relic/agent/instrumentation/active_record_helper_test.rb
+++ test/new_relic/agent/instrumentation/active_record_helper_test.rb
@@ -5,8 +5,20 @@
 require_relative '../../../test_helper'
 require 'new_relic/agent/instrumentation/active_record_helper'
 
+module Animals
+  class Dog
+    def self.table_name
+      'animals'
+    end
+  end
+end
+
 module NewRelic::Agent::Instrumentation
   class ActiveRecordHelperTest < Minitest::Test
+    def teardown
+      ActiveRecordHelper::TABLE_NAME_CACHE.clear
+    end
+
     def test_product_operation_collection_for_find
       product, operation, collection = ActiveRecordHelper.product_operation_collection_for('Namespace::Model Load', nil, nil)
 
@@ -102,6 +114,49 @@ module NewRelic::Agent::Instrumentation
       adapter = NewRelic::Agent::Instrumentation::ActiveRecordHelper::InstanceIdentification.adapter_from_config(config)
 
       assert_equal :postgres, adapter
+    end
+
+    def test_table_name_result_is_stored_in_cache
+      with_config(active_record_metrics_use_table_name: true) do
+        ActiveRecordHelper.product_operation_collection_for('Animals::Dog Load', nil, nil)
+      end
+
+      assert ActiveRecordHelper::TABLE_NAME_CACHE.key?('Animals::Dog')
+    end
+
+    def test_cached_table_name_is_returned_without_resolving_again
+      ActiveRecordHelper::TABLE_NAME_CACHE['Animals::Dog'] = 'animals'
+
+      ActiveRecordHelper.stub(:resolve_table_name, ->(_) { raise 'should not be called' }) do
+        with_config(active_record_metrics_use_table_name: true) do
+          _product, _operation, collection = ActiveRecordHelper.product_operation_collection_for('Animals::Dog Load', nil, nil)
+          assert_equal 'animals', collection
+        end
+      end
+    end
+
+    def test_class_name_used_for_namespaced_model_by_default
+      with_config(active_record_metrics_use_table_name: false) do
+        _product, _operation, collection = ActiveRecordHelper.product_operation_collection_for('Animals::Dog Load', nil, nil)
+
+        assert_equal 'Animals::Dog', collection
+      end
+    end
+
+    def test_table_name_used_for_namespaced_model_when_configured
+      with_config(active_record_metrics_use_table_name: true) do
+        _product, _operation, collection = ActiveRecordHelper.product_operation_collection_for('Animals::Dog Load', nil, nil)
+
+        assert_equal 'animals', collection
+      end
+    end
+
+    def test_class_name_used_as_fallback_when_model_unresolvable
+      with_config(active_record_metrics_use_table_name: true) do
+        _product, _operation, collection = ActiveRecordHelper.product_operation_collection_for('Unresolvable::Model Load', nil, nil)
+
+        assert_equal 'Unresolvable::Model', collection
+      end
     end
   end
 end
