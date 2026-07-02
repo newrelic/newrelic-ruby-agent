5ccba41ded380963a726e84d707c85aa6a6ee875
Hannah Ramadan <hannahr315@gmail.com>
2026-07-02 15:02:56 -0700

    GCR hostname and config

--- lib/new_relic/agent/configuration/default_source.rb
+++ lib/new_relic/agent/configuration/default_source.rb
@@ -2351,6 +2351,20 @@ module NewRelic
           :dynamic_name => true,
           :description => 'If `true`, the agent automatically detects that it is running in an Google Cloud Platform environment.'
         },
+        :'utilization.gcp_cloud_run.use_instance_as_host' => {
+          :default => true,
+          :public => true,
+          :type => Boolean,
+          :allowed_from_server => false,
+          :description => 'If `true`, when the agent detects it is running on Google Cloud Run, it reports the GCP metadata instance id as the hostname rather than `localhost`. This allows individual Cloud Run instances to be distinguished in the New Relic UI.'
+        },
+        :'utilization.gcp_cloud_run.include_revision_in_host' => {
+          :default => false,
+          :public => true,
+          :type => Boolean,
+          :allowed_from_server => false,
+          :description => 'If `true`, when the agent detects it is running on Google Cloud Run, it prepends the `K_REVISION` environment variable value to the instance id to form the hostname (`{K_REVISION}-{instance id}`), which can be easier to identify in the New Relic UI. Has no effect unless `utilization.gcp_cloud_run.use_instance_as_host` is also `true`.'
+        },
         :'utilization.detect_kubernetes' => {
           :default => true,
           :public => true,
--- lib/new_relic/agent/hostname.rb
+++ lib/new_relic/agent/hostname.rb
@@ -3,20 +3,58 @@
 # frozen_string_literal: true
 
 require 'socket'
+require 'net/http'
 require 'new_relic/helper'
 
 module NewRelic
   module Agent
     module Hostname
+      CLOUD_RUN_REVISION = 'K_REVISION'
+      GCP_INSTANCE_ID_URI = 'http://metadata.google.internal/computeMetadata/v1/instance/id'
+      GCP_METADATA_HEADERS = {'Metadata-Flavor' => 'Google'}.freeze
+
       def self.get
         dyno_name = ENV['DYNO']
         @hostname ||= if dyno_name && ::NewRelic::Agent.config[:'heroku.use_dyno_names']
           matching_prefix = heroku_dyno_name_prefix(dyno_name)
           dyno_name = "#{matching_prefix}.*" if matching_prefix
           dyno_name
+        elsif gcp_cloud_run?
+          gcp_cloud_run_host
         else
           Socket.gethostname.force_encoding(Encoding::UTF_8)
         end
+      end
+
+      def self.gcp_cloud_run?
+        ENV.key?(CLOUD_RUN_REVISION) &&
+          ::NewRelic::Agent.config[:'utilization.gcp_cloud_run.use_instance_as_host']
+      end
+
+      def self.gcp_cloud_run_host
+        instance_id = gcp_instance_id
+        return Socket.gethostname.force_encoding(Encoding::UTF_8) unless instance_id
+
+        if ::NewRelic::Agent.config[:'utilization.gcp_cloud_run.include_revision_in_host']
+          "#{ENV[CLOUD_RUN_REVISION]}-#{instance_id}"
+        else
+          instance_id
+        end
+      end
+
+      def self.gcp_instance_id
+        uri = URI(GCP_INSTANCE_ID_URI)
+        response = nil
+        Net::HTTP.start(uri.host, uri.port, open_timeout: 1, read_timeout: 1) do |http|
+          response = http.request(Net::HTTP::Get.new(uri, GCP_METADATA_HEADERS))
+        end
+        return unless response&.code == '200'
+
+        id = response.body.to_s.strip
+        id.empty? ? nil : id
+      rescue => e
+        NewRelic::Agent.logger.debug("Unable to fetch GCP Cloud Run instance id: #{e.class} - #{e.message}")
+        nil
       end
 
       # Pass '-f' to the external executable 'hostname' to request the fully
