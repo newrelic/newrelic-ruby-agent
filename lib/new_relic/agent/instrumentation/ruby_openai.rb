91dad79a80f5a9415b1e58466780798d33c0cee3
Hannah Ramadan <hannahr315@gmail.com>
2026-08-03 11:44:21 -0700

    remove binding.irb

--- lib/new_relic/agent/instrumentation/ruby_openai.rb
+++ lib/new_relic/agent/instrumentation/ruby_openai.rb
@@ -38,7 +38,6 @@ end
 # enabled by server-side config after startup detection has run. Re-check the
 # dependency when the server source is applied.
 NewRelic::Agent.instance.events.subscribe(:server_source_configuration_added) do
-  binding.irb
   item = DependencyDetection.dependency_by_name(:'ruby_openai')
   item.execute if item && !item.executed && item.dependencies_satisfied?
 end
